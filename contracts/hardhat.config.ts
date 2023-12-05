2f9b23ac339172ca0dfe3bb2a32fe1dbc1e89446
Dan Rusnac <rusnac14@gmail.com>
2023-12-05 15:07:56 +0100

    feat: configured hardhat

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,52 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import '@nomiclabs/hardhat-truffle5';
+import { VECHAIN_URL_SOLO, VECHAIN_URL_MAINNET, VECHAIN_URL_TESTNET } from '@vechain/hardhat-vechain'
+import '@vechain/hardhat-ethers'
+import "dotenv/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.20",
+};
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+  },
+  mocha: {
+    timeout: 180000,
+  },
+  defaultNetwork: "vechain_solo",
+  networks: {
+    vechain_solo: {
+      url: VECHAIN_URL_SOLO,
+      accounts: {
+        mnemonic: "denial kitchen pet squirrel other broom bar gas better priority spoil cross",
+        count: 10,
+      },
+      restful: true,
+      gas: 10000000,
+    }
+  },
+  vechain_testnet: {
+    url: VECHAIN_URL_TESTNET,
+    accounts: {
+      mnemonic: process.env.MNEOMIC || "",
+      count: 10,
+    },
+    restful: true,
+    gas: 10000000
+  },
+  vechain_mainnet: {
+    url: VECHAIN_URL_MAINNET,
+    accounts: {
+      mnemonic: process.env.MNEOMIC || "",
+      count: 1,
+    },
+    restful: true,
+    gas: 10000000
+  },
+};
+
+
+export default config;
