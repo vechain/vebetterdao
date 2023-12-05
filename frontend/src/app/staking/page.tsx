60cdd9986c683de7c91a24b6d0daffaa65e29ef4
ve-akanoce <erik.nucibella@vechain.org>
2023-12-05 13:09:48 +0100

    feat(frontend): add navbar, sample pages and components

---
+++ frontend/src/app/staking/page.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+export default function StakingPage() {
+  return (
+    <Box>
+      <Heading as="h1" size="2xl">
+        Staking
+      </Heading>
+      <Text>Swap B3TR for vB3TR and stake it to earn rewards.</Text>
+    </Box>
+  );
+}
