ebac544df593a116594bb879c75fb89330bc520c
sebastianseijo <sebastiangseijo@gmail.com>
2023-12-11 01:57:45 +0100

    add dap-kitt

--- apps/frontend/src/app/providers.tsx
+++ apps/frontend/src/app/providers.tsx
@@ -4,10 +4,33 @@
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
 
+import { DAppKitProvider } from '@vechain/dapp-kit-react';
+import type { WalletConnectOptions } from '@vechain/dapp-kit';
+
+const walletConnectOptions: WalletConnectOptions = {
+    projectId: 'a0b855ceaf109dbc8426479a4c3d38d8',
+    metadata: {
+        name: 'b3tr',
+        description: 'b3tr',
+        url: typeof window !== 'undefined' ? window.location.origin : '',
+        icons: [
+            typeof window !== 'undefined'
+                ? `${window.location.origin}/images/logo/my-dapp.png`
+                : '',
+        ],
+    },
+};
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
-      <ChakraProvider>{children}</ChakraProvider>
+        <DAppKitProvider
+            genesis="test"
+            nodeUrl="https://testnet.vechain.org/"
+            usePersistence
+            walletConnectOptions={walletConnectOptions}
+        >
+            <ChakraProvider>{children}</ChakraProvider>
+        </DAppKitProvider>
     </CacheProvider>
   );
 }
