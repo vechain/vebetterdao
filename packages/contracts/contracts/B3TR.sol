db41f6fa998a55926323958841bdedd1833abcd9
Dan Rusnac <rusnac14@gmail.com>
2023-12-12 16:09:54 +0100

    refactor: use ERC20Capped

--- packages/contracts/contracts/B3TR.sol
+++ packages/contracts/contracts/B3TR.sol
@@ -1,29 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract B3TR is ERC20, AccessControl {
+contract B3TR is ERC20Capped, AccessControl {
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
-    uint256 private _maxSupply = 1000000000 * 10 ** 18;
 
-    constructor(address _defaultOperator) ERC20("B3TR", "B3TR") {
+    constructor(
+        address _defaultOperator
+    ) ERC20Capped(1000000000 * 10 ** 18) ERC20("B3TR", "B3TR") {
         // Grant the contract deployer the default admin role
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(OPERATOR_ROLE, _defaultOperator);
     }
 
-    function maxSupply() public view returns (uint256) {
-        return _maxSupply;
+    /**
+     * @dev See {ERC20Capped-cap()}.
+     * Proxy function to cap()
+     *
+     * @return the cap of the token.
+     */
+    function maxSupply() external view returns (uint256) {
+        return cap();
     }
 
-    function mint(address to, uint256 amount) external onlyRole(OPERATOR_ROLE) {
-        require(
-            totalSupply() + amount <= maxSupply(),
-            "B3TR: max supply reached"
-        );
-
+    /**
+     * @dev See {ERC20-_mint}.
+     *
+     * Requirements:
+     *
+     * - the caller must have the {OPERATOR_ROLE}.
+     * - supply must not exceed cap.
+     */
+    function mint(
+        address to,
+        uint256 amount
+    ) external onlyRole(OPERATOR_ROLE) returns (bool) {
         _mint(to, amount);
+        return true;
     }
 }
