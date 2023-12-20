6811b583e7cd000cc232a002e96d6c845c735e25
ve-akanoce <erik.nucibella@vechain.org>
2023-12-20 16:13:45 +0100

    feat(api): react-query + persist integration

---
+++ apps/frontend/src/api/QueryProvider.ts
@@ -0,0 +1,17 @@
+
+import { QueryClient } from "@tanstack/react-query";
+
+
+export const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: 0,
+            staleTime: 30000,
+            refetchOnWindowFocus: true,
+            refetchOnMount: true,
+            refetchOnReconnect: true,
+            refetchInterval: false,
+            refetchIntervalInBackground: false,
+        },
+    },
+})
--- apps/frontend/src/app/providers.tsx
+++ apps/frontend/src/app/providers.tsx
@@ -1,16 +1,18 @@
 // app/providers.tsx
 "use client";
 
+import { queryClient } from "@/api";
 import { CacheProvider } from "@chakra-ui/next-js";
 import { ChakraProvider } from "@chakra-ui/react";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
 
 import type { WalletConnectOptions } from "@vechain/dapp-kit";
 import dynamic from "next/dynamic";
 
-const DAppKitProvider = dynamic(
-  () => import("@vechain/dapp-kit-react").then((mod) => mod.DAppKitProvider),
-  { ssr: false }
-);
+const DAppKitProvider = dynamic(() => import("@vechain/dapp-kit-react").then(mod => mod.DAppKitProvider), {
+  ssr: false,
+});
 
 const walletConnectOptions: WalletConnectOptions = {
   projectId: "a0b855ceaf109dbc8426479a4c3d38d8",
@@ -18,13 +20,15 @@ const walletConnectOptions: WalletConnectOptions = {
     name: "b3tr",
     description: "b3tr",
     url: typeof window !== "undefined" ? window.location.origin : "",
-    icons: [
-      typeof window !== "undefined"
-        ? `${window.location.origin}/images/logo/my-dapp.png`
-        : "",
-    ],
+    icons: [typeof window !== "undefined" ? `${window.location.origin}/images/logo/my-dapp.png` : ""],
   },
 };
+
+// to persist react-query daya in LS
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
@@ -32,9 +36,10 @@ export function Providers({ children }: { children: React.ReactNode }) {
         genesis="test"
         nodeUrl="https://testnet.vechain.org/"
         usePersistence
-        walletConnectOptions={walletConnectOptions}
-      >
-        <ChakraProvider>{children}</ChakraProvider>
+        walletConnectOptions={walletConnectOptions}>
+        <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
+          <ChakraProvider>{children}</ChakraProvider>
+        </PersistQueryClientProvider>
       </DAppKitProvider>
     </CacheProvider>
   );
