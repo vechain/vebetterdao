a908abfcaa3ee53337698c805dbcef7e23b631c7
Daithi Hearn <daithi.hearn@gmail.com>
2023-12-21 16:45:02 +0100

    chore: disabling semi colon in prettier rules

--- packages/contracts/main.ts
+++ packages/contracts/main.ts
@@ -1,10 +1,10 @@
-import { ethers as _ethers } from "hardhat";
+import { ethers as _ethers } from "hardhat"
 
-const b3trContractAddress = process.env.NEXT_PUBLIC_B3TR_CONTRACT_ADDRESS;
-if (!b3trContractAddress) throw new Error("B3TR_CONTRACT_ADDRESS is not set");
+const b3trContractAddress = process.env.NEXT_PUBLIC_B3TR_CONTRACT_ADDRESS
+if (!b3trContractAddress) throw new Error("B3TR_CONTRACT_ADDRESS is not set")
 
 export const getB3trContractInstance = async () => {
-  return ethers.getContractAt("B3TR", b3trContractAddress);
-};
+  return ethers.getContractAt("B3TR", b3trContractAddress)
+}
 
-export const ethers = _ethers;
+export const ethers = _ethers
--- packages/contracts/test/B3TR.test.ts
+++ packages/contracts/test/B3TR.test.ts
@@ -1,194 +1,194 @@
-import { ethers } from "hardhat";
-import { expect } from "chai";
+import { ethers } from "hardhat"
+import { expect } from "chai"
 
 describe("B3TR", function () {
-  let cachedDeployInstance: any = null;
+  let cachedDeployInstance: any = null
   async function deploy(forceDeploy = false) {
     if (!forceDeploy && cachedDeployInstance !== null) {
-      return cachedDeployInstance;
+      return cachedDeployInstance
     }
 
     // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount, minterAccount] = await ethers.getSigners();
+    const [owner, otherAccount, minterAccount] = await ethers.getSigners()
 
-    const B3TRContract = await ethers.getContractFactory("B3TR");
-    const contractInstance = await B3TRContract.deploy(minterAccount);
+    const B3TRContract = await ethers.getContractFactory("B3TR")
+    const contractInstance = await B3TRContract.deploy(minterAccount)
 
-    const abiArray = B3TRContract.interface["fragments"];
-    const abi = JSON.stringify(abiArray);
+    const abiArray = B3TRContract.interface["fragments"]
+    const abi = JSON.stringify(abiArray)
 
-    cachedDeployInstance = { contractInstance, owner, otherAccount, abi, minterAccount };
+    cachedDeployInstance = { contractInstance, owner, otherAccount, abi, minterAccount }
 
-    return cachedDeployInstance;
+    return cachedDeployInstance
   }
 
   describe("Deployment", function () {
     it("should deploy the contract", async function () {
-      const { contractInstance } = await deploy();
-      await contractInstance.waitForDeployment();
-      const address = await contractInstance.getAddress();
+      const { contractInstance } = await deploy()
+      await contractInstance.waitForDeployment()
+      const address = await contractInstance.getAddress()
 
-      expect(address).not.to.eql(undefined);
-    });
+      expect(address).not.to.eql(undefined)
+    })
 
     it("should have the correct name", async function () {
-      const { contractInstance } = await deploy();
+      const { contractInstance } = await deploy()
 
-      const res = await contractInstance.name();
-      expect(res).to.eql("B3TR");
+      const res = await contractInstance.name()
+      expect(res).to.eql("B3TR")
 
-      const res2 = await contractInstance.symbol();
-      expect(res2).to.eql("B3TR");
-    });
+      const res2 = await contractInstance.symbol()
+      expect(res2).to.eql("B3TR")
+    })
 
     it("should have the correct max supply", async function () {
-      const { contractInstance } = await deploy();
+      const { contractInstance } = await deploy()
 
-      const cap = await contractInstance.cap();
-      expect(String(cap)).to.eql("1000000000000000000000000000");
-    });
+      const cap = await contractInstance.cap()
+      expect(String(cap)).to.eql("1000000000000000000000000000")
+    })
 
     it("admin role is set correctly upon deploy", async function () {
-      const { contractInstance, owner } = await deploy();
+      const { contractInstance, owner } = await deploy()
 
-      const defaultAdminRole = await contractInstance.DEFAULT_ADMIN_ROLE();
+      const defaultAdminRole = await contractInstance.DEFAULT_ADMIN_ROLE()
 
-      const res = await contractInstance.hasRole(defaultAdminRole, owner);
-      expect(res).to.eql(true);
-    });
+      const res = await contractInstance.hasRole(defaultAdminRole, owner)
+      expect(res).to.eql(true)
+    })
 
     it("minter role is set correctly upon deploy", async function () {
-      const { contractInstance, owner, otherAccount, minterAccount } = await deploy();
-      const operatorRole = await contractInstance.MINTER_ROLE();
+      const { contractInstance, owner, otherAccount, minterAccount } = await deploy()
+      const operatorRole = await contractInstance.MINTER_ROLE()
 
-      const res = await contractInstance.hasRole(operatorRole, minterAccount);
-      expect(res).to.eql(true);
+      const res = await contractInstance.hasRole(operatorRole, minterAccount)
+      expect(res).to.eql(true)
 
       // test that operator role is not set for other accounts
-      expect(await contractInstance.hasRole(operatorRole, otherAccount)).to.eql(false);
-    });
-  });
+      expect(await contractInstance.hasRole(operatorRole, otherAccount)).to.eql(false)
+    })
+  })
 
   describe("Access Control", function () {
     it("only admin can grant minter role", async function () {
-      const { contractInstance, owner, otherAccount } = await deploy(true);
+      const { contractInstance, owner, otherAccount } = await deploy(true)
 
-      const operatorRole = await contractInstance.MINTER_ROLE();
+      const operatorRole = await contractInstance.MINTER_ROLE()
 
-      expect(await contractInstance.hasRole(operatorRole, otherAccount)).to.eql(false);
+      expect(await contractInstance.hasRole(operatorRole, otherAccount)).to.eql(false)
 
-      expect(contractInstance.connect(otherAccount).grantRole(operatorRole, otherAccount)).to.be.revertedWithoutReason;
+      expect(contractInstance.connect(otherAccount).grantRole(operatorRole, otherAccount)).to.be.revertedWithoutReason
 
-      await contractInstance.connect(owner).grantRole(operatorRole, otherAccount);
-      expect(await contractInstance.hasRole(operatorRole, otherAccount)).to.eql(true);
-    });
+      await contractInstance.connect(owner).grantRole(operatorRole, otherAccount)
+      expect(await contractInstance.hasRole(operatorRole, otherAccount)).to.eql(true)
+    })
 
     it("only admin can revoke minter role", async function () {
-      const { contractInstance, owner, otherAccount } = await deploy();
+      const { contractInstance, owner, otherAccount } = await deploy()
 
-      const operatorRole = await contractInstance.MINTER_ROLE();
+      const operatorRole = await contractInstance.MINTER_ROLE()
 
-      await contractInstance.connect(owner).grantRole(operatorRole, otherAccount);
-      expect(await contractInstance.hasRole(operatorRole, otherAccount)).to.eql(true);
+      await contractInstance.connect(owner).grantRole(operatorRole, otherAccount)
+      expect(await contractInstance.hasRole(operatorRole, otherAccount)).to.eql(true)
 
-      expect(contractInstance.connect(otherAccount).revokeRole(operatorRole, otherAccount)).to.be.revertedWithoutReason;
+      expect(contractInstance.connect(otherAccount).revokeRole(operatorRole, otherAccount)).to.be.revertedWithoutReason
 
-      await contractInstance.connect(owner).revokeRole(operatorRole, otherAccount);
-      expect(await contractInstance.hasRole(operatorRole, otherAccount)).to.eql(false);
-    });
+      await contractInstance.connect(owner).revokeRole(operatorRole, otherAccount)
+      expect(await contractInstance.hasRole(operatorRole, otherAccount)).to.eql(false)
+    })
 
     it("only admin can grant admin role", async function () {
-      const { contractInstance, owner, otherAccount } = await deploy();
+      const { contractInstance, owner, otherAccount } = await deploy()
 
-      const adminRole = await contractInstance.DEFAULT_ADMIN_ROLE();
+      const adminRole = await contractInstance.DEFAULT_ADMIN_ROLE()
 
       // at the beginning owner is admin
-      expect(await contractInstance.hasRole(adminRole, otherAccount)).to.eql(false);
-      expect(await contractInstance.hasRole(adminRole, owner)).to.eql(true);
+      expect(await contractInstance.hasRole(adminRole, otherAccount)).to.eql(false)
+      expect(await contractInstance.hasRole(adminRole, owner)).to.eql(true)
 
-      expect(contractInstance.connect(otherAccount).grantRole(adminRole, otherAccount)).to.be.revertedWithoutReason;
+      expect(contractInstance.connect(otherAccount).grantRole(adminRole, otherAccount)).to.be.revertedWithoutReason
 
-      await contractInstance.connect(owner).grantRole(adminRole, otherAccount);
-      expect(await contractInstance.hasRole(adminRole, otherAccount)).to.eql(true);
+      await contractInstance.connect(owner).grantRole(adminRole, otherAccount)
+      expect(await contractInstance.hasRole(adminRole, otherAccount)).to.eql(true)
 
       // owner is still admin until it is revoked
-      expect(await contractInstance.hasRole(adminRole, owner)).to.eql(true);
-    });
+      expect(await contractInstance.hasRole(adminRole, owner)).to.eql(true)
+    })
 
     it("only admin can revoke admin role", async function () {
-      const { contractInstance, owner, otherAccount, minterAccount } = await deploy();
+      const { contractInstance, owner, otherAccount, minterAccount } = await deploy()
 
-      const adminRole = await contractInstance.DEFAULT_ADMIN_ROLE();
+      const adminRole = await contractInstance.DEFAULT_ADMIN_ROLE()
 
       // after last test both owner and otherAccount are admin
-      expect(await contractInstance.hasRole(adminRole, otherAccount)).to.eql(true);
-      expect(await contractInstance.hasRole(adminRole, owner)).to.eql(true);
+      expect(await contractInstance.hasRole(adminRole, otherAccount)).to.eql(true)
+      expect(await contractInstance.hasRole(adminRole, owner)).to.eql(true)
 
-      expect(contractInstance.connect(minterAccount).revokeRole(adminRole, owner)).to.be.revertedWithoutReason;
+      expect(contractInstance.connect(minterAccount).revokeRole(adminRole, owner)).to.be.revertedWithoutReason
 
-      await contractInstance.connect(otherAccount).revokeRole(adminRole, owner);
+      await contractInstance.connect(otherAccount).revokeRole(adminRole, owner)
 
       // owner is no longer admin
-      expect(await contractInstance.hasRole(adminRole, owner)).to.eql(false);
+      expect(await contractInstance.hasRole(adminRole, owner)).to.eql(false)
 
       // otherAccount is still admin until
-      expect(await contractInstance.hasRole(adminRole, otherAccount)).to.eql(true);
-    });
-  });
+      expect(await contractInstance.hasRole(adminRole, otherAccount)).to.eql(true)
+    })
+  })
 
   describe("Max supply", function () {
     it("cannot be minted more than max supply", async function () {
-      const { contractInstance, otherAccount, owner, abi } = await deploy(true);
-      const operatorRole = await contractInstance.MINTER_ROLE();
+      const { contractInstance, otherAccount, owner, abi } = await deploy(true)
+      const operatorRole = await contractInstance.MINTER_ROLE()
 
-      await contractInstance.grantRole(operatorRole, owner);
-      expect(contractInstance.mint(otherAccount, ethers.parseEther("1000000001"))).to.be.revertedWithoutReason;
-    });
+      await contractInstance.grantRole(operatorRole, owner)
+      expect(contractInstance.mint(otherAccount, ethers.parseEther("1000000001"))).to.be.revertedWithoutReason
+    })
 
     it("can be minted up to max supply", async function () {
-      const { contractInstance, otherAccount, owner, abi } = await deploy();
-      const operatorRole = await contractInstance.MINTER_ROLE();
+      const { contractInstance, otherAccount, owner, abi } = await deploy()
+      const operatorRole = await contractInstance.MINTER_ROLE()
 
-      await contractInstance.grantRole(operatorRole, owner);
-      await expect(contractInstance.mint(otherAccount, ethers.parseEther("1000000000"))).not.to.be.reverted;
+      await contractInstance.grantRole(operatorRole, owner)
+      await expect(contractInstance.mint(otherAccount, ethers.parseEther("1000000000"))).not.to.be.reverted
 
-      const balance = await contractInstance.balanceOf(otherAccount);
-      expect(String(balance)).to.eql(ethers.parseEther("1000000000").toString());
-    });
-  });
+      const balance = await contractInstance.balanceOf(otherAccount)
+      expect(String(balance)).to.eql(ethers.parseEther("1000000000").toString())
+    })
+  })
 
   describe("Mint", function () {
     it("only accounts with minter role can mint", async function () {
-      const { contractInstance, otherAccount, owner } = await deploy(true);
+      const { contractInstance, otherAccount, owner } = await deploy(true)
 
-      expect(contractInstance.mint(otherAccount, ethers.parseEther("1"))).to.be.revertedWithoutReason;
-      const operatorRole = await contractInstance.MINTER_ROLE();
+      expect(contractInstance.mint(otherAccount, ethers.parseEther("1"))).to.be.revertedWithoutReason
+      const operatorRole = await contractInstance.MINTER_ROLE()
 
-      await contractInstance.grantRole(operatorRole, owner);
-      await expect(contractInstance.mint(otherAccount, ethers.parseEther("1"))).not.to.be.reverted;
+      await contractInstance.grantRole(operatorRole, owner)
+      await expect(contractInstance.mint(otherAccount, ethers.parseEther("1"))).not.to.be.reverted
 
-      const balance = await contractInstance.balanceOf(otherAccount);
-      expect(String(balance)).to.eql(ethers.parseEther("1").toString());
-    });
-  });
+      const balance = await contractInstance.balanceOf(otherAccount)
+      expect(String(balance)).to.eql(ethers.parseEther("1").toString())
+    })
+  })
 
   describe("Token details", function () {
     it("returns expected information", async function () {
-      const { contractInstance } = await deploy();
+      const { contractInstance } = await deploy()
 
-      const name = await contractInstance.name();
-      const symbol = await contractInstance.symbol();
-      const decimals = await contractInstance.decimals();
-      const cap = await contractInstance.cap();
-      const totalSupply = await contractInstance.totalSupply();
+      const name = await contractInstance.name()
+      const symbol = await contractInstance.symbol()
+      const decimals = await contractInstance.decimals()
+      const cap = await contractInstance.cap()
+      const totalSupply = await contractInstance.totalSupply()
 
-      const tokenDetails = await contractInstance.tokenDetails();
+      const tokenDetails = await contractInstance.tokenDetails()
 
-      expect(tokenDetails[0]).to.eql(name);
-      expect(tokenDetails[1]).to.eql(symbol);
-      expect(tokenDetails[2]).to.eql(decimals);
-      expect(tokenDetails[3]).to.eql(totalSupply);
-      expect(tokenDetails[4]).to.eql(cap);
-    });
-  });
-});
+      expect(tokenDetails[0]).to.eql(name)
+      expect(tokenDetails[1]).to.eql(symbol)
+      expect(tokenDetails[2]).to.eql(decimals)
+      expect(tokenDetails[3]).to.eql(totalSupply)
+      expect(tokenDetails[4]).to.eql(cap)
+    })
+  })
+})
