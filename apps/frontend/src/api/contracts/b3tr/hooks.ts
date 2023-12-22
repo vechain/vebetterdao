6c36aea8d6f91dcfbc1fa701c4d8925d59b19d69
ve-akanoce <erik.nucibella@vechain.org>
2023-12-22 15:54:32 +0100

    feat(api): hookk and endpoints to get balance from vot3 contract, refator queryKey

--- apps/frontend/src/api/contracts/b3tr/hooks.ts
+++ apps/frontend/src/api/contracts/b3tr/hooks.ts
@@ -2,23 +2,23 @@ import { useQuery } from "@tanstack/react-query"
 import { getB3trBalance, getB3trTokenDetails } from "./endpoints"
 import { useConnex } from "@vechain/dapp-kit-react"
 
-export const getB3TrTokenDetailsQueryKey = () => ["b3trTokenDetails"]
+export const getB3TrTokenDetailsQueryKey = () => ["tokenDetails", "b3tr"]
 export const useB3trTokenDetails = () => {
-  const { thor } = useConnex()
+    const { thor } = useConnex()
 
-  return useQuery({
-    queryKey: getB3TrTokenDetailsQueryKey(),
-    queryFn: () => getB3trTokenDetails(thor),
-  })
+    return useQuery({
+        queryKey: getB3TrTokenDetailsQueryKey(),
+        queryFn: () => getB3trTokenDetails(thor),
+    })
 }
 
-export const getB3TrBalanceQueryKey = (address?: string) => ["b3trBalance", address]
+export const getB3TrBalanceQueryKey = (address?: string) => ["balance", "b3tr", address]
 export const useB3trBalance = (address?: string) => {
-  const { thor } = useConnex()
+    const { thor } = useConnex()
 
-  return useQuery({
-    queryKey: getB3TrBalanceQueryKey(address),
-    queryFn: () => getB3trBalance(thor, address),
-    enabled: !!address,
-  })
+    return useQuery({
+        queryKey: getB3TrBalanceQueryKey(address),
+        queryFn: () => getB3trBalance(thor, address),
+        enabled: !!address,
+    })
 }
