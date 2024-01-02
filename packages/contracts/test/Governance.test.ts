57f8a3bb7f97ca995462151fc58a1e307ea041d3
Dan Rusnac <rusnac14@gmail.com>
2024-01-02 16:42:39 +0100

    test: deployment parameters

--- packages/contracts/test/Governance.test.ts
+++ packages/contracts/test/Governance.test.ts
@@ -61,30 +61,43 @@ describe("Governor", function () {
     }
     describe.only("Deployment", function () {
         it("should set constructors correctly", async function () {
-            const { governor, B3trContract, otherAccount, vot3, b3tr, owner } = await deploy(true)
+            const { governor, B3trContract, otherAccount, vot3, b3tr, owner, timeLock } = await deploy(true)
             const votingDelay = (await governor.votingDelay()).toString()
             const votesThreshold = (await governor.proposalThreshold()).toString()
+            const votingPeriod = (await governor.votingPeriod()).toString()
 
             expect(votingDelay).to.eql("1")
             expect(votesThreshold).to.eql("0")
+            expect(votingPeriod).to.eql(defaultVotingPeriod.toString())
 
             // proposers votes should be 0
             const clock = await governor.clock()
             const proposerVotes = await governor.getVotes(owner, (clock - BigInt(1)).toString())
             expect(proposerVotes.toString()).to.eql("0")
+
+            // check name of the governor contract
+            const name = await governor.name()
+            expect(name).to.eql("GovernorContract")
+
+            // check that the VOT3 address is correct
+            const voteTokenAddress = await governor.token()
+            expect(voteTokenAddress).to.eql(await vot3.getAddress())
+
+            // check that the TimeLock address is correct
+            const timeLockAddress = await governor.timelock()
+            expect(timeLockAddress).to.eql(await timeLock.getAddress())
         })
     })
 
-    // can create proposal and only owners of VOT3 can vote
     describe.only("Proposal Creation", function () {
         const description = "Test Proposal: testing propsal with random description!"
         let encodedFunctionCall: string = ""
         let proposalId: number = 0
 
-        it("can create a proposal", async function () {
+        it("can create a proposal if VOT3 holder", async function () {
             const { governor, B3trContract, otherAccount, vot3, b3tr, owner } = await deploy(true)
-            const b3trAddress = await b3tr.getAddress()
 
+            const b3trAddress = await b3tr.getAddress()
             encodedFunctionCall = B3trContract.interface.encodeFunctionData("tokenDetails", [])
 
             const tx = await governor.propose(
@@ -99,12 +112,12 @@ describe("Governor", function () {
             // Check that the ProposalCreated event was emitted with the correct parameters
             const event = proposeReceipt.logs[0]
             const decodedLogs = governor.interface.parseLog(event);
-            console.log(decodedLogs);
 
+            //event exists
             expect(decodedLogs.name).to.eql("ProposalCreated")
+            // proposal id
             proposalId = decodedLogs.args[0];
             expect(proposalId).not.to.be.null
-
             // proposer is the owner
             expect(decodedLogs.args[1]).to.eql(await owner.getAddress())
             // targets are correct
@@ -123,12 +136,12 @@ describe("Governor", function () {
             expect(voteEnd).not.to.be.null
             expect(voteEnd).to.eql(voteStart + BigInt(defaultVotingPeriod))
 
-            // che lo stato della proposal sia pending
-
-            // che i parametri della proposal salvati in blockchain sono giusti
+            // proposal should be in pending state
+            const proposalState = await governor.state(proposalId)
+            expect(proposalState.toString()).to.eql("0")
         })
 
-        it("can retrieve the proposal id from the proposal parameters", async function () {
+        it("can calculate the proposal id from the proposal parameters", async function () {
             const { governor, B3trContract, otherAccount, vot3, b3tr, owner } = await deploy(false)
 
             const b3trAddress = await b3tr.getAddress()
@@ -144,6 +157,10 @@ describe("Governor", function () {
         })
     })
 
+    // can index proposals
+
+    // anyone can create a proposal?
+
     // vote can be executed after proposal is passed
 
     // it("should not create a proposal if not a VOT3 owner", async function () {
@@ -152,4 +169,8 @@ describe("Governor", function () {
     //     // Create a proposal
     //     await expect(governor.connect(otherAccount).createProposal("Test Proposal", "This is a test proposal")).to.be.revertedWith("Governor: Only VOT3 owners can create proposals")
     // })
+
+    // PROPOSAL VOTING
+
+    // PROPOSAL EXECUTION
 })
