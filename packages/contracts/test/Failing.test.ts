eed15eb6ad135f7a6c713d747ebe92f1cb80c73c
Daithi Hearn <daithi.hearn@gmail.com>
2024-01-04 17:40:26 +0100

    removing failing test to test if pipeline works

--- packages/contracts/test/Failing.test.ts
+++
@@ -1,8 +0,0 @@
-// This is a temporary file to force the test to fail
-import { expect } from "chai"
-
-describe("Fail on purpose", function () {
-  it.only("should fail", async function () {
-    expect(true).to.eql(false)
-  })
-})
