6933bdee4ff97f3a5bf3d8bc740ed49aae5f8c6c
ve-akanoce <erik.nucibella@vechain.org>
2024-01-08 15:59:36 +0100

    fix: linting, removeo ld files

--- packages/config/local.ts
+++ packages/config/local.ts
@@ -1,7 +1,33 @@
+import { NETWORK_TYPE } from "@repo/constants"
 import { Config } from "."
 export const localConfig: Config = {
-  b3trContractAddress: "0x6BE58620eEaC63fBe066DC09989A0Ac9F9Fd898a",
-  vot3ContractAddress: "0x87464cd39BC0822E08962519810D2234b9F6011a",
+  b3trContractAddress: "0xb3f0fcd3eBb8fcd8C665f54bF8CcC80a672d0229",
+  vot3ContractAddress: "0x32b58E677E77D9FB712a1f15d60182A5C28BfC3f",
   nodeUrl: "http://localhost:8669",
-  networkType: "solo",
+  network: {
+    id: "solo",
+    name: "solo",
+    type: NETWORK_TYPE.SOLO,
+    defaultNet: true,
+    urls: ["http://localhost:8669"],
+    explorerUrl: "https://explore-testnet.vechain.org",
+    genesis: {
+      number: 0,
+      id: "0x00000000c05a20fbca2bf6ae3affba6af4a74b800b585bf7a4988aba7aea69f6",
+      size: 170,
+      parentID: "0xffffffff53616c757465202620526573706563742c20457468657265756d2100",
+      timestamp: 1530316800,
+      gasLimit: 10000000,
+      beneficiary: "0x0000000000000000000000000000000000000000",
+      gasUsed: 0,
+      totalScore: 0,
+      txsRoot: "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
+      txsFeatures: 0,
+      stateRoot: "0x93de0ffb1f33bc0af053abc2a87c4af44594f5dcb1cb879dd823686a15d68550",
+      receiptsRoot: "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
+      signer: "0x0000000000000000000000000000000000000000",
+      isTrunk: true,
+      transactions: [],
+    },
+  },
 }
--- packages/config/model.ts
+++
@@ -1,6 +0,0 @@
-export type EnvConfig = {
-  b3trContractAddress: string
-  vot3ContractAddress: string
-  nodeUrl: string
-  networkType: "main" | "test" | "solo"
-}
