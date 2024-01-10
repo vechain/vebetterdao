67d5829f58d67a5d4ef42488fda995d7bc51acdb
ve-akanoce <erik.nucibella@vechain.org>
2024-01-10 12:24:28 +0100

    feat)MintB3trButton): handle error, suggest minter address when user does not have a minter role

--- apps/frontend/src/components/MintB3trButton.tsx
+++ apps/frontend/src/components/MintB3trButton.tsx
@@ -1,5 +1,5 @@
 import { useUserHasMinterRole } from "@/api"
-import { Button, useDisclosure } from "@chakra-ui/react"
+import { Button, Text, VStack, useDisclosure } from "@chakra-ui/react"
 import { useWallet } from "@vechain/dapp-kit-react"
 import { MinB3trModal } from "./MinB3trModal"
 import { FormattingUtils } from "@repo/utils"
@@ -23,11 +23,26 @@ export const MintB3trButton: React.FC<Props> = () => {
 
   if (!account) return <></>
 
+  if (error)
+    return (
+      <VStack spacing={1} justify="flex-end" align={"flex-end"}>
+        <Text fontSize="sm" color="red.500">
+          Error checking minter role
+        </Text>
+        <Text fontSize="sm">{error.message}</Text>
+      </VStack>
+    )
+
   if (!hasMinterRole && !hasMinterRoleLoading)
     return (
-      <Button size="sm" isDisabled={true}>
-        You do not have the minter role
-      </Button>
+      <VStack spacing={1} justify="flex-end" align={"flex-end"}>
+        <Text fontSize="sm" color="orange.500">
+          You do not have the minter role
+        </Text>
+        <Text fontSize="sm" color="gray.500">
+          Connect with {defaultMinterAddress} to mint B3TR
+        </Text>
+      </VStack>
     )
 
   return (
