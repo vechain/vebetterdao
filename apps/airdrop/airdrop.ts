313e9ba32cc61e9afc5945ab30c18e70499b4148
Daithi Hearn <daithi.hearn@gmail.com>
2024-01-15 12:06:25 +0100

    fix: closing the thor client when airdrop is completed

--- apps/airdrop/airdrop.ts
+++ apps/airdrop/airdrop.ts
@@ -11,37 +11,42 @@ export const airdrop = async (env: Env, nodeUrl: string, b3trContractAddress: st
   const thorNetwork = new HttpClient(nodeUrl)
   const thorClient = new ThorClient(thorNetwork)
 
-  const signer = addressUtils.fromPrivateKey(env.pk)
+  try {
+    const signer = addressUtils.fromPrivateKey(env.pk)
 
-  logger.info(`Sending ${env.type} airdrop for ${signer}`)
+    logger.info(`Sending ${env.type} airdrop for ${signer}`)
 
-  // Read input file
-  const recipients = await readInputFile(env.recipientInputFilePath)
+    // Read input file
+    const recipients = await readInputFile(env.recipientInputFilePath)
 
-  // Group recipients into batches
-  const batches = []
-  for (let i = 0; i < recipients.length; i += env.batchSize) {
-    batches.push(recipients.slice(i, i + env.batchSize))
+    // Group recipients into batches
+    const batches = []
+    for (let i = 0; i < recipients.length; i += env.batchSize) {
+      batches.push(recipients.slice(i, i + env.batchSize))
+    }
+
+    // Send batches
+    for (const batch of batches) {
+      // Send airdrop
+      const tx = await buildTx(thorClient, b3trContractAddress, env.type, batch, signer, env.gasPriceCoef)
+
+      logger.info(`Sending tx with ${batch.length} clauses`)
+
+      // Sign the transaction
+      const receipt = await signAndSendTx(thorClient, tx, env.pk)
+
+      if (receipt.reverted) throw Error("Transaction reverted")
+
+      logger.info(`Tx was successful. TxId: ${receipt.meta.txID}`)
+      logger.info(`The following drops were made: ${JSON.stringify(batch)}`)
+    }
+
+    logger.info("Airdrop completed!")
+  } catch (e) {
+    logger.error(e)
+    throw e
+  } finally {
+    // Close the thor client
+    thorClient.destroy()
   }
-
-  // Send batches
-  for (const batch of batches) {
-    // Send airdrop
-    const tx = await buildTx(thorClient, b3trContractAddress, env.type, batch, signer, env.gasPriceCoef)
-
-    logger.info(`Sending tx with ${batch.length} clauses`)
-
-    // Sign the transaction
-    const receipt = await signAndSendTx(thorClient, tx, env.pk)
-
-    if (receipt.reverted) throw Error("Transaction reverted")
-
-    logger.info(`Tx was successful. TxId: ${receipt.meta.txID}`)
-    logger.info(`The following drops were made: ${JSON.stringify(batch)}`)
-  }
-
-  logger.info("Airdrop completed!")
-
-  // Close the thor client
-  thorClient.destroy()
 }
