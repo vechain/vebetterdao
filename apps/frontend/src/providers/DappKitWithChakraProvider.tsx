93daecf735b200c1d8f0a18491f78f47f694077e
ve-akanoce <erik.nucibella@vechain.org>
2024-01-23 14:25:08 +0000

    feat(theme): wrapepr for dappKitProvider to theme it accordingly

---
+++ apps/frontend/src/providers/DappKitWithChakraProvider.tsx
@@ -0,0 +1,44 @@
+import { useColorMode } from "@chakra-ui/react"
+import { getConfig } from "@repo/config"
+import { WalletConnectOptions } from "@vechain/dapp-kit"
+import dynamic from "next/dynamic"
+
+const appConfig = getConfig()
+
+const DAppKitProvider = dynamic(
+  async () => {
+    const { DAppKitProvider } = await import("@vechain/dapp-kit-react")
+    return DAppKitProvider
+  },
+  {
+    ssr: false,
+  },
+)
+
+const walletConnectOptions: WalletConnectOptions = {
+  projectId: "a0b855ceaf109dbc8426479a4c3d38d8",
+  metadata: {
+    name: "b3tr",
+    description: "b3tr",
+    url: typeof window !== "undefined" ? window.location.origin : "",
+    icons: [typeof window !== "undefined" ? `${window.location.origin}/images/logo/my-dapp.png` : ""],
+  },
+}
+
+export const DappKitWithChakraProvider = ({ children }: { children: React.ReactNode }) => {
+  const { colorMode } = useColorMode()
+  const isDark = colorMode === "dark"
+
+  return (
+    <DAppKitProvider
+      usePersistence
+      themeMode={isDark ? "DARK" : "LIGHT"}
+      requireCertificate={false}
+      genesis={appConfig.network.genesis}
+      nodeUrl={appConfig.nodeUrl}
+      logLevel={"DEBUG"}
+      walletConnectOptions={walletConnectOptions}>
+      {children}
+    </DAppKitProvider>
+  )
+}
