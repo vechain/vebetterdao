f0b4b0c54dbc6397b86cb02dd1a0ed8fe257454c
ve-akanoce <eriknucib@live.it>
2024-02-01 12:31:46 +0100

    feat(components): add AddressIcon and AddressButton

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,5 +1,6 @@
 import * as AddressUtils from "./AddressUtils"
 import * as FormattingUtils from "./FormattingUtils"
 import * as HexUtils from "./HexUtils"
+import * as PicassoUtils from "./PicassoUtils"
 
-export { AddressUtils, FormattingUtils, HexUtils }
+export { AddressUtils, FormattingUtils, HexUtils, PicassoUtils }
