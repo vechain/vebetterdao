6bd74ecd3e3acb1abed0cda17a169afc22c17d0c
ve-akanoce <eriknucib@live.it>
2024-02-08 16:49:33 +0200

    feat(dashboard): improve mobile styles

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,13 +1,18 @@
 "use client"
 
-import { Box, HStack, StackDivider, VStack } from "@chakra-ui/react"
+import { Box, Stack, StackDivider, VStack } from "@chakra-ui/react"
 import { BalanceCard, TvlBreakdownPieChart, CirculatingSupplyPieChart, AllocationRoundsList } from "@/components"
 
 export default function Home() {
   return (
     <VStack w="full" spacing={12}>
       <BalanceCard />
-      <HStack w="full" justify="space-between" align="flex-start" spacing={18}>
+      <Stack
+        direction={["column-reverse", "column-reverse", "row"]}
+        w="full"
+        justify="space-between"
+        align={["stretch", "stretch", "flex-start"]}
+        spacing={18}>
         <VStack flex={4} justifyContent="stretch" alignItems={"stretch"} divider={<StackDivider />} spacing={4}>
           <Box>
             <CirculatingSupplyPieChart />
@@ -16,10 +21,10 @@ export default function Home() {
             <TvlBreakdownPieChart />
           </Box>
         </VStack>
-        <VStack spacing={4} flex={2.5} position={"sticky"} top={100} right={0}>
+        <VStack spacing={4} flex={2.5} position={["static", "static", "sticky"]} top={100} right={0}>
           <AllocationRoundsList maxRounds={3} />
         </VStack>
-      </HStack>
+      </Stack>
     </VStack>
   )
 }
