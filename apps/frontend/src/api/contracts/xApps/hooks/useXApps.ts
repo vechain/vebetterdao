2e6f881b4b2b583d7010329535e4a04de9752c3d
ve-akanoce <eriknucib@live.it>
2024-02-09 20:00:06 +0200

    feat(xDapps): useXDapps hook to render all the dapps

---
+++ apps/frontend/src/api/contracts/xApps/hooks/useXApps.ts
@@ -0,0 +1,52 @@
+import { useQuery } from "@tanstack/react-query"
+import { useConnex } from "@vechain/dapp-kit-react"
+
+import XAllocationPoolContract from "@repo/contracts/artifacts/contracts/XAllocationPool.sol/XAllocationPool.json"
+import { getConfig } from "@repo/config"
+const XALLOCATIONPOOL_CONTRACT = getConfig().xAllocationPoolContractAddress
+import { XAllocationPool__factory as XAllocationPool } from "@repo/contracts/typechain-types"
+/**
+ * xApp type
+ * @property id  the xApp id
+ * @property addr  the xApp address
+ * @property name  the xApp name
+ * @property metadata  the xApp metadata (ipfs hash)
+ * @property appAvailableForAllocationVoting  whether the xApp is available for allocation voting
+ */
+type XApp = {
+  id: string
+  addr: string
+  name: string
+  metadata: string //ipfs hash
+  appAvailableForAllocationVoting: boolean
+}
+
+/**
+ *  Returns all the available xApps (apps that can be voted on for allocation)
+ * @param thor  the thor client
+ * @returns  all the available xApps (apps that can be voted on for allocation) capped to 256 see {@link XApp}
+ */
+export const getXApps = async (thor: Connex.Thor): Promise<XApp[]> => {
+  const functionFragment = XAllocationPool.createInterface().getFunction("getAllApps")
+  const res = await thor.account(XALLOCATIONPOOL_CONTRACT).method(functionFragment).call()
+
+  if (res.vmError) return Promise.reject(new Error(res.vmError))
+
+  return res.decoded[0]
+}
+
+export const getXAppsQueryKey = () => ["xApps"]
+
+/**
+ *  Hook to get all the available xApps (apps that can be voted on for allocation)
+ * @returns all the available xApps (apps that can be voted on for allocation) capped to 256
+ */
+export const useXApps = () => {
+  const { thor } = useConnex()
+
+  return useQuery({
+    queryKey: getXAppsQueryKey(),
+    queryFn: async () => await getXApps(thor),
+    enabled: !!thor,
+  })
+}
