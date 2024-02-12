fd41e312851a35c0057aac1321828dbf7c972de0
ve-akanoce <eriknucib@live.it>
2024-02-12 14:20:33 +0200

    fix(AllocatioNRoundNavbar): polish responseive styles

--- apps/frontend/src/app/rounds/components/AllocationRoundNavbar.tsx
+++ apps/frontend/src/app/rounds/components/AllocationRoundNavbar.tsx
@@ -1,11 +1,25 @@
 import { ProposalState, useAllocationsRound } from "@/api"
-import { HStack, Button, Heading, Box, Text, Icon, Tag } from "@chakra-ui/react"
+import {
+  HStack,
+  Button,
+  Heading,
+  Box,
+  Text,
+  Icon,
+  Tag,
+  Stack,
+  useBreakpoint,
+  useMediaQuery,
+  IconButton,
+  VStack,
+} from "@chakra-ui/react"
 import { useRouter } from "next/navigation"
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6"
 
 export const AllocationRoundNavbar = ({ roundId }: { roundId: string }) => {
   const router = useRouter()
   const { data, isLoading } = useAllocationsRound(roundId)
+  const [isDesktop] = useMediaQuery("(min-width: 800px)")
 
   const prevButtonDisabled = !data.proposalId || data.proposalId === "1"
   const goToPreviousRound = () => {
@@ -22,26 +36,69 @@ export const AllocationRoundNavbar = ({ roundId }: { roundId: string }) => {
     router.push(`/rounds/${nextRound}`)
   }
 
+  if (isDesktop)
+    return (
+      <HStack w="full" justify={"space-between"} align="center">
+        <Button
+          size="sm"
+          aria-label="Go to previous round"
+          isDisabled={prevButtonDisabled}
+          onClick={goToPreviousRound}
+          leftIcon={<FaArrowLeft />}>
+          Previous round
+        </Button>
+
+        <Stack direction={["column", "column", "row"]} spacing={4} align={"center"}>
+          <Heading size="md">{data?.proposalId}° round</Heading>
+          <Box w={1.5} h={1.5} borderRadius={"full"} bg="gray" />
+          <HStack spacing={2} align={"center"}>
+            <Text>{data?.voteStartTimestamp?.format("D MMMM")}</Text>
+            <Icon as={FaArrowRight} />
+            <Text>{data?.voteEndTimestamp?.format("D MMMM")}</Text>
+          </HStack>
+          <Tag colorScheme="primary" variant="solid">
+            {data?.state && ProposalState[data.state]}
+          </Tag>
+        </Stack>
+        <Button
+          size="sm"
+          aria-label="Go to next round"
+          isDisabled={nextButtonDisabled}
+          onClick={goToNextRound}
+          rightIcon={<FaArrowRight />}>
+          Next round
+        </Button>
+      </HStack>
+    )
+
   return (
     <HStack w="full" justify={"space-between"} align="center">
-      <Button size="sm" isDisabled={prevButtonDisabled} onClick={goToPreviousRound} leftIcon={<FaArrowLeft />}>
-        Previous round
-      </Button>
-      <HStack spacing={4} align={"center"}>
-        <Heading size="md">{data?.proposalId}° round</Heading>
-        <Box w={1.5} h={1.5} borderRadius={"full"} bg="gray" />
+      <IconButton
+        icon={<FaArrowLeft />}
+        isDisabled={prevButtonDisabled}
+        onClick={goToPreviousRound}
+        aria-label="Go to previous round"
+      />
+      <VStack w="full">
+        <HStack spacing={4}>
+          <Heading size="md">{data?.proposalId}° round</Heading>
+          <Tag colorScheme="primary" variant="solid">
+            {data?.state && ProposalState[data.state]}
+          </Tag>
+        </HStack>
+
         <HStack spacing={2} align={"center"}>
           <Text>{data?.voteStartTimestamp?.format("D MMMM")}</Text>
           <Icon as={FaArrowRight} />
           <Text>{data?.voteEndTimestamp?.format("D MMMM")}</Text>
         </HStack>
-        <Tag colorScheme="primary" variant="solid">
-          {data?.state && ProposalState[data.state]}
-        </Tag>
-      </HStack>
-      <Button size="sm" isDisabled={nextButtonDisabled} onClick={goToNextRound} rightIcon={<FaArrowRight />}>
-        Next round
-      </Button>
+      </VStack>
+      <IconButton
+        icon={<FaArrowRight />}
+        aria-label="Go to next round"
+        onClick={goToNextRound}
+        isDisabled={nextButtonDisabled}
+      />
     </HStack>
   )
 }
