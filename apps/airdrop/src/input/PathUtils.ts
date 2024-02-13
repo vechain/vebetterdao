3b0d6cd7a72d4219e37c5c8e7e2642226481e9f8
ve-akanoce <eriknucib@live.it>
2024-02-13 15:50:10 +0200

    fix: remove airdrop app

--- apps/airdrop/src/input/PathUtils.ts
+++
@@ -1,3 +0,0 @@
-// Get the path from an env variable or set current directory as default
-export const BASE_PATH = process.env.INPUT_FILE_DIRECTORY ?? "."
-export const parsePath = (file: string): string => BASE_PATH + "/" + file
