67e273e92dbcb0a520c454dd4fba5513261788c7
ve-akanoce <eriknucib@live.it>
2024-02-15 17:46:00 +0200

    feat(Rounds): UI and hook to distribute emission and create new round

---
+++ apps/frontend/src/hooks/useDistributeEmission.ts
@@ -0,0 +1,80 @@
+import { getCurrentAllocationsRoundIdQueryKey, getAllocationsRoundsEventsQueryKey } from "@/api"
+import { useToast } from "@chakra-ui/react"
+import { useQueryClient } from "@tanstack/react-query"
+import { UseSendTransactionReturnValue, useSendTransaction } from "./useSendTransaction"
+import { useCallback } from "react"
+import { useConnex, useWallet } from "@vechain/dapp-kit-react"
+import { Emissions__factory } from "@repo/contracts"
+import { getConfig } from "@repo/config"
+
+const EmissionsInterface = Emissions__factory.createInterface()
+
+type useDistributeEmissionsProps = {
+  onSuccess?: () => void
+  invalidateCache?: boolean
+  onSuccessMessageTitle?: string
+}
+/**
+ * Hook to mint a certain amount of B3TR tokens
+ * This hook will send a mint transaction to the blockchain and wait for the txConfirmation
+ * @param onSuccess callback to run when the upgrade is successful
+ * @param invalidateCache boolean to indicate if the related react-query cache should be updated (default: true)
+ * @returns see {@link UseSendTransactionReturnValue}
+ */
+export const useDistributeEmission = ({
+  onSuccess,
+  invalidateCache = true,
+}: useDistributeEmissionsProps): UseSendTransactionReturnValue => {
+  const { account } = useWallet()
+  const toast = useToast()
+  const queryClient = useQueryClient()
+
+  const buildClauses = useCallback(() => {
+    const clauses = [
+      {
+        to: getConfig().emissionsContractAddress,
+        value: 0,
+        data: EmissionsInterface.encodeFunctionData("distribute", []),
+      },
+    ]
+
+    return clauses
+  }, [])
+
+  //Refetch queries to update ui after the tx is confirmed
+  const handleOnSuccess = useCallback(async () => {
+    if (invalidateCache) {
+      await queryClient.cancelQueries({
+        queryKey: getCurrentAllocationsRoundIdQueryKey(),
+      })
+
+      await queryClient.refetchQueries({
+        queryKey: getCurrentAllocationsRoundIdQueryKey(),
+      })
+      await queryClient.cancelQueries({
+        queryKey: getAllocationsRoundsEventsQueryKey(),
+      })
+      await queryClient.refetchQueries({
+        queryKey: getAllocationsRoundsEventsQueryKey(),
+      })
+    }
+
+    toast({
+      title: "Round distributed successfully",
+      description: `The round has been distributed successfully.`,
+      status: "success",
+      position: "bottom-left",
+      duration: 5000,
+      isClosable: true,
+    })
+    onSuccess?.()
+  }, [invalidateCache, queryClient, toast, onSuccess])
+
+  const result = useSendTransaction({
+    signerAccount: account,
+    clauses: buildClauses,
+    onTxConfirmed: handleOnSuccess,
+  })
+
+  return result
+}
