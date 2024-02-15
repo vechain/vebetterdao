7ed12eb432132ca652be4ad580ac7daaaeec97a5
ve-akanoce <eriknucib@live.it>
2024-02-15 13:21:19 +0200

    refactor: useRoundXApps naming and integration in AllocationRoundVOtesCard

--- apps/frontend/src/components/AllocationXAppsVotesCard.tsx
+++ apps/frontend/src/components/AllocationXAppsVotesCard.tsx
@@ -1,15 +1,15 @@
 import { Box, Card, CardBody, CardHeader, HStack, Heading } from "@chakra-ui/react"
 import { HorizontalChartBar } from "./HorizontalBarChart"
-import { useRoundXAppsWithDetails, useXApps, useXAppsVotes } from "@/api"
+import { useRoundXApps } from "@/api"
 import { useMemo } from "react"
 
 type Props = {
   roundId: string
 }
 export const AllocationXAppsVotesCard = ({ roundId }: Props) => {
-  const { data: xApps } = useRoundXAppsWithDetails(roundId)
+  const { data: xApps } = useRoundXApps(roundId)
 
-  const xAppsVotes = useXAppsVotes(xApps?.map(app => app.id) ?? [], roundId)
+  const xAppsVotes = useRoundXApps(xApps?.map(app => app.id) ?? [], roundId)
 
   const data = useMemo(
     () =>
