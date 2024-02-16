cf3743efefbd3e8c9704e97d26be8eb87dda28c6
ve-akanoce <eriknucib@live.it>
2024-02-16 13:49:14 +0200

    fix(SelectAppVotesInput): add % label in input

--- apps/frontend/src/app/rounds/components/AllocationRoundUserVotes/components/SelectAppVotesInput.tsx
+++ apps/frontend/src/app/rounds/components/AllocationRoundUserVotes/components/SelectAppVotesInput.tsx
@@ -1,7 +1,17 @@
 import { XApp } from "@/api"
 import { FieldErrors, UseFormGetValues, UseFormRegister } from "react-hook-form"
 import { FormData } from "../AllocationRoundUserVotes"
-import { Box, FormControl, FormErrorMessage, HStack, Heading, Icon, Input } from "@chakra-ui/react"
+import {
+  Box,
+  FormControl,
+  FormErrorMessage,
+  HStack,
+  Heading,
+  Icon,
+  Input,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react"
 import { FaRecycle } from "react-icons/fa6"
 
 type Props = {
@@ -31,20 +41,23 @@ export const SelectAppVotesInput = ({ register, getValues, index, xApp, field, e
       </HStack>
       <Box>
         <FormControl isInvalid={!!errors.votes?.[index]}>
-          <Input
-            {...register(`votes.${index}.value`, {
-              valueAsNumber: true,
-              min: 0,
-              max: 100,
-              validate: value => {
-                if (isNaN(value)) return "Please enter a valid number"
-                const allValuesTotal = getValues().votes.reduce((acc, vote) => acc + vote.value, 0)
-                if (allValuesTotal > 100) return "Total votes exceed 100"
-                return true
-              },
-            })}
-            w="full"
-          />
+          <InputGroup>
+            <Input
+              {...register(`votes.${index}.value`, {
+                valueAsNumber: true,
+                min: 0,
+                max: 100,
+                validate: value => {
+                  if (isNaN(value)) return "Please enter a valid number"
+                  const allValuesTotal = getValues().votes.reduce((acc, vote) => acc + vote.value, 0)
+                  if (allValuesTotal > 100) return "Total votes exceed 100"
+                  return true
+                },
+              })}
+              w="full"
+            />
+            <InputRightElement children="%" />
+          </InputGroup>
           {errors.votes?.[index]?.value && <FormErrorMessage>{errors.votes?.[index]?.message}</FormErrorMessage>}
         </FormControl>
       </Box>
