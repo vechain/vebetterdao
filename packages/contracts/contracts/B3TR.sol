522dd4640c5ec61eebc5741c6287c10d9c492822
Dan Rusnac <rusnac14@gmail.com>
2024-02-19 09:40:48 +0100

    fix: uses literals with too many digits

--- packages/contracts/contracts/B3TR.sol
+++ packages/contracts/contracts/B3TR.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 contract B3TR is ERC20Capped, AccessControl {
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-  constructor(address _defaultMinter) ERC20Capped(1000000000 * 10 ** 18) ERC20("B3TR", "B3TR") {
+  constructor(address _defaultMinter) ERC20Capped(1000000000 * 1e18) ERC20("B3TR", "B3TR") {
     // Grant the contract deployer the default admin role
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     _grantRole(MINTER_ROLE, _defaultMinter);
