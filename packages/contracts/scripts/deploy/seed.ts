25861460998512fdc8974bc86b0d50314196da0f
Dan Rusnac <rusnac14@gmail.com>
2024-02-20 11:57:56 +0100

    fix: updated uri

--- packages/contracts/scripts/deploy/seed.ts
+++ packages/contracts/scripts/deploy/seed.ts
@@ -70,7 +70,7 @@ export const seedLocalEnvironment = async (
 
   // Set xApps baseURI
   console.log("Set xApps baseURI...")
-  await xAllocationVoting.setBaseURI("ipfs://bafybeif2t44bhwn2j4zrh2zf4a3pbbjc4yqku5ejzqfis4pgxlvinfpdvm/")
+  await xAllocationVoting.setBaseURI("ipfs://bafybeifwzkwplas7evdjlz2lwnmuu7v2mtxnqecfaoxpfbfwqvalxgjsru/")
 
   //TODO: SEED multiple rounds and votes (we need to execute a proposal to change the votingPeriod to someseconds)
 
