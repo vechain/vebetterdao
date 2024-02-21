08c902931cfb0dd959961380c8b81ee3ad227b97
ve-akanoce <eriknucib@live.it>
2024-02-21 16:36:50 +0200

    fix(AppMetadata): add correct type in hook

--- apps/frontend/src/api/contracts/xApps/hooks/useXAppMetadata.ts
+++ apps/frontend/src/api/contracts/xApps/hooks/useXAppMetadata.ts
@@ -5,13 +5,39 @@ import axios from "axios"
 import { useXAppsMetadataBaseUri } from "./useXAppsMetadataBaseUri"
 
 /**
+ * The metadata of an xApp from the xApps metadata base uri
+ * @property name - The name of the xApp
+ * @property description - The description of the xApp
+ * @property external_url - The external url of the xApp
+ * @property logo - The logo of the xApp (ipfs uri)
+ * @property banner - The banner of the xApp (ipfs uri)
+ * @property screenshots - The screenshots of the xApp (ipfs uri)
+ * @property social_urls - The social urls of the xApp
+ * @property app_urls - The app urls of the xApp
+ */
+type XAppMetadata = {
+  name: string
+  description: string
+  external_url: string
+  logo: string
+  banner: string
+  screenshots: string[]
+  social_urls: {
+    name: string
+    url: string
+  }[]
+  app_urls: {
+    code: string
+    url: string
+  }[]
+}
+/**
  *
  * @param uri  - The uri of the xApps metadata
- * @returns  The metadata of the xApp
+ * @returns  The metadata of the xApp see {@link XAppMetadata}
  */
-export const getXAppMetadata = async (uri: string): Promise<any> => {
-  console.log("uri", uri)
-  const metadata = await axios.get<any>(convertUriToUrl(uri), {
+export const getXAppMetadata = async (uri: string): Promise<XAppMetadata> => {
+  const metadata = await axios.get<XAppMetadata>(convertUriToUrl(uri), {
     timeout: 20000,
   })
 
@@ -31,7 +57,7 @@ export const useXAppMetadata = (xAppId?: string) => {
 
   return useQuery({
     queryKey: getXAppMetadataQueryKey(xAppId),
-    queryFn: async () => !!baseUri && xAppId && (await getXAppMetadata(`${baseUri}${xAppId}`)),
+    queryFn: async () => (!(!baseUri && xAppId) ? await getXAppMetadata(`${baseUri}${xAppId}`) : null),
     enabled: !!thor && !!baseUri && !!xAppId,
   })
 }
