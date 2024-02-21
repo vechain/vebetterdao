eeccd3867850aa49fc163c2fbc7856441896d3ce
Darren Kelly <darren.kelly@vechain.org>
2024-02-21 09:57:45 +0000

    fix gh actions

--- inspector-app/src/b3tr/contracts.ts
+++ inspector-app/src/b3tr/contracts.ts
@@ -1,7 +1,15 @@
 import vot3Artifact from "../../../packages/contracts/artifacts/contracts/VOT3.sol/VOT3.json"
 import b3trArtifact from "../../../packages/contracts/artifacts/contracts/B3TR.sol/B3TR.json"
+import governorArtifact from "../../../packages/contracts/artifacts/contracts/governance/B3TRGovernor.sol/B3TRGovernor.json"
+import timeLockArtifact from "../../../packages/contracts/artifacts/contracts/governance/Timelock.sol/Timelock.json"
+import xAllocationPoolArtifact from "../../../packages/contracts/artifacts/contracts/XAllocationPool.sol/XAllocationPool.json"
+import xAllocationVotingArtifact from "../../../packages/contracts/artifacts/contracts/XAllocationVoting.sol/XAllocationVoting.json"
+import emissionsArtifact from "../../../packages/contracts/artifacts/contracts/Emissions.sol/Emissions.json"
+import voterRewardsArtifact from "../../../packages/contracts/artifacts/contracts/VoterRewards.sol/VoterRewards.json"
+import nftBadeArtifact from "../../../packages/contracts/artifacts/contracts/B3TRBadge.sol/B3TRBadge.json"
 
-type Contract = {
+
+export type Contract = {
   abi: object
   name: string
 }
@@ -14,4 +22,39 @@ export const vot3Contract: Contract = {
 export const b3trContract: Contract = {
   abi: b3trArtifact.abi,
   name: b3trArtifact.contractName,
+}
+
+export const governorContract: Contract = {
+  abi: governorArtifact.abi,
+  name: governorArtifact.contractName,
+}
+
+export const timeLockContract: Contract = {
+  abi: timeLockArtifact.abi,
+  name: timeLockArtifact.contractName,
+}
+
+export const xAllocationPoolContract: Contract = {
+  abi: xAllocationPoolArtifact.abi,
+  name: xAllocationPoolArtifact.contractName,
+}
+
+export const xAllocationVotingContract: Contract = {
+  abi: xAllocationVotingArtifact.abi,
+  name: xAllocationVotingArtifact.contractName,
+}
+
+export const emissionsContract: Contract = {
+  abi: emissionsArtifact.abi,
+  name: emissionsArtifact.contractName,
+}
+
+export const voterRewardsContract: Contract = {
+  abi: voterRewardsArtifact.abi,
+  name: voterRewardsArtifact.contractName,
+}
+
+export const nftBadgeContract: Contract = {
+  abi: nftBadeArtifact.abi,
+  name: nftBadeArtifact.contractName,
 }
--- inspector-app/src/b3tr/index.ts
+++ inspector-app/src/b3tr/index.ts
@@ -1,9 +1,16 @@
 import DB, { Entities } from "@/database"
 import soloStaging from "../../../packages/config/solo-staging"
 import solo from "../../../packages/config/local"
-import { b3trContract, vot3Contract } from "@/b3tr/contracts"
+import {
+  b3trContract,
+  Contract, emissionsContract,
+  governorContract, nftBadgeContract,
+  timeLockContract,
+  vot3Contract, voterRewardsContract,
+  xAllocationPoolContract, xAllocationVotingContract,
+} from "@/b3tr/contracts"
 
-const importContract = async (abi: object, name: string, address: string, network: typeof soloStaging) => {
+const importContract = async (contract: Contract, address: string, network: typeof soloStaging) => {
   const existingContracts = await DB.contracts
     .filter((c) => c.address === address)
     .toArray()
@@ -12,22 +19,27 @@ const importContract = async (abi: object, name: string, address: string, networ
     return
   }
 
-  const contract: Entities.Contract = {
-    abi: abi,
+  const entity: Entities.Contract = {
+    abi: contract.abi,
     address: address,
-    name: name,
+    name: contract.name,
     createdTime: Date.now(),
     network: network.network.genesis.id,
   }
 
-  await DB.contracts.add(contract)
+  await DB.contracts.add(entity)
 }
 
-const networks = [solo, soloStaging]
-
 const insertForNetwork = async (network: typeof soloStaging) => {
-  await importContract(b3trContract.abi, b3trContract.name, network.b3trContractAddress, network)
-  await importContract(vot3Contract.abi, vot3Contract.name, network.vot3ContractAddress, network)
+  await importContract(b3trContract, network.b3trContractAddress, network)
+  await importContract(vot3Contract, network.vot3ContractAddress, network)
+  await importContract(governorContract, network.b3trGovernorAddress, network)
+  await importContract(timeLockContract, network.timelockContractAddress, network)
+  await importContract(xAllocationPoolContract, network.xAllocationPoolContractAddress, network)
+  await importContract(xAllocationVotingContract, network.xAllocationVotingContractAddress, network)
+  await importContract(emissionsContract, network.emissionsContractAddress, network)
+  await importContract(voterRewardsContract, network.voterRewardsContractAddress, network)
+  await importContract(nftBadgeContract, network.nftBadgeContractAddress, network)
 }
 
 export const insertB3trContracts = async () => {
