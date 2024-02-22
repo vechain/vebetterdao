eeb00cfdf06ce645fbc42a42defbb9969df506f7
ve-akanoce <eriknucib@live.it>
2024-02-22 10:36:52 +0200

    feat(logo): add polished vebetterdao logo, remove unused assets

--- apps/frontend/src/components/VeBetterDaoLogo.tsx
+++ apps/frontend/src/components/VeBetterDaoLogo.tsx
@@ -1,14 +1,21 @@
 "use client"
-import { useColorModeValue } from "@chakra-ui/react"
+import { HStack, Text, useColorModeValue } from "@chakra-ui/react"
 import Image from "next/image"
 import React from "react"
 
 export const VeBetterDaoLogo: React.FC = () => {
   //TODO: Add dark mode logo
-  const lightModeUrl = "/images/logo/vebetterdao.svg"
-  const darkModeUrl = "/images/logo/vebetterdao_dark.svg"
+  const lightModeUrl = "/images/logo/vebetter_light.svg"
+  const darkModeUrl = "/images/logo/vebetter_dark.svg"
   const logoUrl = useColorModeValue(lightModeUrl, darkModeUrl)
 
   //Priority is set to true to prevent the logo from being lazy loaded and FOC
-  return <Image height={"100"} width={"100"} alt="VebetterDao logo" src={logoUrl} priority={true} />
+  return (
+    <HStack spacing={1} align={"flex-start"}>
+      <Image height={"100"} width={"100"} alt="VebetterDao logo" src={logoUrl} priority={true} />
+      <Text fontSize="2xs" fontWeight="bold" color="primary.500">
+        DAO
+      </Text>
+    </HStack>
+  )
 }
