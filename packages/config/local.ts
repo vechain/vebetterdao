c093dbd48f4fcd15305a0bce7ae441abd4860aff
ve-akanoce <eriknucib@live.it>
2024-02-23 12:10:24 +0200

    fix: local

--- packages/config/local.ts
+++ packages/config/local.ts
@@ -1,45 +1,5 @@
 import { AppConfig } from "." 
  const config: AppConfig = {
-<<<<<<< Updated upstream
-    b3trContractAddress: "0x45d5CA3f295ad8BCa291cC4ecd33382DE40E4FAc",
-    vot3ContractAddress: "0x45d5CA3f295ad8BCa291cC4ecd33382DE40E4FAc",
-    b3trGovernorAddress: "0x45d5CA3f295ad8BCa291cC4ecd33382DE40E4FAc",
-    timelockContractAddress: "0x45d5CA3f295ad8BCa291cC4ecd33382DE40E4FAc",
-    xAllocationPoolContractAddress: "0x45d5CA3f295ad8BCa291cC4ecd33382DE40E4FAc",
-    xAllocationVotingContractAddress: "0x45d5CA3f295ad8BCa291cC4ecd33382DE40E4FAc",
-    emissionsContractAddress: "0x45d5CA3f295ad8BCa291cC4ecd33382DE40E4FAc",
-    voterRewardsContractAddress: "0x45d5CA3f295ad8BCa291cC4ecd33382DE40E4FAc",
-    nftBadgeContractAddress: "0x45d5CA3f295ad8BCa291cC4ecd33382DE40E4FAc",
-    "nodeUrl": "http://localhost:8669",
-    "network": {
-      "id": "solo",
-      "name": "solo",
-      "type": "solo",
-      "defaultNet": true,
-      "urls": [
-        "http://localhost:8669"
-      ],
-      "explorerUrl": "https://explore-testnet.vechain.org",
-      "blockTime": 1000 * 10,
-      "genesis": {
-        "number": 0,
-        "id": "0x00000000c05a20fbca2bf6ae3affba6af4a74b800b585bf7a4988aba7aea69f6",
-        "size": 170,
-        "parentID": "0xffffffff53616c757465202620526573706563742c20457468657265756d2100",
-        "timestamp": 1530316800,
-        "gasLimit": 10000000,
-        "beneficiary": "0x0000000000000000000000000000000000000000",
-        "gasUsed": 0,
-        "totalScore": 0,
-        "txsRoot": "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
-        "txsFeatures": 0,
-        "stateRoot": "0x93de0ffb1f33bc0af053abc2a87c4af44594f5dcb1cb879dd823686a15d68550",
-        "receiptsRoot": "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
-        "signer": "0x0000000000000000000000000000000000000000",
-        "isTrunk": true,
-        "transactions": []
-      }
-=======
   "b3trContractAddress": "0xf7Bb9fE5e7450f3a97910D88Ec0C2CA599be2070",
   "vot3ContractAddress": "0xa48F6D39bE18cc49F3864cD021aA83559BFe61B5",
   "b3trGovernorAddress": "0xfA4B4D7F42Ce49B5D3FC21a59151a0A9545b8bbC",
@@ -77,7 +37,6 @@ import { AppConfig } from "."
       "signer": "0x0000000000000000000000000000000000000000",
       "isTrunk": true,
       "transactions": []
->>>>>>> Stashed changes
     }
     }
     export default config;
