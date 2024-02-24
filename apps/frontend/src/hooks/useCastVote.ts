f7dcb121627dbbace8ba6ceefc561934e9195cc4
Piero Bassa <pierobassa222@gmail.com>
2024-02-24 14:15:11 +0100

    feat: refreshing badge query keys on vote cast

--- apps/frontend/src/hooks/useCastVote.ts
+++ apps/frontend/src/hooks/useCastVote.ts
@@ -1,4 +1,12 @@
-import { VoteType, buildCastVoteTx, getHasVotedQueryKey, getProposalVotesQuerykey } from "@/api"
+import {
+  VoteType,
+  buildCastVoteTx,
+  getB3trBadgeBalanceQueryKey,
+  getHasVotedQueryKey,
+  getParticipatedInGovernanceQueryKey,
+  getProposalVotesQuerykey,
+  useB3trBadgeBalance,
+} from "@/api"
 import { useToast } from "@chakra-ui/react"
 import { useQueryClient } from "@tanstack/react-query"
 import { UseSendTransactionReturnValue, useSendTransaction } from "./useSendTransaction"
@@ -57,6 +65,19 @@ export const useCastVote = ({
       })
       await queryClient.refetchQueries({
         queryKey: getHasVotedQueryKey(proposalId, account ?? undefined),
+      })
+
+      await queryClient.cancelQueries({
+        queryKey: getB3trBadgeBalanceQueryKey(account),
+      })
+      await queryClient.refetchQueries({
+        queryKey: getB3trBadgeBalanceQueryKey(account),
+      })
+      await queryClient.cancelQueries({
+        queryKey: getParticipatedInGovernanceQueryKey(account),
+      })
+      await queryClient.refetchQueries({
+        queryKey: getParticipatedInGovernanceQueryKey(account),
       })
     }
 
