900283281d42aacf8fc77ecbb094c84e94fa6f8b
Dan Rusnac <rusnac14@gmail.com>
2024-02-24 17:46:35 +0100

    feat: slice to 4 apps

--- apps/frontend/src/components/DashboardXApps.tsx
+++ apps/frontend/src/components/DashboardXApps.tsx
@@ -14,21 +14,30 @@ import {
   Grid,
   useColorModeValue,
   IconButton,
+  Button,
 } from "@chakra-ui/react"
-import { GoArrowUpRight } from "react-icons/go"
+import { useMemo } from "react"
+import { FiArrowUpRight } from "react-icons/fi"
 
 export const DashboardXApps = () => {
-  const { data: xApps, isLoading, isError } = useXApps()
+  const { data: xApps } = useXApps()
+
+  const slicedXApps = useMemo(() => xApps?.slice(0, 4), [xApps])
   return (
     <Card>
       <CardHeader>
         <HStack w="full" justify={"space-between"}>
           <Heading size="md">Explore dApps</Heading>
+          {slicedXApps && slicedXApps.length > 4 && (
+            <Button variant="link" colorScheme="blue" rightIcon={<FiArrowUpRight />}>
+              See all
+            </Button>
+          )}
         </HStack>
       </CardHeader>
       <CardBody>
         <Grid templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)"]} gap={6} w="full">
-          {xApps?.map(xApp => <XApp key={xApp.id} xApp={xApp} />)}
+          {slicedXApps?.map(xApp => <XApp key={xApp.id} xApp={xApp} />)}
         </Grid>
       </CardBody>
     </Card>
@@ -74,7 +83,7 @@ const XApp = ({ xApp }: { xApp: XApp }) => {
                   fontSize="20px"
                   onClick={() => window.open(appMetadata?.external_url, "_blank")}
                   color={buttonIconColor}
-                  icon={<GoArrowUpRight />}
+                  icon={<FiArrowUpRight />}
                 />
               </Skeleton>
             </HStack>
