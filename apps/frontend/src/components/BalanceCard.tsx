956b94e5feebbe82326c3e83c5cc97a429b861b8
ve-akanoce <eriknucib@live.it>
2024-02-26 11:30:55 +0200

    refactor: DashobardWalletNotConnect now wraps rewards card as well

--- apps/frontend/src/components/BalanceCard.tsx
+++ apps/frontend/src/components/BalanceCard.tsx
@@ -19,9 +19,9 @@ import {
   Spinner,
   Image,
 } from "@chakra-ui/react"
-import { WalletButton, useWallet } from "@vechain/dapp-kit-react"
+import { useWallet } from "@vechain/dapp-kit-react"
 import { useMemo } from "react"
-import { B3TRIcon, TwoFingersIcon, VOT3Icon } from "./Icons"
+import { B3TRIcon, VOT3Icon } from "./Icons"
 import { SwapButton } from "./Swap/SwapButton"
 import { useTokenColors } from "@/hooks"
 
@@ -164,50 +164,6 @@ export const BalanceCard: React.FC<Props> = () => {
           </Show>
         </VStack>
       </CardBody>
-      {!account && (
-        <>
-          <Image
-            src="/images/not_connected_wallet_bottom_layer.png"
-            pos={"absolute"}
-            bottom={0}
-            borderRadius={"lg"}
-            left={0}
-            zIndex={1}
-            boxSize={"full"}
-            w="full"
-          />
-          <Image
-            src="/images/not_connected_wallet_top_layer.png"
-            pos={"absolute"}
-            top={0}
-            borderRadius={"lg"}
-            right={0}
-            zIndex={1}
-            h="50%"
-          />
-          <Flex
-            borderRadius={"lg"}
-            bg="primary.400"
-            position={"absolute"}
-            h={"100%"}
-            w={"100%"}
-            align="center"
-            justify="center">
-            <VStack gap={6} zIndex={2}>
-              <TwoFingersIcon boxSize={20} />
-              <Box>
-                <Heading fontSize="xl" textAlign={"center"} color="white">
-                  Wallet not connected
-                </Heading>
-                <Text mt={2} textAlign={"center"} fontSize="md" fontWeight={"400"} color="white">
-                  Connect your wallet to check your balance
-                </Text>
-              </Box>
-              <WalletButton />
-            </VStack>
-          </Flex>
-        </>
-      )}
     </Card>
   )
 }
