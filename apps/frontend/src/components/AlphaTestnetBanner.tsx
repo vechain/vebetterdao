9aeef756dc1914c8134437474a4d386840c84911
ve-akanoce <eriknucib@live.it>
2024-02-26 18:58:31 +0200

    feat(AlphaTestnetBanner): polished component with framer motion

---
+++ apps/frontend/src/components/AlphaTestnetBanner.tsx
@@ -0,0 +1,77 @@
+import { HStack, Text } from "@chakra-ui/react"
+import { motion } from "framer-motion"
+import { useEffect, useRef, useState } from "react"
+
+const text = "Alpha Testnet Phase"
+const MotionHStack = motion(HStack)
+export const AlphaTestnetBanner = () => {
+  const textsNumber = 50
+  const texts = Array.from({ length: textsNumber }, _ => text)
+  const gap = 8
+
+  //Create an ininite horizontal text slider with framer motion
+
+  const [width, setWidth] = useState(0)
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (containerRef.current) {
+      setWidth(containerRef.current.offsetWidth)
+    }
+  }, [containerRef])
+
+  return (
+    <HStack
+      py={3}
+      ref={containerRef}
+      w="full"
+      justify="center"
+      align="center"
+      overflow="hidden"
+      position="relative"
+      bg="black"
+      _before={{
+        content: '""',
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        bg: "linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 8%)",
+        zIndex: 1,
+      }}
+      _after={{
+        content: '""',
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        bg: "linear-gradient(90deg, rgba(0,0,0,0) 92%, rgba(0,0,0,1) 100%)",
+        zIndex: 1,
+      }}>
+      <MotionHStack
+        spacing={gap}
+        initial={{ x: 0 }}
+        animate={{ x: -width - gap }}
+        transition={{
+          duration: 30,
+          ease: "linear",
+          repeat: Infinity,
+          repeatType: "loop",
+        }}>
+        {texts.map((text, index) => (
+          <Text
+            key={index}
+            fontSize="md"
+            color="secondary.500"
+            whiteSpace="nowrap"
+            textTransform={"uppercase"}
+            fontWeight={"300"}>
+            {text}
+          </Text>
+        ))}
+      </MotionHStack>
+    </HStack>
+  )
+}
