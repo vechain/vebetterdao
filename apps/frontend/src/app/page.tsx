92a0ca917f5d8b5a5d75e3ac4a54a5236bc4ae0e
ve-akanoce <eriknucib@live.it>
2024-02-27 19:55:12 +0100

    feat(homepage): display spinner when lazy loading all the components

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,49 +1,32 @@
 "use client"
 
 import { AnalyticsUtils } from "@/utils"
-import { Spinner, Stack, VStack } from "@chakra-ui/react"
+import { Skeleton, Spinner, Stack, VStack } from "@chakra-ui/react"
 import dynamic from "next/dynamic"
-import { Suspense, useEffect } from "react"
+import { useEffect } from "react"
 
-const DashboardSideBar = dynamic(() => import("@/components/DashboardSideBar").then(mod => mod.DashboardSideBar), {
+const HomePageContent = dynamic(() => import("@/components/HomepageContent").then(mod => mod.HomePageContent), {
   ssr: false,
+  loading: () => (
+    <VStack w="full" spacing={12} h="80vh" justify="center">
+      <Spinner size={"lg"} />
+    </VStack>
+  ),
 })
-
-const SupplyBreakdownCard = dynamic(
-  () => import("@/components/SupplyBreakdownCard").then(mod => mod.SupplyBreakdownCard),
-  { ssr: false },
-)
-
-const DashboardAllocationRounds = dynamic(
-  () => import("./rounds/components/DashboardAllocationRounds").then(mod => mod.DashboardAllocationRounds),
-  { ssr: false },
-)
-
-const DashboardXApps = dynamic(() => import("@/components/DashboardXApps").then(mod => mod.DashboardXApps), {
-  ssr: false,
-})
-
 export default function Home() {
   useEffect(() => {
     AnalyticsUtils.trackPage("Home")
   }, [])
   return (
     <VStack w="full" spacing={12}>
-      <Suspense fallback={<Spinner alignSelf={"center"} />}>
-        <Stack
-          direction={["column-reverse", "column-reverse", "row"]}
-          w="full"
-          justify="space-between"
-          align={["stretch", "stretch", "flex-start"]}
-          spacing={12}>
-          <VStack flex={4} justifyContent="stretch" alignItems={"stretch"} spacing={4}>
-            <SupplyBreakdownCard />
-            <DashboardAllocationRounds />
-            <DashboardXApps />
-          </VStack>
-          <DashboardSideBar />
-        </Stack>
-      </Suspense>
+      <Stack
+        direction={["column-reverse", "column-reverse", "row"]}
+        w="full"
+        justify="space-between"
+        align={["stretch", "stretch", "flex-start"]}
+        spacing={12}>
+        <HomePageContent />
+      </Stack>
     </VStack>
   )
 }
