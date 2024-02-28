a1d8ce7f2bf1452fa94713198f8939f676b1c1da
Erik Nucibella <eriknucib@live.it>
2024-02-28 12:12:49 +0100

    fix: faster animation in homepage (#391)

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       transition={{
-        duration: 2,
+        duration: 1,
         delay: 0.5,
         ease: [0, 0.71, 0.2, 1.01],
       }}>
