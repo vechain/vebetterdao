f560b916e58c61cab7ad61c329834b427185502b
ve-akanoce <eriknucib@live.it>
2024-03-01 17:56:35 +0100

    fix: typo, comments, lables when quorum fails

--- apps/frontend/src/components/AllocationXAppsVotesCard/AllocationXAppsVotesCard.tsx
+++ apps/frontend/src/components/AllocationXAppsVotesCard/AllocationXAppsVotesCard.tsx
@@ -67,7 +67,7 @@ export const AllocationXAppsVotesCard = ({ roundId }: Props) => {
               <Box>
                 <AlertTitle>Quorum was not reached for this round</AlertTitle>
                 <AlertDescription>
-                  B3TR allocation will be distrivuted according to the votes of the previous allocation
+                  B3TR allocation will be distributed according to the votes of the previous allocation
                 </AlertDescription>
               </Box>
             </Alert>
--- apps/frontend/src/components/AllocationXAppsVotesCard/AllocationXAppsVotesRankingChart.tsx
+++ apps/frontend/src/components/AllocationXAppsVotesCard/AllocationXAppsVotesRankingChart.tsx
@@ -1,7 +1,14 @@
-import { useAllocationVotes, useRoundXApps, useXAppMetadata, useXAppRoundEarnings, useXAppsVotes } from "@/api"
+import {
+  useAllocationVotes,
+  useAllocationsRoundState,
+  useRoundXApps,
+  useXAppMetadata,
+  useXAppRoundEarnings,
+  useXAppsVotes,
+} from "@/api"
 import { useIpfsImage } from "@/api/ipfs"
 import { notFoundImage } from "@/constants"
-import { Box, HStack, Heading, Image, Skeleton, Text, VStack, useColorModeValue } from "@chakra-ui/react"
+import { Box, HStack, Heading, Image, Skeleton, Text, VStack } from "@chakra-ui/react"
 import { useMemo } from "react"
 import { B3TRIcon } from "../Icons"
 
@@ -15,14 +22,6 @@ type AppVotesData = {
   app: string
 }
 
-const enOrdinalRules = new Intl.PluralRules("en-US", { type: "ordinal" })
-const suffixMap = {
-  one: "st",
-  two: "nd",
-  few: "rd",
-  other: "th",
-}
-
 const compactFormatter = new Intl.NumberFormat("en-US", {
   notation: "compact",
   compactDisplay: "short",
@@ -33,7 +32,7 @@ export const AllocationXAppsVotesRankingChart = ({ roundId, maxRanks }: Props) =
 
   const xAppsVotes = useXAppsVotes(xApps?.map(app => app.id) ?? [], roundId)
 
-  const { data: votes, isLoading: votesLoading } = useAllocationVotes(roundId)
+  const { data: votes } = useAllocationVotes(roundId)
 
   const sortedData = useMemo(
     () =>
@@ -67,23 +66,21 @@ const VotesHorizontalBar = ({
   roundId: string
   totalVotes?: string
 }) => {
-  const { data: appMetadata, error: appMetadatError } = useXAppMetadata(data.app)
+  const { data: appMetadata } = useXAppMetadata(data.app)
   const { data: logo, isLoading: isLogoLoading } = useIpfsImage(appMetadata?.logo)
+
+  const { data: roundState, isLoading: roundStateLoading } = useAllocationsRoundState(roundId)
 
   const { data: forecastedEarnings, isLoading: forecastedEarningsLoading } = useXAppRoundEarnings(roundId, data.app)
 
-  const indexLabel = index + 1
-
   const votesPercentage = Number(totalVotes) === 0 ? 0 : (Number(data.votes) / Number(totalVotes)) * 100
-
-  const bgShade = useColorModeValue("100", "200")
 
   const bgColor = `green`
 
   return (
     <VStack spacing={4} align={"flex-start"} w="full">
       <HStack justify={"space-between"} w="full">
-        <HStack spacing={3} align={"center"} justify={"flex-start"} w="full">
+        <HStack spacing={3} align={"center"} justify={"flex-start"}>
           <Skeleton isLoaded={!isLogoLoading} boxSize={12}>
             <Image src={logo?.image ?? notFoundImage} w="full" borderRadius="9px" alt={appMetadata?.name} />
           </Skeleton>
@@ -91,7 +88,7 @@ const VotesHorizontalBar = ({
             {appMetadata?.name}
           </Heading>
         </HStack>
-        <HStack spacing={8} align={"center"} justify={"flex-start"} w="full" alignSelf={"flex-end"}>
+        <HStack spacing={8} align={"center"} justify={"flex-start"} alignSelf={"flex-end"}>
           <Box>
             <Skeleton isLoaded={!forecastedEarningsLoading}>
               <HStack spacing={1} align={"center"} justify={"flex-start"} w="full">
@@ -101,21 +98,22 @@ const VotesHorizontalBar = ({
                 <B3TRIcon boxSize="20px" colorVariant="dark" />
               </HStack>
             </Skeleton>
-            <Text fontSize={"md"} fontWeight={"300"}>
-              Real time B3TR distribution
-            </Text>
+            <Skeleton isLoaded={!roundStateLoading}>
+              <Text fontSize={"md"} fontWeight={"300"}>
+                {roundState === "0" ? " Real time b3tr distribution" : "Distributed"}
+              </Text>
+            </Skeleton>
           </Box>
           <VStack spacing={0} align="flex-end">
-            <HStack spacing={1} align={"center"} justify={"flex-start"} w="full">
-              <Heading size="md" fontWeight={"medium"}>
-                {compactFormatter.format(Number(data.votes))}
-              </Heading>
-              <B3TRIcon boxSize="20px" colorVariant="dark" />
-            </HStack>
+            <Heading size="md" fontWeight={"700"} color="green">
+              {compactFormatter.format(Number(data.votes))}
+            </Heading>
 
-            <Text fontSize={"md"} fontWeight={"300"}>
-              Real time votes
-            </Text>
+            <Skeleton isLoaded={!roundStateLoading}>
+              <Text fontSize={"md"} fontWeight={"300"}>
+                {roundState === "0" ? " Real time votes" : "Votes"}
+              </Text>
+            </Skeleton>
           </VStack>
         </HStack>
       </HStack>
@@ -124,32 +122,4 @@ const VotesHorizontalBar = ({
       </Box>
     </VStack>
   )
-  //   return (
-  //     <HStack
-  //       py={2}
-  //       w={`${votesPercentage}%`}
-  //       bg={`${bgColor}.${bgShade}`}
-  //       borderRadius={"xl"}
-  //       justify="space-between"
-  //       align="center">
-  //       <Box ml={2}>
-  //         <Heading size="md" fontSize={"20px"} color={`${bgColor}.${rankingPositionShade}`}>
-  //           {rankingPositionLabel}
-  //         </Heading>
-  //         <VStack spacing={0} align={"flex-start"} justify={"flex-end"} w="full">
-  //           <Heading size="xl" color={`${bgColor}.${votesCountShade}`} lineHeight={"100%"}>
-  //             {compactFormatter.format(Number(data.votes))}
-  //           </Heading>
-  //           <HStack spacing={2} align={"center"} justify={"flex-start"} w="full">
-  //             <Skeleton isLoaded={!isLogoLoading} boxSize={6}>
-  //               <Image src={logo?.image ?? notFoundImage} w="full" borderRadius="9px" alt={appMetadata?.name} />
-  //             </Skeleton>
-  //             <Heading size="md" color={`${bgColor}.${nameShade}`} fontWeight={"medium"}>
-  //               {appMetadata?.name}
-  //             </Heading>
-  //           </HStack>
-  //         </VStack>
-  //       </Box>
-  //     </HStack>
-  //   )
 }
