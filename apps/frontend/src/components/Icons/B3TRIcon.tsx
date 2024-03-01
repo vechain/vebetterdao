53346d359f5da5a125e3b4936702275e22b8a686
Erik Nucibella <eriknucib@live.it>
2024-03-01 17:06:35 +0100

    refactor(AllocationRoundDetails): correct icon, fix copy, display voting rewards and base allocation (#409)
    * feat: create useAllocationBaseAmount
    
    * refactor(AllocationRoundDetails): integrate useBaseAllocation, add voting rewards and logo
    
    * fix: copyu of total allocation
    
    * fix: remove unused imports, add paths in dictionaries in b3tricon

--- apps/frontend/src/components/Icons/B3TRIcon.tsx
+++ apps/frontend/src/components/Icons/B3TRIcon.tsx
@@ -1,12 +1,21 @@
 import { Image, ImageProps, useColorModeValue } from "@chakra-ui/react"
 import React from "react"
 
-type Props = ImageProps
+type Props = ImageProps & {
+  colorVariant?: "light" | "dark"
+}
+
+const paths = {
+  light: "/images/logo/b3tr_logo.svg",
+  dark: "/images/logo/b3tr_logo_dark.svg",
+}
 
 /**
  * B3TRIcon is the icon for the B3TR token
  */
-export const B3TRIcon: React.FC<Props> = props => {
-  const logo = useColorModeValue("/images/logo/b3tr_logo.svg", "/images/logo/b3tr_logo_dark.svg")
-  return <Image src={logo} {...props} />
+export const B3TRIcon: React.FC<Props> = ({ colorVariant, ...props }) => {
+  const logo = useColorModeValue(paths.light, paths.dark)
+  const logoVariant = colorVariant ? paths[colorVariant] : logo
+
+  return <Image src={logoVariant} {...props} />
 }
