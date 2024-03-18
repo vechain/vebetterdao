0936d9896d24aae18272c566bb64572f90aba180
ve-akanoce <eriknucib@live.it>
2024-03-18 17:59:25 +0200

    fix: mock hook call in allocationDetail test

--- apps/frontend/src/app/rounds/[roundId]/page.test.tsx
+++ apps/frontend/src/app/rounds/[roundId]/page.test.tsx
@@ -2,7 +2,16 @@ import { expect, test } from "vitest"
 import AllocationDetail from "./page"
 import { render, screen } from "../../../../test"
 
+import * as hooks from "@/api/contracts/xAllocations"
+
 test("Allocations", async () => {
+  //@ts-ignore
+  vi.spyOn(hooks, "useAllocationsRoundState").mockReturnValue({
+    data: "1",
+    isLoading: false,
+    isError: false,
+  })
+
   const roundId = "1"
   render(
     <AllocationDetail
