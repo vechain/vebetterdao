1ceb048c21e442741a0d7c11a75ae4c9996fade1
Dan Rusnac <rusnac14@gmail.com>
2024-03-19 18:07:55 +0100

    fix: B3TRGovernance is initialiazable

--- packages/contracts/contracts/governance/B3TRGovernor.sol
+++ packages/contracts/contracts/governance/B3TRGovernor.sol
@@ -11,6 +11,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract B3TRGovernor is
+  Initializable,
   GovernorUpgradeable,
   GovernorSettingsUpgradeable,
   GovernorCountingSimpleUpgradeable,
