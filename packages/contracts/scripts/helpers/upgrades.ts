ec07f2e3c7f9ad992ac0aa5f36b5e7ae5d78119c
Dan Rusnac <rusnac14@gmail.com>
2024-03-19 17:53:48 +0100

    feat: delete proxy contract

--- packages/contracts/scripts/helpers/upgrades.ts
+++ packages/contracts/scripts/helpers/upgrades.ts
@@ -1,5 +1,6 @@
 import { BaseContract, Interface } from "ethers"
 import { ethers } from "hardhat"
+import ERC1967Proxy from "@openzeppelin/upgrades-core/artifacts/@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol/ERC1967Proxy.json"
 
 export const deployProxy = async (contractName: string, args: any[]): Promise<BaseContract> => {
   // Deploy the implementation contract
@@ -8,7 +9,7 @@ export const deployProxy = async (contractName: string, args: any[]): Promise<Ba
   await implementation.waitForDeployment()
 
   // Deploy the proxy contract, link it to the implementation and call the initializer
-  const proxyFactory = await ethers.getContractFactory("ERC1967Proxy")
+  const proxyFactory = await ethers.getContractFactory(ERC1967Proxy.abi, ERC1967Proxy.bytecode)
   const proxy = await proxyFactory.deploy(
     await implementation.getAddress(),
     getInitializerData(Contract.interface, args),
@@ -30,12 +31,7 @@ export const upgradeProxy = async (
   const implementation = await Contract.deploy()
   await implementation.waitForDeployment()
 
-  const proxyContract = await ethers.getContractAt("ERC1967Proxy", proxyAddress)
-
-  const currentImplementationContract = await ethers.getContractAt(
-    previousVersionContractName,
-    await proxyContract.getAddress(),
-  )
+  const currentImplementationContract = await ethers.getContractAt(previousVersionContractName, proxyAddress)
 
   const tx = await currentImplementationContract.upgradeToAndCall(
     await implementation.getAddress(),
