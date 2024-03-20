a59a972676f0e8241be8e6816617345b1f0dcac4
Piero Bassa <pierobassa222@gmail.com>
2024-03-20 16:43:58 +0100

    chore: skip lambda test

--- packages/lambda/test/startRound/lambda.test.ts
+++ packages/lambda/test/startRound/lambda.test.ts
@@ -3,27 +3,32 @@ import { execute } from "lambda-local"
 import path from "path"
 
 describe("startRound", () => {
-  it("should trigger start round lambda on staging environment", async () => {
-    const event = {}
+  // Remove ".skip" to run the test
+  it.skip(
+    "should trigger start round lambda on staging environment",
+    async () => {
+      const event = {}
 
-    const result = await new Promise((resolve, reject) => {
-      execute({
-        event,
-        lambdaPath: path.join(__dirname, "../../dist/testnet/index.js"),
-        timeoutMs: 5 * 60 * 1000,
-        callback: (error: Error, result: unknown) => {
-          if (error) {
-            reject(error)
-          } else {
-            resolve(result)
-          }
-        },
+      const result = await new Promise((resolve, reject) => {
+        execute({
+          event,
+          lambdaPath: path.join(__dirname, "../../dist/testnet/index.js"),
+          timeoutMs: 5 * 60 * 1000,
+          callback: (error: Error, result: unknown) => {
+            if (error) {
+              reject(error)
+            } else {
+              resolve(result)
+            }
+          },
+        })
       })
-    })
 
-    // Use the result for further assertions if necessary
-    console.log(result)
-    // Example assertion
-    expect(result).toBeDefined()
-  })
+      // Use the result for further assertions if necessary
+      console.log(result)
+      // Example assertion
+      expect(result).toBeDefined()
+    },
+    5 * 60 * 1000,
+  )
 })
