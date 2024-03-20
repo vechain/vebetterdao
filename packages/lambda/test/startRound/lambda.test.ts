9af84c103251c31f7c8f0a5349074a8abcef33f2
Piero Bassa <pierobassa222@gmail.com>
2024-03-20 15:54:36 +0100

    refactor: test timeout and building before test in lambda tests

--- packages/lambda/test/startRound/lambda.test.ts
+++ packages/lambda/test/startRound/lambda.test.ts
@@ -3,31 +3,27 @@ import { execute } from "lambda-local"
 import path from "path"
 
 describe("startRound", () => {
-  it(
-    "should trigger start round lambda on staging environment",
-    async () => {
-      const event = {}
+  it("should trigger start round lambda on staging environment", async () => {
+    const event = {}
 
-      const result = await new Promise((resolve, reject) => {
-        execute({
-          event,
-          lambdaPath: path.join(__dirname, "../../dist/testnet/index.js"),
-          timeoutMs: 5 * 60 * 1000,
-          callback: (error: Error, result: unknown) => {
-            if (error) {
-              reject(error)
-            } else {
-              resolve(result)
-            }
-          },
-        })
+    const result = await new Promise((resolve, reject) => {
+      execute({
+        event,
+        lambdaPath: path.join(__dirname, "../../dist/testnet/index.js"),
+        timeoutMs: 5 * 60 * 1000,
+        callback: (error: Error, result: unknown) => {
+          if (error) {
+            reject(error)
+          } else {
+            resolve(result)
+          }
+        },
       })
+    })
 
-      // Use the result for further assertions if necessary
-      console.log(result)
-      // Example assertion
-      expect(result).toBeDefined()
-    },
-    5 * 60 * 1000,
-  )
+    // Use the result for further assertions if necessary
+    console.log(result)
+    // Example assertion
+    expect(result).toBeDefined()
+  })
 })
