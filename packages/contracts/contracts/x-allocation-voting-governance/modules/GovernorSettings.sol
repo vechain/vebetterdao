78a4efccceae4e07f7866715944f4842d7911780
Dan Rusnac <rusnac14@gmail.com>
2024-03-20 14:23:33 +0100

    feat: use storage location in GovernorSettings

--- packages/contracts/contracts/x-allocation-voting-governance/modules/GovernorSettings.sol
+++ packages/contracts/contracts/x-allocation-voting-governance/modules/GovernorSettings.sol
@@ -9,8 +9,21 @@ import { XAllocationVotingGovernor } from "../XAllocationVotingGovernor.sol";
  * @dev Extension of {XAllocationVotingGovernor} for settings updatable through governance.
  */
 abstract contract GovernorSettings is XAllocationVotingGovernor {
-  // duration: limited to uint32 in core
-  uint32 private _votingPeriod;
+  /// @custom:storage-location erc7201:b3tr.storage.XAllocationVotingGovernor.GovernorSettings
+  struct GovernorSettingsStorage {
+    // duration: limited to uint32 in core
+    uint32 _votingPeriod;
+  }
+
+  // keccak256(abi.encode(uint256(keccak256("b3tr.storage.XAllocationVotingGovernor.GovernorSettings")) - 1)) & ~bytes32(uint256(0xff))
+  bytes32 private constant GovernorSettingsStorageLocation =
+    0x61dedaa499b53d67b3d7e1868cee5772a81e32ad239a9603b0a8a5f779327500;
+
+  function _getGovernorSettingsStorage() private pure returns (GovernorSettingsStorage storage $) {
+    assembly {
+      $.slot := GovernorSettingsStorageLocation
+    }
+  }
 
   event VotingPeriodSet(uint256 oldVotingPeriod, uint256 newVotingPeriod);
 
@@ -25,7 +38,8 @@ abstract contract GovernorSettings is XAllocationVotingGovernor {
    * @dev See {IXAllocationVotingGovernor-votingPeriod}.
    */
   function votingPeriod() public view virtual override returns (uint256) {
-    return _votingPeriod;
+    GovernorSettingsStorage storage $ = _getGovernorSettingsStorage();
+    return $._votingPeriod;
   }
 
   /**
@@ -46,7 +60,9 @@ abstract contract GovernorSettings is XAllocationVotingGovernor {
     if (newVotingPeriod == 0) {
       revert GovernorInvalidVotingPeriod(0);
     }
-    emit VotingPeriodSet(_votingPeriod, newVotingPeriod);
-    _votingPeriod = newVotingPeriod;
+    GovernorSettingsStorage storage $ = _getGovernorSettingsStorage();
+
+    emit VotingPeriodSet($._votingPeriod, newVotingPeriod);
+    $._votingPeriod = newVotingPeriod;
   }
 }
