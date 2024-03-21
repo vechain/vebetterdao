1ceb32f3694a2e1192c93d0600ee8313f17962a9
Dan Rusnac <rusnac14@gmail.com>
2024-03-21 10:16:22 +0100

    feat: use storage layout in B3TRBedge

--- packages/contracts/contracts/B3TRBadge.sol
+++ packages/contracts/contracts/B3TRBadge.sol
@@ -20,34 +20,22 @@ import { IB3TR } from "./interfaces/IB3TR.sol";
 contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, IERC6372, ReentrancyGuard {
   using Checkpoints for Checkpoints.Trace208;
 
-  // XAllocationVotingGovernor contract
-  IXAllocationVotingGovernor public xAllocationsGovernor;
-
-  // B3TRGovernor contract
-  IB3TRGovernor public b3trGovernor;
-
-  // B3TR token contract
-  IB3TR public b3tr;
-
-  address public treasury;
-
-  // Base URI for the Token
-  string private _baseTokenURI;
-
-  // Token ID counter
-  uint256 private _nextTokenId;
-
-  // Current Maximum level the Token can be minted or upgraded to
-  uint256 public MAX_LEVEL; // Set to 0 by allowing only the free minting of the Earth Token
-
-  // Mapping from token ID to level
-  mapping(uint256 => uint256) public levelOf;
-
-  // Mapping from owner to tokenId selected for voting rewards
-  mapping(address => uint256) public selectedTokenId;
-
-  // Mapping from X/Economic node type to maximum mintable level
-  /*
+  /// @custom:storage-location erc7201:b3tr.storage.B3TRBadge
+  struct B3TRBadgeStorage {
+    IXAllocationVotingGovernor xAllocationsGovernor; // XAllocationVotingGovernor contract
+    IB3TRGovernor b3trGovernor; // B3TRGovernor contract
+    IB3TR b3tr; // B3TR token contract
+    address treasury;
+    string _baseTokenURI; // Base URI for the Token
+    uint256 _nextTokenId; // Token ID counter
+    // Current Maximum level the Token can be minted or upgraded to
+    uint256 MAX_LEVEL; // Set to 0 by allowing only the free minting of the Earth Token
+    // Mapping from token ID to level
+    mapping(uint256 => uint256) levelOf;
+    // Mapping from owner to tokenId selected for voting rewards
+    mapping(address => uint256) selectedTokenId;
+    // Mapping from X/Economic node type to maximum mintable level
+    /*
     0 => Strength
     1 => Thunder
     2 => Mjolnir
@@ -55,13 +43,22 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
     4 => StrengthX
     5 => ThunderX
     6 => MjolnirX
-  */
-  mapping(uint8 => uint256) private _xNodeTypeToMaxMintableLevel;
+    */
+    mapping(uint8 => uint256) _xNodeTypeToMaxMintableLevel;
+    mapping(uint256 => uint256) _b3trToUpgradeToLevel;
+    // Mapping from owner to their selected GM NFT level to be used for voter rewards
+    mapping(address owner => Checkpoints.Trace208) _selectedLevelCheckpoints;
+  }
 
-  mapping(uint256 => uint256) private _b3trToUpgradeToLevel;
+  // keccak256(abi.encode(uint256(keccak256("b3tr.storage.B3TRBadge")) - 1)) & ~bytes32(uint256(0xff))
+  bytes32 private constant B3TRBadgeStorageLocation =
+    0xa3a4dbdafa3539d2a7f76379fff3516428de5d09ad2bbe195434cac5e7193900;
 
-  // Mapping from owner to their selected GM NFT level to be used for voter rewards
-  mapping(address owner => Checkpoints.Trace208) private _selectedLevelCheckpoints;
+  function _getB3TRBadgeStorage() private pure returns (B3TRBadgeStorage storage $) {
+    assembly {
+      $.slot := B3TRBadgeStorageLocation
+    }
+  }
 
   /**
    * @dev The clock was incorrectly modified.
@@ -108,21 +105,23 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
     require(_b3tr != address(0), "Galaxy Member: B3TR token address cannot be the zero address");
     require(_treasury != address(0), "Galaxy Member: Treasury address cannot be the zero address");
 
-    MAX_LEVEL = maxLevel;
-    _baseTokenURI = baseTokenURI;
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+
+    $.MAX_LEVEL = maxLevel;
+    $._baseTokenURI = baseTokenURI;
 
     for (uint8 i = 0; i < xNodeMaxMintableLevels.length; i++) {
-      _xNodeTypeToMaxMintableLevel[i] = xNodeMaxMintableLevels[i];
+      $._xNodeTypeToMaxMintableLevel[i] = xNodeMaxMintableLevels[i];
     }
 
     for (uint8 i = 0; i < b3trToUpgradeToLevel.length; i++) {
-      _b3trToUpgradeToLevel[i + 2] = b3trToUpgradeToLevel[i]; // First Level that requires B3TR is level 2
+      $._b3trToUpgradeToLevel[i + 2] = b3trToUpgradeToLevel[i]; // First Level that requires B3TR is level 2
     }
 
-    b3tr = IB3TR(_b3tr);
-    treasury = _treasury;
+    $.b3tr = IB3TR(_b3tr);
+    $.treasury = _treasury;
 
-    _nextTokenId = 1; // First token ID starts from 1
+    $._nextTokenId = 1; // First token ID starts from 1
 
     _grantRole(DEFAULT_ADMIN_ROLE, admin);
   }
@@ -138,14 +137,15 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
   // Mints the highest level Token the caller is allowed to mint
   function freeMint() public {
     require(participatedInGovernance(msg.sender), "Galaxy Member: User has not participated in governance");
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
 
     // TODO: Get User's X/Economic node type and check max mintable level
     // TODO: Check if that X/Economic node has not already been used to mint a Token (e.g., MintedLevelOfXNode[xNodeId])
     /* uint256 mintableLevel = 1; */
 
-    uint256 tokenId = _nextTokenId;
+    uint256 tokenId = $._nextTokenId;
 
-    levelOf[tokenId] = 1;
+    $.levelOf[tokenId] = 1;
 
     safeMint(msg.sender);
   }
@@ -153,38 +153,43 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
   // TODO: Mock X/Economic nodes NFT Contract, add to constructor and use it to check the X/Economic node type of the caller
   function upgrade(uint256 tokenId) public nonReentrant whenNotPaused {
     require(ownerOf(tokenId) == msg.sender, "Galaxy Member: you must own the Token to upgrade it");
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
 
-    uint256 currentLevel = levelOf[tokenId];
+    uint256 currentLevel = $.levelOf[tokenId];
 
-    require(currentLevel < MAX_LEVEL, "Galaxy Member: Token is already at max level");
+    require(currentLevel < $.MAX_LEVEL, "Galaxy Member: Token is already at max level");
 
-    uint256 b3trRequired = _b3trToUpgradeToLevel[currentLevel + 1];
+    uint256 b3trRequired = $._b3trToUpgradeToLevel[currentLevel + 1];
 
-    require(b3tr.balanceOf(msg.sender) >= b3trRequired, "Galaxy Member: Insufficient balance to upgrade");
+    require($.b3tr.balanceOf(msg.sender) >= b3trRequired, "Galaxy Member: Insufficient balance to upgrade");
 
-    levelOf[tokenId] = currentLevel + 1;
+    $.levelOf[tokenId] = currentLevel + 1;
 
-    if (selectedTokenId[msg.sender] == tokenId) {
-      _updateLevelSelected(msg.sender, levelOf[tokenId]);
+    if ($.selectedTokenId[msg.sender] == tokenId) {
+      _updateLevelSelected(msg.sender, $.levelOf[tokenId]);
     }
 
-    require(b3tr.transferFrom(msg.sender, treasury, b3trRequired), "B3TRBadge: Transfer failed");
+    require($.b3tr.transferFrom(msg.sender, $.treasury, b3trRequired), "B3TRBadge: Transfer failed");
 
-    emit Upgraded(tokenId, currentLevel, levelOf[tokenId]);
+    emit Upgraded(tokenId, currentLevel, $.levelOf[tokenId]);
   }
 
   function select(uint256 tokenId) public {
-    require(ownerOf(tokenId) == msg.sender, "Galaxy Member: Caller is not the owner of the Token");
-    require(selectedTokenId[msg.sender] != tokenId, "Galaxy Member: Token already selected");
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
 
-    _updateLevelSelected(msg.sender, levelOf[tokenId]);
+    require(ownerOf(tokenId) == msg.sender, "Galaxy Member: Caller is not the owner of the Token");
+    require($.selectedTokenId[msg.sender] != tokenId, "Galaxy Member: Token already selected");
+
+    _updateLevelSelected(msg.sender, $.levelOf[tokenId]);
 
     _select(msg.sender, tokenId);
   }
 
   function upgradeAndSelect(uint256 tokenId) public {
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+
     require(
-      selectedTokenId[msg.sender] != tokenId,
+      $.selectedTokenId[msg.sender] != tokenId,
       "Galaxy Member: Token already selected, consider upgrading it instead"
     );
 
@@ -195,7 +200,8 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
   // Mints the Token for the given address
   // Can't be called externally but only from the contract
   function safeMint(address to) internal {
-    uint256 tokenId = _nextTokenId++;
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    uint256 tokenId = $._nextTokenId++;
     _safeMint(to, tokenId);
   }
 
@@ -222,15 +228,17 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
   }
 
   function _select(address owner, uint256 tokenId) internal whenNotPaused {
-    selectedTokenId[owner] = tokenId;
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    $.selectedTokenId[owner] = tokenId;
 
     emit Selected(owner, tokenId);
   }
 
   function _updateLevelSelected(address owner, uint256 level) internal whenNotPaused {
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
     // If the selected level is different from the new level then we checkpoint the selected level to the new level
     if (getLevel(owner) != level) {
-      (uint256 oldLevel, uint256 newLevel) = _push(_selectedLevelCheckpoints[owner], SafeCast.toUint208(level));
+      (uint256 oldLevel, uint256 newLevel) = _push($._selectedLevelCheckpoints[owner], SafeCast.toUint208(level));
 
       emit SelectedLevel(owner, oldLevel, newLevel);
     }
@@ -244,14 +252,16 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
    * @dev Get number of checkpoints for `account`.
    */
   function _numCheckpoints(address account) internal view virtual returns (uint32) {
-    return SafeCast.toUint32(_selectedLevelCheckpoints[account].length());
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return SafeCast.toUint32($._selectedLevelCheckpoints[account].length());
   }
 
   // ---------- Setters ---------- //
 
   function setMaxLevel(uint256 level) public onlyRole(DEFAULT_ADMIN_ROLE) {
     require(level > 0, "Galaxy Member: Max level must be greater than 0");
-    MAX_LEVEL = level;
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    $.MAX_LEVEL = level;
   }
 
   function setMaxMintableLevels(uint8[] memory maxMintableLevels) public onlyRole(DEFAULT_ADMIN_ROLE) {
@@ -260,39 +270,42 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
       "Galaxy Member: Invalid number of max mintable levels. There should be 7 levels, one for each X/Economic node type"
     );
 
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
     for (uint8 i = 0; i < maxMintableLevels.length; i++) {
-      _xNodeTypeToMaxMintableLevel[i] = maxMintableLevels[i];
+      $._xNodeTypeToMaxMintableLevel[i] = maxMintableLevels[i];
     }
   }
 
   function setXAllocationsGovernorAddress(address _xAllocationsGovernor) public onlyRole(DEFAULT_ADMIN_ROLE) {
     require(_xAllocationsGovernor != address(0), "Galaxy Member: _xAllocationsGovernor cannot be the zero address");
-
-    xAllocationsGovernor = IXAllocationVotingGovernor(_xAllocationsGovernor);
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    $.xAllocationsGovernor = IXAllocationVotingGovernor(_xAllocationsGovernor);
   }
 
   function setB3trGovernorAddress(address _b3trGovernor) public onlyRole(DEFAULT_ADMIN_ROLE) {
     require(_b3trGovernor != address(0), "Galaxy Member: _b3trGovernor cannot be the zero address");
-
-    b3trGovernor = IB3TRGovernor(payable(_b3trGovernor));
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    $.b3trGovernor = IB3TRGovernor(payable(_b3trGovernor));
   }
 
   function setBaseURI(string memory baseTokenURI) public onlyRole(DEFAULT_ADMIN_ROLE) {
     require(bytes(baseTokenURI).length > 0, "Galaxy Member: Base URI must be set");
-
-    _baseTokenURI = baseTokenURI;
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    $._baseTokenURI = baseTokenURI;
   }
 
   function setB3TRtoUpgradeToLevel(uint256[] memory b3trToUpgradeToLevel) public onlyRole(DEFAULT_ADMIN_ROLE) {
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
     for (uint8 i = 0; i < b3trToUpgradeToLevel.length; i++) {
-      _b3trToUpgradeToLevel[i + 2] = b3trToUpgradeToLevel[i]; // First Level that requires B3TR is level 2
+      $._b3trToUpgradeToLevel[i + 2] = b3trToUpgradeToLevel[i]; // First Level that requires B3TR is level 2
     }
   }
 
   // ---------- Getters ---------- //
 
   function getLevel(address owner) public view returns (uint256) {
-    return _selectedLevelCheckpoints[owner].latest();
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $._selectedLevelCheckpoints[owner].latest();
   }
 
   function getPastLevel(address owner, uint256 timepoint) public view returns (uint256) {
@@ -300,7 +313,8 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
     if (timepoint >= currentTimepoint) {
       revert ERC5805FutureLookup(timepoint, currentTimepoint);
     }
-    return _selectedLevelCheckpoints[owner].upperLookupRecent(SafeCast.toUint48(timepoint));
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $._selectedLevelCheckpoints[owner].upperLookupRecent(SafeCast.toUint48(timepoint));
   }
 
   function numCheckpoints(address account) public view returns (uint32) {
@@ -308,13 +322,14 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
   }
 
   function participatedInGovernance(address user) public view returns (bool) {
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
     require(
-      xAllocationsGovernor != IXAllocationVotingGovernor(address(0)),
+      $.xAllocationsGovernor != IXAllocationVotingGovernor(address(0)),
       "Galaxy Member: XAllocationVotingGovernor not set"
     );
-    require(b3trGovernor != IB3TRGovernor(payable(address(0))), "Galaxy Member: B3TRGovernor not set");
+    require($.b3trGovernor != IB3TRGovernor(payable(address(0))), "Galaxy Member: B3TRGovernor not set");
 
-    if (xAllocationsGovernor.hasVotedOnce(user) || b3trGovernor.hasVotedOnce(user)) {
+    if ($.xAllocationsGovernor.hasVotedOnce(user) || $.b3trGovernor.hasVotedOnce(user)) {
       return true;
     }
 
@@ -326,19 +341,23 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
   }
 
   function getMaxMintableLevelOfXNode(uint8 xNodeType) public view returns (uint256) {
-    return _xNodeTypeToMaxMintableLevel[xNodeType];
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $._xNodeTypeToMaxMintableLevel[xNodeType];
   }
 
   function getB3TRtoUpgradeToLevel(uint256 level) public view returns (uint256) {
-    return _b3trToUpgradeToLevel[level];
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $._b3trToUpgradeToLevel[level];
   }
 
   function getNextLevel(uint256 tokenId) public view returns (uint256) {
-    return levelOf[tokenId] + 1;
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $.levelOf[tokenId] + 1;
   }
 
   function getB3TRtoUpgrade(uint256 tokenId) public view returns (uint256) {
-    return _b3trToUpgradeToLevel[levelOf[tokenId] + 1];
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $._b3trToUpgradeToLevel[$.levelOf[tokenId] + 1];
   }
 
   /**
@@ -362,8 +381,44 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
   }
 
   function tokenURI(uint256 tokenId) public view override(ERC721) returns (string memory) {
-    uint256 levelOfToken = levelOf[tokenId];
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    uint256 levelOfToken = $.levelOf[tokenId];
     return levelOfToken > 0 ? string.concat(baseURI(), Strings.toString(levelOfToken)) : "";
+  }
+
+  function xAllocationsGovernor() public view returns (IXAllocationVotingGovernor) {
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $.xAllocationsGovernor;
+  }
+
+  function b3trGovernor() public view returns (IB3TRGovernor) {
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $.b3trGovernor;
+  }
+
+  function b3tr() public view returns (IB3TR) {
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $.b3tr;
+  }
+
+  function treasury() public view returns (address) {
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $.treasury;
+  }
+
+  function MAX_LEVEL() public view returns (uint256) {
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $.MAX_LEVEL;
+  }
+
+  function levelOf(uint256 tokenId) public view returns (uint256) {
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $.levelOf[tokenId];
+  }
+
+  function selectedTokenId(address owner) public view returns (uint256) {
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $.selectedTokenId[owner];
   }
 
   // ---------- Overrides ---------- //
@@ -373,7 +428,8 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
     uint256 tokenId,
     address auth
   ) internal override(ERC721, ERC721Enumerable, ERC721Pausable) returns (address) {
-    _moveOwnershipLevel(auth, to, levelOf[tokenId], tokenId);
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    _moveOwnershipLevel(auth, to, $.levelOf[tokenId], tokenId);
 
     return super._update(to, tokenId, auth);
   }
@@ -389,6 +445,7 @@ contract B3TRBadge is ERC721, ERC721Enumerable, ERC721Pausable, AccessControl, I
   }
 
   function _baseURI() internal view override returns (string memory) {
-    return _baseTokenURI;
+    B3TRBadgeStorage storage $ = _getB3TRBadgeStorage();
+    return $._baseTokenURI;
   }
 }
