2756b0903ca9c54d3b01ded1f281e30d32f11be3
ve-akanoce <eriknucib@live.it>
2024-03-21 16:04:44 +0200

    feat(AppDetail): add go to dapps button

--- apps/frontend/src/app/apps/[appId]/components/AppDetailPageContent.tsx
+++ apps/frontend/src/app/apps/[appId]/components/AppDetailPageContent.tsx
@@ -1,14 +1,26 @@
-import { VStack } from "@chakra-ui/react"
+import { Box, Button, VStack } from "@chakra-ui/react"
 import { AppDetailCard } from "./AppDetailCard"
+import { FaArrowLeft } from "react-icons/fa6"
+import { useRouter } from "next/navigation"
 
 type Props = {
   appId: string
 }
 
 export const AppDetailPageContent = ({ appId }: Props) => {
+  const router = useRouter()
+  const goToDapps = () => {
+    router.push("/apps")
+  }
+
   return (
     <VStack w="full" spacing={8} align="flex-start" data-testid={`app-${appId}-detail`}>
-      <AppDetailCard appId={appId} />
+      <VStack spacing={4} alignItems={"flex-start"}>
+        <Button colorScheme="gray" size="md" variant="outline" leftIcon={<FaArrowLeft />} onClick={goToDapps}>
+          Apps
+        </Button>
+        <AppDetailCard appId={appId} />
+      </VStack>
     </VStack>
   )
 }
