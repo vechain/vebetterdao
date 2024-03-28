ec889cdea8d80719a1ab305b591ab33ff491e978
ve-akanoce <eriknucib@live.it>
2024-03-28 20:56:56 +0200

    fix: local.ts

--- apps/frontend/src/utils/BlobUtils.ts
+++ apps/frontend/src/utils/BlobUtils.ts
@@ -15,8 +15,8 @@ export const blobToBase64 = async (blob: Blob): Promise<string> =>
 export const base64UrlToFile = async (base64Url: string, filename: string, mimeType: string) => {
   if (base64Url.startsWith("data:")) {
     const arr = base64Url.split(",")
-    const mime = arr[0]?.match(/:(.*?);/)[1]
-    const bstr = atob(arr[arr.length - 1])
+    const mime = arr[0]?.match(/:(.*?);/)?.[1]
+    const bstr = atob(arr[arr.length - 1] ?? "")
     let n = bstr.length
     const u8arr = new Uint8Array(n)
     while (n--) {
