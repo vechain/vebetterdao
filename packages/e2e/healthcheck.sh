3548cac9a2ba556e4ebfe837410dcfcc80e08037
clayton neal <clayton.neal@vechain.org>
2024-04-02 17:48:50 +0100

    fix gha

--- packages/e2e/healthcheck.sh
+++ packages/e2e/healthcheck.sh
@@ -3,18 +3,18 @@
 # URL from command line argument
 url=$1
 
-# Timeout set for 2 minutes
-timeout=120
+# Timeout set for 5 minutes
+timeout=300
 start_time=$(date +%s)
 
 while [ $(($(date +%s) - start_time)) -lt $timeout ]; do
   status_code=$(curl -o /dev/null -s -w "%{http_code}\n" "$url")
   
-  if [ "$status_code" -lt 400 ]; then
-    echo "Status code is $status_code, which is less than 400. Exiting."
+  if [ "$status_code" -lt 400 ] && [ "$status_code" -gt 0 ]; then
+    echo "Status code is $status_code. Exiting."
     exit 0
   else
-    echo "Status code is $status_code, which is not less than 400. Retrying..."
+    echo "Status code is $status_code. Retrying..."
   fi
   
   # Wait for a bit before retrying
