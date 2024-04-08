00041b56c7c8ac7e767ca059e432d8cc292309c3
Piero Bassa <pierobassa222@gmail.com>
2024-04-08 11:57:35 +0200

    feat: useB3trStaked hook

---
+++ apps/frontend/src/api/contracts/b3tr/hooks/useB3trStaked.ts
@@ -0,0 +1,70 @@
+import { FormattingUtils } from "@repo/utils"
+import { UseQueryResult, useQuery } from "@tanstack/react-query"
+import { useConnex } from "@vechain/dapp-kit-react"
+import { getConfig } from "@repo/config"
+import { useB3trTokenDetails } from "./useB3trTokenDetails"
+import { VOT3__factory } from "@repo/contracts/typechain-types"
+import { TokenBalance } from "./useB3trBalance"
+
+const VOT3_CONTRACT = getConfig().vot3ContractAddress
+
+/**
+ * Fetches the staked balance of a B3TR token for a given address.
+ *
+ * @param {Connex.Thor} thor - The Connex instance.
+ * @param {string} [address] - The address to fetch the staked balance for.
+ * @param {number} [scaleDecimals=18] - The number of decimals to scale the balance by.
+ *
+ * @returns {Promise<TokenBalance>} The staked balance of the B3TR token.
+ *
+ * @throws {Error} If the address is not provided or if there is a VM error.
+ */
+export const getB3trStaked = async (
+  thor: Connex.Thor,
+  address?: string,
+  scaleDecimals: number = 18,
+): Promise<TokenBalance> => {
+  if (!address) return Promise.reject(new Error("Address not provided"))
+
+  const functionFragment = VOT3__factory.createInterface().getFunction("stakedBalanceOf").format("json")
+  const res = await thor.account(VOT3_CONTRACT).method(JSON.parse(functionFragment)).call(address)
+
+  if (res.vmError) return Promise.reject(new Error(res.vmError))
+
+  const original = res.decoded[0]
+  const scaled = FormattingUtils.scaleNumberDown(original, scaleDecimals, scaleDecimals)
+  const formatted = scaled === "0" ? "0" : FormattingUtils.humanNumber(scaled)
+
+  return {
+    original,
+    scaled,
+    formatted,
+  }
+}
+
+/**
+ * Returns a unique query key for the staked balance of a B3TR token.
+ *
+ * @param {string} [address] - The address to get the query key for.
+ *
+ * @returns {string[]} The query key.
+ */
+export const getStakedB3TRQueryKey = (address?: string) => ["staked", "b3tr", address]
+
+/**
+ * Returns the staked balance of a B3TR token for a given address.
+ *
+ * @param {string} [address] - The address to get the balance for.
+ *
+ * @returns {UseQueryResult<TokenBalance, unknown>} The staked balance of the B3TR token.
+ */
+export const useB3trStaked = (address?: string) => {
+  const { thor } = useConnex()
+  const { data: tokenDetails } = useB3trTokenDetails()
+
+  return useQuery({
+    queryKey: getStakedB3TRQueryKey(address),
+    queryFn: () => getB3trStaked(thor, address, tokenDetails?.decimals),
+    enabled: !!address && !!tokenDetails?.decimals,
+  })
+}
