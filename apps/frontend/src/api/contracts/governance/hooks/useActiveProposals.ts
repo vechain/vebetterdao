994bf258e99f91c34d52f9aae251b0dfc3aca3ec
ve-akanoce <eriknucib@live.it>
2024-04-10 12:34:55 +0200

    refactor*hook/proposals): align active/pat/incoming proposals with new contracts and react-query

--- apps/frontend/src/api/contracts/governance/hooks/useActiveProposals.ts
+++ apps/frontend/src/api/contracts/governance/hooks/useActiveProposals.ts
@@ -1,33 +1,40 @@
 import { useCurrentBlock } from "@/api/blockchain"
 import { useConnex } from "@vechain/dapp-kit-react"
-import { useMemo } from "react"
 import { useProposalsEvents } from "./useProposalsEvents"
+import { useAllocationsRoundsEvents } from "../../xAllocations"
+import { useQuery } from "@tanstack/react-query"
 
+export const getActiveProposalsQueryKey = () => ["proposals", "active"]
 /**
  *  Hook to get the active proposals events from the governor contract (i.e the proposals created, not canceled, expired and not queued/executed)
  * @returns  the active proposals events (i.e the proposals created, not canceled, expired and not queued/executed)
  */
 export const useActiveProposals = () => {
   const { thor } = useConnex()
-  const proposalsEventsQuery = useProposalsEvents()
   const { data: currentBlock } = useCurrentBlock()
+  const proposalsEventsQuery = useProposalsEvents()
+  const allocationsEventsQuery = useAllocationsRoundsEvents()
 
-  const activeProposals = useMemo(() => {
-    const proposalsEvents = proposalsEventsQuery.data
-    if (!thor || !proposalsEvents) return
-    const lastBlock = currentBlock?.number ?? thor.status.head.number
-    return proposalsEvents.created.filter(
-      proposal =>
-        Number(proposal.voteStart) < lastBlock &&
-        Number(proposal.voteEnd) > lastBlock &&
-        !proposalsEvents.canceled.some(canceledProposal => canceledProposal.proposalId === proposal.proposalId) &&
-        !proposalsEvents.executed.some(executedProposal => executedProposal.proposalId === proposal.proposalId) &&
-        !proposalsEvents.queued.some(queuedProposal => queuedProposal.proposalId === proposal.proposalId),
-    )
-  }, [proposalsEventsQuery, currentBlock, thor])
-
-  return {
-    ...proposalsEventsQuery,
-    data: activeProposals,
-  }
+  return useQuery({
+    queryKey: getActiveProposalsQueryKey(),
+    queryFn: async () => {
+      const proposalsEvents = proposalsEventsQuery.data
+      const createdAllocationsEvents = allocationsEventsQuery.data?.created
+      if (!thor || !proposalsEvents) return
+      const lastBlock = currentBlock?.number ?? thor.status.head.number
+      return proposalsEvents.created.filter(proposal => {
+        const relatedRound = createdAllocationsEvents?.find(
+          round => Number(round.roundId) === Number(proposal.roundIdVoteStart),
+        )
+        return (
+          Number(relatedRound?.voteStart) < lastBlock &&
+          Number(relatedRound?.voteEnd) > lastBlock &&
+          !proposalsEvents.canceled.some(canceledProposal => canceledProposal.proposalId === proposal.proposalId) &&
+          !proposalsEvents.executed.some(executedProposal => executedProposal.proposalId === proposal.proposalId) &&
+          !proposalsEvents.queued.some(queuedProposal => queuedProposal.proposalId === proposal.proposalId)
+        )
+      })
+    },
+    enabled: !!thor && !!thor.status.head.number && !!proposalsEventsQuery.data && !!allocationsEventsQuery.data,
+  })
 }
--- apps/frontend/src/api/contracts/governance/hooks/useIncomingProposals.ts
+++ apps/frontend/src/api/contracts/governance/hooks/useIncomingProposals.ts
@@ -1,8 +1,10 @@
 import { useCurrentBlock } from "@/api/blockchain"
 import { useConnex } from "@vechain/dapp-kit-react"
-import { useMemo } from "react"
 import { useProposalsEvents } from "./useProposalsEvents"
+import { useQuery } from "@tanstack/react-query"
+import { useAllocationsRoundsEvents } from "../../xAllocations"
 
+export const getIncomingProposalsQueryKey = () => ["proposals", "incoming"]
 /**
  *  Hook to get the incoming proposals using on-chain events (i.e the proposals not started yet and not canceled)
  * @returns  the incoming proposals events (i.e the proposals not started yet and not canceled)
@@ -11,19 +13,23 @@ export const useIncomingProposals = () => {
   const { thor } = useConnex()
   const { data: currentBlock } = useCurrentBlock()
   const { data: proposalsEvents } = useProposalsEvents()
+  const { data: allocationsEvents } = useAllocationsRoundsEvents()
 
-  const incomingProposals = useMemo(() => {
-    if (!thor || !proposalsEvents) return
-    const lastBlock = currentBlock?.number ?? thor.status.head.number
-    return proposalsEvents.created.filter(
-      proposal =>
-        Number(proposal.voteStart) >= lastBlock &&
-        !proposalsEvents.canceled.some(canceledProposal => canceledProposal.proposalId === proposal.proposalId),
-    )
-  }, [proposalsEvents, thor, currentBlock])
-
-  return {
-    ...proposalsEvents,
-    data: incomingProposals,
-  }
+  return useQuery({
+    queryKey: getIncomingProposalsQueryKey(),
+    queryFn: async () => {
+      if (!thor || !proposalsEvents) return
+      const lastBlock = currentBlock?.number ?? thor.status.head.number
+      return proposalsEvents.created.filter(proposal => {
+        const relatedRound = allocationsEvents?.created.find(
+          round => Number(round.roundId) === Number(proposal.roundIdVoteStart),
+        )
+        return (
+          Number(relatedRound?.voteStart) >= lastBlock &&
+          !proposalsEvents.canceled.some(canceledProposal => canceledProposal.proposalId === proposal.proposalId)
+        )
+      })
+    },
+    enabled: !!thor && !!proposalsEvents,
+  })
 }
