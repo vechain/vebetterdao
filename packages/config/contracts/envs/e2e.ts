9567a15a660d96404176b811533eb581802f8c62
clayton neal <clayton.neal@vechain.org>
2024-04-17 20:49:14 +0100

    longer voting period for CI

--- packages/config/contracts/envs/e2e.ts
+++ packages/config/contracts/envs/e2e.ts
@@ -3,7 +3,7 @@ export function createE2EConfig() {
 
   console.log("Creating E2E config...")
   const localConfig = createLocalConfig()
-  localConfig.EMISSIONS_CYCLE_DURATION = 24  // 24 blocks - 4 minutes.
+  localConfig.EMISSIONS_CYCLE_DURATION = process.env.CI ? 36 : 24  // 36 blocks in CI (6 mins) minutes, 24 blocks in local (4 mins)
   return localConfig
 
 
