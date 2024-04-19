cab448282ca41eee81d69b6820ff1667af2d9569
Erik Nucibella <eriknucib@live.it>
2024-04-19 10:22:21 +0200

    fix: wrong status modal when transaction reverts (#643)
    * fix(useSendTransaction): merge useEffect to prevent concurrency
    
    * fix(TransactionModals): add error description, improve modal status handling
    
    * fix(useSendTransaction): default message if no revert reason
    
    * fix(useSendTransaction): missing dependencies, memoize functions

--- apps/frontend/src/app/rounds/components/AllocationRoundUserVotes/AllocationRoundUserVotes.tsx
+++ apps/frontend/src/app/rounds/components/AllocationRoundUserVotes/AllocationRoundUserVotes.tsx
@@ -114,6 +114,11 @@ export const AllocationRoundUserVotes = ({ roundId }: Props) => {
     castAllocationVotes.sendTransaction(appVotesPercentagesToValue)
   }
 
+  const onTryAgain = useCallback(() => {
+    castAllocationVotes.resetStatus()
+    handleSubmit(onSubmit)()
+  }, [castAllocationVotes.resetStatus, handleSubmit, onSubmit])
+
   const splitEvenly = () => {
     const totalVotes = xApps?.length ?? 0
     const rawValue = scaledDivision(100, totalVotes)
@@ -249,12 +254,14 @@ export const AllocationRoundUserVotes = ({ roundId }: Props) => {
       <TransactionModal
         isOpen={isOpen}
         onClose={handleClose}
-        status={castAllocationVotes.status}
+        status={castAllocationVotes.error ? "error" : castAllocationVotes.status}
         confirmationTitle={"Confirm Vote"}
         successTitle={"Vote Cast!"}
+        errorTitle={"Error casting vote"}
+        errorDescription={castAllocationVotes.error?.reason}
         showSocialButtons
         socialDescriptionEncoded="%E2%9C%85%20Just%20cast%20my%20vote%20in%20the%20%23VeBetterDAO%20X%20allocation%20round%21%20%0A%0A%F0%9F%8C%B1%20Excited%20to%20be%20part%20of%20the%20decision-making%20process%20for%20sustainable%20projects.%0A%0AJoin%20us%20in%20shaping%20a%20greener%20future%20at%20https%3A%2F%2Fvebetterdao.org.%20%0A%0A%23VeBetterDAO%20%23Vechain"
-        onTryAgain={handleSubmit(onSubmit)}
+        onTryAgain={onTryAgain}
         showTryAgainButton
         showExplorerButton
         txId={castAllocationVotes.txReceipt?.meta.txID ?? castAllocationVotes.sendTransactionTx?.txid}
