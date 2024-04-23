f51ea0a17c2f256ad8660298b2d30304a61e8e17
ve-akanoce <eriknucib@live.it>
2024-04-23 18:26:59 +0200

    feat(useProposalFormStore): store to handle multi page form to create a proposal

---
+++ apps/frontend/src/store/useProposalFormStore.ts
@@ -0,0 +1,49 @@
+import { ProposalAction } from "@/hooks"
+import create from "zustand"
+import { devtools, persist } from "zustand/middleware"
+
+const stepVariant = {
+  1: "stepOne",
+  2: "stepTwo",
+}
+
+type StepOneData = {
+  title: string
+  description: string
+  action: ProposalAction[]
+}
+
+type StepTwoData = {
+  votingSessionRoundId: string | number
+}
+
+type setDataType = { step: 1; data: StepOneData } | { step: 2; data: StepTwoData }
+
+type State = {
+  stepOne: StepOneData | null
+  stepTwo: StepTwoData | null
+  setData: ({ step, data }: setDataType) => void
+}
+
+/**
+ * Store for the multi-step proposal form data
+ */
+export const useProposalFormStore = create<State>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        stepOne: null,
+        stepTwo: null,
+        stepThree: null,
+        setData: ({ step, data }) =>
+          set(state => ({
+            ...state,
+            [stepVariant[step]]: data,
+          })),
+      }),
+      {
+        name: "PROPOSAL_FORM_STORE",
+      },
+    ),
+  ),
+)
