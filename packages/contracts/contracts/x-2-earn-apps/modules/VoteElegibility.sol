981340016a2119e98457d6230c47b2847376f64c
Dan Rusnac <rusnac14@gmail.com>
2024-04-24 18:09:02 +0200

    refactor: updating elegibility code + added comments

--- packages/contracts/contracts/x-2-earn-apps/modules/VoteElegibility.sol
+++ packages/contracts/contracts/x-2-earn-apps/modules/VoteElegibility.sol
@@ -7,16 +7,20 @@ import { X2EarnAppsUpgradeable } from "../X2EarnAppsUpgradeable.sol";
 import { Checkpoints } from "@openzeppelin/contracts/utils/structs/Checkpoints.sol";
 import { SafeCast } from "@openzeppelin/contracts/utils/math/SafeCast.sol";
 
+/**
+ * @title VoteElegibility
+ * @notice Contract module that provides the vote elegibility functionalities of the x2earn apps.
+ * By deafult every new added app becomes elegible for voting. The elegibility can be changed.
+ * The elegibility is stored in a checkpoint so we can track the changes over time.
+ */
 abstract contract VoteElegibility is Initializable, X2EarnAppsUpgradeable {
-  using Checkpoints for Checkpoints.Trace208;
+  using Checkpoints for Checkpoints.Trace208; // Checkpoints used to track elegibility changes over time
+
   /// @custom:storage-location erc7201:b3tr.storage.X2EarnApps.VoteElegibility
   struct VoteElegibilityStorage {
-    // Array containing an up to date list of apps that are elegible for voting
-    bytes32[] _elegibleAppsForNextRound;
-    // Mapping from app ID to index in the _elegibleAppsForNextRound array
-    mapping(bytes32 => uint256) _idToElegibleAppsIndex;
-    // Mapping from app ID to a checkpoint of the app's elegibility in a specific block
-    mapping(bytes32 appId => Checkpoints.Trace208) _isAppElegibleCheckpoints;
+    bytes32[] _elegibleApps; // Array containing an up to date list of apps that are elegible for voting
+    mapping(bytes32 appId => uint256 index) _elegibleAppIndex; // Mapping from app ID to index in the _elegibleApps array, so we can remove an app in O(1)
+    mapping(bytes32 appId => Checkpoints.Trace208) _isAppElegibleCheckpoints; // Checkpoints to track the elegibility changes of an app over time
   }
 
   // keccak256(abi.encode(uint256(keccak256("b3tr.storage.X2EarnApps.VoteElegibility")) - 1)) & ~bytes32(uint256(0xff))
@@ -42,52 +46,77 @@ abstract contract VoteElegibility is Initializable, X2EarnAppsUpgradeable {
    * @dev Update the app availability for voting checkpoint.
    */
   function _setVotingElegibility(bytes32 appId, bool canBeVoted) internal virtual override {
+    if (!appExists(appId)) {
+      revert X2EarnNonexistentApp(appId);
+    }
+
     VoteElegibilityStorage storage $ = _getVoteElegibilityStorage();
 
-    _push($._isAppElegibleCheckpoints[appId], canBeVoted ? 1 : 0);
+    // We update the checkpoint with the new elegibility status
+    _pushCheckpoint($._isAppElegibleCheckpoints[appId], canBeVoted ? 1 : 0);
 
     if (!canBeVoted) {
-      // In order to remove an app from the _elegibleAppsForNextRound array correctly we need to move the element in the last position
-      // to the index we want to remove and pop() the last element of the array.
-      // We also need to update the `_idToElegibleAppsIndex` mapping accordingly.
+      // If the app is not elegible for voting we need to remove it from the _elegibleApps array
+      /**
+       * In order to remove an app from the _elegibleApps array correctly we need to:
+       * 1) move the element in the last position of the array to the index we want to remove
+       * 2) Update the `_elegibleAppIndex` mapping accordingly.
+       * 3) pop() the last element of the _elegibleApps array and delete the index mapping of the app we removed
+       *
+       * Example:
+       *
+       * _elegibleApps = [A, B, C, D, E]
+       * _elegibleAppIndex = {A: 0, B: 1, C: 2, D: 3, E: 4}
+       *
+       * If we want to remove C:
+       *
+       * 1) Move E to the index of C
+       * _elegibleApps = [A, B, E, D, E]
+       *
+       * 2) Update the index of E in the mapping
+       * _elegibleAppIndex = {A: 0, B: 1, C: 2, D: 3, E: 2}
+       *
+       * 3) pop() the last element of the array and delete the index mapping of the app we removed
+       * _elegibleApps = [A, B, E, D]
+       * _elegibleAppIndex = {A: 0, B: 1, D: 3, E: 2}
+       *
+       */
+      uint256 index = $._elegibleAppIndex[appId];
+      uint256 lastIndex = $._elegibleApps.length - 1;
+      bytes32 lastAppId = $._elegibleApps[lastIndex];
 
-      // ID of the last item now points to the new index
-      $._idToElegibleAppsIndex[$._elegibleAppsForNextRound[$._elegibleAppsForNextRound.length - 1]] = $
-        ._idToElegibleAppsIndex[appId];
+      $._elegibleApps[index] = lastAppId;
+      $._elegibleAppIndex[lastAppId] = index;
 
-      // Move last item at the index of the app we are removing and pop the last element of the array
-      $._elegibleAppsForNextRound[$._idToElegibleAppsIndex[appId]] = $._elegibleAppsForNextRound[
-        $._elegibleAppsForNextRound.length - 1
-      ];
-      $._elegibleAppsForNextRound.pop();
-
-      // delete the mapping that belongs to the app we removed
-      delete $._idToElegibleAppsIndex[appId];
+      $._elegibleApps.pop();
+      delete $._elegibleAppIndex[appId];
     } else {
-      // push app to elegible apps
-      $._elegibleAppsForNextRound.push(appId);
-      $._idToElegibleAppsIndex[appId] = $._elegibleAppsForNextRound.length - 1;
-      _push($._isAppElegibleCheckpoints[appId], 1);
+      // If the app is elegible for voting we need to add it to the _elegibleApps array
+      $._elegibleApps.push(appId);
+      $._elegibleAppIndex[appId] = $._elegibleApps.length - 1;
     }
 
     emit VotingElegibilityUpdated(appId, canBeVoted);
   }
 
-  function _push(Checkpoints.Trace208 storage store, uint208 delta) private returns (uint208, uint208) {
+  /**
+   * @dev Store a new checkpoint for the app's elegibility.
+   */
+  function _pushCheckpoint(Checkpoints.Trace208 storage store, uint208 delta) private returns (uint208, uint208) {
     return store.push(clock(), delta);
   }
 
   /**
-   * All apps that are elegible for voting in x-allocation rounds
+   * @dev All apps that are currently elegible for voting in x-allocation rounds
    */
   function allElegibleApps() public view returns (bytes32[] memory) {
     VoteElegibilityStorage storage $ = _getVoteElegibilityStorage();
 
-    return $._elegibleAppsForNextRound;
+    return $._elegibleApps;
   }
 
   /**
-   * @dev Returns true if an app is enabled to be voted and if it was created before the start of the requested round.
+   * @dev Returns true if an app is elegible for voting in a specific timepoint.
    *
    * @param appId the hashed name of the app
    * @param timepoint the timepoint when the app should be checked for elegibility
@@ -107,6 +136,11 @@ abstract contract VoteElegibility is Initializable, X2EarnAppsUpgradeable {
     return $._isAppElegibleCheckpoints[appId].upperLookupRecent(SafeCast.toUint48(timepoint)) == 1;
   }
 
+  /**
+   * @dev Returns true if an app is elegible for voting in the current block.
+   *
+   * @param appId the hashed name of the app
+   */
   function isElegibleNow(bytes32 appId) public view override returns (bool) {
     if (!appExists(appId)) {
       revert X2EarnNonexistentApp(appId);
