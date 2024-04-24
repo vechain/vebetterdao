84918392cfb86c7c06a88aed090761c024a0059b
Dan Rusnac <rusnac14@gmail.com>
2024-04-24 19:08:39 +0200

    fix: allign frontend with new contract

--- apps/frontend/src/api/contracts/xApps/hooks/useRoundXApps.ts
+++ apps/frontend/src/api/contracts/xApps/hooks/useRoundXApps.ts
@@ -22,11 +22,10 @@ export const getRoundXApps = async (thor: Connex.Thor, roundId: string): Promise
   return apps.map((app: any) => ({
     id: app[0],
     receiverAddress: app[1],
-    adminAddress: app[2],
-    name: app[3],
-    metadataURI: app[4],
-    createdAt: app[5],
-    createdAtTimestamp: app[6],
+    name: app[2],
+    metadataURI: app[3],
+    createdAt: app[4],
+    createdAtTimestamp: app[5],
   }))
 }
 
