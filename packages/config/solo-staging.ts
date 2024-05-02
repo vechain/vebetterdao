7f54302673440be1650fa836d98e2bb3138ebd06
Dan Rusnac <rusnac14@gmail.com>
2024-05-02 07:56:39 +0200

    fix: merge conflicts

--- packages/config/solo-staging.ts
+++ packages/config/solo-staging.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { AppConfig } from "."
 const config: AppConfig = {
   basePath: "https://b3tr-frontend.vercel.app",
@@ -44,51 +43,3 @@ const config: AppConfig = {
   },
 }
 export default config
-=======
-import { AppConfig } from "." 
- const config: AppConfig = {
-  "basePath": "https://b3tr-frontend.vercel.app",
-  "b3trContractAddress": "0x73c2bC3c8090c5674E7B533B52C223B1bb43d7DD",
-  "vot3ContractAddress": "0x5206e2D90E9679C18aB5Df82026B5304912a363e",
-  "b3trGovernorAddress": "0x16891E204DBf11a8990929bc9A6DC68ec7d2744e",
-  "timelockContractAddress": "0x97bE2a67B2227A6F58866e68a9d2e20A6696173C",
-  "xAllocationPoolContractAddress": "0x3aC068235b436CC1452FaA7015ad9B83Fa5A9345",
-  "xAllocationVotingContractAddress": "0x282ed4bfdd9b1cb1389a5971cC71433E2aA15a0D",
-  "emissionsContractAddress": "0x1C758BFe6d15d614105e50198b28e7DF6033454d",
-  "voterRewardsContractAddress": "0x972863338BE22a87B2AE67eeF91760d04a33210D",
-  "galaxyMemberContractAddress": "0x69042D6CDDCDe43433C2C7994F3927AcbE0960c1",
-  "treasuryContractAddress": "0x88c38d896eA457E0617d33c67362383B76253cF6",
-  "mixPanelProjectToken": "e03f4f5f6a753dae2dac30a69dfe21f7",
-  "nodeUrl": "https://thor-solo.dev.b3tr.vechain.org",
-  "network": {
-    "id": "solo-staging",
-    "name": "solo-staging",
-    "type": "solo",
-    "defaultNet": true,
-    "urls": [
-      "https://thor-solo.dev.b3tr.vechain.org"
-    ],
-    "explorerUrl": "https://insight.dev.b3tr.vechain.org/#/solo",
-    "blockTime": 10000,
-    "genesis": {
-      "number": 0,
-      "id": "0x00000000c05a20fbca2bf6ae3affba6af4a74b800b585bf7a4988aba7aea69f6",
-      "size": 170,
-      "parentID": "0xffffffff53616c757465202620526573706563742c20457468657265756d2100",
-      "timestamp": 1530316800,
-      "gasLimit": 10000000,
-      "beneficiary": "0x0000000000000000000000000000000000000000",
-      "gasUsed": 0,
-      "totalScore": 0,
-      "txsRoot": "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
-      "txsFeatures": 0,
-      "stateRoot": "0x93de0ffb1f33bc0af053abc2a87c4af44594f5dcb1cb879dd823686a15d68550",
-      "receiptsRoot": "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
-      "signer": "0x0000000000000000000000000000000000000000",
-      "isTrunk": true,
-      "transactions": []
-    }
-  }
-};
-  export default config;
->>>>>>> origin/main
