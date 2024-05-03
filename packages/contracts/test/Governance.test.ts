05e3415692bbeb45192f1222942702ffb8ff251f
Piero Bassa <pierobassa222@gmail.com>
2024-05-03 16:57:48 +0200

    chore: removed test suite limitation

--- packages/contracts/test/Governance.test.ts
+++ packages/contracts/test/Governance.test.ts
@@ -22,7 +22,7 @@ import { createLocalConfig } from "@repo/config/contracts/envs/local"
 import { getImplementationAddress } from "@openzeppelin/upgrades-core"
 import { B3TRGovernor } from "../typechain-types"
 
-describe.only("Governor and TimeLock", function () {
+describe("Governor and TimeLock", function () {
   describe("Governor deployment", function () {
     it("Should set constructors correctly", async function () {
       const config = createLocalConfig()
