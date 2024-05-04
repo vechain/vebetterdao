e39f50ea774635d85d90058df405644542231ae4
Piero Bassa <64158778+pierobassa@users.noreply.github.com>
2024-05-04 08:53:49 +0200

    feat(B3TRGovernor): whitelist targets and functions that can be used when creating a proposal (#707)
    * feat: local config with governor whitelisted functions
    
    * feat: staging & testnet config with governor whitelisted functions
    
    * feat: governor whitelisted functions type in contracts config
    
    * feat: whitelisted functions in b3tr governor
    
    * feat: whitelisted functions upon governor deployment
    
    * feat: create proposal with multiple functions helper
    
    * feat: governor whitelisted methods in test config
    
    * test: governor whitelisted methods
    
    * test: missing tests for coverage
    
    * refactor: config files for whitelisted methods to be only in staging config
    
    * feat: target to whitelisted function selectors
    
    * refactor: setWhitelisted functions
    
    * test: set whitelisted functions in test deployment
    
    * test: target function whitelisting should not interfere with other targets
    
    * chore: only governor tests removed
    
    * fix: failing tests
    
    * feat: added whitelisting in deployment script
    
    * chore: removed log for double check
    
    * fix: whitelist functions after merge
    
    * feat: createProposalWithMultipleFunctions helper
    
    * fix: governance tests after merge w/ main
    
    * refactor: remove updateToAndCall function from whitelist
    
    * feat: governor functions settings module
    
    * refactor: after usage of new module
    
    * chore: removed test suite limitation
    
    ---------
    
    Co-authored-by: Dan Rusnac <rusnac14@gmail.com>

--- packages/config/contracts/envs/testnet.ts
+++ packages/config/contracts/envs/testnet.ts
@@ -9,6 +9,11 @@ export function createTestnetConfig() {
     B3TR_GOVERNOR_MIN_DELAY: 30, //after a vote passes, you have 5 min before you can queue the proposal
     B3TR_GOVERNOR_PROPOSAL_THRESHOLD: 1000, // How many votes are needed to create a proposal
     B3TR_GOVERNOR_MIN_VOTING_DELAY: 25920, // 3 days
+    /*
+      For ambiguous functions (functions with same name), the function signature is used to differentiate them
+      e.g., instead of using "setVoterRewards", we use "setVoterRewards(address)"
+    */
+    B3TR_GOVERNOR_WHITELISTED_METHODS: {},
 
     EMISSIONS_CYCLE_DURATION: 60480, // blocks - 60480 blocks - 1 week.
     EMISSIONS_X_ALLOCATION_DECAY_PERCENTAGE: 4, // 4% decay every cycle
