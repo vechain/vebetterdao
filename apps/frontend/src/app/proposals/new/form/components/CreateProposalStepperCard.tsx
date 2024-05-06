95eacd1be8a44cd30b26c469015212d7b1de497b
ve-akanoce <eriknucib@live.it>
2024-05-06 12:10:56 +0200

    feat(CreateProposalStepperCard): align with new steps, current step working

--- apps/frontend/src/app/proposals/new/form/components/CreateProposalStepperCard.tsx
+++ apps/frontend/src/app/proposals/new/form/components/CreateProposalStepperCard.tsx
@@ -15,25 +15,38 @@ import {
   Stepper,
   useSteps,
 } from "@chakra-ui/react"
+import { usePathname } from "next/navigation"
+import { useEffect } from "react"
 
 type CreateProposalStep = {
   key: string
   title: string
   description?: string
+  pathnames?: string[]
 }
 
 const steps: CreateProposalStep[] = [
   { key: "creationMethod", title: "Creation method" },
-  { key: "basicInfoAndFunctions", title: "Basic information and functions" },
-  { key: "additionalInfo", title: "Additional information" },
-  { key: "votingSessionDate", title: "Voting session date" },
-  { key: "previewAndPublish", title: "Preview and publish" },
+  { key: "proposalTopic", title: "Proposal topic", pathnames: ["/proposals/new/form/functions"] },
+  { key: "basicsAndFunctions", title: "Proposal basics and functions", pathnames: ["/proposals/new/form/details"] },
+  { key: "details", title: "Proposal details", pathnames: ["/proposals/new/form/content"] },
+  { key: "preview", title: "Preview", pathnames: ["/proposals/new/form/preview"] },
+  { key: "round", title: "Round", pathnames: ["/proposals/new/form/round"] },
+  { key: "fundingAndPublish", title: "Funding and publish!", pathnames: ["/proposals/new/form/deposit"] },
 ]
 export const CreateProposalStepperCard = () => {
+  const pathname = usePathname()
   const { activeStep, setActiveStep } = useSteps({
     index: 1,
     count: steps.length,
   })
+
+  useEffect(() => {
+    const step = steps.find(step => step.pathnames?.includes(pathname))
+    if (step) {
+      setActiveStep(steps.indexOf(step))
+    }
+  }, [pathname, setActiveStep])
 
   return (
     <Card>
