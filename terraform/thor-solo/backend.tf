c77da2df3235aae979d60635fe3d98e2ffb49fa5
Dougal Rea <dougal.rea@gmail.com>
2024-05-09 14:48:53 +0100

    B3TR Dev Thor Solo Terraform

--- terraform/thor-solo/backend.tf
+++ terraform/thor-solo/backend.tf
@@ -5,8 +5,12 @@ terraform {
     }
   }
   backend "s3" {
-    key    = "b3tr-thor-solo/terraform.tfstate"
+    # The states of DEV and PROD environments are stored in separate S3 buckets in their
+    # respective AWS accounts. The {{{ENV}}} placeholder is replaced manually (dev/prod)
+    bucket = "b3tr-terraform-state-{{{ENV}}}"
+    key    = "b3tr-thor-solo.tfstate"
     region = "eu-west-1"
+    workspace_key_prefix = "workspaces"
   }
 }
 
--- terraform/thor-solo/ecr.tf
+++ terraform/thor-solo/ecr.tf
@@ -1,6 +1,6 @@
 ## ecr creation
 module "ecr" {
-  source = "git@github.com:vechain/terraform_infrastructure_modules.git//ecr?ref=677fbdb"
+  source = "git@github.com:vechain/terraform_infrastructure_modules.git//ecr?ref=Fix-max-image-count-ECR-lifecycle-policy"
   #ecr_name             = var.ecr_names
   project  = local.config.project
   env      = local.env
