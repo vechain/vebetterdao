74dc452e38376b650343204ad18cf5e0ed897dc2
Dougal Rea <dougal.rea@gmail.com>
2024-05-09 15:40:08 +0100

    gitignore and module sources

--- terraform/thor-solo/ecr.tf
+++ terraform/thor-solo/ecr.tf
@@ -1,6 +1,6 @@
 ## ecr creation
 module "ecr" {
-  source = "git@github.com:vechain/terraform_infrastructure_modules.git//ecr?ref=Fix-max-image-count-ECR-lifecycle-policy"
+  source = "git@github.com:vechain/terraform_infrastructure_modules.git//ecr?ref=v.1.0.21"
   #ecr_name             = var.ecr_names
   project  = local.config.project
   env      = local.env
