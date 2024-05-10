df830760cf1ae8cac114aed3d51e809dd579df0f
Erik Nucibella <eriknucib@live.it>
2024-05-10 10:32:28 +0200

    fix(testnet/seed): add recent apps (#770)
    * fix: solve merge conflicts
    
    * fix: gitleaksignore

--- packages/config/solo-staging.ts
+++ packages/config/solo-staging.ts
@@ -1,45 +1,43 @@
-import { AppConfig } from "." 
- const config: AppConfig = {
-  "basePath": "https://b3tr-frontend.vercel.app",
-  "b3trContractAddress": "0x1D97373F17D1Bfc33398fec4E5dFD4f743683b4C",
-  "vot3ContractAddress": "0x5B67a27DC047833c342d81b75e1bA1988b9b70aC",
-  "b3trGovernorAddress": "0x4E635Dab176847f147A1B2EA45C0299E2947E695",
-  "timelockContractAddress": "0x0303fcF842d4E768D50813e72530A78527B6610b",
-  "xAllocationPoolContractAddress": "0xC67e1f7f7c95332e3aF24B732C7d6926301a5d50",
-  "xAllocationVotingContractAddress": "0x7f39E9E8573a9DC6092bD2E4bB844E769549D21E",
-  "emissionsContractAddress": "0x673329362e3679E137b7162C5fd09128c966e18e",
-  "voterRewardsContractAddress": "0x5cc1B2406D129953a7FfaEE6A5AD3d0e952ceDAa",
-  "nftBadgeContractAddress": "0x2f1573B153E1d8B7ac87DC384ab015e2c9d6fd79",
-  "mixPanelProjectToken": "e03f4f5f6a753dae2dac30a69dfe21f7",
-  "nodeUrl": "https://thor-solo.dev.b3tr.vechain.org/",
-  "network": {
-    "id": "solo-staging",
-    "name": "solo-staging",
-    "type": "solo",
-    "defaultNet": true,
-    "urls": [
-      "https://thor-solo.dev.b3tr.vechain.org/"
-    ],
-    "explorerUrl": "https://insight.dev.b3tr.vechain.org/#/solo/",
-    "blockTime": 10000,
-    "genesis": {
-      "number": 0,
-      "id": "0x00000000c05a20fbca2bf6ae3affba6af4a74b800b585bf7a4988aba7aea69f6",
-      "size": 170,
-      "parentID": "0xffffffff53616c757465202620526573706563742c20457468657265756d2100",
-      "timestamp": 1530316800,
-      "gasLimit": 10000000,
-      "beneficiary": "0x0000000000000000000000000000000000000000",
-      "gasUsed": 0,
-      "totalScore": 0,
-      "txsRoot": "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
-      "txsFeatures": 0,
-      "stateRoot": "0x93de0ffb1f33bc0af053abc2a87c4af44594f5dcb1cb879dd823686a15d68550",
-      "receiptsRoot": "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
-      "signer": "0x0000000000000000000000000000000000000000",
-      "isTrunk": true,
-      "transactions": []
-    }
-  }
-};
-  export default config;
+import { AppConfig } from "."
+const config: AppConfig = {
+  basePath: "https://b3tr-frontend.vercel.app",
+  b3trContractAddress: "0x9855C237628d5AdFBaE64b94A64B44E570Bcc7de",
+  vot3ContractAddress: "0x56892A37A508CC0AF316167e5B6E80b761CeD819",
+  b3trGovernorAddress: "0x5c9eEa9a3B46b12f1a34468fA531c37855be0404",
+  timelockContractAddress: "0x2Ae7114A2B83621aacAE15BBF19Ef971e0529f0d",
+  xAllocationPoolContractAddress: "0x00685715d91e250F5A0EEf6A91560624D59c50b8",
+  xAllocationVotingContractAddress: "0x76A3f65224b6c4EC50AbAaD4af01289C9Fc0115a",
+  emissionsContractAddress: "0x72F8F450B5e96C21810205a389A89EA24315D5A4",
+  voterRewardsContractAddress: "0xC99BBCCFF58b2c9b53F21fB1F09509e8b9208935",
+  nftBadgeContractAddress: "0x58b1710880fDD23f7FB9C407b72101e4C8488C5C",
+  mixPanelProjectToken: "e03f4f5f6a753dae2dac30a69dfe21f7",
+  nodeUrl: "https://thor-solo.dev.b3tr.vechain.org/",
+  network: {
+    id: "solo-staging",
+    name: "solo-staging",
+    type: "solo",
+    defaultNet: true,
+    urls: ["https://thor-solo.dev.b3tr.vechain.org/"],
+    explorerUrl: "https://insight.dev.b3tr.vechain.org/#/solo/",
+    blockTime: 10000,
+    genesis: {
+      number: 0,
+      id: "0x00000000c05a20fbca2bf6ae3affba6af4a74b800b585bf7a4988aba7aea69f6",
+      size: 170,
+      parentID: "0xffffffff53616c757465202620526573706563742c20457468657265756d2100",
+      timestamp: 1530316800,
+      gasLimit: 10000000,
+      beneficiary: "0x0000000000000000000000000000000000000000",
+      gasUsed: 0,
+      totalScore: 0,
+      txsRoot: "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
+      txsFeatures: 0,
+      stateRoot: "0x93de0ffb1f33bc0af053abc2a87c4af44594f5dcb1cb879dd823686a15d68550",
+      receiptsRoot: "0x45b0cfc220ceec5b7c1c62c4d4193d38e4eba48e8815729ce75f9c0ab0e4c1c0",
+      signer: "0x0000000000000000000000000000000000000000",
+      isTrunk: true,
+      transactions: [],
+    },
+  },
+}
+export default config
