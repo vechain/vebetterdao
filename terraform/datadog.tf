faa760b4eb72a2a7ef3cf0936e7d9bb0dc01585d
ShaunKav <shaun.kavanagh@vechain.org>
2024-05-15 13:45:34 +0100

    datadog module initialization

---
+++ terraform/datadog.tf
@@ -0,0 +1,93 @@
+module "datadog_integration_aws" {
+  source = "git::git@github.com:vechain/terraform_infrastructure_modules.git//datadog?ref=main"
+  project_name = local.config.project_name
+  role_name  = "DatadogAWSIntegrationRole"
+  aws_permissions_list = [
+                "apigateway:GET",
+                "autoscaling:Describe*",
+                "backup:List*",
+                "budgets:ViewBudget",
+                "cloudfront:GetDistributionConfig",
+                "cloudfront:ListDistributions",
+                "cloudtrail:DescribeTrails",
+                "cloudtrail:GetTrailStatus",
+                "cloudtrail:LookupEvents",
+                "cloudwatch:Describe*",
+                "cloudwatch:Get*",
+                "cloudwatch:List*",
+                "codedeploy:List*",
+                "codedeploy:BatchGet*",
+                "dynamodb:List*",
+                "dynamodb:Describe*",
+                "ec2:Describe*",
+                "ec2:GetTransitGatewayPrefixListReferences",
+                "ec2:SearchTransitGatewayRoutes",
+                "ecs:Describe*",
+                "ecs:List*",
+                "elasticache:Describe*",
+                "elasticache:List*",
+                "elasticfilesystem:DescribeFileSystems",
+                "elasticfilesystem:DescribeTags",
+                "elasticfilesystem:DescribeAccessPoints",
+                "elasticloadbalancing:Describe*",
+                "elasticmapreduce:List*",
+                "elasticmapreduce:Describe*",
+                "es:ListTags",
+                "es:ListDomainNames",
+                "es:DescribeElasticsearchDomains",
+                "events:CreateEventBus",
+                "health:DescribeEvents",
+                "health:DescribeEventDetails",
+                "health:DescribeAffectedEntities",
+                "lambda:GetPolicy",
+                "lambda:List*",
+                "logs:DeleteSubscriptionFilter",
+                "logs:DescribeLogGroups",
+                "logs:DescribeLogStreams",
+                "logs:DescribeSubscriptionFilters",
+                "logs:FilterLogEvents",
+                "logs:PutSubscriptionFilter",
+                "logs:TestMetricFilter",
+                "organizations:Describe*",
+                "organizations:List*",
+                "rds:Describe*",
+                "rds:List*",
+                "route53:List*",
+                "s3:GetBucketLogging",
+                "s3:GetBucketLocation",
+                "s3:GetBucketNotification",
+                "s3:GetBucketTagging",
+                "s3:ListAllMyBuckets",
+                "s3:PutBucketNotification",
+                "ses:Get*",
+                "sns:List*",
+                "sns:Publish",
+                "sqs:ListQueues",
+                "states:ListStateMachines",
+                "states:DescribeStateMachine",
+                "tag:GetResources",
+                "tag:GetTagKeys",
+                "tag:GetTagValues",
+
+  ]
+  
+  filter_tags = []
+  host_tags   = ["Env:${local.config.environment}", "datadog:enabled"]
+  namespace_rules = {
+    auto_scaling = false
+    opsworks     = false
+  }
+  excluded_regions = ["us-east-2, us-west-1, us-west-2, ca-central-1, eu-west-2, eu-west-3, eu-central-1, eu-north-1, ap-south-1, ap-northeast-1, ap-northeast-2, ap-southeast-1, ap-southeast-2, sa-east-1"]
+
+  dashboard_title       = "${local.config.project_name} Dashboard"
+  dashboard_description = "Monitoring dashboard for ${local.config.project_name}"
+  layout_type          = "ordered"
+  alert_id             = "some-alert-id"
+  widget_type          = "timeseries"
+  widget_title         = "Widget Title"
+  widget_time_span     = "10m"
+  secret_id            = local.config.secret_arn
+}
+
+
+
