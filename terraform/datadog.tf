76a0fcf11a3b9f53b517ef5de3b05db1e322cba9
ShaunKav <shaun.kavanagh@vechain.org>
2024-05-16 10:37:44 +0100

    updating permissions list and clean up

--- terraform/datadog.tf
+++ terraform/datadog.tf
@@ -5,8 +5,6 @@ module "datadog_integration_aws" {
   aws_permissions_list = [
                 "apigateway:GET",
                 "autoscaling:Describe*",
-                "backup:List*",
-                "budgets:ViewBudget",
                 "cloudfront:GetDistributionConfig",
                 "cloudfront:ListDistributions",
                 "cloudtrail:DescribeTrails",
@@ -15,26 +13,9 @@ module "datadog_integration_aws" {
                 "cloudwatch:Describe*",
                 "cloudwatch:Get*",
                 "cloudwatch:List*",
-                "codedeploy:List*",
-                "codedeploy:BatchGet*",
-                "dynamodb:List*",
-                "dynamodb:Describe*",
-                "ec2:Describe*",
-                "ec2:GetTransitGatewayPrefixListReferences",
-                "ec2:SearchTransitGatewayRoutes",
                 "ecs:Describe*",
                 "ecs:List*",
-                "elasticache:Describe*",
-                "elasticache:List*",
-                "elasticfilesystem:DescribeFileSystems",
-                "elasticfilesystem:DescribeTags",
-                "elasticfilesystem:DescribeAccessPoints",
                 "elasticloadbalancing:Describe*",
-                "elasticmapreduce:List*",
-                "elasticmapreduce:Describe*",
-                "es:ListTags",
-                "es:ListDomainNames",
-                "es:DescribeElasticsearchDomains",
                 "events:CreateEventBus",
                 "health:DescribeEvents",
                 "health:DescribeEventDetails",
@@ -48,10 +29,6 @@ module "datadog_integration_aws" {
                 "logs:FilterLogEvents",
                 "logs:PutSubscriptionFilter",
                 "logs:TestMetricFilter",
-                "organizations:Describe*",
-                "organizations:List*",
-                "rds:Describe*",
-                "rds:List*",
                 "route53:List*",
                 "s3:GetBucketLogging",
                 "s3:GetBucketLocation",
@@ -59,25 +36,20 @@ module "datadog_integration_aws" {
                 "s3:GetBucketTagging",
                 "s3:ListAllMyBuckets",
                 "s3:PutBucketNotification",
-                "ses:Get*",
-                "sns:List*",
-                "sns:Publish",
                 "sqs:ListQueues",
                 "states:ListStateMachines",
                 "states:DescribeStateMachine",
                 "tag:GetResources",
                 "tag:GetTagKeys",
                 "tag:GetTagValues",
-
   ]
   
   filter_tags = []
-  host_tags   = ["Env:${local.config.environment_name}", "datadog:enabled"]
+  host_tags   = ["Env:${local.config.environment_name}"]
   namespace_rules = {
     "us-east-1" = true  // Enable monitoring for us-east-1
-    "eu-west-1" = true  // Enable monitoring for eu-east-1
+    "eu-west-1" = true  // Enable monitoring for eu-west-1
   } 
-  //excluded_regions = ["us-east-2, us-west-1, us-west-2, ca-central-1, eu-west-2, eu-west-3, eu-central-1, eu-north-1, ap-south-1, ap-northeast-1, ap-northeast-2, ap-southeast-1, ap-southeast-2, sa-east-1"]
 
   dashboard_title       = "${local.config.environment_name} Dashboard"
   dashboard_description = "Monitoring dashboard for ${local.config.environment_name}"
