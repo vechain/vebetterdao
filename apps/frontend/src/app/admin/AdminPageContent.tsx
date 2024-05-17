49cd33b5b1c50a61329d25842bda5067b93acbb2
Dan Rusnac <rusnac14@gmail.com>
2024-05-17 14:49:33 +0100

    feat: use tabs and renamed files

--- apps/frontend/src/app/admin/AdminPageContent.tsx
+++ apps/frontend/src/app/admin/AdminPageContent.tsx
@@ -1,6 +1,20 @@
 "use client"
 
-import { Card, CardBody, CardHeader, Heading, Stack, VStack, HStack, Text } from "@chakra-ui/react"
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Stack,
+  VStack,
+  HStack,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react"
 import { useEffect } from "react"
 import { AnalyticsUtils } from "@/utils"
 import { useAccountPermissions } from "@/api/contracts/account"
@@ -10,8 +24,8 @@ import { B3trAllowance } from "./components/B3trAllowance"
 import { BulkClaimXAppsAllocations } from "./components/BulkClaimXAppsAllocations"
 import { ClaimXAppAllocations } from "./components/ClaimXAppAllocations"
 import { Pause } from "./components/Pause"
-import { StartEmissions } from "./components/StartEmissions"
-import { StartRound } from "./components/StartRound"
+import { StartEmissionsButton } from "./components/StartEmissionsButton"
+import { StartRoundButton } from "./components/StartRoundButton"
 import { UpdateReceiverAddress } from "./components/UpdateReceiverAddress"
 import { ProposalsAdmin } from "./components/ProposalsAdmin/ProposalsAdmin"
 
@@ -21,62 +35,78 @@ export const AdminPageContent = () => {
   }, [])
 
   const { account } = useWallet()
-  const { isAdminOfB3tr, isAdminOfEmissions, isAdminOfXAllocationVoting, isAdminOfVot3, isAdminOfGalaxyMember, isAdmin } =
-    useAccountPermissions(account ?? "")
+  const {
+    isAdminOfB3tr,
+    isAdminOfEmissions,
+    isAdminOfXAllocationVoting,
+    isAdminOfVot3,
+    isAdminOfGalaxyMember,
+    isAdmin,
+  } = useAccountPermissions(account ?? "")
 
   return (
     <Stack spacing={12} w={"full"} data-testid="admin-page">
-      {isAdmin && <AdminPermissions />}
+      <Tabs>
+        <TabList>
+          <Tab>Emissions</Tab>
+          <Tab>X2Earn Apps</Tab>
+          <Tab>Permissions</Tab>
+          <Tab>Pausing</Tab>
+        </TabList>
 
-      <Stack direction={["column", "row"]} w={"full"} spacing={12} alignItems={"start"}>
-        <Card w={"full"}>
-          <CardHeader>
-            <Heading size="lg">Emissions and Rounds</Heading>
-          </CardHeader>
-          <CardBody>
-            <VStack w={"full"} spacing={4} alignItems={"start"}>
-              {isAdminOfEmissions && <StartEmissions />}
-              <StartRound />
-            </VStack>
-          </CardBody>
-        </Card>
+        <TabPanels>
+          <TabPanel>
+            <Card w={"full"}>
+              <CardHeader>
+                <Heading size="lg">Start round</Heading>
+              </CardHeader>
+              <CardBody>
+                <VStack w={"full"} spacing={4} alignItems={"start"}>
+                  {isAdminOfEmissions && <StartEmissionsButton />}
+                  <StartRoundButton />
+                </VStack>
+              </CardBody>
+            </Card>
 
-        <Card w={"full"}>
-          <CardHeader>
-            <Heading size="lg">B3TR Token Allowance</Heading>
-            <Text fontSize="sm">Allow an external address to spend your B3TR tokens.</Text>
-          </CardHeader>
-          <CardBody>
-            <B3trAllowance />
-          </CardBody>
-        </Card>
-      </Stack>
-
-      <Card w={"full"}>
-        <CardHeader>
-          <Heading size="lg">X-2-Earn Apps</Heading>
-        </CardHeader>
-        <CardBody>
-          <VStack w={"full"} spacing={12} alignItems={"start"}>
-            <Stack direction={["column", "row"]} w={"full"} spacing={12} justify={"space-between"}>
+            <Stack direction={["column", "row"]} w={"full"} spacing={6} pt={6} alignItems={"start"}>
               <ClaimXAppAllocations />
               <BulkClaimXAppsAllocations />
             </Stack>
-            {isAdminOfXAllocationVoting && <UpdateReceiverAddress />}
-          </VStack>
-        </CardBody>
-      </Card>
+          </TabPanel>
+          <TabPanel>
+            <Stack direction={["column", "row"]} w={"full"} spacing={12} alignItems={"start"}>
+              {isAdminOfXAllocationVoting && <UpdateReceiverAddress />}
+            </Stack>
+          </TabPanel>
+          <TabPanel>
+            <Stack direction={["column", "row"]} w={"full"} spacing={12} alignItems={"start"}>
+              {isAdmin && <AdminPermissions />}
 
-      {(isAdminOfGalaxyMember || isAdminOfB3tr || isAdminOfVot3) && (
-        <Card w={"full"}>
-          <CardHeader>
-            <Heading size="lg">Pausing</Heading>
-          </CardHeader>
-          <CardBody>
-            <Pause />
-          </CardBody>
-        </Card>
-      )}
+              <Card w={"full"}>
+                <CardHeader>
+                  <Heading size="lg">B3TR Token Allowance</Heading>
+                  <Text fontSize="sm">Allow an external address to spend your B3TR tokens.</Text>
+                </CardHeader>
+                <CardBody>
+                  <B3trAllowance />
+                </CardBody>
+              </Card>
+            </Stack>
+          </TabPanel>
+          <TabPanel>
+            {(isAdminOfGalaxyMember || isAdminOfB3tr || isAdminOfVot3) && (
+              <Card w={"full"}>
+                <CardHeader>
+                  <Heading size="lg">Pausing</Heading>
+                </CardHeader>
+                <CardBody>
+                  <Pause />
+                </CardBody>
+              </Card>
+            )}
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Stack>
   )
 }
