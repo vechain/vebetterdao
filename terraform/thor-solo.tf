ff1821647baac96a5b908945a44999ede879677a
Dougal Rea <dougal.rea@gmail.com>
2024-05-20 10:26:39 +0100

    Update thor-solo.tf

--- terraform/thor-solo.tf
+++ terraform/thor-solo.tf
@@ -112,7 +112,7 @@ module "ecs-lb-service-thor-solo" {
   ecr_repo_uri               = module.ecr.repository_url[0]
   secrets_enable             = false
   assign_public_ip           = false
-  app_name                   = "thor-solo-${local.env}"
+  app_name                   = "thor-solo"
   ecr_image_tag              = local.config.image_tag
   project                    = local.config.project
   cpu                        = local.config.cpu
@@ -147,41 +147,4 @@ module "ecs-lb-service-thor-solo" {
   # scale_in_cooldown = 300
   # scale_out_cooldown = 300
   name                                = "auto-scaling-group"
-}
-
-##thor-solo ecs service creation
-module "thor_solo_node" {
-  source              = "git@github.com:vechain/devops.git//ecs?ref=main"
-  vpc_id              = local.config.vpc_id
-  public_subnets      = local.config.public_subnets
-  private_subnets     = local.config.private_subnets
-  env                 = local.env
-  common_ecr_repo     = true
-  common_ecr_repo_url = "${local.base_registry_url}/${local.config.project}/thor-solo"
-  internal_url_name   = "thor-solo.local"
-  app_name            = "thor-solo"
-  image_tag           = local.config.image_tag
-  project             = local.config.project
-  cpu                 = local.config.cpu
-  memory              = local.config.memory
-  cidr                = local.config.vpc_cidr
-  desired_capacity    = "1"
-  container_port      = 8669
-  host_port           = 8669
-  certificate_arn     = module.thor-solo_domain.certificate_arn
-  alb_path_rule       = ["/"]
-  health_check_path   = "/blocks/0"
-  secrets_enable      = "false"
-  lb_enable           = "true"
-  namespace           = aws_service_discovery_private_dns_namespace.ns.id
-  log_metric_filters = [
-    {
-      name    = "AppUnhealthy",
-      pattern = "Application is UNHEALTHY"
-    }
-  ]
-  environment_variables = [
-
-  ]
-  runtime_platform = var.runtime_platform
 }
