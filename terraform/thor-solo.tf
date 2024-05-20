1cf5a2b8a7decc7efb08170bbfc192d657bb5daa
Dougal Rea <dougal.rea@gmail.com>
2024-05-20 12:53:27 +0100

    Update thor-solo.tf

--- terraform/thor-solo.tf
+++ terraform/thor-solo.tf
@@ -142,7 +142,7 @@ module "ecs-lb-service-thor-solo" {
   min_capacity                        = 1
   max_capacity                        = local.config.max_capacity
   target_cpu_value                    = 70
-  target_memory_value                 = 70
+  target_memory_value                 = 95
   disable_scale_in                    = false
   # scale_in_cooldown = 300
   # scale_out_cooldown = 300
