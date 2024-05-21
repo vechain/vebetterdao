e65c77979f3902afe557f82bcedb1ff2b0c04f90
Roisin Dowling <roisinadowling@gmail.com>
2024-05-21 14:37:10 +0100

    Create clock logic library

---
+++ packages/contracts/contracts/governance/libraries/GovernorClockLogic.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+
+//                                      #######
+//                                 ################
+//                               ####################
+//                             ###########   #########
+//                            #########      #########
+//          #######          #########       #########
+//          #########       #########      ##########
+//           ##########     ########     ####################
+//            ##########   #########  #########################
+//              ################### ############################
+//               #################  ##########          ########
+//                 ##############      ###              ########
+//                  ############                       #########
+//                    ##########                     ##########
+//                     ########                    ###########
+//                       ###                    ############
+//                                          ##############
+//                                    #################
+//                                   ##############
+//                                   #########
+
+pragma solidity ^0.8.20;
+
+import { GovernorStorageTypes } from "./GovernorStorageTypes.sol";
+import { IVOT3 } from "../../interfaces/IVOT3.sol";
+import { Time } from "@openzeppelin/contracts/utils/types/Time.sol";
+
+library GovernorClockLogic {
+  /**
+   * @dev Clock (as specified in EIP-6372) is set to match the token's clock. Fallback to block numbers if the token
+   * does not implement EIP-6372.
+   */
+  function clock(GovernorStorageTypes.GovernorExternalContractsStorage storage self) public view returns (uint48) {
+    try self.vot3.clock() returns (uint48 timepoint) {
+      return timepoint;
+    } catch {
+      return Time.blockNumber();
+    }
+  }
+
+  /**
+   * @dev Machine-readable description of the clock as specified in EIP-6372.
+   */
+  // solhint-disable-next-line func-name-mixedcase
+  function CLOCK_MODE(
+    GovernorStorageTypes.GovernorExternalContractsStorage storage self
+  ) public view returns (string memory) {
+    try self.vot3.CLOCK_MODE() returns (string memory clockmode) {
+      return clockmode;
+    } catch {
+      return "mode=blocknumber&from=default";
+    }
+  }
+}
