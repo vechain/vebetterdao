ffcf2a5332d4be74ba1c934477b37985e7472aec
Roisin Dowling <roisinadowling@gmail.com>
2024-05-24 13:19:52 +0100

    Use one storage location for all variables]

--- packages/contracts/contracts/governance/GovernorStorage.sol
+++ packages/contracts/contracts/governance/GovernorStorage.sol
@@ -30,93 +30,14 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 /// @title GovernorStorage
 /// @notice Contract used as storage of the B3TRGovernor contract.
 /// @dev It defines the storage layout of the B3TRGovernor contract.
-contract B3TRGovernorStorage is Initializable {
-  // keccak256(abi.encode(uint256(keccak256("GovernorQuoromStorage")) - 1)) & ~bytes32(uint256(0xff))
-  bytes32 private constant GovernorQuoromStorageLocation =
-    0xc4bf39076012325596b2a6f89760cc6608fd95f7cf147b47ae61fb6e5a9a1200;
-
-  // keccak256(abi.encode(uint256(keccak256("GovernorTimeLockStorage")) - 1)) & ~bytes32(uint256(0xff))
-  bytes32 private constant GovernorTimeLockStorageLocation =
-    0x13baad612a1ba34c1ad25cf3b32eafcb05c98f263795fbf41522d389ae5ab100;
-
-  // keccak256(abi.encode(uint256(keccak256("GovernorSettingsStorage")) - 1)) & ~bytes32(uint256(0xff))
-  bytes32 private constant GovernorSettingsStorageLocation =
-    0xc0065be1bc8e77d69a203a49db226e97f882a1490934768d77ec9a4eb951ac00;
-
-  // keccak256(abi.encode(uint256(keccak256("GovernorFunctionRestrictionsStorage")) - 1)) & ~bytes32(uint256(0xff))
-  bytes32 private constant GovernorFunctionRestrictionsStorageLocation =
-    0x5559abecc34569938f68c9644616ef818c03d16a5c20378622558d8161585100;
-
-  // keccak256(abi.encode(uint256(keccak256("GovernorExternalContractsStorage")) - 1)) & ~bytes32(uint256(0xff))
-  bytes32 private constant GovernorExternalContractsStorageLocation =
-    0x97d598a282d3493bebcf9bd0e9d7ba8e1b1c2e1312cb1c8dc6e1717525f7c200;
-
-  // keccak256(abi.encode(uint256(keccak256("GovernorDepositStorage")) - 1)) & ~bytes32(uint256(0xff))
-  bytes32 private constant GovernorDepositStorageLocation =
-    0xfd3a2833e16394588f908aea3be9d05d97d1dfc59c0c2431ef215c019c421800;
-
-  // keccak256(abi.encode(uint256(keccak256("GovernorVotesStorage")) - 1)) & ~bytes32(uint256(0xff))
-  bytes32 private constant GovernorVotesStorageLocation =
-    0xcb6cb12dc59022840a1c776f4f7dc071bcd58829ba542523309c31825375c500;
-
-  // keccak256(abi.encode(uint256(keccak256("GovernorGeneralStorage")) - 1)) & ~bytes32(uint256(0xff))
-  bytes32 private constant GovernorGeneralStorageLocation =
-    0x5af455dc74aeb146c90fc4a1404fc385af450c3a522aa60eedbb49c62be6b800;
-
-  /// @dev Internal function to access the governor quorum storage slot.
-  function getGovernorQuoromStorage() internal pure returns (GovernorStorageTypes.GovernorQuoromStorage storage $) {
-    assembly {
-      $.slot := GovernorQuoromStorageLocation
-    }
-  }
+contract GovernorStorage is Initializable {
+  // keccak256(abi.encode(uint256(keccak256("GovernorStorageLocation")) - 1)) & ~bytes32(uint256(0xff))
+  bytes32 private constant GovernorStorageLocation = 0xd09a0aaf4ab3087bae7fa25ef74ddd4e5a4950980903ce417e66228cf7dc7b00;
 
   /// @dev Internal function to access the governor timelock storage slot.
-  function getGovernorTimeLockStorage() internal pure returns (GovernorStorageTypes.GovernorTimeLockStorage storage $) {
+  function getGovernorStorage() internal pure returns (GovernorStorageTypes.GovernorStorage storage $) {
     assembly {
-      $.slot := GovernorTimeLockStorageLocation
-    }
-  }
-
-  /// @dev Internal function to access the governor function restrictions storage slot.
-  function getGovernorFunctionRestrictionsStorage()
-    internal
-    pure
-    returns (GovernorStorageTypes.GovernorFunctionRestrictionsStorage storage $)
-  {
-    assembly {
-      $.slot := GovernorFunctionRestrictionsStorageLocation
-    }
-  }
-
-  /// @dev Internal function to access the governor external contracts storage slot.
-  function getGovernorExternalContractsStorage()
-    internal
-    pure
-    returns (GovernorStorageTypes.GovernorExternalContractsStorage storage $)
-  {
-    assembly {
-      $.slot := GovernorExternalContractsStorageLocation
-    }
-  }
-
-  /// @dev Internal function to access the governor deposit storage slot.
-  function getGovernorDepositStorage() internal pure returns (GovernorStorageTypes.GovernorDepositStorage storage $) {
-    assembly {
-      $.slot := GovernorDepositStorageLocation
-    }
-  }
-
-  /// @dev Internal function to access the governor votes storage slot.
-  function getGovernorVotesStorage() internal pure returns (GovernorStorageTypes.GovernorVotesStorage storage $) {
-    assembly {
-      $.slot := GovernorVotesStorageLocation
-    }
-  }
-
-  /// @dev Internal function to access the governor general storage slot.
-  function getGovernorGeneralStorage() internal pure returns (GovernorStorageTypes.GovernorGeneralStorage storage $) {
-    assembly {
-      $.slot := GovernorGeneralStorageLocation
+      $.slot := GovernorStorageLocation
     }
   }
 
@@ -138,33 +59,26 @@ contract B3TRGovernorStorage is Initializable {
     //quorumStorage.quorumPercentage = initializationData.quorumPercentage;
 
     // Set the governor time lock storage
-    GovernorStorageTypes.GovernorTimeLockStorage storage timeLockStorage = getGovernorTimeLockStorage();
-    timeLockStorage.timelock = initializationData.timelock;
+    GovernorStorageTypes.GovernorStorage storage governorStorage = getGovernorStorage();
+    governorStorage.timelock = initializationData.timelock;
 
     // Set the governor function restrictions storage
-    GovernorStorageTypes.GovernorFunctionRestrictionsStorage
-      storage functionRestrictionsStorage = getGovernorFunctionRestrictionsStorage();
-    functionRestrictionsStorage.isFunctionRestrictionEnabled = initializationData.isFunctionRestrictionEnabled;
+    governorStorage.isFunctionRestrictionEnabled = initializationData.isFunctionRestrictionEnabled;
 
     // Set the governor external contracts storage
-    GovernorStorageTypes.GovernorExternalContractsStorage
-      storage externalContractsStorage = getGovernorExternalContractsStorage();
-    externalContractsStorage.voterRewards = initializationData.voterRewards;
-    externalContractsStorage.xAllocationVoting = initializationData.xAllocationVoting;
-    externalContractsStorage.b3tr = initializationData.b3tr;
-    externalContractsStorage.vot3 = initializationData.vot3Token;
+    governorStorage.voterRewards = initializationData.voterRewards;
+    governorStorage.xAllocationVoting = initializationData.xAllocationVoting;
+    governorStorage.b3tr = initializationData.b3tr;
+    governorStorage.vot3 = initializationData.vot3Token;
 
     // Set the governor general storage
-    GovernorStorageTypes.GovernorGeneralStorage storage generalStorage = getGovernorGeneralStorage();
-    generalStorage.name = governorName;
-    generalStorage.minVotingDelay = initializationData.initialMinVotingDelay;
+    governorStorage.name = governorName;
+    governorStorage.minVotingDelay = initializationData.initialMinVotingDelay;
 
     // Set the governor deposit storage
-    GovernorStorageTypes.GovernorDepositStorage storage depositStorage = getGovernorDepositStorage();
-    depositStorage.depositThresholdPercentage = initializationData.initialDepositThreshold;
+    governorStorage.depositThresholdPercentage = initializationData.initialDepositThreshold;
 
     // Set the governor votes storage
-    GovernorStorageTypes.GovernorVotesStorage storage votesStorage = getGovernorVotesStorage();
-    votesStorage.votingThreshold = initializationData.initialVotingThreshold;
+    governorStorage.votingThreshold = initializationData.initialVotingThreshold;
   }
 }
