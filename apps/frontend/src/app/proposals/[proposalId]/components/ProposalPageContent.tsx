db35db8b7901d21903af4111425440ee6322d110
ve-akanoce <eriknucib@live.it>
2024-05-27 16:00:09 +0200

    fix: layout

--- apps/frontend/src/app/proposals/[proposalId]/components/ProposalPageContent.tsx
+++ apps/frontend/src/app/proposals/[proposalId]/components/ProposalPageContent.tsx
@@ -18,12 +18,14 @@ export const ProposalPageContent: React.FC<Props> = ({ proposalId }) => {
     <VStack w="full" alignItems="stretch" spacing={"40px"}>
       <ProposalOverview />
       <Grid templateColumns="repeat(3, 1fr)" gap={[4, 4, 8]} w="full">
-        <GridItem colSpan={[3, 3, 2]}>
-          <ProposalContentAndActions proposal={proposal} />
-        </GridItem>
-        <GridItem colSpan={[3, 3, 1]}>
+        <GridItem colSpan={[3, 3, 2]} gap={8}>
           {proposalState === ProposalState.Pending && <ProposalCommunitySupport />}
         </GridItem>
+        <GridItem colSpan={[3, 3, 2]} gap={8}>
+          <ProposalContentAndActions proposal={proposal} />
+        </GridItem>
+
+        <GridItem colSpan={[3, 3, 1]}></GridItem>
       </Grid>
     </VStack>
   )
