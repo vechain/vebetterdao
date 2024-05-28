25d84a483525a9b14c9158589daaeb3f98e779c6
Roisin Dowling <roisinadowling@gmail.com>
2024-05-28 12:31:23 +0100

    add more spacing

--- packages/contracts/contracts/governance/libraries/GovernorStorageTypes.sol
+++ packages/contracts/contracts/governance/libraries/GovernorStorageTypes.sol
@@ -37,6 +37,7 @@ import { Checkpoints } from "@openzeppelin/contracts/utils/structs/Checkpoints.s
 library GovernorStorageTypes {
   struct GovernorStorage {
     // ------------------------------- Version 1 -------------------------------
+
     // ------------------------------- General Storage -------------------------------
     string name; // name of the Governor
     mapping(uint256 proposalId => GovernorTypes.ProposalCore) proposals;
@@ -45,27 +46,43 @@ library GovernorStorageTypes {
     // {onlyGovernance} modifier and eventually reset after {_executeOperations} completes. This ensures that the
     // execution of {onlyGovernance} protected calls can only be achieved through successful proposals.
     DoubleEndedQueue.Bytes32Deque governanceCall;
-    uint256 minVotingDelay; // min delay before voting can start
+    // min delay before voting can start
+    uint256 minVotingDelay;
     // ------------------------------- Quorum Storage -------------------------------
-    Checkpoints.Trace208 quorumNumeratorHistory; // quorum numerator history
+    // quorum numerator history
+    Checkpoints.Trace208 quorumNumeratorHistory;
     // ------------------------------- Timelock Storage -------------------------------
-    TimelockControllerUpgradeable timelock; // Timelock contract
-    mapping(uint256 proposalId => bytes32) timelockIds; // mapping of proposalId to timelockId
+    // Timelock contract
+    TimelockControllerUpgradeable timelock;
+    // mapping of proposalId to timelockId
+    mapping(uint256 proposalId => bytes32) timelockIds;
     // ------------------------------- Function Restriction Storage -------------------------------
-    mapping(address => mapping(bytes4 => bool)) whitelistedFunctions; // mapping of target address to function selector to bool indicating if function is whitelisted for proposals
-    bool isFunctionRestrictionEnabled; // flag to enable/disable function restriction
+    // mapping of target address to function selector to bool indicating if function is whitelisted for proposals
+    mapping(address => mapping(bytes4 => bool)) whitelistedFunctions;
+    // flag to enable/disable function restrictions
+    bool isFunctionRestrictionEnabled;
     // ------------------------------- External Contracts Storage -------------------------------
-    IVoterRewards voterRewards; // Voter Rewards contract
-    IXAllocationVotingGovernor xAllocationVoting; // XAllocationVotingGovernor contract
-    IB3TR b3tr; // B3TR contract
-    IVOT3 vot3; // VOT3 contract
+    // Voter Rewards contract
+    IVoterRewards voterRewards;
+    // XAllocationVotingGovernor contract
+    IXAllocationVotingGovernor xAllocationVoting;
+    // B3TR contract
+    IB3TR b3tr;
+    // VOT3 contract
+    IVOT3 vot3;
     // ------------------------------- Desposits Storage -------------------------------
-    mapping(uint256 => mapping(address => uint256)) deposits; // mapping to track deposits made to proposals by address
-    uint256 depositThresholdPercentage; // percentage of the total supply of B3TR tokens that need to be deposited in VOT3 to create a proposal
+    // mapping to track deposits made to proposals by address
+    mapping(uint256 => mapping(address => uint256)) deposits;
+    // percentage of the total supply of B3TR tokens that need to be deposited in VOT3 to create a proposal
+    uint256 depositThresholdPercentage;
     // ------------------------------- Voting Storage -------------------------------
-    mapping(uint256 => GovernorTypes.ProposalVote) proposalVotes; // mapping to store the votes for a proposal
-    mapping(address => bool) hasVotedOnce; // mapping to store that a user has voted at least one time
-    mapping(uint256 => uint256) proposalTotalVotes; // mapping to store the total votes for a proposal
-    uint256 votingThreshold; // minimum amount of tokens needed to cast a vote
+    // mapping to store the votes for a proposal
+    mapping(uint256 => GovernorTypes.ProposalVote) proposalVotes;
+    // mapping to store that a user has voted at least one time
+    mapping(address => bool) hasVotedOnce;
+    // mapping to store the total votes for a proposal
+    mapping(uint256 => uint256) proposalTotalVotes;
+    // minimum amount of tokens needed to cast a vote
+    uint256 votingThreshold;
   }
 }
