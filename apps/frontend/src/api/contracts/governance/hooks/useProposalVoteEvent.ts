63f5892c212e1b824384669ad5566d8e6b67d655
Davide Carpini <carpini.davide@gmail.com>
2024-05-28 12:33:40 +0200

    ts-doc

--- apps/frontend/src/api/contracts/governance/hooks/useProposalVoteEvent.ts
+++ apps/frontend/src/api/contracts/governance/hooks/useProposalVoteEvent.ts
@@ -3,6 +3,11 @@ import { useProposalsEvents } from "./useProposalsEvents"
 import { useWallet } from "@vechain/dapp-kit-react"
 import { compareAddresses } from "@repo/utils/AddressUtils"
 
+/**
+ * Custom hook that retrieves the vote event for a specific proposal.
+ * @param proposalId - The ID of the proposal.
+ * @returns An object containing information about the vote event.
+ */
 export const useProposalVoteEvent = (proposalId: string) => {
   const { account } = useWallet()
   const events = useProposalsEvents()
