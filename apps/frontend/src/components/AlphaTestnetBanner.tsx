b70d48551ef579e5f3277e016bb9aa2b0919a2bc
Erik Nucibella <eriknucib@live.it>
2024-06-08 22:37:58 +0100

    feat(AlphaTestnetBanner): change test to Alpha Testnet Last Round (#915)

--- apps/frontend/src/components/AlphaTestnetBanner.tsx
+++ apps/frontend/src/components/AlphaTestnetBanner.tsx
@@ -4,7 +4,7 @@ import { getConfig } from "@repo/config"
 import { useEffect, useRef, useState } from "react"
 
 const isTestnetPhase = ["test", "solo", "custom"].includes(getConfig().network.type)
-const text = "Alpha Testnet Phase"
+const text = "Alpha Testnet Last Round"
 const MotionHStack = motion(HStack)
 export const AlphaTestnetBanner = () => {
   const textsNumber = 50
