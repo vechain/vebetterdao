51f011f5349a7984098ab65e69b5aa19e6218938
Davide Carpini <carpini.davide@gmail.com>
2024-06-09 13:21:39 +0200

    899 remove scalenumberdown and scalenumberup (#914)
    * fix: remove scale number down
    
    * fix: remove scale number up
    
    * fix: remove useless token digit references
    
    * fix: current round allocations

--- apps/frontend/src/api/contracts/vot3/utils/buildConvertB3trTx.ts
+++ apps/frontend/src/api/contracts/vot3/utils/buildConvertB3trTx.ts
@@ -1,7 +1,8 @@
 import { getConfig } from "@repo/config"
 import { FormattingUtils } from "@repo/utils"
-
 import { Vot3ContractJson } from "@repo/contracts"
+import { ethers } from "ethers"
+
 const vot3Abi = Vot3ContractJson.abi
 
 const config = getConfig()
@@ -14,16 +15,12 @@ const VOT3_CONTRACT = config.vot3ContractAddress
  * @param decimals the decimals of the token
  * @returns the clause to convert B3TR to VOT3
  */
-export const buildConvertB3trTx = (
-  thor: Connex.Thor,
-  amount: string | number,
-  decimals = 18,
-): Connex.Vendor.TxMessage[0] => {
+export const buildConvertB3trTx = (thor: Connex.Thor, amount: string | number): Connex.Vendor.TxMessage[0] => {
   const functionAbi = vot3Abi.find(e => e.name === "convertToVOT3")
   if (!functionAbi) throw new Error("Function abi not found for mint")
 
   const formattedAmount = FormattingUtils.humanNumber(amount ?? 0, amount)
-  const amountWithDecimals = FormattingUtils.scaleNumberUp(amount, decimals, decimals)
+  const amountWithDecimals = ethers.parseEther(amount.toString()).toString()
 
   const clause = thor.account(VOT3_CONTRACT).method(functionAbi).asClause(amountWithDecimals)
 
