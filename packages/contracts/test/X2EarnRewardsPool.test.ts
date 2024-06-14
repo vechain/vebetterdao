95a8d92e949869be94776c9c848a3e2931c201cf
Dan Rusnac <rusnac14@gmail.com>
2024-06-14 15:27:06 +0200

    fix: run all tests

--- packages/contracts/test/X2EarnRewardsPool.test.ts
+++ packages/contracts/test/X2EarnRewardsPool.test.ts
@@ -5,7 +5,7 @@ import { describe, it } from "mocha"
 import { getImplementationAddress } from "@openzeppelin/upgrades-core"
 import { deployProxy } from "../scripts/helpers"
 
-describe.only("X2EarnRewardsPool", function () {
+describe("X2EarnRewardsPool", function () {
   // deployment
   describe("Deployment", function () {
     it("Cannot deploy contract with zero address", async function () {
