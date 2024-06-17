faab860025db169e4aeb1048a60b28d9d1c73c2b
Davide Carpini <carpini.davide@gmail.com>
2024-06-17 11:37:44 +0200

    fix: edit page button condition

--- apps/frontend/src/app/apps/[appId]/components/AppDetailOverview/components/EditAppPageButton.tsx
+++ apps/frontend/src/app/apps/[appId]/components/AppDetailOverview/components/EditAppPageButton.tsx
@@ -1,7 +1,10 @@
 import { IconButton } from "@chakra-ui/react"
 import { UilPen } from "@iconscout/react-unicons"
 import { useParams, useRouter } from "next/navigation"
-import { useCallback } from "react"
+import { useCallback, useMemo } from "react"
+import { useCurrentAppAdmin, useCurrentAppModerators } from "../../../hooks"
+import { useWallet } from "@vechain/dapp-kit-react"
+import { compareAddresses } from "@repo/utils/AddressUtils"
 
 export const EditAppPageButton = () => {
   const { appId } = useParams()
@@ -10,6 +13,20 @@ export const EditAppPageButton = () => {
   const handleEdit = useCallback(() => {
     router.push(`/apps/${appId}/edit`)
   }, [appId, router])
+
+  const { account } = useWallet()
+  const { admin } = useCurrentAppAdmin()
+  const { moderators } = useCurrentAppModerators()
+
+  const showEditButton = useMemo(() => {
+    if (compareAddresses(account || "", admin)) return true
+    if (moderators?.find(moderator => compareAddresses(account || "", moderator))) return true
+    return false
+  }, [account, admin, moderators])
+
+  if (!showEditButton) {
+    return null
+  }
 
   return (
     <>
---
+++ apps/frontend/src/app/apps/[appId]/hooks/useCurrentAppModerators.ts
@@ -0,0 +1,17 @@
+import { useAppModerators } from "@/api"
+import { useParams } from "next/navigation"
+
+/**
+ * Custom hook to fetch the current app moderators.
+ * @returns An object containing moderators data, loading state, and error state.
+ */
+export const useCurrentAppModerators = () => {
+  const { appId } = useParams<{ appId: string }>()
+  const { data: moderators, isLoading, error } = useAppModerators(appId)
+
+  return {
+    moderators,
+    isLoading,
+    error,
+  }
+}
