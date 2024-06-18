237aaa4ddf291f644ff3423535066e4fa511e786
Dan Rusnac <rusnac14@gmail.com>
2024-06-18 18:46:08 +0200

    feat: use new hook

--- apps/frontend/src/app/apps/[appId]/components/AppDetailSidebar.tsx
+++ apps/frontend/src/app/apps/[appId]/components/AppDetailSidebar.tsx
@@ -1,15 +1,13 @@
 import { VStack } from "@chakra-ui/react"
 import { AppBalanceCard } from "./AppBalanceCard"
 import { useCurrentAppInfo } from "../hooks/useCurrentAppInfo"
-import { useAppAdmin } from "@/api"
+import { useIsAppAdmin } from "@/api"
 import { useWallet } from "@vechain/dapp-kit-react"
 
 export const AppDetailsSidebar = () => {
   const { account } = useWallet()
   const { app } = useCurrentAppInfo()
-  const { data: appAdmin } = useAppAdmin(app?.id ?? "")
-
-  const isAppAdmin = appAdmin?.toUpperCase() === account?.toUpperCase()
+  const { data: isAppAdmin } = useIsAppAdmin(app?.id ?? "", account ?? "")
 
   return (
     <VStack spacing={4} flex={2} position="relative">
