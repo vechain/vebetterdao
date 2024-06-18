a697130634b775ee131213226190e5152b78b5b3
Dan Rusnac <rusnac14@gmail.com>
2024-06-18 18:33:31 +0200

    feat: only app admin can view the section

--- apps/frontend/src/app/apps/[appId]/components/AppDetailSidebar.tsx
+++ apps/frontend/src/app/apps/[appId]/components/AppDetailSidebar.tsx
@@ -1,10 +1,19 @@
 import { VStack } from "@chakra-ui/react"
 import { AppBalanceCard } from "./AppBalanceCard"
+import { useCurrentAppInfo } from "../hooks/useCurrentAppInfo"
+import { useAppAdmin } from "@/api"
+import { useWallet } from "@vechain/dapp-kit-react"
 
 export const AppDetailsSidebar = () => {
+  const { account } = useWallet()
+  const { app } = useCurrentAppInfo()
+  const { data: appAdmin } = useAppAdmin(app?.id ?? "")
+
+  const isAppAdmin = appAdmin === account
+
   return (
     <VStack spacing={4} flex={2} position="relative">
-      <AppBalanceCard />
+      {isAppAdmin && <AppBalanceCard />}
     </VStack>
   )
 }
