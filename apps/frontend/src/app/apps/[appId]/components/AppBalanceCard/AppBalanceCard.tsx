896601e978d624143ec475df8138c48aaa214d30
Dan Rusnac <rusnac14@gmail.com>
2024-06-18 10:04:39 +0200

    feat: app balance card ui

--- apps/frontend/src/app/apps/[appId]/components/AppBalanceCard/AppBalanceCard.tsx
+++ apps/frontend/src/app/apps/[appId]/components/AppBalanceCard/AppBalanceCard.tsx
@@ -1,14 +1,55 @@
-import { Card, CardBody, CardHeader, Heading } from "@chakra-ui/react"
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  HStack,
+  Heading,
+  Icon,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react"
+import { getCompactFormatter } from "@repo/utils/FormattingUtils"
 import { useTranslation } from "react-i18next"
+import { IoWalletOutline } from "react-icons/io5"
+
+const compactFormatter = getCompactFormatter(4)
 
 export const AppBalanceCard = () => {
   const { t } = useTranslation()
+  const b3trLogo = "/images/logo/b3tr_logo_dark.svg"
+
   return (
     <Card w={"full"} variant="baseWithBorder">
       <CardHeader>
         <Heading size="md">{t("App balance")}</Heading>
       </CardHeader>
-      <CardBody></CardBody>
+      <CardBody py={0}>
+        <VStack bg={"#E5EEFF"} py={{ base: 3, md: 4 }} px={6} h="full" w="full" borderRadius={"2xl"} align="start">
+          <Text fontSize="12px" fontWeight="400">
+            {"Total B3TR Balance"}
+          </Text>
+          <HStack>
+            <Image src={b3trLogo} boxSize={"30px"} alt="B3TR Icon" />
+            <Heading size={{ base: "2xl", md: "xl" }}>{compactFormatter.format(Number("2222"))}</Heading>
+          </HStack>
+        </VStack>
+      </CardBody>
+      <CardFooter>
+        <Button
+          mt={2}
+          //   isDisabled={totalRewards?.eq(0)}
+          //   isLoading={isRewardsLoading || isClaimRewardsLoading}
+          //   onClick={handleClaim}
+          colorScheme="primary"
+          borderRadius={"full"}
+          w={"full"}>
+          <Icon as={IoWalletOutline} mr={2} />
+          {t("Withdraw")}
+        </Button>
+      </CardFooter>
     </Card>
   )
 }
