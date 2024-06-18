f1382535888538eebc66edd7ccd175dfe8a3c0c7
ve-akanoce <eriknucib@live.it>
2024-06-18 20:56:43 +0100

    feat(ProposalSessionSection): hide quorum + votes when upcoming, in favour or alert

--- apps/frontend/src/components/ProposalSessionSection/ProposalSessionSection.tsx
+++ apps/frontend/src/components/ProposalSessionSection/ProposalSessionSection.tsx
@@ -1,25 +1,33 @@
-import { Card, CardBody, Heading, VStack } from "@chakra-ui/react"
+import { Alert, AlertTitle, Card, CardBody, Divider, Heading, VStack } from "@chakra-ui/react"
 import { useTranslation } from "react-i18next"
 import { ProposalQuorumStatus } from "./components/ProposalQuorumStatus"
 import { ProposalSessionVot3 } from "./components/ProposalSessionVot3"
-import { ProposalTimeline } from "./components/ProposalTimeline"
 import { UseQueryResult } from "@tanstack/react-query"
+import { useMemo } from "react"
+import { UilClock } from "@iconscout/react-unicons"
 
 type Props = {
   quorumQuery: UseQueryResult<string, Error>
   currentVotesQuery: UseQueryResult<string, Error>
   votesAtSnapshotQuery: UseQueryResult<string, Error>
   userVotesAtSnapshotQuery: UseQueryResult<string, Error>
-  isEnded: boolean
+  isEnded?: boolean
+  renderTimeline?: React.ReactNode
 }
+
 export const ProposalSessionSection = ({
   quorumQuery,
   currentVotesQuery,
   votesAtSnapshotQuery,
   userVotesAtSnapshotQuery,
-  isEnded,
+  renderTimeline,
+  isEnded = false,
 }: Props) => {
   const { t } = useTranslation()
+
+  const isUpcoming = useMemo(() => {
+    return !isEnded && !quorumQuery.isLoading && !quorumQuery.data
+  }, [quorumQuery, isEnded])
   return (
     <Card variant="baseWithBorder">
       <CardBody>
@@ -27,12 +35,25 @@ export const ProposalSessionSection = ({
           <Heading fontSize={"24px"} fontWeight={700}>
             {t("Session information")}
           </Heading>
-          <ProposalQuorumStatus currentVotesQuery={currentVotesQuery} quorumQuery={quorumQuery} isEnded={isEnded} />
-          <ProposalSessionVot3
-            userVotesAtSnapshotQuery={userVotesAtSnapshotQuery}
-            votesAtSnapshotQuery={votesAtSnapshotQuery}
-          />
-          <ProposalTimeline />
+          {isUpcoming ? (
+            <Alert status="error" borderRadius="16px" bg="#FFF3E5">
+              <UilClock size={"36px"} color="#F29B32" />
+              <AlertTitle color="#F29B32" ml={2} fontSize="14px">
+                {t("Quorum information will be available once the round starts.")}
+              </AlertTitle>
+            </Alert>
+          ) : (
+            <>
+              <ProposalQuorumStatus currentVotesQuery={currentVotesQuery} quorumQuery={quorumQuery} isEnded={isEnded} />
+              <Divider />
+              <ProposalSessionVot3
+                userVotesAtSnapshotQuery={userVotesAtSnapshotQuery}
+                votesAtSnapshotQuery={votesAtSnapshotQuery}
+              />
+            </>
+          )}
+
+          {renderTimeline}
         </VStack>
       </CardBody>
     </Card>
