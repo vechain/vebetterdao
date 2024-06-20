fab28b56b15903884acee04dafcddc3a97b8dabc
Dan Rusnac <rusnac14@gmail.com>
2024-06-20 02:12:53 +0200

    feat: added warning card for no vtho

---
+++ apps/frontend/src/components/Banners/LowOnVthoCard.tsx
@@ -0,0 +1,68 @@
+import { useAccountBalance } from "@/api"
+import { Button, Card, CardBody, Heading, Image, Text, VStack } from "@chakra-ui/react"
+import { useMemo } from "react"
+import BigNumber from "bignumber.js"
+import { useWallet } from "@vechain/dapp-kit-react"
+import { FiArrowUpRight } from "react-icons/fi"
+import { useTranslation } from "react-i18next"
+
+const minVtho = 5
+export const LowOnVthoCard = () => {
+  const { t } = useTranslation()
+  const { account } = useWallet()
+  const { data: balance, isLoading: balanceLoading } = useAccountBalance(account ?? undefined)
+
+  const redirectToGetVTHO = () => {
+    window.open("https://www.coinbase.com/en/how-to-buy/vethor-token", "_blank", "noopener noreferrer")
+  }
+
+  const isLowOnVtho = useMemo(() => {
+    return Number(balance?.energy.scaled) < minVtho
+  }, [balance])
+
+  const labels = useMemo(() => {
+    if (!balance) return
+    const balanceNumber = new BigNumber(balance.energy.scaled)
+    if (balanceNumber.isZero())
+      return {
+        heading: "Not enough VTHO",
+        body: "VTHO is used as gas in every transaction you complete in VeBetterDAO, like voting, swapping tokens, etc.",
+      }
+    return {
+      heading: "You're low on VTHO ",
+      body: "You're running low on VTHO, used as gas in every transaction you complete in VeBetterDAO, like voting, swapping tokens, etc.",
+    }
+  }, [balance])
+
+  if (!account || balanceLoading || !isLowOnVtho) return null
+
+  return (
+    <Card
+      borderColor={"#F29B32"}
+      backgroundColor={"#FFF3E5"}
+      variant={"baseWithBorder"}
+      boxShadow={"0px 0px 5px #F29B32"}>
+      <CardBody>
+        <VStack spacing={4} w="full" justifyContent={"start"} alignItems={"start"}>
+          <Image src="/images/alert.svg" boxSize={16} alt="alert-icon" />
+          <Heading size="md" fontWeight={"800"}>
+            {labels?.heading}
+          </Heading>
+
+          <Text size="sm">
+            {labels?.body} <b>{t("Get more VTHO to get the best experience in the platform.")}</b>
+          </Text>
+
+          <Button
+            mt={2}
+            variant={"primaryAction"}
+            borderRadius={"full"}
+            rightIcon={<FiArrowUpRight />}
+            onClick={redirectToGetVTHO}>
+            {t("Get more VTHO")}
+          </Button>
+        </VStack>
+      </CardBody>
+    </Card>
+  )
+}
--- apps/frontend/src/components/DashboardSideBar.tsx
+++ apps/frontend/src/components/DashboardSideBar.tsx
@@ -4,6 +4,7 @@ import { GmNFT } from "./GmNFT"
 import { VoterRewards } from "./VoterRewards"
 import { useWallet } from "@vechain/dapp-kit-react"
 import { WalletNotConnectedOverlay } from "./WalletNotConnectedOverlay"
+import { LowOnVthoCard } from "./Banners"
 
 export const DashboardSideBar = () => {
   const { account } = useWallet()
@@ -11,6 +12,7 @@ export const DashboardSideBar = () => {
     <VStack spacing={4} flex={2.5} position="relative">
       {!account && <WalletNotConnectedOverlay description="Connect your wallet to check your balance" />}
       <BalanceCard />
+      <LowOnVthoCard />
       <VoterRewards />
       <GmNFT />
     </VStack>
