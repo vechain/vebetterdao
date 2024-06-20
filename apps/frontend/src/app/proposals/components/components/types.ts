d3d12ed9469bfc9ed4ebaa0d8ba129ec2021f4d0
Piero Bassa <64158778+pierobassa@users.noreply.github.com>
2024-06-20 15:09:05 +0200

    feat: proposals filters zustand (#1041)
    * refactor: filters typesafety
    
    * feat: cached filter choice with zustand
    
    * refactor: clear filter usage
    
    * refactor: removed log
    
    * refactor: generiic option value type in case we add other filter options.

---
+++ apps/frontend/src/app/proposals/components/components/types.ts
@@ -0,0 +1,16 @@
+export enum ProposalFilter {
+  State = "State",
+  InThisRound = "In this round",
+  LookingForSupport = "Looking for support",
+  UpcomingVoting = "Upcoming voting",
+}
+
+export enum StateFilter {
+  Active = "Active",
+  Canceled = "Canceled",
+  Defeated = "Defeated",
+  Succeeded = "Succeeded",
+  Queued = "Queued",
+  Executed = "Executed",
+  DepositNotMet = "Deposit not met",
+}
