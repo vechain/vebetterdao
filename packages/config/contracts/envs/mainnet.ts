80ce215f8429b452e9b2c75e9263aadc31f5c34e
Dan Rusnac <rusnac14@gmail.com>
2024-06-20 03:40:50 +0200

    fix: mainnet config params

--- packages/config/contracts/envs/mainnet.ts
+++ packages/config/contracts/envs/mainnet.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "../defineConfig"
 export function createMainnetConfig() {
   return defineConfig({
     NEXT_PUBLIC_APP_ENV: "mainnet",
+
     MIGRATION_ADDRESS: "0x",
-    // @ts-ignore
-    MIGRATION_AMOUNT: "3750000",
-    B3TR_CAP: 1_000_000_000,
+    MIGRATION_AMOUNT: BigInt("3750000000000000000000000"), // 3.75 million B3TR tokens from pilot show
+
     B3TR_GOVERNOR_QUORUM_PERCENTAGE: 51, //Need 51% of voters to pass
     TIMELOCK_MIN_DELAY: 60480, // 1 week, time to wait before you can execute a queued proposa
     B3TR_GOVERNOR_DEPOSIT_THRESHOLD: 2, // Percentage of total B3TR supply needed to be deposited to create a proposal
@@ -18,21 +18,25 @@ export function createMainnetConfig() {
     B3TR_GOVERNOR_WHITELISTED_METHODS: {
       Treasury: ["transferB3TR"],
     },
+
+    INITIAL_X_ALLOCATION: BigInt("2000000000000000000000000"), // 2M B3TR
     EMISSIONS_CYCLE_DURATION: 60480, // blocks - 60480 blocks - 1 week.
     EMISSIONS_X_ALLOCATION_DECAY_PERCENTAGE: 4, // 4% decay every cycle
     EMISSIONS_VOTE_2_EARN_DECAY_PERCENTAGE: 20, // 20% decay every cycle
-    EMISSIONS_X_ALLOCATION_DECAY_PERIOD: 999999, // should never decay in pilot show //TODO
-    EMISSIONS_VOTE_2_EARN_ALLOCATION_DECAY_PERIOD: 999999, // should never decay in pilot show //TODO
-    EMISSIONS_TREASURY_PERCENTAGE: 8750, // 87.5% of the emissions go to the treasury during pilot show
-    EMISSIONS_MAX_VOTE_2_EARN_DECAY_PERCENTAGE: 80, //TODO
+    EMISSIONS_X_ALLOCATION_DECAY_PERIOD: 912, // every 12 cycles
+    EMISSIONS_VOTE_2_EARN_ALLOCATION_DECAY_PERIOD: 50, // every 50 cycles
+    EMISSIONS_TREASURY_PERCENTAGE: 2500, // 25% of the emissions go to the treasury
+    EMISSIONS_MAX_VOTE_2_EARN_DECAY_PERCENTAGE: 80,
+
     X_ALLOCATION_VOTING_QUORUM_PERCENTAGE: 40, // 40 -> Need 40% of total supply to succeed
     X_ALLOCATION_VOTING_VOTING_THRESHOLD: BigInt("1000000000000000000"), // 1 vote
     X_ALLOCATION_POOL_BASE_ALLOCATION_PERCENTAGE: 30, // % of tokens from each round that are equally distributed to all apps
     X_ALLOCATION_POOL_APP_SHARES_MAX_CAP: 20, // max % votes an app can receive in a round
-    CONTRACTS_ADMIN_ADDRESS: "0xE3D511ce183D3C53813BEA223Fe1E51BB9fF14a4", //TODO
+
+    CONTRACTS_ADMIN_ADDRESS: "0xE3D511ce183D3C53813BEA223Fe1E51BB9fF14a4",
     VOTE_2_EARN_POOL_ADDRESS: "0xE3D511ce183D3C53813BEA223Fe1E51BB9fF14a4", //temporarily pointing to CONTRACTS_ADMIN_ADDRESS, then updated in the deploy script to point to the voterReward contract
-    INITIAL_X_ALLOCATION: BigInt("66666666666666666666666"), // 1M/15 rounded down -> 1/15th of the total supply for pilot show
-    GM_NFT_BASE_URI: "ipfs://bafybeiahr3qobzujfkxi64o6wrigkmdagrvgfa566rqqth6jm5nq7vf24y/", // IPFS base URI for the GM NFT //TODO: do we need to update?
+
+    GM_NFT_BASE_URI: "ipfs://bafybeidngidiqpenmmvnnmxtnxar3stxtnltpknt5tg7jbgt4dkfhkcxha/", // IPFS base URI for the GM NFT
     /*
       Level => B3TR Required
       2 (Moon) => 10,000 B3TR
@@ -70,6 +74,7 @@ export function createMainnetConfig() {
       10 (Galaxy) => 2400% (25x)
     */
     VOTER_REWARDS_MULTIPLIER: [0, 10, 20, 50, 100, 150, 200, 400, 900, 2400],
+
     XAPP_BASE_URI: "ipfs://",
     /*
       Token transfer limits. These values are not final and are for testing purposes only.
