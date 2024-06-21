c0a975ef0fb6c7bb967e2937902baa170717f94f
Dan Rusnac <rusnac14@gmail.com>
2024-06-21 11:19:13 +0200

    fix: declare functions as external (#1050)

--- packages/contracts/contracts/mocks/MyERC1155.sol
+++ packages/contracts/contracts/mocks/MyERC1155.sol
@@ -8,15 +8,15 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MyERC1155 is ERC1155, Ownable {
   constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {}
 
-  function setURI(string memory newuri) public onlyOwner {
+  function setURI(string memory newuri) external onlyOwner {
     _setURI(newuri);
   }
 
-  function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyOwner {
+  function mint(address account, uint256 id, uint256 amount, bytes memory data) external onlyOwner {
     _mint(account, id, amount, data);
   }
 
-  function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) public onlyOwner {
+  function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) external onlyOwner {
     _mintBatch(to, ids, amounts, data);
   }
 }
