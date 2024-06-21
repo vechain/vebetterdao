fb86685e53ba5d564a2586ce98f5711605b7a767
Dan Rusnac <rusnac14@gmail.com>
2024-06-21 18:51:00 +0200

    refactor: apps section in home

--- apps/frontend/src/components/DashboardXApps.tsx
+++ apps/frontend/src/components/DashboardXApps.tsx
@@ -1,4 +1,5 @@
 import { XApp, useXAppMetadata, useXApps } from "@/api"
+import { useAppAllocations } from "@/api/contracts/governance/hooks/useAppAllocations"
 import { useIpfsImage } from "@/api/ipfs"
 import { notFoundImage } from "@/constants"
 import {
@@ -16,6 +17,7 @@ import {
   IconButton,
   Button,
 } from "@chakra-ui/react"
+import { getCompactFormatter } from "@repo/utils/FormattingUtils"
 import { useRouter } from "next/navigation"
 import { useCallback, useMemo } from "react"
 import { useTranslation } from "react-i18next"
@@ -24,25 +26,45 @@ import { FiArrowUpRight } from "react-icons/fi"
 type Props = {
   maxApps?: number
 }
-export const DashboardXApps = ({ maxApps = 8 }: Props) => {
+
+const compactFormatter = getCompactFormatter(0)
+
+export const DashboardXApps = ({ maxApps = 4 }: Props) => {
   const { t } = useTranslation()
   const { data: xApps } = useXApps()
+  const router = useRouter()
 
-  const slicedXApps = useMemo(() => xApps?.slice(0, maxApps), [xApps, maxApps])
+  const randomXApps = useMemo(() => {
+    if (!xApps) return []
+
+    return xApps.sort(() => 0.5 - Math.random())
+  }, [xApps])
+
+  const slicedXApps = useMemo(() => randomXApps?.slice(0, maxApps), [randomXApps, maxApps])
 
   if (!slicedXApps?.length) return null
 
   return (
     <Card>
       <CardHeader>
-        <HStack w="full" justify={"space-between"}>
-          <Heading size="md">{t("Explore Apps")}</Heading>
-          {slicedXApps && slicedXApps.length > maxApps && (
-            <Button variant="link" colorScheme="blue" rightIcon={<FiArrowUpRight />}>
-              {t("See all")}
-            </Button>
-          )}
-        </HStack>
+        <VStack w="full" justify={"flex-start"} align={"start"}>
+          <HStack w="full" justify={"space-between"}>
+            <Heading size="md">{t("Explore Apps")}</Heading>
+            {xApps && xApps.length > maxApps && (
+              <Button
+                variant="link"
+                colorScheme="primary"
+                rightIcon={<FiArrowUpRight />}
+                onClick={() => router.push("/apps")}>
+                {t("See all")}
+              </Button>
+            )}
+          </HStack>
+
+          <Text fontSize={"md"} color={"gray.500"}>
+            {t("Use our apps to complete sustainable actions and earn token rewards.")}
+          </Text>
+        </VStack>
       </CardHeader>
       <CardBody>
         <Grid templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)"]} gap={6} w="full">
@@ -54,6 +76,7 @@ export const DashboardXApps = ({ maxApps = 8 }: Props) => {
 }
 
 const DashboardXAppCard = ({ xApp }: { xApp: XApp }) => {
+  const { t } = useTranslation()
   const {
     data: appMetadata,
     isLoading: appMetadataLoading,
@@ -62,29 +85,70 @@ const DashboardXAppCard = ({ xApp }: { xApp: XApp }) => {
   } = useXAppMetadata(xApp.id)
   const router = useRouter()
   const { data: logo, isLoading: isLogoLoading } = useIpfsImage(appMetadata?.logo)
+  const { totalAllocationReceived, lastRoundAllocationReceived } = useAppAllocations(xApp.id)
 
   const buttonIconColor = useColorModeValue("primary.500", "white")
-
+  const nonActiveBackgroundColor = useColorModeValue("rgba(166, 217, 110, 0.12)", "rgba(166, 217, 110, 0.12)")
   const navigateToAppDetail = useCallback(() => {
     router.push(`/apps/${xApp.id}`)
   }, [router, xApp.id])
   return (
-    <Card variant={"baseWithBorder"}>
+    <Card
+      variant={"baseWithBorder"}
+      onClick={navigateToAppDetail}
+      _hover={{
+        bg: nonActiveBackgroundColor,
+
+        cursor: "pointer",
+        transition: "all 0.2s ease-in-out",
+      }}>
       <CardBody>
-        <VStack alignItems={"start"} justify={"flex-start"}>
-          <HStack spacing={1} justifyContent={"space-between"} w={"full"}>
+        <VStack alignItems={"start"} justify={"flex-start"} spacing={6}>
+          <HStack spacing={3} justifyContent={"start"} w={"full"} alignItems={"start"}>
             <Skeleton isLoaded={!isLogoLoading} alignContent={"start"}>
-              <Image
-                src={logo?.image ?? notFoundImage}
-                alt={"logo"}
-                boxSize={10}
-                borderRadius="9px"
-                _hover={{
-                  cursor: "pointer",
-                }}
-                onClick={navigateToAppDetail}
-              />
+              <Image src={logo?.image ?? notFoundImage} alt={"logo"} maxW={"40px"} borderRadius="9px" />
             </Skeleton>
+
+            <VStack spacing={1} align="flex-start" w={"fit-content"}>
+              <Skeleton isLoaded={!appMetadataLoading} justifyContent={"end"}>
+                <Heading size={"sm"}>{appMetadata?.name ?? appMetadataError?.message ?? "Error loading name"}</Heading>
+              </Skeleton>
+              <Skeleton isLoaded={!appMetadataLoading} justifyContent={"end"}>
+                <Text fontSize={"sm"} color={"gray.500"}>
+                  {appMetadata?.description.slice(0, 25) + "..." ??
+                    appMetadataError?.message ??
+                    "Error loading description"}
+                </Text>
+              </Skeleton>
+            </VStack>
+          </HStack>
+
+          <HStack spacing={3} justifyContent={"space-between"} w={"full"} alignItems={"start"}>
+            <HStack spacing={6} justifyContent={"start"} w={"full"} alignItems={"start"}>
+              <VStack alignItems={"stretch"} gap={0}>
+                <HStack>
+                  <Image h="20px" w="20px" src="/images/b3tr-token.png" alt="vot3-token" />
+                  <Text fontSize={"lg"} fontWeight={600}>
+                    {compactFormatter.format(lastRoundAllocationReceived)}
+                  </Text>
+                </HStack>
+                <Text color="#6A6A6A" fontSize="xs">
+                  {t("Last allocation")}
+                </Text>
+              </VStack>
+
+              <VStack alignItems={"stretch"} gap={0}>
+                <HStack>
+                  <Image h="20px" w="20px" src="/images/b3tr-token.png" alt="vot3-token" />
+                  <Text fontSize={"lg"} fontWeight={600}>
+                    {compactFormatter.format(totalAllocationReceived)}
+                  </Text>
+                </HStack>
+                <Text color="#6A6A6A" fontSize="xs">
+                  {t("Accumulated")}
+                </Text>
+              </VStack>
+            </HStack>
 
             <Skeleton isLoaded={!appMetadataLoading} justifyContent={"end"}>
               <IconButton
@@ -93,37 +157,15 @@ const DashboardXAppCard = ({ xApp }: { xApp: XApp }) => {
                 aria-label="Go to App"
                 fontSize="20px"
                 disabled={isAppMetadataError}
-                onClick={() => window.open(appMetadata?.external_url, "_blank", "noopener noreferrer")}
+                onClick={e => {
+                  e.stopPropagation()
+                  window.open(appMetadata?.external_url, "_blank", "noopener noreferrer")
+                }}
                 color={buttonIconColor}
                 icon={<FiArrowUpRight />}
               />
             </Skeleton>
           </HStack>
-
-          <VStack spacing={1} align="flex-start">
-            <Skeleton isLoaded={!appMetadataLoading}>
-              <Text
-                fontWeight={"600"}
-                size={"xs"}
-                _hover={{
-                  cursor: "pointer",
-                }}
-                onClick={navigateToAppDetail}>
-                {appMetadata?.name ?? appMetadataError?.message ?? "Error loading name"}
-              </Text>
-            </Skeleton>
-            <Skeleton isLoaded={!appMetadataLoading}>
-              <Text
-                fontSize={"sm"}
-                color={"gray.500"}
-                _hover={{
-                  cursor: "pointer",
-                }}
-                onClick={navigateToAppDetail}>
-                {appMetadata?.description ?? appMetadataError?.message ?? "Error loading description"}
-              </Text>
-            </Skeleton>
-          </VStack>
         </VStack>
       </CardBody>
     </Card>
