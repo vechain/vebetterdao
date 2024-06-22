95ed7bf4307161ccb9ba4b3009fd3f6396cf6f9f
ve-akanoce <eriknucib@live.it>
2024-06-22 20:55:58 +0100

    feat(CastAllocationVote): working reviewAndConfirm page, to be polished

--- apps/frontend/src/app/rounds/[roundId]/vote/confirm/components/ConfirmCastAllocationVotePageContent.tsx
+++ apps/frontend/src/app/rounds/[roundId]/vote/confirm/components/ConfirmCastAllocationVotePageContent.tsx
@@ -1,40 +1,88 @@
+import { useAllocationsRound, useAllocationsRoundState, useGetVotesOnBlock, useHasVotedInRound } from "@/api"
+import { Button, Card, CardBody, HStack, Heading, Text, VStack, useDisclosure } from "@chakra-ui/react"
+import { useCallback, useMemo } from "react"
+import { useWallet } from "@vechain/dapp-kit-react"
+import { useRouter } from "next/navigation"
+import { useTranslation } from "react-i18next"
+import { useCastAllocationFormStore } from "@/store"
+import { AppVotesBreakdown } from "@/app/rounds/components/AppVotesBreakdown/AppVotesBreakdown"
+import { TransactionModal } from "@/components"
+import { useCastAllocationVotes, CastAllocationVotesProps } from "@/hooks"
+import { scaledDivision } from "@/utils/MathUtils"
+
 type Props = {
   roundId: string
 }
+export const ConfirmCastAllocationVotePageContent = ({ roundId }: Props) => {
+  const { t } = useTranslation()
+  const { account } = useWallet()
 
-export const ConfirmCastAllocationVotePageContent = ({ roundId }: Readonly<Props>) => {
-  // const { isOpen, onClose, onOpen } = useDisclosure()
+  const router = useRouter()
 
-  //   const castAllocationVotes = useCastAllocationVotes({ roundId })
+  const { data: votes, setData: onVotesChange } = useCastAllocationFormStore()
+  const { data: state, isLoading: isStateLoading } = useAllocationsRoundState(roundId)
 
-  // const handleClose = useCallback(() => {
-  //   castAllocationVotes.resetStatus()
-  //   onClose()
-  // }, [castAllocationVotes, onClose])
-  // const onSubmit = useCallback(
-  //     (data: CastAllocationVoteFormData) => {
-  //       if (!votesAtSnapshot) throw new Error("Votes at snapshot not found")
-  //       const appVotesPercentagesToValue: CastAllocationVotesProps = data.votes.map(vote => {
-  //         const rawValue = scaledDivision(Number(vote.rawValue) * Number(votesAtSnapshot), 100)
-  //         return {
-  //           appId: vote.appId,
-  //           votes: rawValue,
-  //         }
-  //       })
+  const { data: roundInfo, isLoading: roundInfoLoading } = useAllocationsRound(roundId)
+  const { data: votesAtSnapshot, isLoading: votesAtSnapshotLoading } = useGetVotesOnBlock(
+    Number(roundInfo.voteStart),
+    account ?? undefined,
+  )
 
-  //       onOpen()
-  //       castAllocationVotes.sendTransaction(appVotesPercentagesToValue)
-  //     },
-  //     [castAllocationVotes, onOpen, votesAtSnapshot],
-  //   )
+  const hasNoVotes = !votesAtSnapshot || votesAtSnapshot === "0"
 
-  //   const onTryAgain = useCallback(() => {
-  //     castAllocationVotes.resetStatus()
-  //     handleSubmit(onSubmit)()
-  //   }, [castAllocationVotes, handleSubmit, onSubmit])
+  const { data: hasVoted, isLoading: hasVotedLoading } = useHasVotedInRound(roundId, account ?? undefined)
+  const isVotingConcluded = roundInfo?.voteEndTimestamp?.isBefore() && [1, 2].includes(state ?? 0)
+
+  const { isOpen, onClose, onOpen } = useDisclosure()
+
+  const onSuccess = useCallback(() => router.push(`/rounds/${roundId}`), [router, roundId])
+
+  const castAllocationVotes = useCastAllocationVotes({ roundId, onSuccess })
+
+  const handleClose = useCallback(() => {
+    castAllocationVotes.resetStatus()
+    onClose()
+  }, [castAllocationVotes, onClose])
+
+  const onContinue = useCallback(() => {
+    if (!votesAtSnapshot) throw new Error("Votes at snapshot not found")
+    const appVotesPercentagesToValue: CastAllocationVotesProps = votes.map(vote => {
+      const rawValue = scaledDivision(Number(vote.rawValue) * Number(votesAtSnapshot), 100)
+      return {
+        appId: vote.appId,
+        votes: rawValue,
+      }
+    })
+
+    onOpen()
+    castAllocationVotes.sendTransaction(appVotesPercentagesToValue)
+  }, [castAllocationVotes, onOpen, votesAtSnapshot, votes])
+
+  const onTryAgain = useCallback(() => {
+    castAllocationVotes.resetStatus()
+    onContinue()
+  }, [castAllocationVotes, onContinue])
+
+  const goBack = useCallback(() => {
+    router.back()
+  }, [router])
+
+  const shouldSeeThePage = useMemo(() => {
+    return !hasVoted && !isVotingConcluded
+  }, [isVotingConcluded, hasVoted])
+
+  //redirect to round page if user already voted or voting is concluded
+  //   useLayoutEffect(() => {
+  //     if (!shouldSeeThePage) {
+  //       router.push(`/rounds/${roundId}`)
+  //     }
+  //   }, [shouldSeeThePage, roundId, router])
+
+  //   if (!shouldSeeThePage) return null
+
   return (
     <>
-      {/* <TransactionModal
+      <TransactionModal
         isOpen={isOpen}
         onClose={handleClose}
         status={castAllocationVotes.error ? "error" : castAllocationVotes.status}
@@ -48,7 +96,46 @@ export const ConfirmCastAllocationVotePageContent = ({ roundId }: Readonly<Props
         showTryAgainButton
         showExplorerButton
         txId={castAllocationVotes.txReceipt?.meta.txID ?? castAllocationVotes.sendTransactionTx?.txid}
-      /> */}
+      />
+
+      <Card w="full">
+        <CardBody>
+          <VStack w="full" spacing={8} align={"flex-start"}>
+            <Heading fontSize={"36px"} fontWeight={700}>
+              {t("Review and confirm")}
+            </Heading>
+            <Text fontSize={"16px"} fontWeight={400} color="#6A6A6A">
+              {t(
+                "Make sure that the apps you selected and the distribution percentages are right. If something’s wrong, you can go back and modify it.",
+              )}
+            </Text>
+            <AppVotesBreakdown votes={votes} />
+
+            <HStack w="full" spacing={4} justify={"space-between"}>
+              <HStack alignSelf={"flex-end"} justify={"flex-end"} spacing={4} flex={1}>
+                <Button
+                  data-testid="go-back"
+                  rounded="full"
+                  variant={"primarySubtle"}
+                  colorScheme="primary"
+                  size="lg"
+                  onClick={goBack}>
+                  {t("Go back")}
+                </Button>
+                <Button
+                  form="cast-allocation-vote-form"
+                  data-testid="continue"
+                  rounded="full"
+                  colorScheme="primary"
+                  size="lg"
+                  onClick={onContinue}>
+                  {t("Continue")}
+                </Button>
+              </HStack>
+            </HStack>
+          </VStack>
+        </CardBody>
+      </Card>
     </>
   )
 }
