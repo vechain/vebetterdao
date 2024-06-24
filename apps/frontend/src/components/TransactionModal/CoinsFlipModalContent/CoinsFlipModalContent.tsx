1d29cdd68458c93a1cae90f314d36852c9057447
ve-akanoce <eriknucib@live.it>
2024-06-24 23:29:04 +0200

    fix: polish apps icon in navbar

--- apps/frontend/src/components/TransactionModal/CoinsFlipModalContent/CoinsFlipModalContent.tsx
+++ apps/frontend/src/components/TransactionModal/CoinsFlipModalContent/CoinsFlipModalContent.tsx
@@ -7,8 +7,8 @@ const MotionImage = motion(Image)
 export const CoinsFlipModalContent = () => {
   const { t } = useTranslation()
   return (
-    <VStack alignItems={"center"}>
-      <MotionImage {...coinFlipAnimation} src="/images/b3tr-token-3d.png" maxH="250px" />
+    <VStack alignItems={"center"} p={6}>
+      <MotionImage {...coinFlipAnimation} src="/images/b3tr-token-3d.png" boxSize={200} />
       <Text fontWeight={400} lineHeight="22px" fontSize={{ base: "16px", md: "16px" }} align={"center"}>
         {t("Please confirm the transaction in your wallet")}
       </Text>
