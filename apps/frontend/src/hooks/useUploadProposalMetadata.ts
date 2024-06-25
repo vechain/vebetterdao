e88ae6cd7108c97f72d8b5c8dda8d83c18a58de8
roisindowling <roisin.dowling@vechain.org>
2024-06-25 11:58:04 +0100

    Update upload proposal metadata to use new ipfs service

--- apps/frontend/src/hooks/useUploadProposalMetadata.ts
+++ apps/frontend/src/hooks/useUploadProposalMetadata.ts
@@ -1,33 +1,35 @@
 import { useCallback, useState } from "react"
-import { NFTStorageUtils } from "@repo/utils"
 import { ProposalMetadata } from "@/api"
+import { uploadBlobToIPFS } from "@/utils"
 
 /**
- *  Uploads proposal metadata to IPFS
- * @returns metadataUploading, metadataUploadError, onMetadataUpload
+ * Uploads proposal metadata to IPFS.
+ * @returns { metadataUploading, metadataUploadError, onMetadataUpload }
  */
 export const useUploadProposalMetadata = () => {
-  const [metadataUploading, setMetadataUploading] = useState(false)
-  const [metadataUploadError, setMetadataUploadError] = useState<Error>()
+  const [metadataUploading, setMetadataUploading] = useState(false);
+  const [metadataUploadError, setMetadataUploadError] = useState<Error>();
 
   const onMetadataUpload = useCallback(async (data: ProposalMetadata) => {
     try {
-      setMetadataUploading(true)
+      setMetadataUploading(true);
 
-      const blobData = new Blob([JSON.stringify(data)], {
-        type: "application/json",
-      })
-      const metadataUri = await NFTStorageUtils.nftStorageClient.storeBlob(blobData)
-      setMetadataUploading(false)
-      return metadataUri
+      // Create a Blob from the proposal metadata
+      const metadataBlob = new Blob([JSON.stringify(data)], { type: "application/json" });
+
+      // Upload the metadata Blob to IPFS
+      const metadataUri = await uploadBlobToIPFS(metadataBlob, "metadata.json");
+
+      setMetadataUploading(false);
+      return metadataUri;
     } catch (error) {
-      console.error("Error uploading metadata", error)
-      setMetadataUploadError(error as Error)
-      return undefined
+      console.error("Error uploading metadata", error);
+      setMetadataUploadError(error as Error);
+      return undefined;
     } finally {
-      setMetadataUploading(false)
+      setMetadataUploading(false);
     }
-  }, [])
+  }, []);
 
-  return { onMetadataUpload, metadataUploading, metadataUploadError }
-}
+  return { onMetadataUpload, metadataUploading, metadataUploadError };
+};
