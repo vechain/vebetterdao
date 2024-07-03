6a564034765d024d4a8d176717db8bedb9ad1c4e
Piero Bassa <pierobassa222@gmail.com>
2024-07-03 14:03:58 -0700

    fix: mainnet get x apps

--- packages/lambda/startRound/helpers/xApps/getIdsOfUnclaimed.ts
+++ packages/lambda/startRound/helpers/xApps/getIdsOfUnclaimed.ts
@@ -8,24 +8,25 @@ import { XAllocationPool__factory as XAllocationPool } from "@repo/contracts"
 /**
  * Retrieves the IDs of the xApps that have not yet claimed their rewards for the specified round.
  * @param thor - The ThorClient instance.
- * @param xapps - The xApps to check for unclaimed rewards.
+ * @param xapps - The xApps ids to check for unclaimed rewards.
  * @param roundId - The round ID to check for unclaimed rewards.
+ *
  * @returns an array of xApp IDs that have not yet claimed their rewards.
  */
-export const getIdsOfUnclaimed = async (thor: ThorClient, xapps: XApp[], roundId: string): Promise<string[]> => {
+export const getIdsOfUnclaimed = async (thor: ThorClient, xapps: string[], roundId: string): Promise<string[]> => {
   const unclaimed: string[] = []
 
-  for (const xapp of xapps) {
+  for (const xappId of xapps) {
     const res = await thor.contracts.executeContractCall(
       testnetConfig.xAllocationPoolContractAddress,
       XAllocationPool.createInterface().getFunction("claimed") as FunctionFragment,
-      [roundId, xapp.id],
+      [roundId, xappId],
     )
 
     if (res.vmError) return Promise.reject(new Error(res.vmError))
 
     if (!res[0]) {
-      unclaimed.push(xapp.id)
+      unclaimed.push(xappId)
     }
   }
 
--- packages/lambda/startRound/helpers/xApps/getXApps.ts
+++ packages/lambda/startRound/helpers/xApps/getXApps.ts
@@ -2,29 +2,25 @@ import { ThorClient } from "@vechain/sdk-network"
 import mainnetConfig from "@repo/config/mainnet"
 import { FunctionFragment, coder } from "@vechain/sdk-core"
 import { XApp } from "../types"
-import { X2EarnApps__factory as X2EarnApps } from "@repo/contracts"
+import { XAllocationVoting__factory as XAllocationVoting } from "@repo/contracts"
 
 /**
  * Retrieves the xApps for the specified round.
  * @param thor - The ThorClient instance.
  * @param roundId - The round ID to retrieve xApps for.
- * @returns an array of xApps for the specified round.
+ *
+ * @returns an array of xApps Ids for the specified round.
  */
-export const getRoundXApps = async (thor: ThorClient, roundId: string): Promise<XApp[]> => {
+export const getRoundXApps = async (thor: ThorClient, roundId: string): Promise<string[]> => {
   const res = await thor.contracts.executeContractCall(
-    mainnetConfig.x2EarnAppsContractAddress,
-    X2EarnApps.createInterface().getFunction("apps") as FunctionFragment,
-    [roundId],
+    mainnetConfig.xAllocationVotingContractAddress,
+    XAllocationVoting.createInterface().getFunction("getAppIdsOfRound") as FunctionFragment,
+    [Number(roundId)],
   )
 
   if (res.vmError) return Promise.reject(new Error(res.vmError))
 
   const apps = res[0]
 
-  return apps.map((app: any) => ({
-    id: app[0],
-    teamWalletAddress: app[1],
-    name: app[2],
-    createdAt: app[3],
-  }))
+  return apps
 }
