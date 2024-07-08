d212668be19bf0c5e636b380e67d75ddee5639bb
Davide Carpini <carpini.davide@gmail.com>
2024-07-08 12:21:52 +0200

    fix: add app grant button

--- apps/frontend/src/app/apps/components/AppsPageContent.tsx
+++ apps/frontend/src/app/apps/components/AppsPageContent.tsx
@@ -1,12 +1,17 @@
 import { useXApps } from "@/api"
-import { HStack, Heading, VStack, Grid, Spinner } from "@chakra-ui/react"
+import { HStack, Heading, VStack, Grid, Spinner, Button } from "@chakra-ui/react"
 import { AppCard } from "./AppCard"
 import { AddNewAppCard } from "./AddNewAppCard"
 import { useTranslation } from "react-i18next"
+import { FaPlus } from "react-icons/fa6"
 
 export const AppsPageContent = () => {
   const { data, isLoading } = useXApps()
   const { t } = useTranslation()
+
+  const openGrantPage = () => {
+    window.open("https://vechain.org/grants/", "_blank", "noopener noreferrer")
+  }
 
   if (isLoading)
     return (
@@ -22,6 +27,9 @@ export const AppsPageContent = () => {
     <VStack spacing={8} data-testid="apps-page">
       <HStack w="full" justify={"space-between"}>
         <Heading size="md">{t("Explore Apps")}</Heading>
+        <Button variant="primaryAction" onClick={openGrantPage} leftIcon={<FaPlus />}>
+          {t("Apply now")}
+        </Button>
       </HStack>
       <Grid templateColumns={["repeat(1, 1fr)", "repeat(3, 1fr)"]} gap={6} w="full">
         {data?.map(xApp => <AppCard key={xApp.id} xApp={xApp} />)}
