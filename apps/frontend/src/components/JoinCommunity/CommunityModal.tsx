4cb8f241ecb7b5a535c7752d5658d307a759e6c7
Piero Bassa <pierobassa222@gmail.com>
2024-07-10 10:32:39 +0200

    feat: stack animation on socials

--- apps/frontend/src/components/JoinCommunity/CommunityModal.tsx
+++ apps/frontend/src/components/JoinCommunity/CommunityModal.tsx
@@ -3,11 +3,29 @@ import { CustomModalContent } from "../CustomModalContent"
 import { useCallback } from "react"
 import { DiscordButton, FreshDeskButton, TelegramButton } from "../Footer"
 import { useTranslation } from "react-i18next"
+import { motion } from "framer-motion"
 
 type Props = {
   isOpen: boolean
   onClose: () => void
 }
+
+const variants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+}
+
+const itemVariants = {
+  hidden: { y: 20, opacity: 0 },
+  visible: { y: 0, opacity: 1 },
+}
+
+const MotionVStack = motion(VStack)
 
 export const CommunityModal = ({ isOpen, onClose }: Props) => {
   const { t } = useTranslation()
@@ -19,11 +37,26 @@ export const CommunityModal = ({ isOpen, onClose }: Props) => {
         <Text fontSize={20} fontWeight={700}>
           {t("Join Our Community!")}
         </Text>
-        <VStack align={"flex-start"} maxW={"590px"} minW={{ base: "90vw", md: "350px" }} px={4} spacing={2} mt={8}>
-          <DiscordButton isFullWidth />
-          <TelegramButton isFullWidth />
-          <FreshDeskButton isFullWidth />
-        </VStack>
+        <MotionVStack
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+          align={"flex-start"}
+          maxW={"590px"}
+          minW={{ base: "90vw", md: "350px" }}
+          px={4}
+          spacing={2}
+          mt={6}>
+          <motion.div variants={itemVariants}>
+            <DiscordButton isFullWidth />
+          </motion.div>
+          <motion.div variants={itemVariants}>
+            <TelegramButton isFullWidth />
+          </motion.div>
+          <motion.div variants={itemVariants}>
+            <FreshDeskButton isFullWidth />
+          </motion.div>
+        </MotionVStack>
       </CardBody>
     )
   }, [t])
