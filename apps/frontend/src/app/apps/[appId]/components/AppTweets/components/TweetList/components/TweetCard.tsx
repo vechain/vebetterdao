348a30569f32b3eb90ee307b6d073782ee0bfc09
Davide Carpini <carpini.davide@gmail.com>
2024-07-11 15:14:12 +0200

    fix: tweets sorting

--- apps/frontend/src/app/apps/[appId]/components/AppTweets/components/TweetList/components/TweetCard.tsx
+++ apps/frontend/src/app/apps/[appId]/components/AppTweets/components/TweetList/components/TweetCard.tsx
@@ -1,54 +1,117 @@
-import { IconButton, VStack } from "@chakra-ui/react"
+import { HStack, VStack } from "@chakra-ui/react"
 import { UseQueryResult } from "@tanstack/react-query"
 import { EmbeddedTweet, TweetSkeleton } from "react-tweet"
 import { Tweet } from "react-tweet/api"
-import { motion } from "framer-motion"
-import { UilTrash } from "@iconscout/react-unicons"
+import { UilArrowDown, UilArrowUp, UilTopArrowToTop, UilTrash } from "@iconscout/react-unicons"
+import { Dispatch, ReactNode, SetStateAction, useCallback } from "react"
 
 type Props = {
   tweetQuery: UseQueryResult<Tweet, Error>
   editMode: boolean
-  removeTweet: (tweet: string) => void
+  index: number
+  tweets: string[]
+  setTweets: Dispatch<SetStateAction<string[]>>
 }
 
-export const TweetCard = ({ tweetQuery, editMode, removeTweet }: Props) => {
+export const TweetCard = ({ tweetQuery, editMode, index, tweets, setTweets }: Props) => {
   const { data: tweet, isLoading: isTweetLoading, error: tweetError } = tweetQuery
 
-  const animation = editMode
-    ? {
-        transition: {
-          duration: 0.2,
-          repeat: Infinity,
-          delay: Math.random() * 0.2,
-        },
-        animate: {
-          rotate: [0, 0.1, -0.1, 0],
-        },
-      }
-    : {}
+  const removeTweet = useCallback(
+    (tweetId: string) => {
+      setTweets(prev => prev.filter(tweet => tweet !== tweetId))
+    },
+    [setTweets],
+  )
+
+  const moveTweetUp = useCallback(
+    (tweetId: string) => {
+      const tweetIndex = tweets.indexOf(tweetId)
+      if (tweetIndex === -1) return
+      const newTweets = [...tweets]
+      const [removedTweet] = newTweets.splice(tweetIndex, 1)
+      newTweets.splice(tweetIndex - 1, 0, removedTweet as string)
+      setTweets(newTweets)
+    },
+    [setTweets, tweets],
+  )
+
+  const moveTweetDown = useCallback(
+    (tweetId: string) => {
+      const tweetIndex = tweets.indexOf(tweetId)
+      if (tweetIndex === -1) return
+      const newTweets = [...tweets]
+      const [removedTweet] = newTweets.splice(tweetIndex, 1)
+      newTweets.splice(tweetIndex + 1, 0, removedTweet as string)
+      setTweets(newTweets)
+    },
+    [setTweets, tweets],
+  )
+
+  const moveTweetOnTop = useCallback(
+    (tweetId: string) => {
+      const newTweets = [tweetId, ...tweets.filter(tweet => tweet !== tweetId)]
+      setTweets(newTweets)
+    },
+    [setTweets, tweets],
+  )
+
+  const EditContainer = useCallback(
+    ({ children }: { children: ReactNode }) => {
+      return editMode ? (
+        <HStack bg="#E0E9FE" rounded="10px" p={4} align={"stretch"}>
+          <VStack py={"16px"} px={"8px"} gap={4}>
+            {index !== 0 && (
+              <>
+                {index !== 1 && (
+                  <UilTopArrowToTop
+                    cursor="pointer"
+                    size="24px"
+                    color="#004CFC"
+                    onClick={() => tweet?.id_str && moveTweetOnTop(tweet.id_str)}
+                  />
+                )}
+                <UilArrowUp
+                  cursor="pointer"
+                  size="24px"
+                  color="#004CFC"
+                  onClick={() => tweet?.id_str && moveTweetUp(tweet.id_str)}
+                />
+              </>
+            )}
+            {index !== tweets.length - 1 && (
+              <UilArrowDown
+                cursor="pointer"
+                size="24px"
+                color="#004CFC"
+                onClick={() => tweet?.id_str && moveTweetDown(tweet.id_str)}
+              />
+            )}
+            <UilTrash
+              cursor="pointer"
+              size="24px"
+              color="#004CFC"
+              onClick={() => tweet?.id_str && removeTweet(tweet.id_str)}
+            />
+          </VStack>
+          <VStack flex={1} align={"stretch"}>
+            {children}
+          </VStack>
+        </HStack>
+      ) : (
+        children
+      )
+    },
+    [editMode, index, moveTweetDown, moveTweetOnTop, moveTweetUp, removeTweet, tweet?.id_str, tweets.length],
+  )
+
   if (tweetError) {
     return null
   }
   return (
-    <motion.div {...animation}>
-      <VStack align="stretch">
+    <EditContainer>
+      <VStack align="stretch" mt={"-8px"}>
         {tweet && !isTweetLoading ? <EmbeddedTweet key={tweet.id_str} tweet={tweet} /> : <TweetSkeleton />}
-        {!isTweetLoading && editMode && (
-          <IconButton
-            rounded="full"
-            color="#D23F63"
-            bgColor="#FCEEF1"
-            _hover={{ bgColor: "#FCEEF1DD" }}
-            aria-label="Delete screenshot"
-            icon={<UilTrash size="24px" />}
-            position="absolute"
-            top={"-10px"}
-            right={"-10px"}
-            colorScheme="red"
-            onClick={() => tweet?.id_str && removeTweet(tweet.id_str)}
-          />
-        )}
       </VStack>
-    </motion.div>
+    </EditContainer>
   )
 }
