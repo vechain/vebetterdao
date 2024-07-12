80c2ccce023b6a6ba7a58c45f0f72e29633fdbd1
Dan Rusnac <rusnac14@gmail.com>
2024-07-12 18:16:43 +0200

    style: add stickiness back

--- apps/frontend/src/components/DashboardSideBar.tsx
+++ apps/frontend/src/components/DashboardSideBar.tsx
@@ -9,7 +9,7 @@ import { ManagedAppsCard } from "./ManagedAppsCard"
 export const DashboardSideBar = () => {
   const { account } = useWallet()
   return (
-    <VStack spacing={4} flex={2.5} position="relative" left={0}>
+    <VStack spacing={4} flex={2.5} position="relative" pos={"sticky"} top={24} left={0}>
       {!account && <WalletNotConnectedOverlay description="Connect your wallet to check your balance" />}
       <BalanceCard />
       <VoterRewards />
