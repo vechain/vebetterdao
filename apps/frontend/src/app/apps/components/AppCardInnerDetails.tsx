89e614802a5c8e63f2ab45c44dddd61c46f90750
ve-akanoce <eriknucib@live.it>
2024-07-16 19:16:17 -0700

    fix: type mismatch

--- apps/frontend/src/app/apps/components/AppCardInnerDetails.tsx
+++ apps/frontend/src/app/apps/components/AppCardInnerDetails.tsx
@@ -23,9 +23,7 @@ export const AppCardInnerDetails = ({ xApp }: Props) => {
 
   // Generate roundIds from 1 to currentRoundId or previous round if current round is not active
   const roundIds = useMemo(() => {
-    return Array.from({ length: Number(currentRoundId) - (currentRound.state === 0 ? 1 : 0) }, (_, i) =>
-      (i + 1).toString(),
-    )
+    return Array.from({ length: Number(currentRoundId) - (currentRound.state === 0 ? 1 : 0) }, (_, i) => i + 1)
   }, [currentRoundId, currentRound])
 
   const previousRoundId = useMemo(() => {
