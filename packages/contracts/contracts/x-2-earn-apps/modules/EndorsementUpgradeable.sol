a8f0a1dfbbe5269558e1c9ae8ec8da6886754d3d
roisindowling <roisin.dowling@vechain.org>
2024-07-22 16:57:53 +0100

    add check endorsement fucntion

--- packages/contracts/contracts/x-2-earn-apps/modules/EndorsementUpgradeable.sol
+++ packages/contracts/contracts/x-2-earn-apps/modules/EndorsementUpgradeable.sol
@@ -33,7 +33,12 @@ abstract contract EndorsementUpgradeable is Initializable, X2EarnAppsUpgradeable
   struct EndorsementStorage {
     mapping(bytes32 appId => X2EarnAppsDataTypes.App apps) appsPendingEndorsements; // The pending endorsements for each app
     mapping(bytes32 appId => address[] endorsers) appEndorsers; // Mapping to the endorsers of an app
+    mapping(uint8 nodeLevel => uint256 score) nodeEnodorsmentScore; // The endorsement score for each node
+    mapping(address endorsers => uint256 nodeId) nodeEndorsers; // The endorsers and their node ID
+    mapping(bytes32 appId => uint256 periodsElasapsed) appGracePeriod; // The grace period for the endorsement
+    mapping(uint256 => bool) endorsers; // The nodes thatare being used for endorsement
     uint256 gracePeriod; // The grace period for the endorsement in cycles
+    ITokenAuction tokenAuction; // The token auction contract
   }
 
   // keccak256(abi.encode(uint256(keccak256("b3tr.storage.X2EarnApps.Endorsement")) - 1)) & ~bytes32(uint256(0xff))
@@ -56,6 +61,50 @@ abstract contract EndorsementUpgradeable is Initializable, X2EarnAppsUpgradeable
   function __Endorsement_init_unchained(uint256 _gracePeriod) internal onlyInitializing {
     EndorsementStorage storage $ = _getEndorsementStorage();
     $.gracePeriod = _gracePeriod;
+  }
+
+  // ---------- Public ---------- //
+  /**
+   * @notice Checks endorsements for a given app and updates its voting eligibility based on the endorsements' scores.
+   * @dev This function is intended to be called by a cron job prior to the start of each voting round.
+   * If the app has less than 100 points, the grace period is increased by 1.
+   * If the grace period elapsed by the app is greater than the threshold grace period, the app is marked as not eligible for voting.
+   * If an endorser has lost its node status (level 0), it is removed from the endorsers list.
+   * @param appId The unique identifier of the app being checked.
+   * @return True if the app is eligible for voting.
+   */
+  function checkEndorsement(bytes32 appId) external returns (bool) {
+    EndorsementStorage storage $ = _getEndorsementStorage();
+
+    uint256 score;
+    for (uint256 i; i < $.appEndorsers[appId].length; ) {
+      address endorser = $.appEndorsers[appId][i];
+      uint256 endorserTokenID = $.tokenAuction.ownerToId(endorser);
+      (,uint8 nodeLevel,,,,,) = $.tokenAuction.getMetadata(endorserTokenID);
+
+      if (nodeLevel == 0) {
+        // Remove endorser by swapping with the last element and then reducing the length
+        $.appEndorsers[appId][i] = $.appEndorsers[appId][$.appEndorsers[appId].length - 1];
+        $.appEndorsers[appId].pop();
+      } else {
+        score += $.nodeEnodorsmentScore[nodeLevel];
+        i++; // Only increment i if we didn't remove an endorser
+      }
+    }
+
+    // Check the total score and update the grace period and voting eligibility accordingly
+    if (score < 100) {
+      $.appGracePeriod[appId] += 1;
+      if ($.appGracePeriod[appId] > $.gracePeriod) {
+        _setVotingEligibility(appId, false);
+        return false;
+      }
+    } else if ($.appGracePeriod[appId] > 0) {
+      $.appGracePeriod[appId] = 0;
+      _setVotingEligibility(appId, true);
+    }
+
+    return true;
   }
 
   // ---------- Internal ---------- //
