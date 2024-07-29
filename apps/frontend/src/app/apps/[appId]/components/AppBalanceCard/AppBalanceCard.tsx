82cabbc0f80d082c6971ae2319daf3451b236893
Dan Rusnac <rusnac14@gmail.com>
2024-07-29 16:40:57 +0200

    feat: allow all to see

--- apps/frontend/src/app/apps/[appId]/components/AppBalanceCard/AppBalanceCard.tsx
+++ apps/frontend/src/app/apps/[appId]/components/AppBalanceCard/AppBalanceCard.tsx
@@ -20,6 +20,10 @@ import { useCurrentAppInfo } from "../../hooks/useCurrentAppInfo"
 import { useAppBalance } from "@/api/contracts/x2EarnRewardsPool"
 import { WithdrawModal } from "./WithdrawModal"
 import { DepositModal } from "./DepositModal"
+import { BaseTooltip } from "@/components"
+import { FiInfo } from "react-icons/fi"
+import { useWallet } from "@vechain/dapp-kit-react"
+import { useIsAppAdmin } from "@/api"
 
 const compactFormatter = getCompactFormatter(4)
 
@@ -29,12 +33,21 @@ export const AppBalanceCard = () => {
   const { isOpen: isOpenDeposit, onOpen: onOpenDeposit, onClose: onCloseDeposit } = useDisclosure()
   const { app } = useCurrentAppInfo()
   const { data: balance, isLoading: isBalanceLoading } = useAppBalance(app?.id ?? "")
+  const { account } = useWallet()
+  const { data: isAppAdmin } = useIsAppAdmin(app?.id ?? "", account ?? "")
 
   return (
     <>
       <Card w={"full"} variant="baseWithBorder">
         <CardHeader>
-          <Heading size="md">{t("App balance")}</Heading>
+          <HStack justify={"space-between"} w={"full"}>
+            <Heading size="md">{t("Rewards Pool")}</Heading>
+            <BaseTooltip text={t("Total amount of B3TR tokens that the app has available for rewards distribution.")}>
+              <span>
+                <Icon as={FiInfo} color="rgba(0, 76, 252, 1)" position={"relative"} />
+              </span>
+            </BaseTooltip>
+          </HStack>
         </CardHeader>
         <CardBody py={0}>
           <VStack bg={"#E5EEFF"} py={{ base: 3, md: 4 }} px={6} h="full" w="full" borderRadius={"2xl"} align="start">
@@ -55,16 +68,18 @@ export const AppBalanceCard = () => {
               <Icon as={IoAddCircleOutline} mr={2} />
               {t("Deposit")}
             </Button>
-            <Button
-              mt={2}
-              isDisabled={balance?.scaled === "0.0" || !balance || isBalanceLoading}
-              onClick={onOpenWithdraw}
-              variant={"primaryAction"}
-              borderRadius={"full"}
-              w={"full"}>
-              <Icon as={IoWalletOutline} mr={2} />
-              {t("Withdraw")}
-            </Button>
+            {isAppAdmin && (
+              <Button
+                mt={2}
+                isDisabled={balance?.scaled === "0.0" || !balance || isBalanceLoading}
+                onClick={onOpenWithdraw}
+                variant={"primaryAction"}
+                borderRadius={"full"}
+                w={"full"}>
+                <Icon as={IoWalletOutline} mr={2} />
+                {t("Withdraw")}
+              </Button>
+            )}
           </VStack>
         </CardFooter>
       </Card>
