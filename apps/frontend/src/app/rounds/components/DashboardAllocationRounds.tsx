23ea835ec42dba1335e8cf62b467417ee46edb1d
ve-akanoce <eriknucib@live.it>
2024-08-13 22:41:55 +0200

    feat(fe/dashboard): wip new allocations section, almost polished rounds

--- apps/frontend/src/app/rounds/components/DashboardAllocationRounds.tsx
+++ apps/frontend/src/app/rounds/components/DashboardAllocationRounds.tsx
@@ -1,19 +1,76 @@
-import { useAllocationsRoundsEvents } from "@/api"
-import { AllocationRoundsList } from "@/components"
-import { useBreakpointValue } from "@chakra-ui/react"
+import { useAllocationsRound, useAllocationsRoundsEvents, useCurrentAllocationsRoundId } from "@/api"
+import { DotSymbol, ResponsiveCard } from "@/components"
+import { AllocationRoundCard } from "@/components/AllocationRoundsList/components/AllocationRoundCard"
+import { Button, Heading, HStack, Text, VStack } from "@chakra-ui/react"
+import { useEffect, useMemo, useState } from "react"
+import { useTranslation } from "react-i18next"
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6"
 
 export const DashboardAllocationRounds = () => {
+  const { t } = useTranslation()
+
   const { data: allocationRoundsEvents } = useAllocationsRoundsEvents()
+  const { data: currentRoundId, isLoading: currentRoundIdLoading } = useCurrentAllocationsRoundId()
 
-  const renderInsideCard = useBreakpointValue(
-    { base: false, md: true },
-    {
-      fallback: "false",
-    },
+  const [selectedRoundId, setSelectedRoundId] = useState<string | undefined>()
+
+  const { data: roundInfo, isLoading: roundInfoLoading } = useAllocationsRound(selectedRoundId)
+
+  useEffect(() => {
+    if (currentRoundId && !selectedRoundId) {
+      setSelectedRoundId(currentRoundId)
+    }
+  }, [currentRoundId, selectedRoundId])
+
+  const selectedRound = useMemo(() => {
+    if (!allocationRoundsEvents || !selectedRoundId) return
+    return allocationRoundsEvents.created.find(round => round.roundId === selectedRoundId)
+  }, [allocationRoundsEvents, selectedRoundId])
+
+  const isLastRound = selectedRoundId === allocationRoundsEvents?.created.length.toString()
+  const isFirstRound = selectedRoundId === "1"
+
+  const onRoundChange = (roundId: string) => () => {
+    setSelectedRoundId(roundId)
+  }
+
+  return (
+    <ResponsiveCard>
+      <VStack spacing={8} w="full">
+        <HStack spacing={4} justifyContent="space-between" w="full">
+          <Button
+            variant="link"
+            colorScheme="primary"
+            leftIcon={<FaAngleLeft />}
+            isDisabled={isFirstRound}
+            onClick={onRoundChange((parseInt(selectedRoundId || "1") - 1).toString())}>
+            {t("Previous round")}
+          </Button>
+          <VStack spacing={2}>
+            <Heading fontSize="24px" fontWeight={400}>
+              We're on Round <b>#{selectedRoundId}</b>
+            </Heading>
+            <HStack spacing={2}>
+              <Text fontSize="14px" color="#6A6A6A" fontWeight={400}>
+                {roundInfo.voteStartTimestamp?.format("MMM D")} to {roundInfo.voteEndTimestamp?.format("MMM D")}
+              </Text>
+              <DotSymbol color="#6A6A6A" size="2px" />
+              <Text fontSize="14px" color="primary.500" fontWeight={600}>
+                {roundInfo.voteEndTimestamp?.fromNow()}
+              </Text>
+            </HStack>
+          </VStack>
+          <Button
+            variant="link"
+            colorScheme="primary"
+            rightIcon={<FaAngleRight />}
+            isDisabled={isLastRound}
+            onClick={onRoundChange((parseInt(selectedRoundId || "1") + 1).toString())}>
+            {t("Next round")}
+          </Button>
+        </HStack>
+        {selectedRound && <AllocationRoundCard round={selectedRound} />}
+      </VStack>
+    </ResponsiveCard>
   )
-
-  if (allocationRoundsEvents && allocationRoundsEvents?.created.length > 0)
-    return <AllocationRoundsList maxRoundsToShow={3} headingSize="md" renderInsideCard={renderInsideCard} />
-
-  return null
 }
