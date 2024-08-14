2653e002e7e71c8d8f1a9e089b0ba0216f5a6eb8
roisindowling <roisin.dowling@vechain.org>
2024-08-14 17:57:06 +0100

    update upgrade scipt

--- packages/contracts/scripts/helpers/upgrades.ts
+++ packages/contracts/scripts/helpers/upgrades.ts
@@ -2,12 +2,14 @@ import { BaseContract, Interface } from "ethers"
 import { ethers } from "hardhat"
 import { getImplementationAddress } from "@openzeppelin/upgrades-core"
 import { AddressUtils } from "@repo/utils"
+import { DeployUpgradeOptions } from "./type"
 
 export const deployProxy = async (
   contractName: string,
   args: any[],
   libraries: { [libraryName: string]: string } = {},
   logOutput: boolean = false,
+  version?: number,
 ): Promise<BaseContract> => {
   // Deploy the implementation contract
   const Contract = await ethers.getContractFactory(contractName, {
@@ -21,7 +23,7 @@ export const deployProxy = async (
   const proxyFactory = await ethers.getContractFactory("B3TRProxy")
   const proxy = await proxyFactory.deploy(
     await implementation.getAddress(),
-    getInitializerData(Contract.interface, args),
+    getInitializerData(Contract.interface, args, version),
   )
   await proxy.waitForDeployment()
   logOutput && console.log(`${contractName} proxy: ${await proxy.getAddress()}`)
@@ -42,9 +44,12 @@ export const upgradeProxy = async (
   newVersionContractName: string,
   proxyAddress: string,
   args: any[] = [],
+  options?: { version?: number; libraries?: { [libraryName: string]: string } },
 ): Promise<BaseContract> => {
   // Deploy the implementation contract
-  const Contract = await ethers.getContractFactory(newVersionContractName)
+  const Contract = await ethers.getContractFactory(newVersionContractName, {
+    libraries: options?.libraries,
+  })
   const implementation = await Contract.deploy()
   await implementation.waitForDeployment()
 
@@ -52,7 +57,7 @@ export const upgradeProxy = async (
 
   const tx = await currentImplementationContract.upgradeToAndCall(
     await implementation.getAddress(),
-    args.length > 0 ? getInitializerData(Contract.interface, args) : "0x",
+    args.length > 0 ? getInitializerData(Contract.interface, args, options?.version) : "0x",
   )
   await tx.wait()
 
@@ -62,12 +67,57 @@ export const upgradeProxy = async (
       `The implementation address is not the one expected: ${newImplementationAddress} !== ${await implementation.getAddress()}`,
     )
   }
-
   return Contract.attach(proxyAddress)
 }
 
-export function getInitializerData(contractInterface: Interface, args: any[]) {
-  const initializer = "initialize"
+export const deployAndUpgrade = async (
+  contractNames: string[],
+  args: any[][],
+  options: DeployUpgradeOptions,
+): Promise<BaseContract> => {
+  if (contractNames.length === 0) throw new Error("No contracts to deploy")
+
+  if (contractNames.length !== args.length) throw new Error("Contract names and arguments must have the same length")
+
+  if (options.libraries && contractNames.length !== options.libraries.length)
+    throw new Error("Contract names and libraries must have the same length")
+
+  if (options.versions && contractNames.length !== options.versions.length)
+    throw new Error("Contract names and versions must have the same length")
+
+  // 1. Deploy proxy and first implementation
+  const contractName = contractNames[0]
+  const contractArgs = args[0]
+
+  let proxy = await deployProxy(
+    contractName,
+    contractArgs,
+    options.libraries?.[0],
+    options.logOutput,
+    options.versions?.[0],
+  )
+
+  // 2. Upgrade the proxy to the next versions
+  for (let i = 1; i < contractNames.length; i++) {
+    const previousVersionContractName = contractNames[i - 1]
+    const newVersionContractName = contractNames[i]
+    const contractArgs = args[i]
+
+    proxy = await upgradeProxy(
+      previousVersionContractName,
+      newVersionContractName,
+      await proxy.getAddress(),
+      contractArgs,
+      { version: options.versions?.[i], libraries: options.libraries?.[i] },
+    )
+  }
+
+  return proxy
+}
+
+export function getInitializerData(contractInterface: Interface, args: any[], version?: number) {
+  const initializer = version ? `initializeV${version}` : "initialize"
+
   const fragment = contractInterface.getFunction(initializer)
   if (!fragment) {
     throw new Error(`Contract initializer not found`)
