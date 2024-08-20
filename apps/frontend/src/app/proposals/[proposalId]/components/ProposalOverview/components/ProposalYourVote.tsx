d5746c69247e8736b800ce0d7c918adbd37f9e0d
ve-akanoce <eriknucib@live.it>
2024-08-20 13:30:10 +0200

    fix(ProposalYourVote): render if user has still to ote or no snapshot

--- apps/frontend/src/app/proposals/[proposalId]/components/ProposalOverview/components/ProposalYourVote.tsx
+++ apps/frontend/src/app/proposals/[proposalId]/components/ProposalOverview/components/ProposalYourVote.tsx
@@ -1,17 +1,53 @@
-import { VoteType } from "@/api"
-import { HStack, Image, Skeleton, Text, VStack } from "@chakra-ui/react"
+import { ProposalState, VoteType } from "@/api"
+import { Box, HStack, Icon, Image, Skeleton, Text } from "@chakra-ui/react"
 import { UilThumbsDown, UilThumbsUp } from "@iconscout/react-unicons"
 import { useTranslation } from "react-i18next"
 import { useProposalDetail } from "../../../hooks"
+import { useMemo } from "react"
+import { MdHowToVote } from "react-icons/md"
 
 export const ProposalYourVote = () => {
   const { proposal } = useProposalDetail()
   const { t } = useTranslation()
 
+  const hasVotesAtSnapshot = useMemo(() => {
+    return Number(proposal.userVot3OnSnapshot ?? 0) > 0
+  }, [proposal])
+
+  const shouldRender = useMemo(() => {
+    return [
+      ProposalState.Active,
+      ProposalState.Defeated,
+      ProposalState.Executed,
+      ProposalState.Queued,
+      ProposalState.Succeeded,
+    ].includes(proposal.state as ProposalState)
+  }, [proposal])
+
+  if (!shouldRender) return null
+
+  if (!proposal.hasUserVoted)
+    return (
+      <Box>
+        <Text color="#6A6A6A" fontSize={["lg", "lg", "md"]} fontWeight={400}>
+          {t("Your vote")}
+        </Text>
+        <Skeleton isLoaded={!proposal.isUserVot3OnSnapshotLoading}>
+          <HStack spacing={2}>
+            <Icon as={MdHowToVote} boxSize={4} color={"#252525"} />
+
+            <Text fontSize={["lg", "lg", "md"]} color={"#252525"} fontWeight={400}>
+              {hasVotesAtSnapshot ? t("You have not voted") : t("No votes to cast")}
+            </Text>
+          </HStack>
+        </Skeleton>
+      </Box>
+    )
+
   switch (Number(proposal.userVote?.support)) {
     case VoteType.VOTE_FOR:
       return (
-        <VStack alignItems={"stretch"}>
+        <Box>
           <Text fontWeight={"400"} color="#6A6A6A">
             {t("Your vote")}
           </Text>
@@ -24,11 +60,11 @@ export const ProposalYourVote = () => {
               <Text fontWeight={600}>{t("For")}</Text>
             </HStack>
           </Skeleton>
-        </VStack>
+        </Box>
       )
     case VoteType.VOTE_AGAINST:
       return (
-        <VStack alignItems={"stretch"}>
+        <Box>
           <Text fontWeight={"400"} color="#6A6A6A">
             {t("Your vote")}
           </Text>
@@ -41,11 +77,11 @@ export const ProposalYourVote = () => {
               <Text fontWeight={600}>{t("Against")}</Text>
             </HStack>
           </Skeleton>
-        </VStack>
+        </Box>
       )
     case VoteType.ABSTAIN:
       return (
-        <VStack alignItems={"stretch"}>
+        <Box>
           <Text fontWeight={"400"} color="#6A6A6A">
             {t("Your vote")}
           </Text>
@@ -58,7 +94,7 @@ export const ProposalYourVote = () => {
               <Text fontWeight={600}>{t("Abstain")}</Text>
             </HStack>
           </Skeleton>
-        </VStack>
+        </Box>
       )
     default:
       return null
