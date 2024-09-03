7d7f505a79808eb9ecaf08994cdc687137cee81a
roisindowling <roisin.dowling@vechain.org>
2024-09-03 11:41:29 +0100

    align upgrade scripts with main

--- packages/contracts/scripts/upgrade/upgradesConfig.ts
+++ packages/contracts/scripts/upgrade/upgradesConfig.ts
@@ -5,10 +5,14 @@ export const upgradeConfig = {
   },
   "B3TR Governor": {
     name: "b3tr-governor",
-    versions: ["v2"],
+    versions: ["v2", "v3"],
   },
   "XAllocation Voting": {
     name: "x-allocation-voting",
+    versions: ["v2"],
+  },
+  "XAllocation Pool": {
+    name: "x-allocation-pool",
     versions: ["v2"],
   },
 } as const
