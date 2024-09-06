f41ec640ba713f8eb961db00ca54cf2c3d2985ef
Erik Nucibella <eriknucib@live.it>
2024-09-06 18:33:49 +0200

    feat(endorsement): multiple hooks to get endorsement data

---
+++ apps/frontend/src/api/contracts/xApps/hooks/useAppEndorsers.ts
@@ -0,0 +1,28 @@
+import { getConfig } from "@repo/config"
+import { X2EarnApps__factory } from "@repo/contracts"
+import { getCallKey, useCall } from "@/hooks"
+import { UseQueryResult } from "@tanstack/react-query"
+
+const contractAddress = getConfig().x2EarnAppsContractAddress
+const contractInterface = X2EarnApps__factory.createInterface()
+const method = "getEndorsers"
+
+/**
+ * Get the query key for the list of endorsers for an app
+ */
+export const getEndorsersQueryKey = (appId: string) => {
+  getCallKey({ method, keyArgs: [appId] })
+}
+
+/**
+ *  Hook to get the list of endorsers for an app
+ * @returns The endorsers for an app
+ */
+export const useAppEndorsers = (appId: string): UseQueryResult<string[], Error> => {
+  return useCall({
+    contractInterface,
+    contractAddress,
+    method,
+    args: [appId],
+  })
+}
