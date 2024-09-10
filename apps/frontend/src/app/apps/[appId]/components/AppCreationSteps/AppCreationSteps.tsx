88f401f51adc08ee64c13b54c128ef06c589a935
Victor Emanuel <versaujob@gmail.com>
2024-09-10 09:20:46 +0100

    refactor(apps): making creation steps card responsive

--- apps/frontend/src/app/apps/[appId]/components/AppCreationSteps/AppCreationSteps.tsx
+++ apps/frontend/src/app/apps/[appId]/components/AppCreationSteps/AppCreationSteps.tsx
@@ -1,52 +1,55 @@
 import { Handshake } from "@/components"
 import { SignIcon } from "@/components/Icons/SignIcon"
 import { VoteCheckmarkIcon } from "@/components/Icons/VoteCheckmarkIcon"
-import { Box, Card, CardBody, Circle, Heading, HStack, Icon, Text, useColorModeValue, VStack } from "@chakra-ui/react"
+import { XAppsCreationSteps, XAppsCreationStepStatus } from "@/types/appDetails"
+import { Box, Card, CardBody, Circle, Grid, Heading, HStack, Icon, Stack, Text, VStack } from "@chakra-ui/react"
 import { UilCheck, UilInfoCircle } from "@iconscout/react-unicons"
 import { useTranslation } from "react-i18next"
 
-export enum XAppsSteps {
-  SUBMISSION,
-  ENDORSEMENT,
-  ALLOCATION,
-}
 type AppCreationStepsProps = {
-  currentStep: XAppsSteps
-}
-enum StepStatus {
-  COMPLETED = "COMPLETED",
-  ACTIVE = "ACTIVE",
-  PENDING = "PENDING",
+  currentStep: XAppsCreationSteps
 }
 
 type StepBoxesProps = {
-  stepText: string
   title: string
-  type: XAppsSteps
   description: string
-  status: StepStatus
+  type: XAppsCreationSteps
+  status: XAppsCreationStepStatus
+  stepText: string
 }
 const statusIcon = {
-  [StepStatus.PENDING]: null,
-  [StepStatus.ACTIVE]: <Circle size="80%" bg="#004CFC" />,
-  [StepStatus.COMPLETED]: <UilCheck size={20} color="#004CFC" />,
+  [XAppsCreationStepStatus.PENDING]: null,
+  [XAppsCreationStepStatus.ACTIVE]: <Circle size="80%" bg="#004CFC" />,
+  [XAppsCreationStepStatus.COMPLETED]: <UilCheck size={20} color="#004CFC" />,
 }
 
 const stepIcon = {
-  [XAppsSteps.SUBMISSION]: SignIcon,
-  [XAppsSteps.ENDORSEMENT]: Handshake,
-  [XAppsSteps.ALLOCATION]: VoteCheckmarkIcon,
+  [XAppsCreationSteps.SUBMISSION]: SignIcon,
+  [XAppsCreationSteps.ENDORSEMENT]: Handshake,
+  [XAppsCreationSteps.ALLOCATION]: VoteCheckmarkIcon,
 }
 
-const StepBoxes = ({ stepText, title, type, description, status = StepStatus.PENDING }: StepBoxesProps) => {
-  const cardBackgroundColor = useColorModeValue("#F7F7F7", "#131313")
-  const borderColor = status === StepStatus.ACTIVE ? "#004CFC" : "transparent"
+const StepBoxes = ({
+  stepText,
+  title,
+  type,
+  description,
+  status = XAppsCreationStepStatus.PENDING,
+}: StepBoxesProps) => {
+  const borderColor = status === XAppsCreationStepStatus.ACTIVE ? "#004CFC" : "transparent"
   const IconComponent = stepIcon[type]
-  const iconColor = status === StepStatus.COMPLETED ? "#004CFC" : "#000000"
+  const iconColor = status === XAppsCreationStepStatus.COMPLETED ? "#004CFC" : "#000000"
 
   return (
-    <Box w="full" h="full" position="relative" borderRadius="24px" borderWidth="2px" borderColor={borderColor}>
-      <Box
+    <Stack
+      minW={["60vw", "45vw", "15vw", "5vw"]}
+      w="full"
+      h="full"
+      position="relative"
+      borderRadius="24px"
+      borderWidth="2px"
+      borderColor={borderColor}>
+      <Stack
         position="absolute"
         borderRadius="25px"
         borderWidth="2px"
@@ -59,51 +62,50 @@ const StepBoxes = ({ stepText, title, type, description, status = StepStatus.PEN
         alignItems="center"
         justifyContent="center">
         {statusIcon[status]}
-      </Box>
-      <VStack
+      </Stack>
+      <Stack
         w="full"
         h="full"
+        p={3}
+        backgroundColor={"#F7F7F7"}
         borderRadius="24px"
-        backgroundColor={cardBackgroundColor}
-        opacity={status === StepStatus.PENDING ? 0.5 : 1}
-        py={4}
-        px={6}>
-        <HStack w="full" alignItems="center">
+        opacity={status === XAppsCreationStepStatus.PENDING ? 0.5 : 1}>
+        <HStack w="full" h="full" alignItems="center">
           <IconComponent size={124} color={iconColor} />
         </HStack>
         <VStack alignItems="start" w="full">
           <Text fontSize="12px" color="#6A6A6A" lineHeight={"1px"}>
             {stepText}
           </Text>
-          <Text fontSize="20px" fontWeight={800} color="#252525">
-            {title}
-          </Text>
+          <Heading size={["md", "sm", "sm"]}>{title}</Heading>
         </VStack>
         <Text fontSize="14px" color="#6A6A6A" w="full">
           {description}
         </Text>
-      </VStack>
-    </Box>
+      </Stack>
+    </Stack>
   )
 }
 
 export const AppCreationSteps = ({ currentStep }: AppCreationStepsProps) => {
   const { t } = useTranslation()
-  const getStepStatus = (step: number): StepStatus => {
-    if (step < currentStep) return StepStatus.COMPLETED
-    if (step === currentStep) return StepStatus.ACTIVE
-    return StepStatus.PENDING
+
+  const getXAppsCreationStepStatus = (step: XAppsCreationSteps): XAppsCreationStepStatus => {
+    if (step < currentStep) return XAppsCreationStepStatus.COMPLETED
+    if (step === currentStep) return XAppsCreationStepStatus.ACTIVE
+    return XAppsCreationStepStatus.PENDING
   }
+
   return (
-    <Card variant="base">
-      <CardBody>
-        <VStack w="full" pb={8}>
-          <VStack w="full" align="start">
+    <Box>
+      <Card>
+        <CardBody>
+          <VStack spacing={8} align="flex-start">
             <HStack w="full">
               <HStack w="full" justify="start">
                 <Heading size="lg">{t("Your App is almost ready!")}</Heading>
               </HStack>
-              <HStack w="full" justify={"end"} alignItems={"center"}>
+              <HStack w="full" justify="end" alignItems="center" display={{ base: "none", md: "flex" }}>
                 <Icon as={UilInfoCircle} color="rgba(0, 76, 252, 1)" />
                 <Text color="rgba(0, 76, 252, 1)">{t("Know more about Apps")}</Text>
               </HStack>
@@ -113,42 +115,45 @@ export const AppCreationSteps = ({ currentStep }: AppCreationStepsProps) => {
                 "Before adding your App to the public listing and seeing stats and updates, it has to go through these three steps. You can",
               )}
               <Text as="span" color="rgba(0, 76, 252, 1)">
-                {t("fill the App information")}
+                {" "}
+                {t("fill the App information")}{" "}
               </Text>
               {t("while waiting!")}
             </Text>
+            <Box w="full" maxW={"100%"} overflowX="auto">
+              <Grid gridTemplateColumns="repeat(3,  1fr)" gap={4} w="full">
+                <StepBoxes
+                  stepText={t("STEP {{value}}", { value: XAppsCreationSteps.SUBMISSION + 1 })}
+                  title={t("App submission")}
+                  type={XAppsCreationSteps.SUBMISSION}
+                  status={getXAppsCreationStepStatus(XAppsCreationSteps.SUBMISSION)}
+                  description={t(
+                    "Submit your app into the ecosystem with all the necessary information, including logo, creator bio, and social media links.",
+                  )}
+                />
+                <StepBoxes
+                  stepText={t("STEP {{value}}", { value: XAppsCreationSteps.ENDORSEMENT + 1 })}
+                  title={t("Endorsement")}
+                  type={XAppsCreationSteps.ENDORSEMENT}
+                  status={getXAppsCreationStepStatus(XAppsCreationSteps.ENDORSEMENT)}
+                  description={t(
+                    "X Node Holders will use their NFTs to endorse your dApp. Once it reaches 100 points, it becomes eligible for allocations.",
+                  )}
+                />
+                <StepBoxes
+                  stepText={t("STEP {{value}}", { value: XAppsCreationSteps.ALLOCATION + 1 })}
+                  title={t("Allocation voting")}
+                  type={XAppsCreationSteps.ALLOCATION}
+                  status={getXAppsCreationStepStatus(XAppsCreationSteps.ALLOCATION)}
+                  description={t(
+                    "The allocation rounds determine the resources and support your dApp receives from the ecosystem community.",
+                  )}
+                />
+              </Grid>
+            </Box>
           </VStack>
-        </VStack>
-        <Box display="grid" gridTemplateColumns="repeat(3, 1fr)" gap={4} w="full">
-          <StepBoxes
-            stepText={t("STEP {{value}}", { value: 1 })}
-            title={t("App submission")}
-            type={XAppsSteps.SUBMISSION}
-            status={getStepStatus(XAppsSteps.SUBMISSION)}
-            description={t(
-              "Submit your app into the ecosystem with all the necessary information, including logo, creator bio, and social media links.",
-            )}
-          />
-          <StepBoxes
-            stepText={t("STEP {{value}}", { value: 2 })}
-            title={t("Endorsement")}
-            type={XAppsSteps.ENDORSEMENT}
-            status={getStepStatus(XAppsSteps.ENDORSEMENT)}
-            description={t(
-              "X Node Holders will use their NFTs to endorse your dApp. Once it reaches 100 points, it becomes eligible for allocations.",
-            )}
-          />
-          <StepBoxes
-            stepText={t("STEP {{value}}", { value: 3 })}
-            title={t("Allocation voting")}
-            type={XAppsSteps.ALLOCATION}
-            status={getStepStatus(XAppsSteps.ALLOCATION)}
-            description={t(
-              "The allocation rounds determine the resources and support your dApp receives from the ecosystem community.",
-            )}
-          />
-        </Box>
-      </CardBody>
-    </Card>
+        </CardBody>
+      </Card>
+    </Box>
   )
 }
--- apps/frontend/src/app/apps/[appId]/components/AppDetailPageContent.tsx
+++ apps/frontend/src/app/apps/[appId]/components/AppDetailPageContent.tsx
@@ -1,30 +1,21 @@
+import { XAppsCreationSteps } from "@/types"
 import { Stack, VStack } from "@chakra-ui/react"
-import { AppCreationSteps, XAppsSteps } from "./AppCreationSteps/AppCreationSteps"
+import { AppCreationSteps } from "./AppCreationSteps/AppCreationSteps"
 import { AppDetailOverview } from "./AppDetailOverview"
 import { AppDetailsSidebar } from "./AppDetailSidebar"
-import { AppEndorsementInfoCard } from "./AppEndorsementInfoCard/AppEndorsementInfoCard"
-import { AppInfoProgressCard } from "./AppInfoProgressCard/AppInfoProgressCard"
 import { AppScreenshots } from "./AppScreenshots"
-import { AppTeam } from "./AppTeam/AppTeam"
 import { AppTweets } from "./AppTweets"
 
 export const AppDetailPageContent = () => {
-  //TODO: Replace mock data with real data
-  const mockTeamMembers = [{ address: "0X123", role: "Member" }]
   return (
     <VStack w="full" alignItems="stretch" gap={8}>
       <AppDetailOverview />
-      <Stack w="full" spacing={8} flexDirection={["column-reverse", "row"]} align={["stretch", "flex-start"]}>
-        <VStack align="stretch" gap={8} flex={3.5} minW={0}>
-          <AppCreationSteps currentStep={XAppsSteps.ENDORSEMENT} />
+      <Stack w="full" spacing={8} flexDirection={["column", "column", "column", "row"]} align="stretch">
+        <Stack direction="column" gap={8} flex={3.5} minW={0} w="full" maxW={"100vw"}>
+          <AppCreationSteps currentStep={XAppsCreationSteps.ALLOCATION} />
           <AppScreenshots />
           <AppTweets />
-        </VStack>
-        <VStack align="stretch" gap={8} flex={1.5} minW={0}>
-          <AppEndorsementInfoCard currentScore={0} endorsementThreshold={100} />
-          <AppInfoProgressCard />
-          <AppTeam teamMembers={mockTeamMembers} />
-        </VStack>
+        </Stack>
         <AppDetailsSidebar />
       </Stack>
     </VStack>
