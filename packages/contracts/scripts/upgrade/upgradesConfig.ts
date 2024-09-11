71228bc74d564e6a7f2caec01f3afbfede52cde5
roisindowling <roisin.dowling@vechain.org>
2024-09-11 11:06:50 +0100

    update script

--- packages/contracts/scripts/upgrade/upgradesConfig.ts
+++ packages/contracts/scripts/upgrade/upgradesConfig.ts
@@ -1,4 +1,10 @@
-export const upgradeConfig = {
+export interface UpgradeContract {
+  name: string
+  versions: readonly string[]
+  descriptions: Record<string, string>
+}
+
+export const upgradeConfig: Record<string, UpgradeContract> = {
   "Voter Rewards": {
     name: "voter-rewards",
     versions: ["v2"],
@@ -10,28 +16,28 @@ export const upgradeConfig = {
     name: "b3tr-governor",
     versions: ["v2"],
     descriptions: {
-      v2: "Give ability to contract admins to can call governance only functions",
+      v2: "Give ability to contract admins to call governance-only functions.",
     },
   },
   "XAllocation Voting": {
     name: "x-allocation-voting",
     versions: ["v2"],
     descriptions: {
-      v2: "Fix XAllocation voting so that a user cannot vote for an XApp with a vote weight less than 1",
+      v2: "Fix XAllocation voting so that a user cannot vote for an XApp with a vote weight less than 1.",
     },
   },
   "X2Earn Rewards Pool": {
     name: "x2earn-rewards-pool",
     versions: ["v2"],
     descriptions: {
-      v2: "Add onchain impacts and proof generation",
+      v2: "Add on-chain impacts and proof generation.",
     },
   },
   "Node Management": {
     name: "node-management",
     versions: ["v1"],
     descriptions: {
-      v1: "Deploy the initial version of the NodeManagement contract",
+      v1: "Deploy the initial version of the NodeManagement contract.",
     },
   },
-} as const
+}
