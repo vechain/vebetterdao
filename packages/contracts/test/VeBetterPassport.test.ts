de92dde7ca90d33500311b0d88f32d7bc7d436c7
roisindowling <roisin.dowling@vechain.org>
2024-09-12 13:01:49 +0100

    add tests for personhood settings module

--- packages/contracts/test/VeBetterPassport.test.ts
+++ packages/contracts/test/VeBetterPassport.test.ts
@@ -40,6 +40,7 @@ describe.only("VeBetterPassport - @shard3", function () {
 
       // Whitelist owner
       await veBetterPassport.connect(owner).whitelist(owner.address)
+      await veBetterPassport.connect(owner).whitelist(otherAccounts[1].address)
 
       // Enable whitelist check
       await veBetterPassport.connect(owner).toggleWhitelistCheck()
@@ -122,6 +123,159 @@ describe.only("VeBetterPassport - @shard3", function () {
           [app1Id, app2Id, app3Id],
           [ethers.parseEther("0"), ethers.parseEther("900"), ethers.parseEther("100")],
         )
+    })
+  })
+
+  describe("PersonhoodSettings", function () {
+    it("Should initialize correctly", async function () {
+      const {
+        owner: settingsManager,
+        veBetterPassport,
+        otherAccount,
+      } = await getOrDeployContractInstances({
+        forceDeploy: true,
+      })
+
+      // Verify non admin account cannot toggle checks by default
+      await expect(veBetterPassport.connect(otherAccount).toggleWhitelistCheck()).to.be.reverted
+
+      const settingsManagerRole = await veBetterPassport.SETTINGS_MANAGER_ROLE()
+
+      // Verify settingsManager has the role
+      expect(await veBetterPassport.hasRole(settingsManagerRole, settingsManager.address)).to.be.true
+    })
+
+    it("Should allow only settings manager to toggle checks", async function () {
+      const {
+        owner: settingsManager,
+        veBetterPassport,
+        otherAccount,
+      } = await getOrDeployContractInstances({
+        forceDeploy: false,
+      })
+      await expect(veBetterPassport.connect(otherAccount).toggleWhitelistCheck()).to.be.reverted
+
+      // Settings manager should be able to toggle the checks
+      await expect(veBetterPassport.connect(settingsManager).toggleWhitelistCheck())
+        .to.emit(veBetterPassport, "CheckToggled")
+        .withArgs("Whitelist Check", true)
+
+      // Whitelist check should be enabled
+      expect(await veBetterPassport.whitelistCheckEnabled()).to.be.true
+
+      // Cast SETTING_MANAGER_ROLE to otherAccount
+      const settingsManagerRole = await veBetterPassport.SETTINGS_MANAGER_ROLE()
+      await veBetterPassport.connect(settingsManager).grantRole(settingsManagerRole, otherAccount.address)
+
+      // Other account should be able to toggle the checks
+      await expect(veBetterPassport.connect(otherAccount).toggleWhitelistCheck())
+        .to.emit(veBetterPassport, "CheckToggled")
+        .withArgs("Whitelist Check", false)
+
+      // Whitelist check should be disabled
+      expect(await veBetterPassport.whitelistCheckEnabled()).to.be.false
+    })
+
+    it("Should be able to toggle whitelist check", async function () {
+      const { owner: settingsManager, veBetterPassport } = await getOrDeployContractInstances({
+        forceDeploy: false,
+      })
+
+      // Whitelist check should be disabled by default
+      expect(await veBetterPassport.whitelistCheckEnabled()).to.be.false
+
+      // Settings manager should be able to toggle the checks
+      await expect(veBetterPassport.connect(settingsManager).toggleWhitelistCheck())
+        .to.emit(veBetterPassport, "CheckToggled")
+        .withArgs("Whitelist Check", true)
+
+      // Whitelist check should be enabled
+      expect(await veBetterPassport.whitelistCheckEnabled()).to.be.true
+    })
+
+    it("Should be able to toggle blacklist check", async function () {
+      const { owner: settingsManager, veBetterPassport } = await getOrDeployContractInstances({
+        forceDeploy: false,
+      })
+
+      // Blacklist check should be disabled by default
+      expect(await veBetterPassport.blacklistCheckEnabled()).to.be.false
+
+      // Settings manager should be able to toggle the checks
+      await expect(veBetterPassport.connect(settingsManager).toggleBlacklistCheck())
+        .to.emit(veBetterPassport, "CheckToggled")
+        .withArgs("Blacklist Check", true)
+
+      // Blacklist check should be enabled
+      expect(await veBetterPassport.blacklistCheckEnabled()).to.be.true
+    })
+
+    it("Should be able to toggle signaling check", async function () {
+      const { owner: settingsManager, veBetterPassport } = await getOrDeployContractInstances({
+        forceDeploy: false,
+      })
+
+      // Signaling check should be disabled by default
+      expect(await veBetterPassport.signalingCheckEnabled()).to.be.false
+
+      // Settings manager should be able to toggle the checks
+      await expect(veBetterPassport.connect(settingsManager).toggleSignalingCheck())
+        .to.emit(veBetterPassport, "CheckToggled")
+        .withArgs("Signaling Check", true)
+
+      // Signaling check should be enabled
+      expect(await veBetterPassport.signalingCheckEnabled()).to.be.true
+    })
+
+    it("Should be able to toggle participation check", async function () {
+      const { owner: settingsManager, veBetterPassport } = await getOrDeployContractInstances({
+        forceDeploy: false,
+      })
+
+      // Participation check should be disabled by default
+      expect(await veBetterPassport.participationScoreCheckEnabled()).to.be.false
+
+      // Settings manager should be able to toggle the checks
+      await expect(veBetterPassport.connect(settingsManager).toggleParticipationScoreCheck())
+        .to.emit(veBetterPassport, "CheckToggled")
+        .withArgs("Participation Score Check", true)
+
+      // Participation check should be enabled
+      expect(await veBetterPassport.participationScoreCheckEnabled()).to.be.true
+    })
+
+    it("Should be able to toggle node ownership check", async function () {
+      const { owner: settingsManager, veBetterPassport } = await getOrDeployContractInstances({
+        forceDeploy: false,
+      })
+
+      // Node ownership check should be disabled by default
+      expect(await veBetterPassport.nodeOwnershipCheckEnabled()).to.be.false
+
+      // Settings manager should be able to toggle the checks
+      await expect(veBetterPassport.connect(settingsManager).toggleNodeOwnershipCheck())
+        .to.emit(veBetterPassport, "CheckToggled")
+        .withArgs("Node Ownership Check", true)
+
+      // Node ownership check should be enabled
+      expect(await veBetterPassport.nodeOwnershipCheckEnabled()).to.be.true
+    })
+
+    it("Should be able to toggle gm ownership check", async function () {
+      const { owner: settingsManager, veBetterPassport } = await getOrDeployContractInstances({
+        forceDeploy: false,
+      })
+
+      // Whitelist check should be disabled by default
+      expect(await veBetterPassport.gmOwnershipCheckEnabled()).to.be.false
+
+      // Settings manager should be able to toggle the checks
+      await expect(veBetterPassport.connect(settingsManager).toggleGMOwnershipCheck())
+        .to.emit(veBetterPassport, "CheckToggled")
+        .withArgs("GM Ownership Check", true)
+
+      // Whitelist check should be enabled
+      expect(await veBetterPassport.gmOwnershipCheckEnabled()).to.be.true
     })
   })
 })
