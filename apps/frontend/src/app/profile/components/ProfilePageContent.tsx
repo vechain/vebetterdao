7da7777419cb325f77fd2ccea24740968c1dbcaa
Davide Carpini <carpini.davide@gmail.com>
2024-09-26 10:25:21 +0200

    balance card

--- apps/frontend/src/app/profile/components/ProfilePageContent.tsx
+++ apps/frontend/src/app/profile/components/ProfilePageContent.tsx
@@ -1,15 +1,25 @@
-import { VStack, HStack, Button } from "@chakra-ui/react"
+import { VStack, HStack, Button, Heading } from "@chakra-ui/react"
 import { ProfileHeader } from "./ProfileHeader/ProfileHeader"
 import { useState } from "react"
 import { ProfileBetterActions } from "./ProfileBetterActions"
-import { ProfileBalance } from "./ProfileBalance"
+import { TokensBalance } from "../../components/TokensBalance"
 import { useTranslation } from "react-i18next"
 
 export const ProfilePageContent = () => {
   const { t } = useTranslation()
   const [selectedTab, setSelectedTab] = useState<"balance" | "better-actions">("balance")
 
-  const selectedTabContent = selectedTab === "balance" ? <ProfileBalance /> : <ProfileBetterActions />
+  const selectedTabContent =
+    selectedTab === "balance" ? (
+      <VStack align={"stretch"} gap={4}>
+        <Heading fontSize="lg" fontWeight={700}>
+          {t("Your tokens")}
+        </Heading>
+        <TokensBalance />
+      </VStack>
+    ) : (
+      <ProfileBetterActions />
+    )
 
   return (
     <VStack gap={6} align="stretch">
