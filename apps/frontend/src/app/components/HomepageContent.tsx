f958e7df58befa26289d905c28d056045949665f
Erik Nucibella <eriknucib@live.it>
2024-09-26 20:09:51 +0800

    feat(dashboard): wip round bottomsheet, trigger polished

--- apps/frontend/src/app/components/HomepageContent.tsx
+++ apps/frontend/src/app/components/HomepageContent.tsx
@@ -9,29 +9,33 @@ import { RoundInfoBottomSheet } from "./RoundInfoBottomSheet"
 
 export const HomePageContent = () => {
   return (
-    <Grid
-      templateColumns={["repeat(1, 1fr)", "repeat(1, 1fr)", "repeat(3, 1fr)"]}
-      gap={"32px"}
-      w="full"
-      data-testid="form-proposal-layout">
-      <GridItem colSpan={[1, 1, 2]} order={[2, 2, 1]}>
-        <VStack flex={4.5} justifyContent="stretch" alignItems={"stretch"} spacing={"32px"} data-testid="homepage">
-          <Hide above="md">
-            <RoundInfoBottomSheet />
-          </Hide>
-          <LowOnVthoCard />
-          <CastYourVoteCard />
-          <SupplyBreakdownCard />
-          <YourBetterActionsCard />
-          <Show above="md">
-            <DashboardAllocationRounds />
-          </Show>
-          <DashboardXApps />
-        </VStack>
-      </GridItem>
-      <GridItem colSpan={1} order={[1, 1, 2]}>
-        <DashboardSideBar />
-      </GridItem>
-    </Grid>
+    <>
+      <Hide above="md">
+        <RoundInfoBottomSheet />
+      </Hide>
+
+      <Grid
+        templateColumns={["repeat(1, 1fr)", "repeat(1, 1fr)", "repeat(3, 1fr)"]}
+        gap={"32px"}
+        w="full"
+        overflowY={"auto"}
+        data-testid="form-proposal-layout">
+        <GridItem colSpan={[1, 1, 2]} order={[2, 2, 1]}>
+          <VStack flex={4.5} justifyContent="stretch" alignItems={"stretch"} spacing={"32px"} data-testid="homepage">
+            <LowOnVthoCard />
+            <CastYourVoteCard />
+            <SupplyBreakdownCard />
+            <YourBetterActionsCard />
+            <Show above="md">
+              <DashboardAllocationRounds />
+            </Show>
+            <DashboardXApps />
+          </VStack>
+        </GridItem>
+        <GridItem colSpan={1} order={[1, 1, 2]}>
+          <DashboardSideBar />
+        </GridItem>
+      </Grid>
+    </>
   )
 }
--- apps/frontend/src/app/components/RoundInfoBottomSheet.tsx
+++ apps/frontend/src/app/components/RoundInfoBottomSheet.tsx
@@ -1,105 +1,104 @@
 "use client"
 
-import * as React from "react"
-import { Drawer } from "vaul"
-import { Box, VStack, Text, Heading, Flex, Button, Badge, Icon } from "@chakra-ui/react"
-import { FaArrowUp } from "react-icons/fa6"
+import { Box, VStack, Text, Heading, Flex, Button, Badge, useDisclosure, HStack, Skeleton } from "@chakra-ui/react"
+import { BaseBottomSheet } from "@/components/BaseBottomSheet"
+import { AllocationRoundParticipatingXApps } from "@/components/AllocationRoundsList/components/AllocationRoundParticipatingXApps"
+import { useAllocationsRound, useAllocationsRoundsEvents, useCurrentAllocationsRoundId } from "@/api"
 
 export const RoundInfoBottomSheet = () => {
-  const [isOpen, setIsOpen] = React.useState(true)
-  const [activeSnap, setActiveSnap] = React.useState(0.1)
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
-  React.useEffect(() => {
-    setIsOpen(true)
-  }, [])
+  const { data: allocationRoundsEvents } = useAllocationsRoundsEvents()
+  const { data: currentRoundId } = useCurrentAllocationsRoundId()
+
+  const { data: allocationRound, isLoading } = useAllocationsRound(currentRoundId)
 
   return (
-    <Drawer.Root
-      modal={false}
-      shouldScaleBackground
-      open={isOpen}
-      onOpenChange={setIsOpen}
-      snapPoints={[0.1, 0.5, 0.95]}
-      activeSnapPoint={activeSnap}
-      setActiveSnapPoint={snap => setActiveSnap(Number(snap))}
-      dismissible={false}>
-      <Box
-        as={Drawer.Overlay}
-        pos={"fixed"}
-        inset={0}
-        bg={"blackAlpha.400"}
-        pointerEvents={activeSnap === 0.1 ? "none" : "auto"}
-      />
-      <Box
-        zIndex={100}
-        as={Drawer.Content}
-        pos={"fixed"}
-        bottom={0}
-        left={0}
-        right={0}
-        bg={"white"}
-        roundedTop={"20px"}
-        display={"flex"}
-        pointerEvents={activeSnap === 0.1 ? "none" : "auto"}>
-        <Box
-          p={4}
-          bg="white"
+    <>
+      {!isOpen && (
+        <HStack
+          w="full"
+          justify={"space-between"}
+          onClick={onOpen}
+          position="fixed"
+          bottom={0}
+          left={0}
+          right={0}
+          bg="#B1F16C"
+          py={5}
+          px={4}
           borderTopRadius="20px"
-          boxShadow="0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)"
-          pointerEvents={"auto"}>
-          <Flex justify="center" mb={4}>
-            <Box w="40px" h="4px" bg="gray.300" borderRadius="full" />
-          </Flex>
-          <VStack spacing={4} align="stretch" maxW="md" mx="auto">
-            <Flex justify="space-between" align="center">
-              <Box>
-                <Heading size="lg">We're on Round #15</Heading>
-                <Flex align="center" mt={1}>
-                  <Box w={2} h={2} borderRadius="full" bg="red.500" mr={2} />
-                  <Text color="gray.500">Active session</Text>
-                </Flex>
-              </Box>
-              <Icon as={FaArrowUp} boxSize={6} color="gray.400" />
-            </Flex>
-            <Text color="gray.600">July 12th to July 21th</Text>
-            <Badge colorScheme="red" alignSelf="flex-start">
-              Active session
-            </Badge>
-            <Heading size="md">Allocations voting</Heading>
-            <Text color="gray.600">
-              Each week, you can vote for your favorite apps to help distribute resources among them!
+          boxShadow="0px -5px 16px 0px #0000000F"
+          cursor="pointer"
+          zIndex={3}>
+          <Box>
+            <Skeleton isLoaded={!!currentRoundId}>
+              <Heading fontSize={"20px"} fontWeight={400}>
+                We're on <b>Round #{currentRoundId}</b>
+              </Heading>
+            </Skeleton>
+            <Text fontSize={"14px"} fontWeight={400}>
+              {allocationRound.voteStartTimestamp?.format("MMM D")} to{" "}
+              {allocationRound.voteEndTimestamp?.format("MMM D")}
             </Text>
-            <Box bg="gray.100" p={4} borderRadius="lg">
-              <Flex justify="space-between" align="center" mb={2}>
-                <Text fontWeight="semibold">#15 Allocation round</Text>
-                <Text color="blue.600" fontWeight="semibold">
-                  4.8M
-                </Text>
-              </Flex>
-              <Text color="gray.500" mb={4}>
-                total to distribute
+          </Box>
+          {currentRoundId && <AllocationRoundParticipatingXApps roundId={currentRoundId} iconSize={36} />}
+        </HStack>
+      )}
+
+      <BaseBottomSheet isOpen={isOpen} onClose={onClose} height="70vh">
+        <VStack spacing={4} align="stretch" mx="auto">
+          <HStack spacing={4} justify="space-between" w="full">
+            <Box>
+              <Skeleton isLoaded={!!currentRoundId}>
+                <Heading fontSize={"20px"} fontWeight={400}>
+                  We're on <b>Round #{currentRoundId}</b>
+                </Heading>
+              </Skeleton>
+              <Text fontSize={"14px"} fontWeight={400}>
+                {allocationRound.voteStartTimestamp?.format("MMM D")} to{" "}
+                {allocationRound.voteEndTimestamp?.format("MMM D")}
               </Text>
-              <Flex justify="space-between">
-                <Button variant="outline">See more</Button>
-                <Button colorScheme="blackAlpha">Vote now</Button>
-              </Flex>
             </Box>
-            <Heading size="md">Proposals</Heading>
-            <Text color="gray.600">Proposals shape the ecosystem. Vote on ideas and build our community together!</Text>
-            <Box bg="gray.100" p={4} borderRadius="lg">
-              <Text fontWeight="semibold" mb={2}>
-                This is a proposal name, a long title about what is the proposal about
+            {currentRoundId && <AllocationRoundParticipatingXApps roundId={currentRoundId} iconSize={36} />}
+          </HStack>
+          <Badge colorScheme="red" alignSelf="flex-start">
+            Active session
+          </Badge>
+          <Heading size="md">Allocations voting</Heading>
+          <Text color="gray.600">
+            Each week, you can vote for your favorite apps to help distribute resources among them!
+          </Text>
+          <Box bg="gray.100" p={4} borderRadius="lg">
+            <Flex justify="space-between" align="center" mb={2}>
+              <Text fontWeight="semibold">#15 Allocation round</Text>
+              <Text color="blue.600" fontWeight="semibold">
+                4.8M
               </Text>
-              <Text color="gray.500" mb={4}>
-                You didn't vote yet
-              </Text>
-              <Flex justify="flex-end">
-                <Button colorScheme="blackAlpha">Vote now</Button>
-              </Flex>
-            </Box>
-          </VStack>
-        </Box>
-      </Box>
-    </Drawer.Root>
+            </Flex>
+            <Text color="gray.500" mb={4}>
+              total to distribute
+            </Text>
+            <Flex justify="space-between">
+              <Button variant="outline">See more</Button>
+              <Button colorScheme="blackAlpha">Vote now</Button>
+            </Flex>
+          </Box>
+          <Heading size="md">Proposals</Heading>
+          <Text color="gray.600">Proposals shape the ecosystem. Vote on ideas and build our community together!</Text>
+          <Box bg="gray.100" p={4} borderRadius="lg">
+            <Text fontWeight="semibold" mb={2}>
+              This is a proposal name, a long title about what is the proposal about
+            </Text>
+            <Text color="gray.500" mb={4}>
+              You didn't vote yet
+            </Text>
+            <Flex justify="flex-end">
+              <Button colorScheme="blackAlpha">Vote now</Button>
+            </Flex>
+          </Box>
+        </VStack>
+      </BaseBottomSheet>
+    </>
   )
 }
