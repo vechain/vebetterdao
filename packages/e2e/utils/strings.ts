06208a1154f73d0b29ece0bc1be81ee2f0b47707
Alexander Khrystych <32546926+alexander-khrystych@users.noreply.github.com>
2024-09-27 14:59:24 +0300

    [E2E] Update Swap and Auth Tests (#1436)

---
+++ packages/e2e/utils/strings.ts
@@ -0,0 +1,16 @@
+/**
+ * Takes an integer and formats it into a compacted string. E.g. 200000 -> 200K
+ * The output is the same to what's displayed in the UI.
+ * @param number
+ * @returns string
+ */
+export const compact = (number: number | bigint): string => {
+  const getCompactFormatter = (decimalPlaces?: number) =>
+    new Intl.NumberFormat("en-US", {
+      notation: "compact",
+      compactDisplay: "short",
+      maximumFractionDigits: decimalPlaces,
+    })
+
+  return getCompactFormatter(4).format(number)
+}
