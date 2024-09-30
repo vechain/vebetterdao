f286457cca0c8f1806c6003bea03efadce619dfe
Davide Carpini <carpini.davide@gmail.com>
2024-09-30 12:22:28 +0200

    fix: profile b3tr action summary

--- apps/frontend/src/app/profile/components/ProfileBetterActions/ProfileBetterActions.tsx
+++ apps/frontend/src/app/profile/components/ProfileBetterActions/ProfileBetterActions.tsx
@@ -3,12 +3,14 @@ import { ActivityCalendar } from "./components/ActivityCalendar"
 import { useState } from "react"
 import { ActivityList } from "./components/ActivityList"
 import { PendingActions } from "./components/PendingActions"
+import { YourBetterActionsSummary } from "./components/YourBetterActionsSummary"
 
 export const ProfileBetterActions = () => {
   const [isCalendarView, setIsCalendarView] = useState(true)
   return (
     <VStack gap={4}>
       <PendingActions />
+      <YourBetterActionsSummary />
       {isCalendarView ? (
         <ActivityCalendar setIsCalendarView={setIsCalendarView} />
       ) : (
