152d946869624e09c04a75a3f2063244dd8e6ba9
Erik Nucibella <eriknucib@live.it>
2024-10-03 19:29:19 +0800

    fix: tsdoc, remove thor

--- apps/frontend/src/api/blockchain/hooks/useCurrentBlock.ts
+++ apps/frontend/src/api/blockchain/hooks/useCurrentBlock.ts
@@ -1,19 +1,15 @@
 import { getConfig } from "@repo/config"
 import { useQuery } from "@tanstack/react-query"
-import { useConnex } from "@vechain/dapp-kit-react"
 
 export const currentBlockQueryKey = () => ["CURRENT_BLOCK"]
 
 const nodeUrl = getConfig().nodeUrl
 
 /**
- *
- * @returns  the current block
+ * Fetches the current block from the blockchain. The block is refetched every 10 seconds.
+ * @returns the current block
  */
 export const useCurrentBlock = () => {
-  const { thor } = useConnex()
-
-  thor.status.head
   return useQuery({
     queryKey: currentBlockQueryKey(),
     queryFn: async () => {
