6e3693aa09fb9b35d3429f01d9f4b1f67bdab548
Piero Bassa <pierobassa222@gmail.com>
2024-10-03 11:00:33 +0200

    refactor: use pagination with async possibility of fetching items

--- apps/frontend/src/hooks/usePagination.ts
+++ apps/frontend/src/hooks/usePagination.ts
@@ -1,23 +1,37 @@
-import { useState } from "react"
+// hooks/usePagination.ts
+import { useState, useEffect } from "react"
 
 /**
- * Hook to paginate items
+ * Hook to paginate items with infinite scroll support
  * @param items - The items to paginate
  * @param itemsPerPage - The number of items per page
- * @returns An object containing the current items, whether there are more items to load, and a function to load more items
+ * @returns An object containing the current items, whether there are more items to load, a function to load more items, and the loading state
  */
-function usePagination<T>(items: T[], itemsPerPage: number) {
+export function usePagination<T>(items: T[], itemsPerPage: number) {
   const [currentPage, setCurrentPage] = useState(1)
+  const [loading, setLoading] = useState(false)
+
   const maxPage = Math.ceil(items.length / itemsPerPage)
 
   const currentItems = items.slice(0, currentPage * itemsPerPage)
   const hasMore = currentPage < maxPage
 
   const loadMore = () => {
-    if (hasMore) setCurrentPage(prevPage => prevPage + 1)
+    if (hasMore && !loading) {
+      setLoading(true)
+      setTimeout(() => {
+        setCurrentPage(prevPage => prevPage + 1)
+        setLoading(false)
+      }, 200)
+    }
   }
 
-  return { currentItems, hasMore, loadMore }
-}
+  useEffect(() => {
+    // Reset pagination only if the total number of items decreases
+    if (items.length < (currentPage - 1) * itemsPerPage + 1) {
+      setCurrentPage(1)
+    }
+  }, [items.length, currentPage, itemsPerPage])
 
-export default usePagination
+  return { currentItems, hasMore, loadMore, loading }
+}
