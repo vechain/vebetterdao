45b719a6d8833c38dfa7279384aae7086daa42e3
aya <aya.belarbi16@gmail.com>
2024-10-04 10:53:37 +0200

    feat(mp): constant for mp events

---
+++ apps/frontend/src/constants/AnalyticsEvents.ts
@@ -0,0 +1,8 @@
+export enum ButtonClickProperties {
+  // DASHBOARD PAGE EVENTS
+  SWAP_TOKENS = "Swap Tokens",
+  GET_VOT3 = "Get VOT3",
+  GET_B3TR = "Get B3TR",
+  CONVERT_NOW = "Convert now",
+  SWAP_CONFIRMED = "Swap Confirmed",
+}
