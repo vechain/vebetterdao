68663b6b1cde2e3c99b9828852295fec16286b5c
Davide Carpini <carpini.davide@gmail.com>
2024-10-09 17:33:17 +0200

    fix: add link

--- apps/frontend/src/app/components/ActionBanners/ActionBanner.tsx
+++ apps/frontend/src/app/components/ActionBanners/ActionBanner.tsx
@@ -67,7 +67,7 @@ export const ActionBanner = () => {
   const showLowVthoBanner = !!account && isLowOnVtho && ownsTokens && !isBalanceLoading
 
   const slides = useMemo(() => {
-    const bannerComponents = []
+    const bannerComponents = [<DoActionBanner key="do-action" />]
     if (showLowVthoBanner) bannerComponents.push(<LowVthoBanner key="low-vtho" />)
     if (showDoActionBanner) bannerComponents.push(<DoActionBanner key="do-action" />)
     if (showCastVoteBanner) bannerComponents.push(<CastVoteBanner key="cast-vote" />)
