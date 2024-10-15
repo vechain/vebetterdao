2e8459099c89766a285b38179e1c97ee4939434f
aya <aya.belarbi16@gmail.com>
2024-10-15 18:12:37 +0100

    chore(apps): modification from review

--- apps/frontend/src/app/apps/[appId]/error.tsx
+++ apps/frontend/src/app/apps/[appId]/error.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Heading, Image, VStack, Button, Text, Box } from "@chakra-ui/react"
+import { Heading, Image, VStack, Button, Text } from "@chakra-ui/react"
 import { useTranslation } from "react-i18next"
 import { useRouter } from "next/navigation"
 
@@ -16,20 +16,14 @@ export default function CustomError({ error }: CustomErrorProps) {
     router.push("/")
   }
 
-  console.log("error", error)
-
   return (
-    <VStack h="full" alignItems={"left"}>
-      <Box py={"7%"}>
-        <Image src="/images/error-to-define.svg" alt="error" />
-      </Box>
-      <VStack alignItems={"left"} w={["100%", "100%", "100%", "50%"]}>
-        <Text color="#6A6A6A">{t("{{value}}", { value: error })}</Text>
-        <Heading>{t("Something went wrong. Let’s try that again!")}</Heading>
-        <Button w={"150px"} variant={"primaryAction"} onClick={goToHomePage} my={"20px"}>
-          {t("Go back home")}
-        </Button>
-      </VStack>
+    <VStack h="full" alignItems="start" alignSelf="start" w={["100%", "100%", "100%", "50%"]}>
+      <Image boxSize={"190px"} src="/images/error-to-define.svg" alt="error" />
+      <Text color="#6A6A6A">{t("{{value}}", { value: error })}</Text>
+      <Heading>{t("Something went wrong. Let’s try that again!")}</Heading>
+      <Button variant={"primaryAction"} onClick={goToHomePage} my={"20px"}>
+        {t("Go back home")}
+      </Button>
     </VStack>
   )
 }
--- apps/frontend/src/app/not-found.tsx
+++ apps/frontend/src/app/not-found.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import { Heading, Image, VStack, Button, Text, Box } from "@chakra-ui/react"
-import React from "react"
+import { Heading, Image, VStack, Button, Text } from "@chakra-ui/react"
 import { useTranslation } from "react-i18next"
 import { useRouter } from "next/navigation"
 
@@ -14,17 +13,13 @@ export default function Custom404() {
   }
 
   return (
-    <VStack h="full" alignItems={"left"}>
-      <Box py={"7%"}>
-        <Image src="/images/not-found-404.svg" alt="404" />
-      </Box>
-      <VStack alignItems={"left"} w={["100%", "100%", "100%", "50%"]}>
-        <Text color="#6A6A6A">{t("Error 404")}</Text>
-        <Heading>{t("We can't find what you're looking for. Let's head back and try again!")}</Heading>
-        <Button w={"150px"} variant={"primaryAction"} onClick={goToHomePage} my={"20px"}>
-          {t("Go back home")}
-        </Button>
-      </VStack>
+    <VStack h="full" alignSelf="start" alignItems="start" w={["100%", "100%", "100%", "50%"]}>
+      <Image boxSize={"190px"} src="/images/not-found-404.svg" alt="404" />
+      <Text color="#6A6A6A">{t("Error 404")}</Text>
+      <Heading>{t("We can't find what you're looking for. Let's head back and try again!")}</Heading>
+      <Button variant={"primaryAction"} onClick={goToHomePage} my={"20px"}>
+        {t("Go back home")}
+      </Button>
     </VStack>
   )
 }
