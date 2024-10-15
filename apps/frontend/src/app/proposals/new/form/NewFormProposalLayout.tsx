b654f887698adca625b2a51a26a0d0c053d8cfda
Erik Nucibella <eriknucib@live.it>
2024-10-15 13:19:43 +0700

    fix: pre-rendering ssr issue in new/fomr/layout

---
+++ apps/frontend/src/app/proposals/new/form/NewFormProposalLayout.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { Grid, GridItem } from "@chakra-ui/react"
+import { CreateProposalStepperCard } from "./components/CreateProposalStepperCard"
+import { useNewProposalPageGuard } from "./hooks/useNewProposalPageGuard"
+import { useLayoutEffect } from "react"
+import { useRouter } from "next/navigation"
+
+type Props = {
+  children: React.ReactNode
+}
+export const NewFormProposalLayoutContent = ({ children }: Readonly<Props>) => {
+  const router = useRouter()
+  const pageGuardResult = useNewProposalPageGuard()
+
+  //   redirect the user to the beginning of the form if the required data is missing
+  //   this happens in case the user tries to access this page directly
+  useLayoutEffect(() => {
+    if (!pageGuardResult.isVisitAuthorized) {
+      router.push(pageGuardResult.redirectPath ?? "/proposals/new")
+    }
+  }, [pageGuardResult, router])
+
+  if (!pageGuardResult.isVisitAuthorized) return null
+
+  return (
+    <Grid
+      templateColumns={["repeat(1, 1fr)", "repeat(1, 1fr)", "repeat(3, 1fr)"]}
+      gap={8}
+      w="full"
+      data-testid="form-proposal-layout">
+      <GridItem colSpan={[1, 1, 2]}>{children}</GridItem>
+      <GridItem colSpan={1}>
+        <CreateProposalStepperCard />
+      </GridItem>
+    </Grid>
+  )
+}
