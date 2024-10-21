ff39d3d67541e2f9198cbd1e1c0d432ae506d18c
roisindowling <roisin.dowling@vechain.org>
2024-10-21 12:39:10 +0100

    add imports after pragma

--- packages/contracts/contracts/interfaces/IVOT3.sol
+++ packages/contracts/contracts/interfaces/IVOT3.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-import "@openzeppelin/contracts/utils/structs/Checkpoints.sol";
-
 pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/utils/structs/Checkpoints.sol";
 
 interface IVOT3 {
   error AccessControlBadConfirmation();
