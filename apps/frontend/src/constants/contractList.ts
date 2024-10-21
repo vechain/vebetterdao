e3f232083e19ee0bb865ef50f668e2e604c3a0c0
Dan Rusnac <rusnac14@gmail.com>
2024-10-21 18:24:26 +0200

    fix: address to uppercase

--- apps/frontend/src/constants/contractList.ts
+++ apps/frontend/src/constants/contractList.ts
@@ -3,7 +3,7 @@ const config = getConfig()
 
 // function that returns the contract from the list by the address
 export const getContractByAddress = (address: string) => {
-  return CONTRACT_LIST.find(contract => contract.contractAddress === address)
+  return CONTRACT_LIST.find(contract => contract.contractAddress.toUpperCase() === address.toUpperCase())
 }
 
 export const CONTRACT_LIST = [
