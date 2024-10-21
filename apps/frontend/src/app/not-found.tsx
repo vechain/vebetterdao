ebd58b87b2c31f7a9ac0d4888164487a366e8a79
aya <75953386+ayabelarbi@users.noreply.github.com>
2024-10-21 15:00:12 +0200

    Feat/error handling enhancements (#1446)
    * fix(fe): custom error 404 handle
    
    * docs: traduction
    
    * fix(fe): custom Unhandled Runtime Error
    
    * docs(apps): generating translation
    
    * chore(apps): modification from review
    
    * feat(docs): complete translation
    
    * feat(fe): remove unused sizing height

---
+++ apps/frontend/src/app/not-found.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { Heading, Image, VStack, Button, Text } from "@chakra-ui/react"
+import { useTranslation } from "react-i18next"
+import { useRouter } from "next/navigation"
+
+export default function Custom404() {
+  const { t } = useTranslation()
+  const router = useRouter()
+
+  const goToHomePage = () => {
+    router.push("/")
+  }
+
+  return (
+    <VStack alignSelf="start" alignItems="start" w={["100%", "100%", "100%", "50%"]}>
+      <Image boxSize={"190px"} src="/images/not-found-404.svg" alt="404" />
+      <Text color="#6A6A6A">{t("Error 404")}</Text>
+      <Heading>{t("We can't find what you're looking for. Let's head back and try again!")}</Heading>
+      <Button variant={"primaryAction"} onClick={goToHomePage} my={"20px"}>
+        {t("Go back home")}
+      </Button>
+    </VStack>
+  )
+}
