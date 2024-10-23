64d71dafe76f2c6069425f7d465b4164c0c2d47b
Dan Rusnac <rusnac14@gmail.com>
2024-10-23 12:36:05 +0200

    fix: tests

--- apps/frontend/src/app/components/DashboardSideBar.tsx
+++ apps/frontend/src/app/components/DashboardSideBar.tsx
@@ -1,11 +1,20 @@
-import { Show, VStack } from "@chakra-ui/react"
+import { Show, Spinner, VStack } from "@chakra-ui/react"
 import { GmNFT } from "../../components/GmNFT"
 import { ManagedAppsCard } from "../../components/ManagedAppsCard"
 import { YourBetterActionsCard } from "./YourBetterActionsCard"
 import { TokensBalance } from "./TokensBalance"
-import { Leaderboard } from "./Leaderboard"
 import { SupplyBreakdownCard } from "./SupplyBreakdownCard"
 import { CantVoteCard } from "./CantVoteCard/CantVoteCard"
+import dynamic from "next/dynamic"
+
+const Leaderboard = dynamic(() => import("./Leaderboard").then(mod => mod.Leaderboard), {
+  ssr: false,
+  loading: () => (
+    <VStack w="full" spacing={12} h="80vh" justify="center">
+      <Spinner size={"lg"} />
+    </VStack>
+  ),
+})
 
 export const DashboardSideBar = () => {
   return (
