cf217d0bf6599d2564f4ab5e380195956e02ebcd
Erik Nucibella <eriknucib@live.it>
2024-10-29 10:22:45 +0700

    fix(SwapB3trBalance): align with new explorer, polish

--- apps/frontend/src/app/profile/components/ProfileBalance/ProfileBalance.tsx
+++ apps/frontend/src/app/profile/components/ProfileBalance/ProfileBalance.tsx
@@ -1,14 +1,20 @@
 import { VStack } from "@chakra-ui/react"
 import { UserTransactions } from "./components/UserTransactions"
 import { SwapB3trVot3 } from "@/components/GmNFTAndNodeCard/components/SwapB3trVot3"
+import { compareAddresses } from "@repo/utils/AddressUtils"
+import { useWallet } from "@vechain/dapp-kit-react"
 
 type Props = {
   address: string
 }
 export const ProfileBalance = ({ address }: Props) => {
+  const { account } = useWallet()
+  const isConnectedUser = compareAddresses(account ?? "", address)
   return (
     <VStack align={"stretch"} gap={4}>
       <SwapB3trVot3
+        address={address}
+        isConnectedUser={isConnectedUser}
         containerProps={{
           w: "full",
           align: "stretch",
