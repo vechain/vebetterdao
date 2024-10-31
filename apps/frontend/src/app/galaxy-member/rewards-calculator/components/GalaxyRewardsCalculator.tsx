54e184f64b7af13b63503e304c2fe204bbeae51e
aya <aya.belarbi16@gmail.com>
2024-10-31 19:45:46 +0700

    feat(FE): wip  calculator component

--- apps/frontend/src/app/galaxy-member/rewards-calculator/components/GalaxyRewardsCalculator.tsx
+++ apps/frontend/src/app/galaxy-member/rewards-calculator/components/GalaxyRewardsCalculator.tsx
@@ -1,66 +1,54 @@
 // import { useTranslation } from "react-i18next"
 import React, { useState, useEffect } from "react"
+import { useWallet } from "@vechain/dapp-kit-react"
 import { GalaxyRewardsCarrousel } from "./GalaxyRewardsCarrousel"
-import { Heading, Text, HStack, Stack, Image, VStack, Skeleton, Select, useMediaQuery } from "@chakra-ui/react"
+import {
+  Heading,
+  Text,
+  HStack,
+  Stack,
+  Image,
+  VStack,
+  Skeleton,
+  Select,
+  useMediaQuery,
+  Card,
+  CardBody,
+} from "@chakra-ui/react"
+import { useSelectedGmNft, useAllocationsRoundsEvents, useVotingRewards } from "@/api"
+import { usePotentialRewards } from "../utils/usePotentialRewards"
+
 import { gmNfts } from "@/constants/gmNfts"
 
-// import { useTranslation } from "react-i18next"
-
-import { useSelectedGmNft, useAllocationsRoundsEvents, useVotingRewards, usePotentialRewards } from "@/api"
-import { useWallet } from "@vechain/dapp-kit-react"
+import { useTranslation } from "react-i18next"
 
 export const GalaxyRewardsCalculator = async () => {
-  const [isAbove800] = useMediaQuery("(min-width: 800px)")
-  // const { t } = useTranslation()
-  const { account } = useWallet()
-
-  if (!account) throw new Error("Account is required")
-
-  const { isGMLoading, gmLevel } = useSelectedGmNft()
-  // gmRewardMultiplier, isXNodeAttachedToGMn gmName
-  const { data: allocationRoundsEvents } = useAllocationsRoundsEvents()
-  // error: allocationRoundEventsError
-
   const [selectedAccountRoundId, setselectedAccountRoundId] = useState<string | undefined>()
   const [selectedRoundId, setselectedRoundId] = useState<string | undefined>()
   const [selectedNft, setSelectedNft] = useState(gmNfts[0])
-
   const [b3TRRewardsApprox, setB3TRRewardsApprox] = useState<number | null>(null)
 
-  const { data: roundsRewards, isLoading: rewardsAccountLoading } = useVotingRewards(selectedAccountRoundId, account)
-  console.log("votingRewardsData", roundsRewards)
-  const roundsRewardsA = roundsRewards?.roundsRewards
+  const { account } = useWallet()
+  if (!account) throw new Error("Account is required")
 
-  // TODO : find the hooks that fetch that
-  // rounds should look like that [] a list with a value roundId: "1", roundId: "2", roundId: "3"...
+  const { t } = useTranslation()
+  const [isAbove800] = useMediaQuery("(min-width: 800px)")
 
+  const { isGMLoading, gmLevel } = useSelectedGmNft()
+  const { data: allocationRoundsEvents } = useAllocationsRoundsEvents()
+  const roundsRewards = useVotingRewards(selectedAccountRoundId, account)
+  const {
+    potentialRewards,
+    isLoading,
+    error: errorGetPotentialRewards,
+  } = usePotentialRewards(selectedRoundId, account, Number(selectedNft?.level))
+
+  console.log("selectedAccountRoundId before calling useVotingRewards", selectedAccountRoundId)
+  console.log("account before calling useVotingRewards", account)
+  console.log("roundsRewards", roundsRewards)
+
+  const showClaimB3trBanner = !!account && roundsRewards.data?.total && Number(roundsRewards.data.total) !== 0
   const invertedCreatedRounds = allocationRoundsEvents?.created.slice().reverse()
-
-  const handleAccountRoundChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setselectedAccountRoundId(e.target.value)
-  }
-
-  const handleRoundChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setselectedRoundId(e.target.value)
-  }
-
-  const selectedRoundReward = roundsRewardsA?.find(reward => reward.roundId === selectedAccountRoundId)
-  console.log("selectedRoundReward", selectedRoundReward)
-  // CAROUSEL
-
-  useEffect(() => {
-    const FetchPotentialRewards = async () => {
-      const { data } = await usePotentialRewards(selectedRoundId, account, selectedNft?.level)
-      if (data) {
-        setB3TRRewardsApprox(data.rewards)
-      } else {
-        console.log("response of usePotentialRewards", data)
-        setB3TRRewardsApprox(null) // or handle the undefined case as needed
-      }
-    }
-
-    FetchPotentialRewards()
-  }, [selectedNft, account, selectedRoundId])
 
   const handleNftSelect = (GMId: any) => {
     const selected = gmNfts.find(nft => nft.level === GMId)
@@ -69,17 +57,29 @@ export const GalaxyRewardsCalculator = async () => {
     }
   }
 
-  console.log("selectedGMLevel", selectedNft)
-  // TODO: remember to put in the PR the technical adaptation of the formula for the rewards calculation for each NFTMultiplier
+  const handleAccountRoundChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setselectedAccountRoundId(e.target.value)
+  }
+
+  const handleRoundChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setselectedRoundId(e.target.value)
+  }
+  // const roundsRewardsA = roundsRewards?.roundsRewards
+  // const selectedRoundReward = roundsRewardsA?.find(reward => reward.roundId === selectedAccountRoundId)
+
+  useEffect(() => {
+    if (!isLoading) {
+      setB3TRRewardsApprox(potentialRewards)
+    }
+  }, [potentialRewards, isLoading])
+
+  useEffect(() => {
+    console.log("selectedNft", selectedNft)
+  }, [selectedNft])
 
   return (
     <Stack border={"1px solid black"}>
-      {/* OK Premiere partie */}
-      {/* Maybe no need because it's taking in charge in the previous modal ???? Need to check when running the project */}
-      {/* <Heading>Your NFT</Heading> */}
-
       <HStack>
-        {/* Nothing to show if the connected wallet don't have GMNFT attached to his wallet ( or say, you don't have GMNFT attached ) */}
         <Skeleton isLoaded={!isGMLoading}>
           {gmLevel ? (
             <Image
@@ -94,8 +94,8 @@ export const GalaxyRewardsCalculator = async () => {
           )}
         </Skeleton>
 
-        <VStack>
-          <Heading> {}</Heading>
+        <VStack w="30%">
+          <Heading></Heading>
 
           <Select placeholder="Select round" onChange={handleAccountRoundChange}>
             {invertedCreatedRounds?.map(round => (
@@ -105,34 +105,56 @@ export const GalaxyRewardsCalculator = async () => {
             ))}
           </Select>
 
-          <Skeleton isLoaded={!rewardsAccountLoading}>
-            {/* <Text >Rewards: {selectedRoundReward?.formattedRewards}</Text> */}
+          {/* <Skeleton isLoaded={!rewardsAccountLoading}> */}
+          {/* <Text>Rewards: {selectedRoundReward?.formattedRewards}</Text> */}
 
-            <Text>{selectedRoundReward?.formattedRewards}</Text>
-          </Skeleton>
+          {/* <Text>{selectedRoundReward?.formattedRewards}</Text> */}
+          <Text>{showClaimB3trBanner}</Text>
+
+          {/* </Skeleton> */}
         </VStack>
       </HStack>
 
-      <GalaxyRewardsCarrousel selectedGMLevel={handleNftSelect} />
-
       <HStack>
-        {/* <Heading> Estimated Rewards: </Heading> */}
+        <GalaxyRewardsCarrousel selectedGMLevel={handleNftSelect} />
 
-        <Select placeholder="Select round" onChange={handleRoundChange}>
-          {invertedCreatedRounds?.map(round => (
-            <option key={round.roundId} value={round.roundId}>
-              {round.roundId}
-            </option>
-          ))}
-        </Select>
-        {/* TODO see how that works */}
+        <VStack w={"30%"}>
+          <Heading></Heading>
 
-        <Text>{b3TRRewardsApprox}</Text>
-        {/* Estimate Rewards:  */}
-        <HStack>
-          {/* TODO:  MAKE A LITTLE ANIMATION TO SAY -> YOU ARE EARNING +x% BY UPGRADING YOUR NFT (make a pourcentage ) */}
-          {/* Percentage_Increase=((Old_Value - New_Value)/Old_Value)*100 */}
-        </HStack>
+          <Select placeholder="Select round" onChange={handleRoundChange}>
+            {invertedCreatedRounds?.map(round => (
+              <option key={round.roundId} value={round.roundId}>
+                {round.roundId}
+              </option>
+            ))}
+          </Select>
+
+          {errorGetPotentialRewards ? (
+            <Card variant={"filled"} w="full" rounded={"xl"}>
+              <CardBody>
+                <Text color={"red.500"}>{errorGetPotentialRewards.message}</Text>
+              </CardBody>
+            </Card>
+          ) : (
+            <Card variant={"filled"} w="full" rounded={"xl"}>
+              <CardBody>
+                <Text fontSize="xl">{b3TRRewardsApprox}</Text>
+              </CardBody>
+            </Card>
+          )}
+          <Text>
+            {t(
+              "The displayed rewards are indicative and based on the rounds you have voted in. If you select a round you haven't voted in, you won't be able to project the upcoming rewards.",
+            )}
+          </Text>
+        </VStack>
+      </HStack>
+      {/* <Heading> Rewards Increase </Heading> */}
+      <HStack>
+        {/* TODO: little animation for rendment % ) */}
+        {/* Percentage_Increase=((Old_Value - New_Value)/Old_Value)*100 */}
+        {/* <Text>+</Text>
+        <Heading> X% </Heading> */}
       </HStack>
     </Stack>
   )
--- apps/frontend/src/app/galaxy-member/rewards-calculator/components/GalaxyRewardsCarrousel.tsx
+++ apps/frontend/src/app/galaxy-member/rewards-calculator/components/GalaxyRewardsCarrousel.tsx
@@ -6,110 +6,119 @@ import { Box, Flex, IconButton, useBreakpointValue, Image, useMediaQuery, Text }
 import { gmNfts } from "@/constants/gmNfts"
 
 type Props = {
-  selectedGMLevel: (GMLevel: any) => void
+  selectedGMLevel: (GMLevel: number) => void
 }
 
 export const GalaxyRewardsCarrousel = ({ selectedGMLevel }: Props) => {
   const { t } = useTranslation()
   const [isAbove800] = useMediaQuery("(min-width: 800px)")
   const [currentIndex, setCurrentIndex] = useState(0)
-  // TODO change the useBrakPoint with ChakraUI [ "", "", ""]
   const visibleCards = useBreakpointValue({ base: 1, md: 3, lg: 5 }) || 1
 
   const nextCard = () => {
-    setCurrentIndex(prevIndex => (prevIndex + 1 >= gmNfts.length ? 0 : prevIndex + 1))
+    const newIndex = (currentIndex + 1) % gmNfts.length
+    setCurrentIndex(newIndex)
+    if (gmNfts[newIndex]) {
+      selectedGMLevel(Number(gmNfts[newIndex].level)) // TODO: Find a way to uniform type ( avoiding casting)
+    }
   }
 
   const prevCard = () => {
-    setCurrentIndex(prevIndex => (prevIndex - 1 < 0 ? gmNfts.length - 1 : prevIndex - 1))
+    const newIndex = (currentIndex - 1 + gmNfts.length) % gmNfts.length
+    setCurrentIndex(newIndex)
+    if (gmNfts[newIndex]) {
+      selectedGMLevel(Number(gmNfts[newIndex].level)) // TODO: Find a way to uniform type ( avoiding casting)
+    }
   }
 
   const getVisibleNFTs = () => {
     const visibleNFTs = []
     for (let i = 0; i < visibleCards; i++) {
       const index = (currentIndex + i) % gmNfts.length
-      console.log("index", index)
       visibleNFTs.push(gmNfts[index])
     }
-    console.log("visibleNFTs", visibleNFTs)
     return visibleNFTs
   }
 
-  const handleSelect = (GMLevel: any) => {
+  const handleSelect = (GMLevel: number) => {
     selectedGMLevel(GMLevel)
   }
 
   return (
-    <Box border={"1px solid black"}>
-      <Flex alignItems="center" justifyContent="center" height={{ base: "300px", md: "400px" }}>
-        {getVisibleNFTs().map((nft, index) => (
-          <Box
-            key={nft?.level}
-            width={{ base: "200px", md: "250px" }}
-            height={{ base: "250px", md: "300px" }}
-            margin="0 10px"
-            position="relative"
-            transform={index === Math.floor(visibleCards / 2) ? "scale(1.1)" : "scale(0.9)"}
-            transition="all 0.3s ease"
-            // JUST MAKE SURE THAT nft.level = GMId ( normally yes but not sure about that)
-            onClick={() => handleSelect(nft?.level)}
-            cursor="pointer">
+    <Box position="relative" overflow="hidden" py={4} border={"1px solid black"}>
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        // border={"1px solid black"}
+        height={{ base: "250px", md: "300px" }}>
+        {getVisibleNFTs()
+          .filter(nft => nft !== undefined)
+          .map((nft, index) => (
             <Box
-              position="absolute"
-              top="0"
-              left="0"
-              right="0"
-              bottom="0"
-              borderRadius="20px"
-              background="linear-gradient(180deg, rgba(255, 255, 255, 0.1) 0%, rgba(255, 255, 255, 0) 100%)"
-              border="1px solid rgba(255, 255, 255, 0.2)"
-            />
-            <Flex
-              direction="column"
-              alignItems="center"
-              justifyContent="space-between"
-              height="100%"
-              padding="20px"
+              key={nft?.level}
+              width={{ base: "150px" }}
+              height={{ base: "200px" }}
+              margin="0 5px"
               position="relative"
-              zIndex="1">
-              <Text fontSize="lg" fontWeight="bold">
-                {t("Level")} {nft?.level}
-              </Text>
-              <Image
-                src={nft?.image}
-                alt={nft?.name}
-                w={isAbove800 ? "126px" : "64px"}
-                h={isAbove800 ? "126px" : "64px"}
-                rounded="7px"
-                borderRadius="20px"
-              />{" "}
-              <Text fontSize="xl" fontWeight="bold">
-                {nft?.name}
-              </Text>
-            </Flex>
-          </Box>
-        ))}
+              transform={index === Math.floor(visibleCards / 2) ? "scale(1.1)" : "scale(0.9)"}
+              transition="all 0.3s ease"
+              onClick={() => handleSelect(Number(nft.level))}
+              cursor="pointer">
+              <Box
+                position="absolute"
+                top="0"
+                left="0"
+                right="0"
+                bottom="0"
+                borderRadius="200px"
+                background="linear-gradient(180deg, rgba(255, 255, 255, 0.1) 0%, rgba(255, 255, 255, 0) 100%)"
+                border="1px solid rgba(255, 255, 255, 0.2)"
+              />
+              <Flex
+                direction="column"
+                alignItems="center"
+                justifyContent="space-between"
+                height="100%"
+                position="relative"
+                zIndex="1">
+                <Text fontSize="md" fontWeight="bold">
+                  {t("Level")} {nft?.level}
+                </Text>
+                <Image
+                  src={nft?.image}
+                  alt={nft?.name}
+                  w={isAbove800 ? "300px" : "120px"}
+                  h={isAbove800 ? "300px" : "120px"}
+                  rounded="7px"
+                  borderRadius="20px"
+                />
+                <Text fontSize="lg" fontWeight="bold">
+                  {nft.name}
+                </Text>
+              </Flex>
+            </Box>
+          ))}
       </Flex>
-      <IconButton
-        aria-label="Previous card"
-        icon={<FaChevronLeft />}
-        position="absolute"
-        left="10px"
-        top="50%"
-        transform="translateY(-50%)"
-        onClick={prevCard}
-        rounded="full"
-      />
-      <IconButton
-        aria-label="Next card"
-        icon={<FaChevronRight />}
-        position="absolute"
-        right="10px"
-        top="50%"
-        transform="translateY(-50%)"
-        onClick={nextCard}
-        rounded="full"
-      />
+      <Box display="flex" justifyContent="center" alignItems="center" position="relative">
+        <IconButton
+          aria-label="Previous card"
+          icon={<FaChevronLeft />}
+          alignSelf="center"
+          transform="translateY(-50%)"
+          onClick={prevCard}
+          rounded="full"
+          zIndex={2}
+        />
+        <IconButton
+          aria-label="Next card"
+          alignSelf="center"
+          icon={<FaChevronRight />}
+          transform="translateY(-50%)"
+          onClick={nextCard}
+          rounded="full"
+          zIndex={2}
+        />
+      </Box>
     </Box>
   )
 }
