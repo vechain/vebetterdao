a8539312501cc748ede9a6cf72b87eac3dbac50a
roisindowling <roisin.dowling@vechain.org>
2024-10-31 15:41:30 +0000

    Add X2EarnCreator SC to be used for creator NFTs

---
+++ packages/contracts/contracts/X2EarnCreator.sol
@@ -0,0 +1,188 @@
+// SPDX-License-Identifier: MIT
+
+//                                      #######
+//                                 ################
+//                               ####################
+//                             ###########   #########
+//                            #########      #########
+//          #######          #########       #########
+//          #########       #########      ##########
+//           ##########     ########     ####################
+//            ##########   #########  #########################
+//              ################### ############################
+//               #################  ##########          ########
+//                 ##############      ###              ########
+//                  ############                       #########
+//                    ##########                     ##########
+//                     ########                    ###########
+//                       ###                    ############
+//                                          ##############
+//                                    #################
+//                                   ##############
+//                                   #########
+
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract X2EarnCreator is
+  Initializable,
+  ERC721Upgradeable,
+  ERC721URIStorageUpgradeable,
+  ERC721PausableUpgradeable,
+  ERC721EnumerableUpgradeable,
+  AccessControlUpgradeable,
+  UUPSUpgradeable
+{
+  // ---------------- Roles ----------------
+  bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+  bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+  bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
+  // ---------------- Errors ----------------
+  /// @dev Error thrown when a transfer attempt is made, as transfers are disabled
+  error TransfersDisabled();
+
+  // ---------------- Storage ----------------
+  /// @notice Storage structure for X2EarnCreator
+  /// @custom:storage-location erc7201:b3tr.storage.X2EarnCreator
+  struct X2EarnCreatorStorage {
+    uint256 nextTokenId;
+  }
+
+  /// @notice Storage slot for X2EarnCreator
+  /// @dev keccak256(abi.encode(uint256(keccak256("b3tr.storage.X2EarnCreator")) - 1)) & ~bytes32(uint256(0xff))
+  bytes32 private constant X2EarnCreatorStorageLocation =
+    0xaf8fa2a2e81e5e00a4ef8747fbca475174c33b675ca2c56fe05a83bfd2d8fc00;
+
+  /// @dev Retrieves the stored `X2EarnCreator` from its designated slot
+  function _getX2EarnCreatorStorage() private pure returns (X2EarnCreatorStorage storage $) {
+    assembly {
+      $.slot := X2EarnCreatorStorageLocation
+    }
+  }
+
+  /// @custom:oz-upgrades-unsafe-allow constructor
+  constructor() {
+    _disableInitializers();
+  }
+
+  /// @notice Initializes the contract with role-based access control
+  /// @param defaultAdmin Address to be assigned the default admin role
+  /// @param pauser Address to be assigned the pauser role
+  /// @param minter Address to be assigned the minter role
+  /// @param upgrader Address to be assigned the upgrader role
+  function initialize(address defaultAdmin, address pauser, address minter, address upgrader) public initializer {
+    __ERC721_init("XAppCreator", "XAC");
+    __ERC721URIStorage_init();
+    __ERC721Pausable_init();
+    __AccessControl_init();
+    __UUPSUpgradeable_init();
+    __ERC721Enumerable_init();
+
+    require(defaultAdmin != address(0), "X2EarnCreator: zero address");
+
+    _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+    _grantRole(PAUSER_ROLE, pauser);
+    _grantRole(MINTER_ROLE, minter);
+    _grantRole(UPGRADER_ROLE, upgrader);
+  }
+
+  /// @notice Pauses all token transfers and minting functions
+  /// @dev Only callable by accounts with the PAUSER_ROLE
+  function pause() public onlyRole(PAUSER_ROLE) {
+    _pause();
+  }
+
+  /// @notice Unpauses the contract to resume token transfers and minting
+  /// @dev Only callable by accounts with the PAUSER_ROLE
+  function unpause() public onlyRole(PAUSER_ROLE) {
+    _unpause();
+  }
+
+  /// @notice Burns a specific token, removing it from circulation
+  /// @param tokenId ID of the token to burn
+  /// @dev Only callable by accounts with the BURNER_ROLE
+  function burn(uint256 tokenId) public onlyRole(BURNER_ROLE) whenNotPaused {
+    _burn(tokenId);
+  }
+
+  /// @notice Mints a new token to a specified address with metadata
+  /// @param to Address that will receive the new token
+  /// @param uri URI pointing to the token's metadata
+  /// @dev Only callable by accounts with the MINTER_ROLE
+  function safeMint(address to, string memory uri) public onlyRole(MINTER_ROLE) whenNotPaused {
+    X2EarnCreatorStorage storage $ = _getX2EarnCreatorStorage();
+    uint256 tokenId = $.nextTokenId++;
+    _safeMint(to, tokenId);
+    _setTokenURI(tokenId, uri);
+  }
+
+  // ---------------- Overrides for Non-Transferability ----------------
+
+  /// @notice Prevents token transfers by reverting any call to transferFrom
+  /// @dev Override to disable token transfers, making tokens non-transferable
+  function transferFrom(address, address, uint256) public pure override(ERC721Upgradeable, IERC721) {
+    revert TransfersDisabled();
+  }
+
+  /// @notice Prevents safe token transfers by reverting any call to safeTransferFrom
+  /// @dev Override to disable safe token transfers, making tokens non-transferable
+  function safeTransferFrom(address, address, uint256, bytes memory) public pure override(ERC721Upgradeable, IERC721) {
+    revert TransfersDisabled();
+  }
+
+  /// @notice Prevents approvals by reverting any call to approve
+  /// @dev Override to disable approval functionality
+  function approve(address, uint256) public pure override(ERC721Upgradeable, IERC721) {
+    revert TransfersDisabled();
+  }
+
+  /// @notice Prevents setting approval for all by reverting any call to setApprovalForAll
+  /// @dev Override to disable approval functionality
+  function setApprovalForAll(address, bool) public pure override(ERC721Upgradeable, IERC721) {
+    revert TransfersDisabled();
+  }
+
+  // ---------------- Upgrade and Utility Overrides ----------------
+  function _authorizeUpgrade(address newImplementation) internal override onlyRole(UPGRADER_ROLE) {}
+
+  function _update(
+    address to,
+    uint256 tokenId,
+    address auth
+  ) internal override(ERC721Upgradeable, ERC721PausableUpgradeable, ERC721EnumerableUpgradeable) returns (address) {
+    return super._update(to, tokenId, auth);
+  }
+
+  function tokenURI(
+    uint256 tokenId
+  ) public view override(ERC721Upgradeable, ERC721URIStorageUpgradeable) returns (string memory) {
+    return super.tokenURI(tokenId);
+  }
+
+  function supportsInterface(
+    bytes4 interfaceId
+  )
+    public
+    view
+    override(ERC721Upgradeable, ERC721URIStorageUpgradeable, ERC721EnumerableUpgradeable, AccessControlUpgradeable)
+    returns (bool)
+  {
+    return super.supportsInterface(interfaceId);
+  }
+
+  function _increaseBalance(
+    address account,
+    uint128 value
+  ) internal override(ERC721Upgradeable, ERC721EnumerableUpgradeable) {
+    super._increaseBalance(account, value);
+  }
+}
