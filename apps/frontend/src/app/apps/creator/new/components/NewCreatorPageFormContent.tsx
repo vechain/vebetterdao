0d83f20dfaaed898162c3cdccff0799d0d3b7903
Victor Emanuel <versaujob@gmail.com>
2024-11-06 12:18:45 +0000

    refactor(creator): redirect whitelisted creators to new app page

--- apps/frontend/src/app/apps/creator/new/components/NewCreatorPageFormContent.tsx
+++ apps/frontend/src/app/apps/creator/new/components/NewCreatorPageFormContent.tsx
@@ -6,7 +6,9 @@ import { useRouter } from "next/navigation"
 import { useCreatorSubmissionFormStore } from "@/store"
 import { useTranslation } from "react-i18next"
 import { CreatorApplicationModal } from "./CreatorApplicationModal"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { useHasCreatorNFT } from "@/api/contracts/x2EarnCreator/hooks"
+import { useWallet } from "@vechain/dapp-kit-react"
 
 export const NewCreatorPageFormContent = () => {
   const { register, reset, setValue, setError, watch, formState, control, handleSubmit, clearErrors } =
@@ -30,6 +32,18 @@ export const NewCreatorPageFormContent = () => {
   const router = useRouter()
   const [submitStatus, setSubmitStatus] = useState<"success" | "error">("success")
   const [submitErrorMessage, setSubmitErrorMessage] = useState("")
+  const { account } = useWallet()
+  const hasCreatorNft = useHasCreatorNFT(account ?? "")
+
+  useEffect(() => {
+    //Users with Creator NFT should be redirected to the new app page
+    if (hasCreatorNft) router.push("/apps/new")
+  }, [hasCreatorNft])
+
+  const navigateToHome = () => {
+    onClose()
+    router.push("/")
+  }
 
   const onSubmit = async (formValues: SubmitCreatorFormData) => {
     try {
@@ -44,18 +58,14 @@ export const NewCreatorPageFormContent = () => {
       signOut({ redirect: false })
       setSubmitStatus("success")
       setSubmitErrorMessage("")
-      onOpen()
-    } catch (error: any) {
+    } catch (error: unknown) {
+      let errorMessage = "An error occurred while submitting the form."
+      if (error instanceof Error) errorMessage = error.message
       setSubmitStatus("error")
-      setSubmitErrorMessage(error.message || "An error occurred while submitting the form.")
+      setSubmitErrorMessage(errorMessage)
+    } finally {
       onOpen()
-      console.error("Failed to submit creator application", error)
     }
-  }
-
-  const navigateToHome = () => {
-    onClose()
-    router.push("/")
   }
 
   return (
@@ -63,11 +73,7 @@ export const NewCreatorPageFormContent = () => {
       <VStack w={{ base: "100%", sm: "100%", md: "80%" }}>
         <HStack justify={{ base: "space-between", md: "center" }} bgColor="#004CFC" w="full" borderRadius="12px" px={5}>
           <Stack maxW={{ base: "100%", sm: "100%", md: "30%" }} py={{ base: 3, md: 5 }}>
-            <Text
-              color="white"
-              fontWeight="bold"
-              fontSize={{ base: "lg", md: "xl" }} // Responsive font size
-            >
+            <Text color="white" fontWeight="bold" fontSize={{ base: "lg", md: "xl" }}>
               {t("Apply for Creator's NFT")}
             </Text>
             <Text color="white" fontSize={{ base: "sm", md: "md" }}>
