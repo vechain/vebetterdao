ab3a0e73165c4a1d8861886c03c5bfb39ec13dc1
aya <aya.belarbi16@gmail.com>
2024-11-08 12:56:05 +0700

    feat(apps):  adding banner bottom

---
+++ apps/frontend/src/components/Banners/JoinB3TRAppsBanner.tsx
@@ -0,0 +1,64 @@
+import { Card, Button, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react"
+import { useTranslation } from "react-i18next"
+import { FaPlus } from "react-icons/fa"
+import { useBreakpoints } from "@/hooks"
+
+export const JoinB3TRAppsBanner = () => {
+  const { t } = useTranslation()
+  const { isMobile } = useBreakpoints()
+
+  // TODO : change that once the endorsement will be complete
+  const openGrantPage = () => {
+    window.open("https://vechain.org/grants/", "_blank", "noopener noreferrer")
+  }
+
+  return (
+    <Card
+      w={"full"}
+      bg="#B1F16C"
+      color="black"
+      overflow={"hidden"}
+      borderRadius={"12px"}
+      bgImage="url('/images/community-green-blob.png')"
+      bgPosition="top"
+      bgSize="full">
+      <HStack justifyContent={"space-between"}>
+        <VStack alignItems={"flex-start"} my={"50px"} mx={"70px"} w={"50%"}>
+          {isMobile && (
+            // TODO: double check if only robot's head is display
+            <Image
+              src="/images/mascote/mascote-welcoming-left-head.png"
+              alt="mascote-welcoming-head"
+              width="100%"
+              boxSize="100px"
+              objectFit="cover"
+              objectPosition="top"
+            />
+          )}
+          <Heading fontSize="2xl" fontWeight="bold" color="#252525">
+            {t("Do you have a dApp to join the VeBetter DAO ecosystem?")}
+          </Heading>
+          <Text color="#252525">
+            {t(
+              "Do you have a sustainable application and want to become part of our ecosystem? Join our discord channel and introduce yourself and your app!",
+            )}
+          </Text>
+          <Button variant="applyButton" onClick={openGrantPage} leftIcon={<FaPlus />}>
+            {t("Apply now")}
+          </Button>
+        </VStack>
+        {!isMobile && (
+          <Image
+            alignSelf={"flex-end"}
+            src="/images/mascote/mascote-welcoming.png"
+            alt="mascote-welcoming"
+            boxSize="200px"
+            overflow={"hidden"}
+            objectFit="contain"
+            transform="rotate(-15deg) scale(2.3) translateY(15px) translateX(10px)"
+          />
+        )}
+      </HStack>
+    </Card>
+  )
+}
