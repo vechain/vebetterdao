e161aa0e1aa3c5bfcb9e1666e853e84ec26b9f30
imagobea <himagobea@gmail.com>
2024-11-08 17:40:14 +0100

    refactor: gm to render new endorsement styles

--- apps/frontend/src/app/xnode/XNodeContent/components/EndorsingAppCard.tsx
+++ apps/frontend/src/app/xnode/XNodeContent/components/EndorsingAppCard.tsx
@@ -1,5 +1,7 @@
-import { useAppEndorsementScore, useAppEndorsers, useXNode } from "@/api"
+import { useAppEndorsementStatus, useAppEndorsers, useXNode } from "@/api"
 import { useAppEndorsedEvents } from "@/api/contracts/xApps/hooks/endorsement/useAppEndorsedEvents"
+import { EndorsementDetails } from "@/app/apps/[appId]/components/AppEndorsementInfoCard/EndorsementDetails"
+import { EndorsementStatusCallout } from "@/app/apps/[appId]/components/AppEndorsementInfoCard/EndorsementStatusCallout"
 import { UnendorseAppModal } from "@/app/apps/components/UnendorseAppModal"
 import { useEstimateBlockTimestamp } from "@/hooks/useEstimateBlockTimestamp"
 import {
@@ -11,25 +13,31 @@ import {
   Heading,
   HStack,
   Image,
-  Show,
+  Stack,
   Text,
   useBreakpointValue,
   useDisclosure,
   VStack,
 } from "@chakra-ui/react"
-import { UilCheckCircle, UilInfoCircle, UilSearch } from "@iconscout/react-unicons"
+import { UilInfoCircle, UilSearch } from "@iconscout/react-unicons"
 import dayjs from "dayjs"
 import { useRouter } from "next/navigation"
-import { useCallback, useMemo } from "react"
+import { useCallback } from "react"
 import { useTranslation } from "react-i18next"
 
 export const EndorsingAppCard = () => {
   const { t } = useTranslation()
-  const { isEndorsingApp, endorsedApp, xNodePoints, xNodeId } = useXNode()
+
+  const { isXNodeLoading, isEndorsingApp, endorsedApp, xNodePoints, xNodeId } = useXNode()
   // get the number of endorsers for the endorsed app
-  const endorsersCount = useAppEndorsers(endorsedApp?.id)?.data?.length ?? 0
-  // get app total endorsement score
-  const appScore = useAppEndorsementScore(endorsedApp?.id)?.data ?? 0
+  const { data: appEndorsers, isLoading: isAppEndorsersLoading } = useAppEndorsers(endorsedApp?.id ?? "")
+  // get app status and score
+  const {
+    score: endorsementScore,
+    status: endorsementStatus,
+    threshold: endorsementThreshold,
+    isLoading: isEndorsementStatusLoading,
+  } = useAppEndorsementStatus(endorsedApp?.id ?? "")
 
   // get the last endorsement event for the endorsed app
   const { data: appEndorsedEvents } = useAppEndorsedEvents({
@@ -42,14 +50,6 @@ export const EndorsingAppCard = () => {
 
   const lastEndorsementTimestamp = useEstimateBlockTimestamp({ blockNumber: appEndorsedEvents?.[0]?.blockNumber })
   const endorsingSince = dayjs(lastEndorsementTimestamp).fromNow()
-
-  const stopEndorsingButton = useMemo(() => {
-    return (
-      <Button variant="dangerGhost" onClick={unendorseAppModal.onOpen}>
-        {t("Stop endorsing")}
-      </Button>
-    )
-  }, [t, unendorseAppModal.onOpen])
 
   const router = useRouter()
   const goToApps = useCallback(() => {
@@ -78,55 +78,70 @@ export const EndorsingAppCard = () => {
           {isEndorsingApp ? (
             <Card variant={"baseWithBorder"} p={4} rounded="lg">
               <VStack align="stretch" spacing={6}>
-                <HStack justify={"space-between"}>
-                  <HStack>
-                    <Image src={endorsedApp?.logo} alt="endorsed-app" w="12" h="12" rounded="xl" />
-                    <VStack align="stretch">
-                      <HStack bg="#E9FDF1" p={"4px 10px"} rounded="12px">
-                        <UilCheckCircle color="#3DBA67" size={"1rem"} />
-                        <Text color="#3DBA67" fontSize="sm">
-                          {t("Endorsed")}
-                        </Text>
-                      </HStack>
+                <HStack w="full" spacing={4}>
+                  <Image src={endorsedApp?.logo} alt="endorsed-app" w="12" h="12" rounded="xl" />
+                  <Stack
+                    direction={["column", "column", "row"]}
+                    alignItems={["flex-start", "flex-start", "center"]}
+                    justifyContent={["flex-start", "flex-start", "space-between"]}
+                    spacing={2}
+                    w="full">
+                    <VStack alignItems="flex-start" spacing={2}>
+                      <EndorsementStatusCallout
+                        endorsementStatus={endorsementStatus}
+                        showDescription={false}
+                        padding={2}
+                      />
                       <Heading fontSize="lg" fontWeight={"600"}>
                         {endorsedApp?.name}
                       </Heading>
                     </VStack>
-                  </HStack>
-                  <Show above="md">{stopEndorsingButton}</Show>
+                    <Stack
+                      direction={["row", "row", "column"]}
+                      alignItems={["flex-start", "flex-start", "flex-end"]}
+                      justifyContent={["flex-start", "flex-start", "flex-end"]}
+                      gap={0}>
+                      <Text
+                        fontSize={["xs", "xs", "md"]}
+                        color={["#6A6A6A", "#6A6A6A", "inherit"]}
+                        order={[1, 1, 2]} // Change order for large viewports
+                      >
+                        {endorsingSince}
+                      </Text>
+                      <Text
+                        fontSize="xs"
+                        color="#6A6A6A"
+                        order={[0, 0, 1]} // Change order for large viewports
+                        pr={[1, 2, 0]} // Change padding for large viewports
+                      >
+                        {t("Endorsing since")}
+                      </Text>
+                    </Stack>
+                  </Stack>
                 </HStack>
+
                 <Divider />
-                <HStack justify={"space-between"} flexWrap={"wrap"}>
-                  <VStack align="flex-start" gap={0} my={"3"}>
-                    <Text>
-                      {appScore} {t("points")}
-                    </Text>
-                    <Text fontSize="xs" color="#6A6A6A">
-                      {t("Total score")}
-                    </Text>
-                  </VStack>
-                  <VStack align="flex-start" gap={0} my={"3"}>
-                    <Text>{endorsersCount}</Text>
-                    <Text fontSize="xs" color="#6A6A6A">
-                      {t("Users endorsing")}
-                    </Text>
-                  </VStack>
-                  <VStack align="flex-start" gap={0} my={"3"}>
-                    <Text>
-                      {xNodePoints} {t("points")}
-                    </Text>
-                    <Text fontSize="xs" color="#6A6A6A">
-                      {t("Your score")}
-                    </Text>
-                  </VStack>
-                  <VStack align="flex-start" gap={0} my={"3"}>
-                    <Text>{endorsingSince}</Text>
-                    <Text fontSize="xs" color="#6A6A6A">
-                      {t("Endorsing since")}
-                    </Text>
-                  </VStack>
-                </HStack>
-                <Show below="md">{stopEndorsingButton}</Show>
+
+                <Stack
+                  direction={["column", "column", "row"]}
+                  alignItems={["flex-start", "flex-start", "center"]}
+                  justifyContent={["flex-start", "flex-start", "space-between"]}
+                  spacing={4}
+                  w="full">
+                  <EndorsementDetails
+                    endorsementScore={endorsementScore}
+                    endorsementStatus={endorsementStatus}
+                    endorsementThreshold={endorsementThreshold}
+                    isEndorsementStatusLoading={isEndorsementStatusLoading}
+                    xNodePoints={xNodePoints}
+                    isUserAppEndorser={true}
+                    isXNodeLoading={isXNodeLoading}
+                    endorsers={appEndorsers || []}
+                    isAppEndorsersLoading={isAppEndorsersLoading}></EndorsementDetails>
+                  <Button variant="dangerGhost" onClick={unendorseAppModal.onOpen} w={["full", "full", "auto"]}>
+                    {t("Stop endorsing")}
+                  </Button>
+                </Stack>
               </VStack>
             </Card>
           ) : (
