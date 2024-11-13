805f6fb8e0bcff786123039309bf1cc6f9401a59
imagobea <himagobea@gmail.com>
2024-11-13 21:01:45 +0100

    chore: top banner

--- apps/frontend/src/components/Banners/AppsBanner.tsx
+++ apps/frontend/src/components/Banners/AppsBanner.tsx
@@ -1,35 +1,34 @@
-import { Card, Text, Heading, Container, VStack, HStack } from "@chakra-ui/react"
+import { Card, Image, Text, Stack, useMediaQuery } from "@chakra-ui/react"
 import { useTranslation } from "react-i18next"
 
 export const AppsBanner = () => {
+  const [isAbove1200] = useMediaQuery("(min-width: 1200px)")
   const { t } = useTranslation()
   return (
     <Card
-      w={"full"}
       bg="#004CFC"
+      rounded="12px"
+      p="24px"
       color="white"
+      position="relative"
       overflow={"hidden"}
-      borderRadius={"0px"}
-      bgImage="url('/images/cloud-background.png')"
+      bgImage={"/images/cloud-background.png"}
       bgSize="cover"
-      alignItems={"center"}>
-      <Container alignItems={"space-between"} maxW={"container.xl"} flex={1}>
-        <HStack justifyContent={"space-between"}>
-          <VStack alignItems={"flex-start"}>
-            <Heading fontSize="2xl" fontWeight="bold">
+      bgPosition="center"
+      bgRepeat="no-repeat">
+      <Stack justify={"space-between"} direction={isAbove1200 ? "row" : "column"} align="center" gap="24px">
+        <Stack direction={isAbove1200 ? "row" : "column"} gap="24px" align="center">
+          <Image src="/images/gift.svg" alt="rewards" boxSize={"72px"} />
+          <Stack align="stretch">
+            <Text fontSize="2xl" fontWeight={600} align={isAbove1200 ? "left" : "center"}>
               {t("Explore apps")}
-            </Heading>
-            <Text>{t("Browse and discover dApps where you can earn tokens for sustainable actions")}</Text>
-          </VStack>
-          {/* TODO: mascot release <Image
-            alignSelf={"flex-end"}
-            src="/images/mascote/mascote-with-phone.svg"
-            alt="mascote-with-phone"
-            boxSize="200px"
-            objectFit="contain"
-          /> */}
-        </HStack>
-      </Container>
+            </Text>
+            <Text align={isAbove1200 ? "left" : "center"}>
+              {t("Browse and discover dApps where you can earn tokens for sustainable actions.")}
+            </Text>
+          </Stack>
+        </Stack>
+      </Stack>
     </Card>
   )
 }
