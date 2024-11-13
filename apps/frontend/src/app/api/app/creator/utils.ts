92c8ddf4d195fa91cec9f19dcb1901c855bbdfad
Victor Emanuel <versaujob@gmail.com>
2024-11-13 21:22:55 +0000

    feat(auth): setup next auth

---
+++ apps/frontend/src/app/api/app/creator/utils.ts
@@ -0,0 +1,16 @@
+import { SubmitCreatorFormData } from "@/components/SubmitCreatorForm"
+
+const requiredFields: (keyof SubmitCreatorFormData)[] = [
+  "appName",
+  "appDescription",
+  "adminWalletAddress",
+  "adminEmail",
+  "githubUsername",
+  "twitterUsername",
+]
+
+export const checkMissingFields = (data: Partial<SubmitCreatorFormData>): string[] | null => {
+  const missingFields = requiredFields.filter(field => !data[field])
+
+  return missingFields.length > 0 ? missingFields : null
+}
---
+++ apps/frontend/src/app/api/auth/[...nextauth]/next-auth.d.ts
@@ -0,0 +1,19 @@
+// next-auth.d.ts
+import { DefaultSession } from "next-auth"
+import { JWT as DefaultJWT } from "next-auth/jwt"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      githubUsername: string | null
+      twitterUsername: string | null
+    } & DefaultSession["user"]
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    githubUsername: string | null
+    twitterUsername: string | null
+  }
+}
