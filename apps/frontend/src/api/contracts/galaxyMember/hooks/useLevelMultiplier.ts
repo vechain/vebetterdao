605d9cc9d26aea2396433886591ca67de2f134e5
Piero Bassa <64158778+pierobassa@users.noreply.github.com>
2024-11-22 14:58:15 +0100

    fix(GM): b3tr upgrades allowance (#1691)
    * feat: useb3trToUpgrade
    
    * fix: approval required for gm upgrade and missing query refetching
    
    * feat: b3tr required for upgrade prop passing

--- apps/frontend/src/api/contracts/galaxyMember/hooks/useLevelMultiplier.ts
+++ apps/frontend/src/api/contracts/galaxyMember/hooks/useLevelMultiplier.ts
@@ -1,12 +1,15 @@
 import { getConfig } from "@repo/config"
 import { VoterRewards__factory } from "@repo/contracts"
-import { useCall } from "@/hooks"
+import { getCallKey, useCall } from "@/hooks"
 
 const contractAddress = getConfig().voterRewardsContractAddress
 const contractInterface = VoterRewards__factory.createInterface()
 const method = "levelToMultiplier"
 
+export const getLevelMultiplierQueryKey = (level?: string) => getCallKey({ method, keyArgs: [level] })
+
 const percentageToMultiplier = (percentage: number) => 1 + percentage / 100
+
 export const useLevelMultiplier = (level: string, enabled = true) => {
   return useCall({
     contractInterface,
