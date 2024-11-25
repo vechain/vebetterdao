97cc3aab0ad14176957fa0672fdcfd763910f547
Victor Emanuel <versaujob@gmail.com>
2024-11-25 16:05:37 +0000

    Revert "chore: 1.10.9 (#1674)"
    This reverts commit 851edea0cffd380f868289985f9eff1d794d9194.

--- apps/frontend/src/app/proposals/new/form/content/page.test.tsx
+++ apps/frontend/src/app/proposals/new/form/content/page.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest"
-import NewProposalPageDiscussion from "./page"
+import NewproposalContentPage from "./page"
 import { fireEvent, render, screen, waitFor, within } from "../../../../../../test"
 import * as router from "next/navigation"
 import * as dappKit from "@vechain/dapp-kit-react"
@@ -25,7 +25,7 @@ describe("NewProposalContent", async () => {
     })
     render(
       <FormProposalLayout>
-        <NewProposalPageDiscussion />
+        <NewproposalContentPage />
       </FormProposalLayout>,
     )
 
@@ -35,7 +35,7 @@ describe("NewProposalContent", async () => {
   it("form errors - should render correctly", async () => {
     render(
       <FormProposalLayout>
-        <NewProposalPageDiscussion />
+        <NewproposalContentPage />
       </FormProposalLayout>,
     )
     await screen.findByTestId("new-proposal-content-page")
@@ -62,7 +62,7 @@ describe("NewProposalContent", async () => {
   it("form ok - should navigate to the correct page", async () => {
     render(
       <FormProposalLayout>
-        <NewProposalPageDiscussion />
+        <NewproposalContentPage />
       </FormProposalLayout>,
     )
 
