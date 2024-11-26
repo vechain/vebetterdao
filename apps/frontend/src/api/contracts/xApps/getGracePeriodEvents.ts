059034cd38596ede948a3c3817553ce04fd6375a
imagobea <himagobea@gmail.com>
2024-11-26 23:05:10 +0100

    fix: update hook to return sorted events

--- apps/frontend/src/api/contracts/xApps/getGracePeriodEvents.ts
+++ apps/frontend/src/api/contracts/xApps/getGracePeriodEvents.ts
@@ -1,22 +1,21 @@
 import { abi } from "thor-devkit"
 import { getAllEvents } from "@/api/blockchain/getEvents"
 import { getConfig } from "@repo/config"
-import { X2EarnAppsJson } from "@repo/contracts"
-const X2EarnAppsAbi = X2EarnAppsJson.abi
+import { X2EarnApps__factory } from "@repo/contracts"
 
 const X2_EARN_APP_CONTRACT = getConfig().x2EarnAppsContractAddress
 
 export type AppGracePeriodStartedEvent = {
   appId: string
-  clock: string
+  startBlock: string
   endBlock: string
-  blockMeta: Connex.Thor.Filter.WithMeta["meta"]
+  blockNumber: number
+  txOrigin: string
 }
 
-export const getGracePeriodEvent = async (thor: Connex.Thor, appId?: string) => {
-  const appGracePeriodStartedAbi = X2EarnAppsAbi.find(abi => abi.name === "AppUnendorsedGracePeriodStarted")
-  if (!appGracePeriodStartedAbi) throw new Error("AppUnendorsedGracePeriodStarted event not found")
-  const appGracePeriodStartedEvent = new abi.Event(appGracePeriodStartedAbi as abi.Event.Definition)
+export const getGracePeriodEvent = async (thor: Connex.Thor, appId?: string): Promise<AppGracePeriodStartedEvent[]> => {
+  const eventFragment = X2EarnApps__factory.createInterface().getEvent("AppUnendorsedGracePeriodStarted").format("json")
+  const appGracePeriodStartedEvent = new abi.Event(JSON.parse(eventFragment) as abi.Event.Definition)
 
   const appIdBytes = appId ? `0x${BigInt(appId).toString(16).padStart(64, "0")}` : undefined
 
@@ -34,30 +33,14 @@ export const getGracePeriodEvent = async (thor: Connex.Thor, appId?: string) => 
 
   const events = await getAllEvents({ thor, filterCriteria })
 
-  /**
-   * Decode the events to get the data we are interested in (i.e the endorsement grace period of the app)
-   */
-  const decodedAppGracePeriodStartedEvent: AppGracePeriodStartedEvent[] = []
-
-  events.forEach(event => {
-    switch (event.topics[0]) {
-      case appGracePeriodStartedEvent.signature: {
-        const decoded = appGracePeriodStartedEvent.decode(event.data, event.topics)
-        decodedAppGracePeriodStartedEvent.push({
-          appId: decoded[0],
-          clock: decoded[1],
-          endBlock: decoded[2],
-          blockMeta: event.meta,
-        })
-        break
-      }
-      default: {
-        throw new Error("Unknown event")
-      }
+  return events.map(event => {
+    const decoded = appGracePeriodStartedEvent.decode(event.data, event.topics)
+    return {
+      appId: decoded[0],
+      startBlock: decoded[1].toString(),
+      endBlock: decoded[2].toString(),
+      blockNumber: event.meta.blockNumber,
+      txOrigin: event.meta.txOrigin,
     }
   })
-
-  return {
-    gracePeriodEvent: decodedAppGracePeriodStartedEvent,
-  }
 }
