fe207ed452a23abb896f0d4275f7c89e965dab48
imagobea <himagobea@gmail.com>
2024-11-26 13:20:43 +0100

    refactor: allow copy to clipboard

--- apps/frontend/src/app/apps/[appId]/components/AppEndorsementInfoCard/EndorsementHistoryItem.tsx
+++ apps/frontend/src/app/apps/[appId]/components/AppEndorsementInfoCard/EndorsementHistoryItem.tsx
@@ -1,6 +1,8 @@
 import { AppEndorsedEvent } from "@/api/contracts/xApps/hooks/endorsement/useAppEndorsedEvents"
 import { humanAddress } from "@repo/utils/FormattingUtils"
 import { Text, HStack, VStack, Skeleton } from "@chakra-ui/react"
+import { UilCheck, UilCopy } from "@iconscout/react-unicons"
+import { useCallback, useState } from "react"
 import { Trans, useTranslation } from "react-i18next"
 import dayjs from "dayjs"
 import { useEstimateBlockTimestamp } from "@/hooks/useEstimateBlockTimestamp"
@@ -28,6 +30,16 @@ export const EndorsementHistoryItem = ({ event }: Props) => {
   const endorsementEpoch = useEstimateBlockTimestamp({ blockNumber })
   const endorsingDate = dayjs(endorsementEpoch).format("MMM D, YYYY")
 
+  // Allow users to copy endorser addresses on history list to clipboard
+  const [showCopiedLink, setShowCopiedLink] = useState(false)
+  const handleCopyEndorserAddress = useCallback(async () => {
+    await navigator.clipboard.writeText(endorserAddress ?? "")
+    setShowCopiedLink(true)
+    setTimeout(() => {
+      setShowCopiedLink(false)
+    }, 2000)
+  }, [endorserAddress])
+
   return (
     <HStack
       p={2}
@@ -38,7 +50,14 @@ export const EndorsementHistoryItem = ({ event }: Props) => {
       justify={"space-between"}>
       <VStack align="start" justifyContent={"flex-start"} spacing={0} flex={1}>
         <Skeleton isLoaded={!endorserAddressLoading}>
-          <Text>{humanAddress(endorserAddress ?? "", 6, 3)}</Text>
+          <HStack>
+            {showCopiedLink ? (
+              <UilCheck size={"18px"} color="#6DCB09" />
+            ) : (
+              <UilCopy size={"18px"} color="#6A6A6A" onClick={handleCopyEndorserAddress} cursor="pointer" />
+            )}
+            <Text>{humanAddress(endorserAddress ?? "", 6, 3)}</Text>
+          </HStack>
         </Skeleton>
 
         <Text fontSize="xs" color="#6A6A6A">
