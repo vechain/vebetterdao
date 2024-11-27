8c470241c16232e3fbad6bd6471d8bf8778f4ee5
Victor Emanuel <versaujob@gmail.com>
2024-11-27 20:13:46 +0000

    GM: "Max Level Reached" Text + "Attach Now" Button (#1750)
    * fix(gm): adding max level reached text
    
    * fix(gm): removing unused hstack
    
    * refactor(gm): refactor attach/upgrade button logic
    
    * refactor(gm): align max level text left + add background
    
    * refactor(gm): show all levels if max level is 3 or below

--- apps/frontend/src/components/GmActionButton.tsx
+++ apps/frontend/src/components/GmActionButton.tsx
@@ -1,15 +1,24 @@
 import { useCallback, useMemo } from "react"
-import { Button, ButtonProps, useDisclosure } from "@chakra-ui/react"
+import { Button, ButtonProps, useDisclosure, Text, HStack } from "@chakra-ui/react"
 import { useRouter } from "next/navigation"
 import { useMintNFT } from "@/hooks"
 import { AttachGMToXNodeModal } from "@/app/apps/components/AttachGMToXNodeModal"
 import { UpgradeGMModal } from "@/app/apps/components/UpgradeGMModal"
-import { useCurrentAllocationsRoundId, useParticipatedInGovernance, useSelectedGmNft, useXNode } from "@/api"
+import {
+  getGMLevel,
+  getLevelGradient,
+  useB3trDonated,
+  useCurrentAllocationsRoundId,
+  useParticipatedInGovernance,
+  useSelectedGmNft,
+  useXNode,
+} from "@/api"
 import { useTranslation } from "react-i18next"
 import { useWallet } from "@vechain/dapp-kit-react"
 import { MintNFTModal } from "./MintNFTModal"
 import { FeatureFlagWrapper } from "./FeatureFlagWrapper"
 import { FeatureFlag } from "@/constants"
+import { xNodeToGMstartingLevel } from "@/constants/gmNfts"
 
 export const GmActionButton = ({ buttonProps }: { buttonProps: ButtonProps }) => {
   const { t } = useTranslation()
@@ -19,13 +28,13 @@ export const GmActionButton = ({ buttonProps }: { buttonProps: ButtonProps }) =>
   const {
     isGMOwned,
     isEnoughBalanceToUpgradeGM,
-
     gmId,
+    gmLevel,
+    maxGmLevel,
     isMaxGmLevelReached,
     b3trToUpgradeGMToNextLevel,
   } = useSelectedGmNft()
-  const { isXNodeHolder, isXNodeDelegator, isXNodeAttachedToGM } = useXNode()
-
+  const { xNodeLevel, isXNodeHolder, isXNodeDelegator, isXNodeAttachedToGM } = useXNode()
   const router = useRouter()
   const mintNftModal = useDisclosure()
   const {
@@ -53,6 +62,22 @@ export const GmActionButton = ({ buttonProps }: { buttonProps: ButtonProps }) =>
 
   const upgradeGMModal = useDisclosure()
 
+  const canAttach = useMemo(
+    () => isXNodeHolder && !isXNodeDelegator && isGMOwned && !isXNodeAttachedToGM,
+    [isXNodeAttachedToGM, isXNodeDelegator, isXNodeHolder, isGMOwned],
+  )
+  const { data: b3trDonated } = useB3trDonated(gmId)
+
+  const gmStartingLevel = useMemo(() => {
+    const gmStartingLevel = xNodeToGMstartingLevel[xNodeLevel]
+
+    return Math.min(gmStartingLevel ?? 1, maxGmLevel ?? 1)
+  }, [maxGmLevel, xNodeLevel])
+
+  const levelAfterAttach = useMemo(() => {
+    return getGMLevel(gmStartingLevel, Number(b3trDonated ?? 0)) ?? 1
+  }, [b3trDonated, gmStartingLevel])
+
   const actionButton = useMemo(() => {
     if (!hasUserVoted && !isGMOwned) {
       return (
@@ -68,7 +93,42 @@ export const GmActionButton = ({ buttonProps }: { buttonProps: ButtonProps }) =>
         </Button>
       )
     }
-    if (isXNodeHolder && !isXNodeAttachedToGM && !isXNodeDelegator) {
+
+    if (isMaxGmLevelReached && isXNodeAttachedToGM) {
+      return (
+        <HStack bg={"rgba(255, 255, 255, 0.1)"} alignSelf="start" rounded="8px" px={5} py={1} gap={1} justify="center">
+          <Text
+            bg={getLevelGradient(1)} //Setting first gradient which is more visible
+            style={{
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+            }}
+            fontSize={"lg"}
+            fontWeight={"bold"}
+            noOfLines={1}>
+            {t("Max Level Reached!")}
+          </Text>
+        </HStack>
+      )
+    }
+
+    if (canAttach && gmLevel >= levelAfterAttach) {
+      return (
+        <FeatureFlagWrapper
+          feature={FeatureFlag.GALAXY_MEMBER_UPGRADES}
+          fallback={
+            <Button {...buttonProps} isDisabled={true}>
+              {t("Coming soon!")}
+            </Button>
+          }>
+          <Button {...buttonProps} onClick={attachGmToXNodeModal.onOpen}>
+            {t("Attach now!")}
+          </Button>
+        </FeatureFlagWrapper>
+      )
+    }
+
+    if (canAttach && gmLevel < levelAfterAttach) {
       return (
         <FeatureFlagWrapper
           feature={FeatureFlag.GALAXY_MEMBER_UPGRADES}
