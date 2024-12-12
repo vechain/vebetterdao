bb268abce43712d31880d24b4f555ff141cc6be8
Victor Emanuel <versaujob@gmail.com>
2024-12-12 17:55:26 +0000

    refactor(components): cleanup wallet input + fix types

--- apps/frontend/src/app/components/Input/WalletAddressInput.tsx
+++ apps/frontend/src/app/components/Input/WalletAddressInput.tsx
@@ -1,16 +1,14 @@
 import { useEffect, useId } from "react"
-import { Input } from "@chakra-ui/react"
+import { Input, InputProps } from "@chakra-ui/react"
 import { useTranslation } from "react-i18next"
-import { UseFormRegister, UseFormWatch } from "react-hook-form"
+import { UseFormRegister, UseFormWatch, type FieldValues as TFieldValues } from "react-hook-form"
 import { useVechainDomain } from "@vechain/dapp-kit-react"
 import { isValid } from "@repo/utils/AddressUtils"
 
-type Props = {
+type Props = InputProps & {
   inputName: string
-  //TODO: Replace any with the correct type
-  register: UseFormRegister<any>
-  watch: UseFormWatch<any>
-  customValidation?: (value: string) => boolean | string | Promise<boolean | string>
+  register: UseFormRegister<TFieldValues>
+  watch: UseFormWatch<TFieldValues>
   onDomainResolved?: (domain?: string) => void
   onAddressResolved?: (address?: string) => void
 }
@@ -19,9 +17,9 @@ export const WalletAddressInput = ({
   inputName,
   register,
   watch,
-  customValidation,
   onDomainResolved,
   onAddressResolved,
+  ...props
 }: Props) => {
   const id = useId()
   const { t } = useTranslation()
@@ -40,32 +38,21 @@ export const WalletAddressInput = ({
     onAddressResolved?.(address)
   }, [domain, address, onDomainResolved, onAddressResolved])
 
-  //Default Validation is only against wallet address
-  const validateInput = async (value: string) => {
-    //Check if starts with 0x
-    if (value.startsWith("0x") && !isValid(value)) {
-      return t("Please enter a valid wallet address")
-    }
-  }
-
   return (
     <Input
+      {...props}
       id={id}
       {...register(inputName, {
         required: t("Wallet address or domain is required"),
-        validate: async (value: string) => {
-          await validateInput(value)
-
-          //Allows custom validation to be passed
-          if (customValidation) {
-            const result = await customValidation(value)
-            if (result !== true) return result
+        validate: (value: string) => {
+          //Default Validation is only against wallet address
+          if (value.startsWith("0x") && !isValid(value)) {
+            return t("Please enter a valid wallet address")
           }
-          return true
         },
       })}
       value={inputValue}
-      placeholder={t("Enter your wallet address or domain")}
+      placeholder={props?.placeholder || t("Enter a wallet address or domain")}
     />
   )
 }
