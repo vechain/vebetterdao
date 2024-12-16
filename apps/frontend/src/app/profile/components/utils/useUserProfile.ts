57acf02e9e35aa888c911960e20abe00d14326f4
aya <aya.belarbi16@gmail.com>
2024-12-16 18:30:55 +0100

    feat: improve hook logic and formatiing vet domains

--- apps/frontend/src/app/profile/components/utils/useUserProfile.ts
+++ apps/frontend/src/app/profile/components/utils/useUserProfile.ts
@@ -2,24 +2,36 @@ import { useParams } from "next/navigation"
 import { useVechainDomain, useWallet } from "@vechain/dapp-kit-react"
 import { compareAddresses } from "@repo/utils/AddressUtils"
 import { useMemo } from "react"
+import { humanDomain } from "@repo/utils/FormattingUtils"
 
 type Profile = {
-  profile: string
+  profile: string | null
   isConnectedUser: boolean
   domain?: string
 }
 
 export const useUserProfile = (): Profile => {
   // Comparing the connected use with the profile page params
-  const { address: profile } = useParams<{ address: string }>()
-  const { account } = useWallet()
+  const params = useParams<{ address: string }>()
+  const profile = params?.address
 
+  const { account } = useWallet()
   const { domain } = useVechainDomain({ addressOrDomain: profile ?? account })
-  const isConnectedUser = useMemo(() => compareAddresses(account ?? "", profile ?? ""), [account, profile])
+
+  const isConnectedUser = useMemo(() => compareAddresses(profile, account ?? ""), [account, profile])
+
+  // Not in the profil page, so on the connected user page profile
+  if (!profile && account) {
+    return {
+      profile: account,
+      isConnectedUser: true,
+      domain: humanDomain(domain ?? "", 4, 26),
+    }
+  }
 
   return {
-    profile: profile ?? account ?? "",
+    profile: profile,
     isConnectedUser,
-    domain,
+    domain: humanDomain(domain ?? "", 4, 26),
   }
 }
