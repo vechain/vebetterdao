211492f8cff07161dc42cb5710d38888f04d7a61
Victor Emanuel <versaujob@gmail.com>
2024-12-17 12:53:45 +0000

    fix(scripts): Fix local setup script to prevent endorsement from reverting

--- packages/config/contracts/envs/local.ts
+++ packages/config/contracts/envs/local.ts
@@ -156,6 +156,8 @@ export function createLocalConfig() {
 
     CREATOR_NFT_URI: "ipfs://bafybeie2onvzl3xsod5becuswpdmi63gtq7wgjqhqjecehytt7wdeg4py4/metadata/1.json",
 
+    //WARNING: Cooldown depends on the current round. If the current round is 1, the cooldown cannot be greater than 1. Otherwise the endorsement will fail.
+    //Check `EndorsementUtils.sol` `checkCooldown` method for more details.
     X2EARN_NODE_COOLDOWN_PERIOD: 0, // 0 cycles
   })
 }
