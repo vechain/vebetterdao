bbd7089595b10912a2c63a6ccb23eb5d4efbc9c1
aya <aya.belarbi16@gmail.com>
2024-12-17 10:36:21 +0100

    fix: check if user isOnProfilePage

--- apps/frontend/src/app/profile/components/utils/useUserProfile.ts
+++ apps/frontend/src/app/profile/components/utils/useUserProfile.ts
@@ -1,4 +1,4 @@
-import { useParams } from "next/navigation"
+import { useParams, usePathname } from "next/navigation"
 import { useVechainDomain, useWallet } from "@vechain/dapp-kit-react"
 import { compareAddresses } from "@repo/utils/AddressUtils"
 import { useMemo } from "react"
@@ -8,17 +8,20 @@ type Profile = {
   profile: string | null
   isConnectedUser: boolean
   domain?: string
+  isOnProfilePage?: boolean // true if the user is on profile page
 }
 
 export const useUserProfile = (): Profile => {
   // Comparing the connected use with the profile page params
   const params = useParams<{ address: string }>()
+  const pathname = usePathname()
   const profile = params?.address
 
   const { account } = useWallet()
   const { domain } = useVechainDomain({ addressOrDomain: profile ?? account })
 
   const isConnectedUser = useMemo(() => compareAddresses(profile, account ?? ""), [account, profile])
+  const isOnProfilePage = pathname.includes("profile")
 
   // Not in the profil page, so on the connected user page profile
   if (!profile && account) {
@@ -26,6 +29,7 @@ export const useUserProfile = (): Profile => {
       profile: account,
       isConnectedUser: true,
       domain: humanDomain(domain ?? "", 4, 26),
+      isOnProfilePage,
     }
   }
 
@@ -33,5 +37,6 @@ export const useUserProfile = (): Profile => {
     profile: profile,
     isConnectedUser,
     domain: humanDomain(domain ?? "", 4, 26),
+    isOnProfilePage,
   }
 }
