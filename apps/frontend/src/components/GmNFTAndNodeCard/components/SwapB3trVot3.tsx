5f8697eef957640a7374a45b411d1d4e46c8b828
aya <aya.belarbi16@gmail.com>
2024-12-19 18:09:10 +0100

    fix: rmv console

--- apps/frontend/src/components/GmNFTAndNodeCard/components/SwapB3trVot3.tsx
+++ apps/frontend/src/components/GmNFTAndNodeCard/components/SwapB3trVot3.tsx
@@ -41,16 +41,13 @@ export const SwapB3trVot3 = ({ address, containerProps, innerContent }: Props) =
   const isSwapDisabled = isLoading || hasNoBalance
   const { isConnectedUser, domain, profile, isOnProfilePage } = useRetrieveProfilIdentity()
 
-  console.log({ isConnectedUser, domain, profile, isOnProfilePage })
-  console.log(!profile)
-  console.log("isConnectedUser in the SwapB3trVot3 component", isConnectedUser)
   return (
     <>
       <VStack flex="2" align={"stretch"} gap="24px" {...containerProps}>
         {innerContent}
         <Text fontSize="xl" fontWeight={700}>
           {t("{{value}} tokens", {
-            value: isConnectedUser || !isOnProfilePage ? "Your" : domain ?? humanAddress(profile ?? "", 6, 3),
+            value: isConnectedUser ? "Your" : domain ?? humanAddress(profile ?? "", 6, 3),
           })}
         </Text>
         <Stack gap="24px" direction={isAbove800 ? "row" : "column"}>
