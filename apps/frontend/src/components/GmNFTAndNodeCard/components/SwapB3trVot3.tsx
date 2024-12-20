87eb46d9ffeafcd7cbf12f36cf4f265fb3e5da3c
aya <75953386+ayabelarbi@users.noreply.github.com>
2024-12-20 16:54:14 +0100

    hotfix/display address (#1835)
    * fix: change nullish condition
    
    * docs: generate translation
    
    * fix: align with sonar issue

--- apps/frontend/src/components/GmNFTAndNodeCard/components/SwapB3trVot3.tsx
+++ apps/frontend/src/components/GmNFTAndNodeCard/components/SwapB3trVot3.tsx
@@ -39,7 +39,9 @@ export const SwapB3trVot3 = ({ address, containerProps, innerContent }: Props) =
   const isLoading = isB3trBalanceLoading || isVot3BalanceLoading
 
   const isSwapDisabled = isLoading || hasNoBalance
+
   const { isConnectedUser, domain, profile, isOnProfilePage } = useRetrieveProfilIdentity()
+  const domainOrAddress = domain && domain !== "" ? domain : humanAddress(profile ?? "", 6, 3)
 
   console.log({ isConnectedUser, domain, profile, isOnProfilePage })
   console.log(!profile)
