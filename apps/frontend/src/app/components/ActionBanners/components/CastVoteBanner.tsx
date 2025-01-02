44f10e8c1bf200c5336b5e2d41eac07c1f1b6a63
Victor Emanuel <versaujob@gmail.com>
2025-01-02 17:14:19 +0000

    refactor(banner): refactor action banners to use a generic component

--- apps/frontend/src/app/components/ActionBanners/components/CastVoteBanner.tsx
+++ apps/frontend/src/app/components/ActionBanners/components/CastVoteBanner.tsx
@@ -1,15 +1,13 @@
-import { Heading, Text, VStack, Card, CardBody, HStack, Image, Button, Show, useMediaQuery } from "@chakra-ui/react"
 import { useTranslation } from "react-i18next"
 import { UilArrowRight } from "@iconscout/react-unicons"
 import { useCurrentAllocationsRoundId } from "@/api"
 import { useCallback } from "react"
 import { useRouter } from "next/navigation"
+import { GenericBanner } from "../../Banners/GenericBanner"
 
 export const CastVoteBanner = () => {
   const { t } = useTranslation()
   const { data: roundId } = useCurrentAllocationsRoundId()
-
-  const [isVerySmallMobile] = useMediaQuery("(max-height: 667px)")
 
   const router = useRouter()
   const handleVote = useCallback(() => {
@@ -17,69 +15,18 @@ export const CastVoteBanner = () => {
   }, [router, roundId])
 
   return (
-    <Card bg="#B1F16C" borderRadius="xl" w="full" h="full">
-      <CardBody
-        position="relative"
-        overflow="hidden"
-        alignContent={"center"}
-        justifyContent={"center"}
-        borderRadius="xl"
-        padding={{ base: 4, md: 6 }}>
-        <Image
-          src="/images/community-green-blob.png"
-          alt="cloud-background"
-          position="absolute"
-          right={["-50%", "-50%", "-10%"]}
-          top={["-50%", "-50%", "-150%"]}
-        />
-        <Show above="md">
-          <HStack align="stretch" zIndex={1} position="relative" w="full">
-            <Image src="/images/vote-icon.png" alt="Vote" w={24} h={24} />
-            <HStack flex={1}>
-              <VStack gap={2} align="stretch" flex={1}>
-                <Text size="xs" color="#3A5798" fontWeight="600">
-                  {t("CAST YOUR VOTE NOW! ⚖️")}
-                </Text>
-                <Heading fontSize="lg" fontWeight="700" color="#0C2D75">
-                  {t("It’s time to make your voice heard in this round and earn exciting rewards!")}
-                </Heading>
-              </VStack>
-              <Button
-                variant="primaryAction"
-                onClick={handleVote}
-                borderRadius="full"
-                rightIcon={<UilArrowRight color="white" />}>
-                <Text fontWeight="500">{t("See round")}</Text>
-              </Button>
-            </HStack>
-          </HStack>
-        </Show>
-        <Show below="md">
-          <HStack align="stretch" zIndex={1} position="relative" w="full" alignItems={"center"}>
-            <VStack gap={2} align="stretch" justify={"space-between"}>
-              <Text fontSize={12} color="#3A5798" fontWeight="600">
-                {t("CAST YOUR VOTE NOW! ⚖️")}
-              </Text>
-              <Heading fontSize="18" fontWeight="700" color="#0C2D75">
-                {t("It’s time to make your voice heard in this round and earn exciting rewards!")}
-              </Heading>
-              <Button
-                variant="primaryAction"
-                onClick={handleVote}
-                borderRadius="full"
-                rightIcon={<UilArrowRight color="white" />}>
-                <Text fontWeight="500">{t("See round")}</Text>
-              </Button>
-            </VStack>
-            <Image
-              src="/images/vote-icon.png"
-              alt="Vote"
-              w={isVerySmallMobile ? 16 : 24}
-              h={isVerySmallMobile ? 16 : 24}
-            />
-          </HStack>
-        </Show>
-      </CardBody>
-    </Card>
+    <GenericBanner
+      title={t("CAST YOUR VOTE NOW! ⚖️")}
+      titleColor="#3A5798"
+      description={t("It’s time to make your voice heard in this round and earn exciting rewards!")}
+      descriptionColor="#0C2D75"
+      logoSrc="/images/vote-icon.png"
+      backgroundColor="#B1F16C"
+      backgroundImageSrc="/images/community-green-blob.png"
+      buttonLabel={t("See round")}
+      onButtonClick={handleVote}
+      buttonVariant="primaryAction"
+      buttonIcon={<UilArrowRight color="white" />}
+    />
   )
 }
--- apps/frontend/src/app/components/ActionBanners/components/LowVthoBanner/LowVthoBanner.tsx
+++ apps/frontend/src/app/components/ActionBanners/components/LowVthoBanner/LowVthoBanner.tsx
@@ -1,4 +1,3 @@
-import { Heading, Text, VStack, Card, CardBody, HStack, Image, Button, Show, useMediaQuery } from "@chakra-ui/react"
 import { useTranslation } from "react-i18next"
 import { UilArrowRight } from "@iconscout/react-unicons"
 import { useCallback, useMemo } from "react"
@@ -6,6 +5,7 @@ import { useQueryClient } from "@tanstack/react-query"
 import { Transak, TransakConfig } from "@transak/transak-sdk"
 import { useWallet } from "@vechain/dapp-kit-react"
 import { getAccountBalanceQueryKey } from "@/api"
+import { GenericBanner } from "@/app/components/Banners/GenericBanner"
 
 const isProduction = process.env.NODE_ENV === "production"
 export const apiKey = process.env.NEXT_PUBLIC_TRANSAK_API_KEY ?? ""
@@ -14,9 +14,6 @@ export const LowVthoBanner = () => {
   const { t } = useTranslation()
   const { account } = useWallet()
   const queryClient = useQueryClient()
-
-  const [isVerySmallMobile] = useMediaQuery("(max-height: 667px)")
-
   const transakConfig: TransakConfig = useMemo(
     () => ({
       apiKey,
@@ -63,63 +60,18 @@ export const LowVthoBanner = () => {
   }, [initTransak])
 
   return (
-    <Card bg="#FFD979" borderRadius="xl" w="full" h="full">
-      <CardBody
-        position="relative"
-        overflow="hidden"
-        alignContent={"center"}
-        justifyContent={"center"}
-        borderRadius="xl"
-        padding={{ base: 4, md: 6 }}>
-        <Image
-          src="/images/cloud-background-orange.png"
-          alt="cloud-background-orange"
-          position="absolute"
-          right={["-50%", "-50%", "-10%"]}
-          top={["-50%", "-50%", "-150%"]}
-        />
-        <Show above="md">
-          <HStack align="stretch" zIndex={1} position="relative" w="full">
-            <Image src="/images/lightning.png" alt="Pending actions" w={24} h={24} />
-            <HStack flex={1}>
-              <VStack gap={2} align="stretch" flex={1}>
-                <Text size="xs" color="#8D6602" fontWeight="600">
-                  {t("NOT ENOUGH VTHO")}
-                </Text>
-                <Heading fontSize="lg" fontWeight="700" color="#5F4400">
-                  {t("Get more VTHO to be able to vote and perform transactions!")}
-                </Heading>
-              </VStack>
-              <Button onClick={handleOnPress} rightIcon={<UilArrowRight />} variant="primaryAction">
-                <Text fontWeight="500">{t("Get more VTHO")}</Text>
-              </Button>
-            </HStack>
-          </HStack>
-        </Show>
-        <Show below="md">
-          <HStack align="center" zIndex={1} position="relative" w="full" h="full" alignItems={"center"}>
-            <VStack gap={2} align="stretch" justify={"space-between"} h="full">
-              <Text fontSize={12} color="#8D6602" fontWeight="600">
-                {t("NOT ENOUGH VTHO")}
-              </Text>
-              <Heading fontSize="18" fontWeight="700" color="#5F4400">
-                {t("Get more VTHO to be able to vote!")}
-              </Heading>
-              <Button onClick={handleOnPress} rightIcon={<UilArrowRight />} variant="primaryAction">
-                <Text fontWeight="500" fontSize={16}>
-                  {t("Get more VTHO")}
-                </Text>
-              </Button>
-            </VStack>
-            <Image
-              src="/images/lightning.png"
-              alt="Pending actions"
-              w={isVerySmallMobile ? 16 : 24}
-              h={isVerySmallMobile ? 16 : 24}
-            />
-          </HStack>
-        </Show>
-      </CardBody>
-    </Card>
+    <GenericBanner
+      title={t("NOT ENOUGH VTHO")}
+      titleColor="#8D6602"
+      description={t("Get more VTHO to be able to vote and perform transactions!")}
+      descriptionColor="#5F4400"
+      logoSrc="/images/lightning.png"
+      backgroundColor="#FFD979"
+      backgroundImageSrc="/images/cloud-background-orange.png"
+      buttonLabel={t("Get more VTHO")}
+      onButtonClick={handleOnPress}
+      buttonVariant="primaryAction"
+      buttonIcon={<UilArrowRight />}
+    />
   )
 }
