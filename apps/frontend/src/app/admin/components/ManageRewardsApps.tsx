4ce2d505c29d653fa00dace172ae66573824bccb
aya <aya.belarbi16@gmail.com>
2025-01-10 19:59:19 +0100

    feat: manage percentage locked from admin page

---
+++ apps/frontend/src/app/admin/components/ManageRewardsApps.tsx
@@ -0,0 +1,146 @@
+import { useTranslation } from "react-i18next"
+import {
+  Card,
+  CardHeader,
+  Heading,
+  CardBody,
+  useDisclosure,
+  VStack,
+  HStack,
+  FormControl,
+  FormLabel,
+  Select,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  Button,
+  Text,
+} from "@chakra-ui/react"
+import { useCallback, useState } from "react"
+import { TransactionModal } from "@/components"
+import { useXApps } from "@/api"
+import { useAppLockedPercentage } from "@/api/contracts/x2EarnRewardsPool"
+import { useAdminLockedFundsPercentage } from "@/hooks"
+
+export const ManageRewardsApps = () => {
+  const { onOpen, onClose, isOpen } = useDisclosure()
+  const { t } = useTranslation()
+
+  const [appId, setAppId] = useState<string | undefined>()
+  const [percentage, setPercentage] = useState<string | undefined>()
+  const { data: xApps } = useXApps()
+
+  const {
+    sendTransaction,
+    isTxReceiptLoading,
+    sendTransactionPending,
+    status,
+    error,
+    txReceipt,
+    // sendTransactionTx,
+  } = useAdminLockedFundsPercentage({
+    appId: appId ?? "",
+    percentage: percentage ?? "",
+  })
+
+  const { data: lockedFundsPercentage } = useAppLockedPercentage(appId ?? "")
+
+  const isLoading = isTxReceiptLoading || sendTransactionPending
+
+  const handleSubmit = useCallback(
+    (event?: { preventDefault: () => void }) => {
+      if (event) event.preventDefault()
+      sendTransaction(undefined)
+      onOpen()
+    },
+    [sendTransaction, onOpen],
+  )
+
+  // {/* todo: filter the select with only the xapp admin owns */}
+
+  return (
+    <>
+      <Card w="full">
+        <CardHeader>
+          <Heading size="lg">{t("Manage rewards distribution")}</Heading>
+        </CardHeader>
+
+        <CardBody>
+          <VStack flex={1} align="flex-start" spacing={8}>
+            <VStack align={"flex-start"} flex={1} spacing={4}>
+              <Text>{t("Actual locked percentage: {{value}} % ", { value: lockedFundsPercentage })}</Text>
+            </VStack>
+
+            <form onSubmit={handleSubmit}>
+              <VStack spacing={4} alignItems={"start"}>
+                <HStack w={"full"}>
+                  <FormControl isRequired>
+                    <FormLabel>
+                      <strong>{"App"}</strong>
+                    </FormLabel>
+                    <Select
+                      placeholder="Select app"
+                      isDisabled={isLoading}
+                      onChange={e => setAppId(e.target.value)}
+                      value={appId}>
+                      {xApps?.active.map(item => {
+                        return (
+                          <option key={item.id} value={item.id}>
+                            {item.name + " - id: " + item.id}
+                          </option>
+                        )
+                      })}
+                    </Select>
+                  </FormControl>
+                </HStack>
+
+                <FormControl isRequired>
+                  <FormLabel>
+                    <strong>{"Percentage"}</strong>
+                  </FormLabel>
+                  <InputGroup size="md">
+                    <Input
+                      pr="4.5rem"
+                      type="number"
+                      placeholder="percentage"
+                      isDisabled={isLoading}
+                      onChange={e => setPercentage(e.target.value)}
+                      value={percentage}
+                    />
+                    <InputRightAddon
+                      pointerEvents="none"
+                      pl={1}
+                      pr={1}
+                      ml={0}
+                      backgroundColor={"transparent"}
+                      borderColor={"inherit"}
+                      borderLeft={"none"}>
+                      {"%"}
+                    </InputRightAddon>
+                  </InputGroup>
+                </FormControl>
+                <Button type="submit" colorScheme="blue" isLoading={isLoading} disabled={isLoading}>
+                  {t("Lock funds")}
+                </Button>
+              </VStack>
+            </form>
+          </VStack>
+        </CardBody>
+      </Card>
+
+      <TransactionModal
+        isOpen={isOpen}
+        onClose={onClose}
+        status={error ? "error" : status}
+        successTitle={t("Transaction successful")}
+        onTryAgain={handleSubmit}
+        showTryAgainButton
+        showExplorerButton
+        txId={txReceipt?.meta.txID}
+        pendingTitle={t("Processing transaction...")}
+        errorTitle={t("Transaction error")}
+        errorDescription={error?.reason}
+      />
+    </>
+  )
+}
