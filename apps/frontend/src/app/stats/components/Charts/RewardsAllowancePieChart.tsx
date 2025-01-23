a0444d7b4b7c2af48e10289c620b87ee416d3d12
aya <aya.belarbi16@gmail.com>
2025-01-23 19:17:16 +0100

    feat: charts for allowance

---
+++ apps/frontend/src/app/stats/components/Charts/RewardsAllowancePieChart.tsx
@@ -0,0 +1,20 @@
+import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts"
+
+interface Props {
+  pieData: { name: string; value: number; color: string }[]
+}
+
+export const RewardsAllowancePieChart: React.FC<Props> = ({ pieData }) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <PieChart>
+        <Pie data={pieData} dataKey="value" nameKey="name" outerRadius={80} innerRadius={60}>
+          {pieData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={entry.color} />
+          ))}
+        </Pie>
+        <Tooltip formatter={(value: any) => `${value}%`} />
+      </PieChart>
+    </ResponsiveContainer>
+  )
+}
