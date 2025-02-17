1364d840d91b4d65c13781d2b7fd877369144b06
aya <aya.belarbi16@gmail.com>
2025-02-17 13:14:52 +0100

    fix: modifying the docs link

--- apps/frontend/src/app/components/Countdown/SnapshotExplainationModal.tsx
+++ apps/frontend/src/app/components/Countdown/SnapshotExplainationModal.tsx
@@ -23,7 +23,7 @@ interface Props {
 
 export const SnapshotExplainationModal = ({ isOpen, onClose }: Props) => {
   const LINK_TO_DOCS = () => {
-    window.open("https://docs.vebetterdao.org/vebetterdao/b3tr-and-vot3-tokens", "_blank", "noopener")
+    window.open("https://docs.vebetterdao.org/vebetterdao/x2earn-allocations", "_blank", "noopener")
   }
 
   const renderStep = (step: number) => (
