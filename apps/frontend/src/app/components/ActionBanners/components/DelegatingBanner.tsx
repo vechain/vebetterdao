5f3f8db3eb38ec9c83aa23511a7550423afae664
aya <aya.belarbi16@gmail.com>
2025-02-26 17:43:08 +0100

    feat: link to veDelegate

--- apps/frontend/src/app/components/ActionBanners/components/DelegatingBanner.tsx
+++ apps/frontend/src/app/components/ActionBanners/components/DelegatingBanner.tsx
@@ -1,7 +1,5 @@
-import { useDisclosure } from "@chakra-ui/react"
 import { useTranslation } from "react-i18next"
 import { UilInfoCircle } from "@iconscout/react-unicons"
-import { SnapshotExplainationModal } from "@/app/components/Countdown/SnapshotExplainationModal"
 
 import { GenericBanner } from "@/app/components/Banners/GenericBanner"
 
@@ -12,7 +10,9 @@ export const DelegatingBanner = () => {
     "Your voting power has been transferred to VeDelegate. To be able to vote, you must be the primary account at snapshot.",
   )
 
-  const whatIsSnapshot = useDisclosure()
+  const whatIsVeDelegate = () => {
+    window.open("https://docs.vedelegate.vet/faq#what-is-a-vepassport", "_blank", "noopener noreferrer")
+  }
 
   return (
     <>
@@ -23,11 +23,10 @@ export const DelegatingBanner = () => {
         backgroundColor="#FFD979"
         backgroundImageSrc=""
         buttonLabel={t("Know more")}
-        onButtonClick={whatIsSnapshot.onOpen}
+        onButtonClick={whatIsVeDelegate}
         buttonVariant="outline"
         buttonIcon={<UilInfoCircle />}
       />
-      <SnapshotExplainationModal isOpen={whatIsSnapshot.isOpen} onClose={whatIsSnapshot.onClose} />
     </>
   )
 }
