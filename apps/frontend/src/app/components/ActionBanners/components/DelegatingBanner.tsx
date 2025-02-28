506d016ae72995c53d492eff15e42e51e9d634a5
aya <75953386+ayabelarbi@users.noreply.github.com>
2025-02-28 13:45:19 +0100

    feat: veDelegating banner (#1975)
    * feat: veDelegating banner
    
    * feat: link to veDelegate
    
    * fix: assets and copy
    
    * nit: console.log
    
    * fix: wrap with useQuery
    
    * fix: rollback
    
    * Update apps/frontend/src/i18n/languages/en.json
    
    Co-authored-by: Beatriz Martinez Gonzalez <7704645+imagobea@users.noreply.github.com>
    
    * fix: propaging translation
    
    ---------
    
    Co-authored-by: Beatriz Martinez Gonzalez <7704645+imagobea@users.noreply.github.com>

---
+++ apps/frontend/src/app/components/ActionBanners/components/DelegatingBanner.tsx
@@ -0,0 +1,44 @@
+import { Text } from "@chakra-ui/react"
+import { useTranslation, Trans } from "react-i18next"
+import { UilInfoCircle } from "@iconscout/react-unicons"
+
+import { GenericBanner } from "@/app/components/Banners/GenericBanner"
+
+export const DelegatingBanner = () => {
+  const { t } = useTranslation()
+
+  const whatIsVeDelegate = () => {
+    window.open("https://docs.vedelegate.vet/faq#what-is-a-vepassport", "_blank", "noopener noreferrer")
+  }
+
+  const goToVeDelegate = () => {
+    window.open("https://vedelegate.vet", "_blank", "noopener noreferrer")
+  }
+
+  const description = (
+    <Text fontSize="lg" fontWeight="700" color="#5F4400">
+      <Trans
+        i18nKey="Your voting power has been transferred to <platform>veDelegate.vet</platform> which votes on your behalf. If you want to vote here, you must remove delegation on veDelegate before snapshot."
+        components={{
+          platform: <Text as="span" cursor="pointer" fontWeight="900" onClick={goToVeDelegate} />,
+        }}
+      />
+    </Text>
+  )
+
+  return (
+    <>
+      <GenericBanner
+        title={t("Voting Power Delegated")}
+        description={description}
+        logoSrc="/images/logo/veDelegate.svg"
+        backgroundColor="#FFD979"
+        backgroundImageSrc=""
+        buttonLabel={t("Learn more")}
+        onButtonClick={whatIsVeDelegate}
+        buttonVariant="outline"
+        buttonIcon={<UilInfoCircle />}
+      />
+    </>
+  )
+}
