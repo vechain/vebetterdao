46b3e81c0715d9118409e6cca56e4f894b26c0c4
Victor Emanuel <versaujob@gmail.com>
2025-03-13 13:02:04 +0000

    refactor: remove unused code from convertModal

--- apps/frontend/src/components/Convert/ConvertModal.tsx
+++ apps/frontend/src/components/Convert/ConvertModal.tsx
@@ -1,17 +1,15 @@
 "use-client"
-import { Card, CardBody, HStack, Text, Modal, ModalOverlay, useDisclosure } from "@chakra-ui/react"
+import { Card, CardBody, Text, Modal, ModalOverlay, useDisclosure } from "@chakra-ui/react"
 import { useCallback, useMemo, useState } from "react"
-import { useConvertB3tr, useTokenColors, useConvertVot3, useSmartAccountUpgradeRequired } from "@/hooks"
+import { useConvertB3tr, useConvertVot3, useSmartAccountUpgradeRequired } from "@/hooks"
 import { useForm } from "react-hook-form"
 import { CustomModalContent } from "../CustomModalContent"
 import { TransactionModal, TransactionModalStatus } from "../TransactionModal"
-import { getCompactFormatter } from "@repo/utils/FormattingUtils"
 import { TokenSelectionContent, SwapTokenContent } from "./components"
 import { useB3trBalance, useB3trConverted, useVot3Balance } from "@/api"
 import { useWallet } from "@vechain/vechain-kit"
 import BigNumber from "bignumber.js"
 import { useTranslation } from "react-i18next"
-import { FaArrowRight } from "react-icons/fa6"
 import { UpgradeSmartAccountModal } from "../UpgradeSmartAccountModal"
 import { ConfirmationConvertModalContent } from "../TransactionModal/ConfirmationConvertModalContent"
 import { SuccessConvertModalContent } from "../TransactionModal/SuccessConvertModalContent"
@@ -25,11 +23,6 @@ enum CardContentStep {
   SELECT_TOKEN,
   CONFIRM_SWAP,
 }
-
-const DECIMAL_PLACES = 4
-
-// Maximum precision of 4 decimals. Must also round down
-const compactFormatter = getCompactFormatter(DECIMAL_PLACES)
 
 const zoomInVariants = {
   hidden: { scale: 0.95, opacity: 0.8 },
@@ -112,16 +105,6 @@ export const ConvertModal = ({ isOpen, onClose }: Props) => {
     setValue("amount", "")
   }, [setValue])
 
-  const { b3trColor, vot3Color } = useTokenColors()
-
-  const amountText = useMemo(() => {
-    const amountNumber = Number(amount)
-
-    if (amountNumber < 0.0001) return `< 0.${"0".repeat(DECIMAL_PLACES - 1)}1`
-
-    return compactFormatter.format(amountNumber)
-  }, [amount])
-
   const b3trBalanceAfterSwap = useMemo(() => {
     if (isB3trToVot3) {
       return new BigNumber(b3trBalanceScaled).minus(amount).toString()
@@ -137,30 +120,6 @@ export const ConvertModal = ({ isOpen, onClose }: Props) => {
       return new BigNumber(vot3BalanceScaled).minus(amount).toString()
     }
   }, [isB3trToVot3, vot3BalanceScaled, amount])
-
-  const swapText = useMemo(() => {
-    if (isB3trToVot3) {
-      return (
-        <HStack>
-          <Text as="b">{amountText}</Text>
-          <Text color={b3trColor}>{t("B3TR")}</Text>
-          <FaArrowRight />
-          <Text as="b">{amountText}</Text>
-          <Text color={vot3Color}>{t("VOT3")}</Text>
-        </HStack>
-      )
-    } else {
-      return (
-        <HStack>
-          <Text as="b">{amountText}</Text>
-          <Text color={vot3Color}>{t("VOT3")}</Text>
-          <FaArrowRight />
-          <Text as="b">{amountText}</Text>
-          <Text color={b3trColor}>{t("B3TR")}</Text>
-        </HStack>
-      )
-    }
-  }, [isB3trToVot3, amountText, b3trColor, t, vot3Color])
 
   const convertTitle = useMemo(() => {
     return isB3trToVot3 ? t("Turn B3TR into VOT3") : t("Turn VOT3 into B3TR")
@@ -238,7 +197,6 @@ export const ConvertModal = ({ isOpen, onClose }: Props) => {
           [TransactionModalStatus.Success]: t("Swap Completed!"),
           [TransactionModalStatus.Error]: t("Error swapping"),
           [TransactionModalStatus.Pending]: t("Swapping..."),
-          [TransactionModalStatus.WaitingConfirmation]: swapText,
         }}
         txId={mutationData.txReceipt?.meta.txID}
         customContent={{
