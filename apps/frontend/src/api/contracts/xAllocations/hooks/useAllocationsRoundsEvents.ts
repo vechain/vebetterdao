38d1d773e57f8d0db98943199c1ff03276bfb772
aya <aya.belarbi16@gmail.com>
2025-03-17 11:05:12 +0100

    feat: refactoring useEvents and remove !!thor.status.head.number

--- apps/frontend/src/api/contracts/xAllocations/hooks/useAllocationsRoundsEvents.ts
+++ apps/frontend/src/api/contracts/xAllocations/hooks/useAllocationsRoundsEvents.ts
@@ -1,13 +1,9 @@
-import { useQuery } from "@tanstack/react-query"
-import { useConnex } from "@vechain/dapp-kit-react"
-
-import { abi } from "thor-devkit"
-import { getAllEvents } from "@/api/blockchain"
+import { useEvents } from "@/hooks"
 import { getConfig } from "@repo/config"
-import { XAllocationVotingJson } from "@repo/contracts"
+import { XAllocationVoting__factory } from "@repo/contracts"
 
-const XALLOCATION_CONTRACT = getConfig().xAllocationVotingContractAddress
-
+const contractInterface = XAllocationVoting__factory.createInterface()
+const contractAddress = getConfig().xAllocationVotingContractAddress
 export type RoundCreated = {
   roundId: string
   proposer: string
@@ -16,67 +12,37 @@ export type RoundCreated = {
   appsIds: string[]
 }
 
-export const getAllocationsRoundsEvents = async (thor: Connex.Thor) => {
-  const allocationCreatedAbi = XAllocationVotingJson.abi.find(abi => abi.name === "RoundCreated")
-  if (!allocationCreatedAbi) throw new Error("RoundCreated event not found")
-  const allocationCreatedEvent = new abi.Event(allocationCreatedAbi as abi.Event.Definition)
-
-  /**
-   * Filter criteria to get the events from the governor contract that we are interested in
-   * This way we can get all of them in one call
-   */
-  const filterCriteria = [
-    {
-      address: XALLOCATION_CONTRACT,
-      topic0: allocationCreatedEvent.signature,
-    },
-  ]
-
-  const events = await getAllEvents({ thor, filterCriteria })
-
-  /**
-   * Decode the events to get the data we are interested in (i.e the proposals)
-   */
-  const decodedCreatedAllocationEvents: RoundCreated[] = []
-
-  //   TODO: runtime validation with zod ?
-  events.forEach(event => {
-    switch (event.topics[0]) {
-      case allocationCreatedEvent.signature: {
-        const decoded = allocationCreatedEvent.decode(event.data, event.topics)
-        decodedCreatedAllocationEvents.push({
-          roundId: decoded[0],
-          proposer: decoded[1],
-          voteStart: decoded[2],
-          voteEnd: decoded[3],
-          appsIds: decoded[4],
-        })
-        break
-      }
-
-      default: {
-        throw new Error("Unknown event")
-      }
-    }
+/**
+ * Hook to get the allocation rounds events from the xAllocationVoting contract
+ * @returns  the allocation rounds events (i.e information about the rounds created)
+ */
+export const useAllocationsRoundsEvents = () => {
+  const rawAllocationCreatedEvents = useEvents({
+    contractAddress,
+    contractInterface,
+    eventName: "RoundCreated",
+    mapResponse: (decoded, meta) => ({
+      roundId: decoded.roundId,
+      proposer: decoded.proposer,
+      voteStart: decoded.voteStart,
+      voteEnd: decoded.voteEnd,
+      appsIds: decoded.appsIds,
+      blockNumber: meta.blockNumber,
+      txOrigin: meta.txOrigin,
+    }),
   })
 
+  const allocationEvents = rawAllocationCreatedEvents.data || []
+  const isLoading = rawAllocationCreatedEvents.isLoading
+
   return {
-    created: decodedCreatedAllocationEvents,
+    isLoading,
+    data: {
+      created: allocationEvents,
+    },
+    isError: rawAllocationCreatedEvents.isError,
+    error: rawAllocationCreatedEvents.error,
   }
 }
 
 export const getAllocationsRoundsEventsQueryKey = () => ["allocationRoundsEvents"]
-
-/**
- *  Hook to get the allocation rounds events from the xAllocationVoting contract (i.e the proposals created)
- * @returns  the allocation rounds events (i.e the proposals created)
- */
-export const useAllocationsRoundsEvents = () => {
-  const { thor } = useConnex()
-
-  return useQuery({
-    queryKey: getAllocationsRoundsEventsQueryKey(),
-    queryFn: async () => await getAllocationsRoundsEvents(thor),
-    enabled: !!thor && !!thor.status.head.number,
-  })
-}
