d6f6d1ba0bbc48d72dd9587a72dbbde888bae12a
Victor Emanuel <versaujob@gmail.com>
2025-03-18 16:52:06 +0000

    Refactor(VechainKit): Remove `SocialLoginTooltip` (#2043)
    * refactor: remove social login tooltip
    
    * chore: update translation
    
    * chore: remove unused image

--- apps/frontend/src/components/Navbar/DesktopNavbar.tsx
+++ apps/frontend/src/components/Navbar/DesktopNavbar.tsx
@@ -4,10 +4,6 @@ import { NavbarMenu } from "./NavbarMenu"
 import dynamic from "next/dynamic"
 import { Route } from "./Routes"
 import { NavbarBalance } from "./NavbarBalance"
-import { SocialLoginTooltip } from "../SocialLoginTooltip"
-import { FeatureFlag } from "@/constants"
-import { useFeatureFlag } from "@/hooks"
-import { useWallet, useWalletModal } from "@vechain/vechain-kit"
 
 const ConnectWalletButton = dynamic(
   () => import("@/components/ConnectWalletButton").then(mod => mod.ConnectWalletButton),
@@ -19,18 +15,6 @@ type Props = {
 }
 export const DesktopNavBar: React.FC<Props> = ({ routesToRender }) => {
   const [isLargerThan1800] = useMediaQuery("(min-width: 1800px)")
-
-  //VechainKit feature flag
-  const { isEnabled: isVechainKitFlagOn } = useFeatureFlag(FeatureFlag.VECHAIN_KIT)
-
-  const { account } = useWallet()
-  const { isOpen: isWalletModalOpen } = useWalletModal()
-
-  // Show the VeChainKit tooltip if :
-  // - the feature flag is enabled
-  // - the user is not connected
-  // - the wallet modal is not open
-  const shouldRenderVeChainKitTooltip = isVechainKitFlagOn && !account?.address && !isWalletModalOpen
 
   return (
     <>
@@ -55,11 +39,9 @@ export const DesktopNavBar: React.FC<Props> = ({ routesToRender }) => {
       <HStack flex={1} spacing={4} justifyContent={"end"}>
         {/* <ThemeSwitcher /> */}
         {isLargerThan1800 && <NavbarBalance />}
-        <SocialLoginTooltip isOpen={shouldRenderVeChainKitTooltip}>
-          <Box as="span">
-            <ConnectWalletButton />
-          </Box>
-        </SocialLoginTooltip>
+        <Box as="span">
+          <ConnectWalletButton />
+        </Box>
       </HStack>
     </>
   )
