47d57e68296c0da7b844038cbd74073fa37a3911
Beatriz Martinez Gonzalez <7704645+imagobea@users.noreply.github.com>
2025-03-20 17:16:27 +0100

    refactor: handling of apps and apps blacklisted (#2023)
    * refactor: use flag for including or excluding blacklisted apps
    
    * chore: filter blacklisted from apps hub
    
    * chore: filter blacklisted from alloc most voted apps
    
    * chore: clean cosoles
    
    * chore: filter blacklisted from action banners
    
    * docs: document functions changes
    
    * chore: rm unused component
    
    * test: fix rounds page test
    
    * chore: pr requested changes

--- apps/frontend/src/api/contracts/xApps/hooks/useXApps.ts
+++ apps/frontend/src/api/contracts/xApps/hooks/useXApps.ts
@@ -2,18 +2,27 @@ import { useQuery } from "@tanstack/react-query"
 import { useConnex } from "@vechain/vechain-kit"
 import { getXApps } from "../getXApps"
 
-export const getXAppsQueryKey = () => ["getXApps"]
+/**
+ * Query key for the xApps query
+ * @param filterBlacklisted - whether to filter blacklisted xApps
+ * @returns the query key
+ */
+export const getXAppsQueryKey = (filterBlacklisted: boolean = false) => [
+  "getXApps",
+  filterBlacklisted ? "includes-blacklisted" : "excludes-blacklisted",
+]
 
 /**
- *  Hook to get all the available xApps in the B3TR ecosystem
+ * Hook to get all the available xApps in the B3TR ecosystem
+ * @param filterBlacklisted - whether to filter blacklisted xApps
  * @returns all the available xApps in the B3TR ecosystem capped to 256
  */
-export const useXApps = () => {
+export const useXApps = ({ filterBlacklisted = false } = {}) => {
   const { thor } = useConnex()
 
   return useQuery({
-    queryKey: getXAppsQueryKey(),
-    queryFn: async () => await getXApps(thor),
+    queryKey: getXAppsQueryKey(filterBlacklisted),
+    queryFn: async () => await getXApps(thor, filterBlacklisted),
     enabled: !!thor,
   })
 }
