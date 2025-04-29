19465353b14e2c071d920a4b203d7707a812da0b
roisindowling <roisin.dowling@vechain.org>
2025-04-29 13:43:04 +0100

    Add function call script for GM B3TR required

--- packages/contracts/test/helpers/config.ts
+++ packages/contracts/test/helpers/config.ts
@@ -143,5 +143,30 @@ export function createTestConfig() {
     GM_PERCENTAGE_OF_TREASURY: 2500,
 
     GM_MULTIPLIERS_V2: [0, 110, 120, 150, 200, 250, 300, 500, 1000, 2500], // GM multipiers according
+
+    /*
+    Level => B3TR Required (halved)
+
+    2 (Moon) => 5,000 B3TR
+    3 (Mercury) => 12,500 B3TR
+    4 (Venus) => 25,000 B3TR
+    5 (Mars) => 50,000 B3TR
+    6 (Jupiter) => 125,000 B3TR
+    7 (Saturn) => 250,000 B3TR
+    8 (Uranus) => 1,250,000 B3TR
+    9 (Neptune) => 2,500,000 B3TR
+    10 (Galaxy) => 12,500,000 B3TR
+*/
+    GM_NFT_B3TR_REQUIRED_TO_UPGRADE_TO_LEVEL_V2: [
+      5000000000000000000000n,
+      12500000000000000000000n,
+      25000000000000000000000n,
+      50000000000000000000000n,
+      125000000000000000000000n,
+      250000000000000000000000n,
+      1250000000000000000000000n,
+      2500000000000000000000000n,
+      12500000000000000000000000n,
+    ],
   })
 }
