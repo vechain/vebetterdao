e714491e4267cfb66b179f213637e85ae6c26f20
Giulia Argiolas <giulia.argiolas@vechain.org>
2025-05-07 12:18:25 +0200

    support for multiple backends

--- terraform/vebetterpassport/provider.tf
+++ terraform/vebetterpassport/provider.tf
@@ -10,7 +10,6 @@ terraform {
 
   required_version = ">= 1.0.0"
   backend "s3" {
-    bucket               = "b3tr-terraform-state-dev"
     key                  = "b3tr-vebetterpassport.tfstate"
     region               = "eu-west-1"
     workspace_key_prefix = "workspaces"
