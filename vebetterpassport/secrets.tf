a3985f471930215436106bf138ca827379bc9fba
Giulia Argiolas <giulia.argiolas@vechain.org>
2025-05-14 09:35:56 +0200

    align to commit 60787dd

--- vebetterpassport/secrets.tf
+++ vebetterpassport/secrets.tf
@@ -1,7 +1,3 @@
-# Conditionally determine which secret to fetch based on environment
-locals {
-  secret_name = terraform.workspace == "prod" ? "vebetterpassport_reset_signal_mainnet" : "vebetterpassport_reset_signal_testnet"
-}
 
 # Data resource to fetch the secret containing multiple keys
 data "aws_secretsmanager_secret" "vebetterpassport_secret" {
