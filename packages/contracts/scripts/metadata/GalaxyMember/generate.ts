7876fc9ecf49f026b481ec6a25c0047abe265037
Victor Emanuel <versaujob@gmail.com>
2025-05-20 13:40:57 +0100

    refactor: improve gm level ipfs upload script

--- packages/contracts/scripts/metadata/GalaxyMember/generate.ts
+++ packages/contracts/scripts/metadata/GalaxyMember/generate.ts
@@ -1,6 +1,7 @@
 import fs from "fs/promises"
-import { toIPFSURL, uploadDirectoryToIPFS, zipFolder } from "../../helpers"
 import path from "path"
+import { toIPFSURL, uploadBlobToIPFS } from "../../helpers"
+import { IMAGE_PATH, GM_IMAGES_DATA, METADATA_PATH, description } from "./const"
 
 /**
  * Interface for an NFT attribute.
@@ -20,27 +21,6 @@ interface Metadata {
   image: string
   attributes: Attribute[]
 }
-
-// Array of attributes for each level of the NFT
-const levelAttributes: Record<string, string | number>[] = [
-  {
-    Level: 1,
-  },
-  {
-    Level: 2,
-  },
-  {
-    Level: 3,
-  },
-]
-
-const levelNames = ["Earth", "Moon", "Mercury"]
-const description =
-  "Galaxy Member is a membership NFT to the VeBetterDAO ecosystem. It grants you access to exclusive features and benefits."
-
-const METADATA_PATH = path.join(__dirname, "../../../metadata/galaxyMember/metadata")
-const IMAGE_ZIP_PATH = path.join(__dirname, "../../../metadata/galaxyMember/images.zip")
-const IMAGE_PATH = path.join(__dirname, "../../../metadata/galaxyMember/images")
 
 /**
  * Converts a record of attributes into an array of `Attribute` objects.
@@ -87,24 +67,60 @@ async function saveMetadataToFile(metadata: Metadata, fileName: string): Promise
 }
 
 /**
+ * Asynchronously gets the NFT images from the IMAGE_PATH.
+ * @returns An array of NFT image file names.
+ */
+async function getNFTImages(): Promise<string[]> {
+  //Get only the png files in the IMAGE_PATH
+  const images = (await fs.readdir(IMAGE_PATH)).filter(file => file.toLowerCase().endsWith(".png"))
+  return images
+}
+
+/**
+ * Asynchronously gets the image blob for a given image name.
+ * @param imageName - The name of the image to get the blob for.
+ * @returns A promise that resolves to the image blob.
+ */
+async function getImageBlob(imageName: string): Promise<Blob> {
+  const imagePath = path.join(IMAGE_PATH, imageName)
+  const imageBuffer = await fs.readFile(imagePath)
+  return new Blob([imageBuffer], { type: "image/png" })
+}
+
+/**
  * Main function to generate and save NFT metadata.
  */
 async function generateAndSaveMetadata(): Promise<void> {
   try {
-    // 1. Ensure the zip folder exists
-    await zipFolder(IMAGE_PATH, IMAGE_ZIP_PATH)
-
-    // 2. Upload images to IPFS and get URL
-    const [imagesIpfsUrl, images, folderName] = await uploadDirectoryToIPFS(IMAGE_ZIP_PATH, IMAGE_PATH)
-
-    console.log("Galaxy Member Images IPFS URL:", toIPFSURL(imagesIpfsUrl, undefined, folderName))
-
-    // 3. Generate metadata for each level
-    for (let i = 0; i < levelAttributes.length; i++) {
-      const image = toIPFSURL(imagesIpfsUrl, images[i].name, folderName)
-      const metadata = generateMetadata(levelNames[i], description, levelAttributes[i], image)
-      await saveMetadataToFile(metadata, String(i + 1))
+    // 1. Check if levelNames matches the amount of images in the IMAGE_PATH
+    const images = await getNFTImages()
+    if (GM_IMAGES_DATA.length !== images.length) {
+      throw new Error("Galaxy Member images and data do not match")
     }
+
+    // 2. Upload each image individually and store the CID
+    const imageCids = await Promise.all(
+      GM_IMAGES_DATA.map(async ({ levelName }, index) => {
+        console.log(`Uploading image ${levelName}, path -> ${images[index]}`)
+        const imageBlob = await getImageBlob(images[index])
+        const cid = await uploadBlobToIPFS(imageBlob, images[index])
+        console.log(`GM Image ${levelName} uploaded to CID -> ${cid}`)
+        return cid
+      }),
+    )
+
+    // 3. Generate and save the metadata for each level
+    await Promise.all(
+      GM_IMAGES_DATA.map(async ({ levelName, levelAttributes }, index) => {
+        //Format the image CID to be used in the metadata (eg. ipfs://<cid>)
+        const image = toIPFSURL(imageCids[index])
+        const metadata = generateMetadata(levelName, description, levelAttributes, image)
+        const fileName = String(index + 1)
+        return await saveMetadataToFile(metadata, fileName)
+      }),
+    )
+
+    return
   } catch (error) {
     console.error("Error generating metadata:", error)
     throw error // Rethrow the error after logging to handle it further up the call stack.
