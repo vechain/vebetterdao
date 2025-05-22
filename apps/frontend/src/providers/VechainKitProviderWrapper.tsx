993fea032fbd17506a88b3f6299601715ad0f89a
Victor Emanuel <versaujob@gmail.com>
2025-05-22 10:10:26 +0100

    feat: adding config for terms and conditions

--- apps/frontend/src/providers/VechainKitProviderWrapper.tsx
+++ apps/frontend/src/providers/VechainKitProviderWrapper.tsx
@@ -84,7 +84,18 @@ export function VechainKitProviderWrapper({ children }: Props) {
       network={{
         type: networkType,
       }}
-      allowCustomTokens={allowCustomTokens}>
+      allowCustomTokens={allowCustomTokens}
+      legalDocuments={{
+        allowAnalytics: true,
+        termsAndConditions: [
+          {
+            url: "https://vechain.org/terms-of-use/", //TODO: Update to the correct URL
+            displayName: "VeBetterDAO Terms of Service",
+            version: 1,
+            required: true,
+          },
+        ],
+      }}>
       {children}
     </VeChainKitProvider>
   )
