299ed742b6a1aabf5e3be71b6c6693b81f546d25
aya <75953386+ayabelarbi@users.noreply.github.com>
2025-06-26 19:35:29 +0100

    Fix: Indexer calls optimisation on app page  (#2274)

--- apps/frontend/src/app/apps/hooks/useAppsSustainabilityData.ts
+++ apps/frontend/src/app/apps/hooks/useAppsSustainabilityData.ts
@@ -1,69 +1,27 @@
-import { useState, useEffect } from "react"
-import { XApp, UnendorsedApp } from "@/api"
-import { getSustainabilityAppOverview } from "@/api/indexer/sustainability/useSustainabilityAppOverview"
-
-type AllApps = XApp | UnendorsedApp
+import { useSustainabilityAppOverview } from "@/api/indexer/sustainability/useSustainabilityAppOverview"
+import { useMemo } from "react"
 
 /**
- * Hook to fetch rewards data for a list of apps
- * @param apps Array of apps to fetch rewards data for
- * @returns Object containing rewards map
+ * Hook to fetch appId sorted by totalRewardAmount
+ * @returns Map containing appId and their position in the sorted list
  */
-export function useAppsSustainabilityData(apps: AllApps[]) {
-  const [rewardsMap, setRewardsMap] = useState<Map<string, number>>(new Map())
-  const [isLoading, setIsLoading] = useState(true)
+export function useAppsSustainabilityData() {
+  const querySearchData = {
+    direction: "desc" as const,
+    sortBy: "totalRewardAmount",
+  }
 
-  useEffect(() => {
-    if (!apps.length) {
-      setIsLoading(false)
-      return
-    }
+  const { data, isLoading, error } = useSustainabilityAppOverview(querySearchData)
 
-    setIsLoading(true)
-
-    const newRewardsMap = new Map<string, number>()
-
-    let completedRequests = 0
-
-    const checkAllComplete = () => {
-      if (completedRequests === apps.length) {
-        setRewardsMap(newRewardsMap)
-        setIsLoading(false)
-      }
-    }
-
-    // Fetch data for each app
-    apps.forEach(app => {
-      getSustainabilityAppOverview({
-        appId: app.id,
-        direction: "desc",
-        page: 0,
-        size: 1,
-      })
-        .then(data => {
-          // Store the rewards data
-          newRewardsMap.set(app.id, data.data?.[0]?.totalRewardAmount || 0)
-        })
-        .catch(error => {
-          console.error(`Error fetching data for app ${app.id}:`, error)
-          // Set default values on error
-          newRewardsMap.set(app.id, 0)
-        })
-        .finally(() => {
-          completedRequests++
-          checkAllComplete()
-        })
-    })
-
-    if (apps.length === 0) {
-      setIsLoading(false)
-    }
-
-    return () => {}
-  }, [apps])
+  const allAppsSortedByRewards = useMemo(() => {
+    if (!data?.pages[0]) return new Map()
+    const appIds = data.pages[0].data || []
+    return new Map(appIds.map((item, index) => [item.entity, index]))
+  }, [data])
 
   return {
-    rewardsMap,
+    allAppsSortedByRewards,
     isLoading,
+    error,
   }
 }
