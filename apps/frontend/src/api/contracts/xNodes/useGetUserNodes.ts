61e20fab7956d5a5aa43f2ee1446599920c86861
Victor Emanuel <versaujob@gmail.com>
2025-06-30 19:25:43 +0100

    feat(hooks): updated use get user nodes to fetch legacy info

--- apps/frontend/src/api/contracts/xNodes/useGetUserNodes.ts
+++ apps/frontend/src/api/contracts/xNodes/useGetUserNodes.ts
@@ -1,7 +1,8 @@
-import { getCallKey, useCall } from "@/hooks"
+import { getCallKey, useCall, useMultipleCalls, UseCallParams } from "@/hooks"
 import { getConfig } from "@repo/config"
 import { NodeManagement__factory } from "@repo/contracts"
 import { UseQueryResult } from "@tanstack/react-query"
+import { useMemo } from "react"
 
 const contractAddress = getConfig().nodeManagementContractAddress
 const contractInterface = NodeManagement__factory.createInterface()
@@ -17,6 +18,9 @@ export type UserNode = {
   isXNodeDelegatee: boolean
   delegatee: string
 }
+type UserNodeWithIsLegacy = UserNode & {
+  isLegacyNode: boolean
+}
 
 /**
  * Get the query key for fetching user nodes
@@ -29,12 +33,14 @@ export const getUserNodesQueryKey = (user?: string) => getCallKey({ method, keyA
  * @param user - The address of the user to check
  * @returns An array of objects containing user node details
  */
-export const useGetUserNodes = (user?: string): UseQueryResult<UserNode[], Error> => {
-  return useCall({
+export const useGetUserNodes = (user?: string): UseQueryResult<UserNodeWithIsLegacy[], Error> => {
+  // First get the token ID owned by the address (legacy contract only supports one token per address)
+  const userNodes = useCall({
     contractInterface,
     contractAddress,
     method,
     args: [user],
+    enabled: !!user,
     mapResponse: response => {
       // Response will be an array of node structs
       return response.decoded[0].map((node: any) => ({
@@ -48,8 +54,45 @@ export const useGetUserNodes = (user?: string): UseQueryResult<UserNode[], Error
         delegatee: node.delegatee,
       }))
     },
-    enabled: !!user,
   })
+
+  const nodes = userNodes?.data
+
+  // For each node, call isLegacyNode to determine if it is a legacy node or not
+  const isLegacyCheckCalls = useMemo(() => {
+    if (!nodes) return []
+    return (nodes as UserNode[]).map(
+      ({ nodeId }): UseCallParams<any> => ({
+        contractInterface,
+        contractAddress,
+        method: "isLegacyNode",
+        args: [nodeId],
+        mapResponse: res => {
+          if (!res.decoded) return false
+          return {
+            nodeId,
+            isLegacyNode: res.decoded[0],
+          }
+        },
+      }),
+    )
+  }, [nodes])
+
+  const { data: nodeIdsWithIsLegacy } = useMultipleCalls(isLegacyCheckCalls)
+
+  //Merge nodeIdsWithIsLegacy with nodes based on nodeId
+  const allNodesWithIsLegacy = useMemo(() => {
+    if (!nodeIdsWithIsLegacy || !nodes) return []
+    return nodes.map((node: UserNode, index: number) => ({
+      ...node,
+      isLegacyNode: nodeIdsWithIsLegacy[index].isLegacyNode,
+    }))
+  }, [nodeIdsWithIsLegacy, nodes])
+
+  return {
+    ...userNodes,
+    data: allNodesWithIsLegacy,
+  }
 }
 
 // For backward compatibility (if needed)
