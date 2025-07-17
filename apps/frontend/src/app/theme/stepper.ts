dcb238962b203c1d9babc92b8c30bdb2e9bba977
Victor Emanuel <versaujob@gmail.com>
2025-07-17 15:41:34 +0100

    feat: setup stepper theme

--- apps/frontend/src/app/theme/stepper.ts
+++ apps/frontend/src/app/theme/stepper.ts
@@ -3,6 +3,20 @@ import { ComponentStyleConfig } from "@chakra-ui/react"
 export const StepperStyle: ComponentStyleConfig = {
   // styles for different visual variants ("outline", "solid")
   variants: {
+    grants: {
+      indicator: {
+        bg: "#EFEFEF",
+        color: "#6A6A6A",
+        "&[data-status=complete]": {
+          bg: "#E0E9FE",
+          color: "#004CFC",
+        },
+        "&[data-status=active]": {
+          bg: "#004CFC",
+          color: "#FFFFFF",
+        },
+      },
+    },
     primaryVertical: {
       indicator: {
         bg: "#E0E9FE",
