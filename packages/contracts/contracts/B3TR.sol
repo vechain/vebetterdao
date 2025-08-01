fc0467739d00b0f73c94be0c552594cb99bc50f8
Luca Bandini <luca.bandini@vechain.org>
2025-08-01 15:32:06 +0200

    test: trigger slither

--- packages/contracts/contracts/B3TR.sol
+++ packages/contracts/contracts/B3TR.sol
@@ -21,7 +21,6 @@
 //                                   ##############
 //                                   #########
 
-
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/access/AccessControl.sol";
