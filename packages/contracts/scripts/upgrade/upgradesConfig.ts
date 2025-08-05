c52d629c685b6073bf1c362f0d60fe1fa27b9ae5
Victor Emanuel <versaujob@gmail.com>
2025-08-05 18:10:42 +0100

    feat: setup governor v7 upgrade script

--- packages/contracts/scripts/upgrade/upgradesConfig.ts
+++ packages/contracts/scripts/upgrade/upgradesConfig.ts
@@ -19,7 +19,7 @@ export const upgradeConfig: Record<string, UpgradeContract> = {
   },
   B3TRGovernor: {
     name: "b3tr-governor",
-    versions: ["v2", "v3", "v4", "v5", "v6"],
+    versions: ["v2", "v3", "v4", "v5", "v6", "v7"],
     configAddressField: "b3trGovernorAddress",
     descriptions: {
       v2: "Give ability to contract admins to call governance only functions",
@@ -27,6 +27,7 @@ export const upgradeConfig: Record<string, UpgradeContract> = {
       v4: "Integrate VeBetterPassport contract",
       v5: "Vechain Nodes x GM upgrades feature",
       v6: "Proposal Execution: Add GM Pool for GM Holder Rewards (Align IVoterRwards)",
+      v7: "Proposal Execution + Grants Feature: Add extra voting power based on support tokens + deposit threshold cap + proposal type concept",
     },
   },
   XAllocationVoting: {
