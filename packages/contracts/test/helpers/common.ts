f658bef7e04e3ee044d21589b68de508947d1056
Victor Emanuel <versaujob@gmail.com>
2025-08-05 22:37:46 +0100

    fix: governance common test

--- packages/contracts/test/helpers/common.ts
+++ packages/contracts/test/helpers/common.ts
@@ -321,9 +321,8 @@ export const createProposalWithMultipleFunctions = async (
       return Contract.interface.encodeFunctionData(func, args[index])
     }),
     description,
-    functionsToCall[0],
-    args[0],
     roundId,
+    0, //No deposit amount
   )
 
   return tx
