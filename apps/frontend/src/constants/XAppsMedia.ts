7c72ce6ad7d91b556a3bf7c62968d396df25ee32
dara-vechain <dara.heng@vechain.org>
2025-08-08 14:20:27 +0100

    feat: update constraint to min 720x720 - 1:1 ratio

--- apps/frontend/src/constants/XAppsMedia.ts
+++ apps/frontend/src/constants/XAppsMedia.ts
@@ -5,7 +5,7 @@ export const LOGO_UPLOAD_GUIDELINES =
 export const BANNER_UPLOAD_GUIDELINES = "Recommended size: 1240×460. Keep key content centered for consistent display."
 export const VEWORLD_BANNER_UPLOAD_GUIDELINES = "Recommended size: 800×400. Maintain a 2:1 aspect ratio."
 export const VEWORLD_FEATURED_IMAGE_UPLOAD_GUIDELINES =
-  "Recommended size: 686×515. Maintain a 4:3 aspect ratio. Assets should not contain logos, brands, text, or any unrelated objects/elements to the app's purpose"
+  "Recommended size: 720x720 (minimum). Maintain a 1:1 aspect ratio. Assets should not contain logos, brands, text, or any unrelated objects/elements to the app's purpose"
 export const SCREENSHOT_UPLOAD_GUIDELINES = "Maintain a 9:16 aspect ratio."
 export const IMAGE_REQUIREMENTS = {
   logo: {
@@ -52,10 +52,10 @@ export const IMAGE_REQUIREMENTS = {
     extension: "png",
     mimeType: "image/png",
     dimensions: {
-      minWidth: 686,
-      minHeight: 515,
-      ratio: 1.333333333333333, // 4:3
-      ratioString: "4:3",
+      minWidth: 720,
+      minHeight: 720,
+      ratio: 1, // 1:1
+      ratioString: "1:1",
     },
   },
 } as const
