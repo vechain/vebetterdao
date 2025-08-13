f458d71e718be0c1dc4051e628e25b1c484960de
Victor Emanuel <versaujob@gmail.com>
2025-08-13 13:59:12 +0100

    chore: align contract addresses with new grants manager implementation

--- packages/config/testnet-staging.ts
+++ packages/config/testnet-staging.ts
@@ -21,7 +21,7 @@ const config: AppConfig = {
   x2EarnCreatorContractAddress: "0xb89f0ecdaf9987f87912d6c77756435fe4085b05",
   tokenAuctionContractAddress: "0x0747b39abc0de3d11c8ddfe2e7eed00aaa8d475c",
   stargateNFTContractAddress: "0x1ec1d168574603ec35b9d229843b7c2b44bcb770",
-  grantsManagerContractAddress: "0x6ae5f67faccf1207dc26cf3d26868c7a090df519",
+  grantsManagerContractAddress: "0x005af71e9b2d629c0c6e2f9d39fdfd1bb967c0ea",
   indexerUrl: "https://b3tr.testnet.vechain.org/api/v1",
   nodeUrl: "https://testnet.vechain.org",
   network: {
