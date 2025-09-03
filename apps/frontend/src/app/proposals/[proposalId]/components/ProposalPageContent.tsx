2f0c94a3cb7c232f70f99825dfe3ae0bff85be97
Victor Emanuel <versaujob@gmail.com>
2025-09-03 18:43:02 +0100

    feat: move to a separate component the interaction cards

--- apps/frontend/src/app/proposals/[proposalId]/components/ProposalPageContent.tsx
+++ apps/frontend/src/app/proposals/[proposalId]/components/ProposalPageContent.tsx
@@ -14,7 +14,7 @@ import {
 } from "@chakra-ui/react"
 import { ProposalOverview } from "./ProposalOverview"
 import { useMemo } from "react"
-import { ProposalType } from "@/hooks/proposals/grants/types"
+import { ProposalType, ProposalEnriched } from "@/hooks/proposals/grants/types"
 import { PageBreadcrumb } from "@/app/components/PageBreadcrumb"
 import { useTranslation } from "react-i18next"
 import { useProposalEnriched } from "@/hooks"
@@ -22,12 +22,70 @@ import { CountdownBoxes } from "@/components"
 import { TbClockHour8 } from "react-icons/tb"
 import { FiBarChart2 } from "react-icons/fi"
 import { FaRegHeart, FaHeart } from "react-icons/fa"
+import { useProposalInteractionDates } from "@/api/contracts/governance/hooks/useProposalInteractionDates"
+import dayjs from "dayjs"
 type Props = {
   proposalId: string
 }
 
-export const ProposalPageContent: React.FC<Props> = ({ proposalId }) => {
+const ProposalInteractionCard = ({ proposal }: { proposal: ProposalEnriched }) => {
   const { t } = useTranslation()
+
+  const { supportEndDate, votingEndDate } = useProposalInteractionDates(proposal)
+  const daysLeftToSupport = dayjs(supportEndDate).diff(dayjs(), "days")
+  const hoursLeftToSupport = dayjs(supportEndDate).diff(dayjs(), "hours") % 24
+  const minutesLeftToSupport = dayjs(supportEndDate).diff(dayjs(), "minutes") % 60
+
+  const daysLeftToVoting = dayjs(votingEndDate).diff(dayjs(), "days")
+  const hoursLeftToVoting = dayjs(votingEndDate).diff(dayjs(), "hours") % 24
+  const minutesLeftToVoting = dayjs(votingEndDate).diff(dayjs(), "minutes") % 60
+
+  const isVotingPhase = dayjs().isAfter(dayjs(supportEndDate))
+  const daysLeft = isVotingPhase ? daysLeftToVoting : daysLeftToSupport
+  const hoursLeft = isVotingPhase ? hoursLeftToVoting : hoursLeftToSupport
+  const minutesLeft = isVotingPhase ? minutesLeftToVoting : minutesLeftToSupport
+  return (
+    <Card.Root variant="baseWithBorder">
+      <Card.Header as={HStack}>
+        <Icon as={TbClockHour8} boxSize={5} />
+        <Card.Title>
+          <Heading>{t("Ends in")}</Heading>
+        </Card.Title>
+      </Card.Header>
+      <Card.Body gap={4}>
+        <CountdownBoxes days={daysLeft} hours={hoursLeft} minutes={minutesLeft} />
+        <Separator />
+        <HStack justify="space-between">
+          <HStack>
+            <Icon as={FiBarChart2} boxSize={5} />
+            <Heading>{t("Results")}</Heading>
+          </HStack>
+          <Button variant="primaryGhost">{t("Details")}</Button>
+        </HStack>
+        <Progress.Root key="results" value={100}>
+          <Progress.Track borderRadius="full" height="8px">
+            <Progress.Range borderRadius="full" bg="success.primary" />
+          </Progress.Track>
+        </Progress.Root>
+        <HStack color="success.primary">
+          <Icon as={FaRegHeart} boxSize={5} />
+          <Text>{t("100%")}</Text>
+        </HStack>
+        <HStack>
+          <Text color="gray.600">{t("You supported with")}</Text>
+          <Box border="2px solid" borderColor="success.primary" color="success.primary" borderRadius={"lg"}>
+            <HStack gap={2} px={"12px"} py={"8px"}>
+              <Icon as={FaHeart} boxSize={5} color="success.primary" />
+              <Text>{t("{{votingPower}} VOT3", { votingPower: 1.56 })}</Text>
+            </HStack>
+          </Box>
+        </HStack>
+      </Card.Body>
+    </Card.Root>
+  )
+}
+
+export const ProposalPageContent: React.FC<Props> = ({ proposalId }) => {
   const { data: { proposals } = { proposals: [] }, isLoading } = useProposalEnriched()
 
   const proposal = useMemo(() => {
@@ -61,43 +119,7 @@ export const ProposalPageContent: React.FC<Props> = ({ proposalId }) => {
           <ProposalOverview isGrant={isGrant} proposal={proposal} isLoading={isLoading} />
         </GridItem>
         <GridItem colSpan={[3, 3, 1]}>
-          <Card.Root variant="baseWithBorder">
-            <Card.Header as={HStack}>
-              <Icon as={TbClockHour8} boxSize={5} />
-              <Card.Title>
-                <Heading>{t("Ends in")}</Heading>
-              </Card.Title>
-            </Card.Header>
-            <Card.Body gap={4}>
-              <CountdownBoxes days={1} hours={1} minutes={2} />
-              <Separator />
-              <HStack justify="space-between">
-                <HStack>
-                  <Icon as={FiBarChart2} boxSize={5} />
-                  <Heading>{t("Results")}</Heading>
-                </HStack>
-                <Button variant="primaryGhost">{t("Details")}</Button>
-              </HStack>
-              <Progress.Root key="results" value={100}>
-                <Progress.Track borderRadius="full" height="8px">
-                  <Progress.Range borderRadius="full" bg="success.primary" />
-                </Progress.Track>
-              </Progress.Root>
-              <HStack color="success.primary">
-                <Icon as={FaRegHeart} boxSize={5} />
-                <Text>{t("100%")}</Text>
-              </HStack>
-              <HStack>
-                <Text color="gray.600">{t("You supported with")}</Text>
-                <Box border="2px solid" borderColor="success.primary" color="success.primary" borderRadius={"lg"}>
-                  <HStack gap={2} px={"12px"} py={"8px"}>
-                    <Icon as={FaHeart} boxSize={5} color="success.primary" />
-                    <Text>{t("{{votingPower}} VOT3", { votingPower: 1.56 })}</Text>
-                  </HStack>
-                </Box>
-              </HStack>
-            </Card.Body>
-          </Card.Root>
+          <ProposalInteractionCard proposal={proposal} />
         </GridItem>
       </Grid>
     </VStack>
