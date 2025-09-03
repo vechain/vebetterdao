327575fefd0921a4d7bc77fd47ca77286f82671f
Victor Emanuel <versaujob@gmail.com>
2025-09-03 18:08:20 +0100

    feat: add ends in component

--- apps/frontend/src/app/proposals/[proposalId]/components/ProposalPageContent.tsx
+++ apps/frontend/src/app/proposals/[proposalId]/components/ProposalPageContent.tsx
@@ -1,11 +1,27 @@
-import { Grid, GridItem, VStack, Text } from "@chakra-ui/react"
+import {
+  Grid,
+  GridItem,
+  VStack,
+  Card,
+  Icon,
+  HStack,
+  Separator,
+  Heading,
+  Button,
+  Progress,
+  Text,
+  Box,
+} from "@chakra-ui/react"
 import { ProposalOverview } from "./ProposalOverview"
 import { useMemo } from "react"
 import { ProposalType } from "@/hooks/proposals/grants/types"
 import { PageBreadcrumb } from "@/app/components/PageBreadcrumb"
 import { useTranslation } from "react-i18next"
 import { useProposalEnriched } from "@/hooks"
-
+import { CountdownBoxes } from "@/components"
+import { TbClockHour8 } from "react-icons/tb"
+import { FiBarChart2 } from "react-icons/fi"
+import { FaRegHeart, FaHeart } from "react-icons/fa"
 type Props = {
   proposalId: string
 }
@@ -45,9 +61,43 @@ export const ProposalPageContent: React.FC<Props> = ({ proposalId }) => {
           <ProposalOverview isGrant={isGrant} proposal={proposal} isLoading={isLoading} />
         </GridItem>
         <GridItem colSpan={[3, 3, 1]}>
-          <VStack align="stretch" gap={8}>
-            <Text>{t("proposal")}</Text>
-          </VStack>
+          <Card.Root variant="baseWithBorder">
+            <Card.Header as={HStack}>
+              <Icon as={TbClockHour8} boxSize={5} />
+              <Card.Title>
+                <Heading>{t("Ends in")}</Heading>
+              </Card.Title>
+            </Card.Header>
+            <Card.Body gap={4}>
+              <CountdownBoxes days={1} hours={1} minutes={2} />
+              <Separator />
+              <HStack justify="space-between">
+                <HStack>
+                  <Icon as={FiBarChart2} boxSize={5} />
+                  <Heading>{t("Results")}</Heading>
+                </HStack>
+                <Button variant="primaryGhost">{t("Details")}</Button>
+              </HStack>
+              <Progress.Root key="results" value={100}>
+                <Progress.Track borderRadius="full" height="8px">
+                  <Progress.Range borderRadius="full" bg="success.primary" />
+                </Progress.Track>
+              </Progress.Root>
+              <HStack color="success.primary">
+                <Icon as={FaRegHeart} boxSize={5} />
+                <Text>{t("100%")}</Text>
+              </HStack>
+              <HStack>
+                <Text color="gray.600">{t("You supported with")}</Text>
+                <Box border="2px solid" borderColor="success.primary" color="success.primary" borderRadius={"lg"}>
+                  <HStack gap={2} px={"12px"} py={"8px"}>
+                    <Icon as={FaHeart} boxSize={5} color="success.primary" />
+                    <Text>{t("{{votingPower}} VOT3", { votingPower: 1.56 })}</Text>
+                  </HStack>
+                </Box>
+              </HStack>
+            </Card.Body>
+          </Card.Root>
         </GridItem>
       </Grid>
     </VStack>
