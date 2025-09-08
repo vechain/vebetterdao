1047fc45ce1c0854daf9b467a2f23f8dee08b25d
Victor Emanuel <versaujob@gmail.com>
2025-09-08 16:55:08 +0100

    fix: handling undefined param

--- apps/frontend/src/app/proposals/[proposalId]/components/ProposalTimeline/ProposalTimeline.tsx
+++ apps/frontend/src/app/proposals/[proposalId]/components/ProposalTimeline/ProposalTimeline.tsx
@@ -7,7 +7,7 @@ import { useMemo, useState, useEffect } from "react"
 import { TimelineElements } from "./TimelineElements"
 
 type Props = {
-  proposal: ProposalEnriched | GrantProposalEnriched
+  proposal?: ProposalEnriched | GrantProposalEnriched
 }
 
 export const ProposalTimeline = ({ proposal }: Props) => {
@@ -20,58 +20,58 @@ export const ProposalTimeline = ({ proposal }: Props) => {
 
   const invalidState = useMemo(() => {
     return (
-      proposal.state === ProposalState.Defeated ||
-      proposal.state === ProposalState.Canceled ||
-      proposal.state === ProposalState.DepositNotMet
+      proposal?.state === ProposalState.Defeated ||
+      proposal?.state === ProposalState.Canceled ||
+      proposal?.state === ProposalState.DepositNotMet
       // If it is a grant, proposa.State == GrantState
-      // proposal.state === ProposalState.Rejected
+      // proposal?.state === ProposalState.Rejected
     )
-  }, [proposal.state])
+  }, [proposal?.state])
 
   const steps = useMemo(() => {
     return [
       {
         key: 0,
         state: ProposalState.Pending,
-        round: proposal.votingRoundId,
-        startDate: proposal.createdAt,
-        endDate: proposal.createdAt, // round ID to date ( to get beggining and end of the round)
+        round: proposal?.votingRoundId,
+        startDate: proposal?.createdAt,
+        endDate: proposal?.createdAt, // round ID to date ( to get beggining and end of the round)
       },
       {
         key: 1,
         state: ProposalState.Active,
-        round: proposal.votingRoundId,
-        startDate: proposal.createdAt,
-        endDate: proposal.createdAt,
+        round: proposal?.votingRoundId,
+        startDate: proposal?.createdAt,
+        endDate: proposal?.createdAt,
       },
       {
         key: 2,
         state:
-          proposal.state === ProposalState.Defeated
+          proposal?.state === ProposalState.Defeated
             ? ProposalState.Defeated
-            : proposal.state === ProposalState.Canceled
+            : proposal?.state === ProposalState.Canceled
               ? ProposalState.Canceled
               : ProposalState.Succeeded,
-        round: proposal.votingRoundId,
-        startDate: proposal.createdAt,
-        endDate: proposal.createdAt,
+        round: proposal?.votingRoundId,
+        startDate: proposal?.createdAt,
+        endDate: proposal?.createdAt,
       },
-      ...(proposal.state !== ProposalState.Defeated && proposal.state !== ProposalState.Canceled
+      ...(proposal?.state !== ProposalState.Defeated && proposal?.state !== ProposalState.Canceled
         ? [
             // If the grant got rejected, the proposal is rejected ( GrantState.Rejected )
             {
               key: 3,
               state: ProposalState.InDevelopment,
-              round: proposal.votingRoundId,
-              startDate: proposal.createdAt,
-              endDate: proposal.createdAt,
+              round: proposal?.votingRoundId,
+              startDate: proposal?.createdAt,
+              endDate: proposal?.createdAt,
             },
             {
               key: 4,
               state: ProposalState.Completed,
-              round: proposal.votingRoundId,
-              startDate: proposal.createdAt,
-              endDate: proposal.createdAt,
+              round: proposal?.votingRoundId,
+              startDate: proposal?.createdAt,
+              endDate: proposal?.createdAt,
             },
           ]
         : []),
@@ -81,17 +81,17 @@ export const ProposalTimeline = ({ proposal }: Props) => {
   const [activeStep, setActiveStep] = useState(1) // the active step by default is the support phase ( once created, it's directly in support phase)
   useEffect(() => {
     setActiveStep(1)
-  }, [proposal.state])
+  }, [proposal?.state])
 
   // Check if the proposal is queuable and executable
   // only if the targets > 0
   const isQueuable = useMemo(() => {
-    return proposal.state === ProposalState.Succeeded
-  }, [proposal.state])
+    return proposal?.state === ProposalState.Succeeded
+  }, [proposal?.state])
 
   const isExecutable = useMemo(() => {
-    return proposal.state === ProposalState.Active
-  }, [proposal.state])
+    return proposal?.state === ProposalState.Active
+  }, [proposal?.state])
 
   return (
     <>
@@ -125,8 +125,8 @@ export const ProposalTimeline = ({ proposal }: Props) => {
                   <TimelineElements
                     state={step.state}
                     round={step.round}
-                    startDate={step.startDate}
-                    endDate={step.endDate}
+                    startDate={step?.startDate ?? 0} //TODO: Fix this fallback
+                    endDate={step?.endDate ?? 0}
                   />
                 </Steps.Item>
               ))}
