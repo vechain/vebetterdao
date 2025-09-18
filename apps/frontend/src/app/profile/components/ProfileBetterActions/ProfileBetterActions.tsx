740d15a4a0bc48da7fb088fea32b80af57a32154
akgunberk <50302697+akgunberk@users.noreply.github.com>
2025-09-18 16:58:29 +0300

    fix: clarify cant vote criterias (#2520)
    * chore: vote button know more modal
    
    * fix: cant vote card roundness
    
    * fix: clarify user voting eligibility
    
    * chore: i18n

--- apps/frontend/src/app/profile/components/ProfileBetterActions/ProfileBetterActions.tsx
+++ apps/frontend/src/app/profile/components/ProfileBetterActions/ProfileBetterActions.tsx
@@ -2,17 +2,19 @@ import { VStack } from "@chakra-ui/react"
 import { ActivityCalendar } from "./components/ActivityCalendar"
 import { useState } from "react"
 import { ActivityList } from "./components/ActivityList"
-import { PendingActions } from "./components/PendingActions"
 import { YourBetterActionsCard } from "@/app/components/YourBetterActionsCard"
+import { useCanUserVote } from "@/api"
+import { CantVoteCard } from "@/app/components/CantVoteCard/CantVoteCard"
 
 type Props = {
   address: string
 }
 export const ProfileBetterActions = ({ address }: Props) => {
   const [isCalendarView, setIsCalendarView] = useState(true)
+  const { isPerson, isLoading } = useCanUserVote(address)
   return (
     <VStack gap={4} w="full">
-      <PendingActions address={address} />
+      {!isLoading && !isPerson && <CantVoteCard />}
       <YourBetterActionsCard address={address} renderActions={false} />
       {isCalendarView ? (
         <ActivityCalendar address={address} setIsCalendarView={setIsCalendarView} />
