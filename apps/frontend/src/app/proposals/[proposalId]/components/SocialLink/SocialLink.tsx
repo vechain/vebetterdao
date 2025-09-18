8e4186b77816aba82c63a4df76197ba48e137498
Victor Emanuel <versaujob@gmail.com>
2025-09-18 17:21:59 +0100

    refactor: shared component for social links in proposal

---
+++ apps/frontend/src/app/proposals/[proposalId]/components/SocialLink/SocialLink.tsx
@@ -0,0 +1,20 @@
+import { HStack, Icon, Link } from "@chakra-ui/react"
+import { IconType } from "react-icons"
+
+type SocialLinkProps = {
+  icon: IconType
+  href: string
+  label: string
+  value?: string
+}
+
+export const SocialLink: React.FC<SocialLinkProps> = ({ icon, href, label, value }) => {
+  return (
+    <HStack>
+      <Icon as={icon} size="lg" />
+      <Link cursor="pointer" color="actions.tertiary.default" href={href} target="_blank" rel="noopener noreferrer">
+        {value || label}
+      </Link>
+    </HStack>
+  )
+}
