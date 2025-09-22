7749296434ed1d7a39dd3b78cc858b246f0610a3
Victor Emanuel <versaujob@gmail.com>
2025-09-22 17:52:36 +0100

    refactor: improve useproposalvotes calc

--- apps/frontend/src/api/indexer/proposals/useProposalVotes.ts
+++ apps/frontend/src/api/indexer/proposals/useProposalVotes.ts
@@ -1,5 +1,6 @@
 import { paths } from "../schema"
 import { indexerQueryClient } from "../api"
+import BigNumber from "bignumber.js"
 
 type ProposalVotesQuery = paths["/api/v1/b3tr/proposals/{proposalId}/results"]["get"]
 
@@ -29,24 +30,35 @@ export const useProposalVotes = (proposalId: string) =>
     },
     {
       select(data) {
-        const totalPower = data.reduce((acc, item) => acc + BigInt(item.totalPower), BigInt(0))
+        const totalPower = data.reduce((acc, item) => acc.plus(BigNumber(item.totalPower ?? 0)), BigNumber(0))
         const totalVoters = data.reduce((acc, item) => acc + item.voters, 0)
-        const totalWeight = data.reduce((acc, item) => acc + BigInt(item.totalWeight), BigInt(0))
+        const totalWeight = data.reduce((acc, item) => acc.plus(BigNumber(item.totalWeight ?? 0)), BigNumber(0))
 
         const groupedVotes = data.reduce((acc, item) => {
+          const itemWeight = BigNumber(item.totalWeight ?? 0)
+          const itemPower = BigNumber(item.totalPower ?? 0)
+
+          // Calculate percentages as (item / total) * 100 using BigNumber math
+          const percentage = totalWeight.isGreaterThan(0)
+            ? itemWeight.dividedBy(totalWeight).multipliedBy(100).toNumber()
+            : 0
+          const percentagePower = totalPower.isGreaterThan(0)
+            ? itemPower.dividedBy(totalPower).multipliedBy(100).toNumber()
+            : 0
+
           acc[item.support.toLowerCase() as Lowercase<ProposalVotes["support"]>] = {
-            totalWeight: Number(item.totalWeight),
+            totalWeight: itemWeight.toNumber(),
             voters: item.voters,
-            percentage: Number(BigInt(BigInt(item?.totalWeight ?? 0) * BigInt(10000)) / BigInt(totalWeight)) / 100,
-            percentagePower: Number((BigInt(item?.totalPower ?? 0) * BigInt(10000)) / BigInt(totalPower)) / 100,
+            percentage,
+            percentagePower,
           }
           return acc
         }, {} as GroupedProposalVotes)
 
         return {
           totalVoters,
-          totalPower,
-          totalWeight,
+          totalPower: totalPower.toNumber(),
+          totalWeight: totalWeight.toNumber(),
           votes: groupedVotes,
         }
       },
