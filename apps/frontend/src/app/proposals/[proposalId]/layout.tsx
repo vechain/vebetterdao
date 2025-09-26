c5d67244da3f2b04f87e1ba687b330041e21159f
dara-vechain <dara.heng@vechain.org>
2025-09-26 17:07:04 +0100

    fix: decode event data properly to get proposal title and desc for dynamic metadata

--- apps/frontend/src/app/proposals/[proposalId]/layout.tsx
+++ apps/frontend/src/app/proposals/[proposalId]/layout.tsx
@@ -7,6 +7,7 @@ import { B3TRGovernor__factory } from "@vechain/vebetterdao-contracts/typechain-
 import { Props } from "./page"
 import { getDefaultMetadata } from "@/utils/metadata"
 import { APPLICATION_NAME, IMAGE_DIMENSION, pagesMetadata } from "@/metadata/pages"
+import { decodeEventLog } from "@/api/contracts/governance/getEvents"
 
 const abi = B3TRGovernor__factory.abi
 const address = getConfig().b3trGovernorAddress as `0x${string}`
@@ -24,7 +25,7 @@ export async function generateMetadata({ params }: Props, _parent: ResolvingMeta
 
     const eventTopics = contract.getEventAbi("ProposalCreated")?.encodeFilterTopicsNoNull({ proposalId: id })
 
-    const [proposal] = await thor.logs.filterRawEventLogs({
+    const [rawProposalEvent] = await thor.logs.filterRawEventLogs({
       options: { limit: 1 },
       criteriaSet: [
         {
@@ -35,17 +36,23 @@ export async function generateMetadata({ params }: Props, _parent: ResolvingMeta
       ],
     })
 
-    const description = (proposal?.decodedData?.[6] || "") as string
+    const proposal = decodeEventLog(rawProposalEvent!, abi)
+    if (proposal.decodedData?.eventName !== "ProposalCreated") {
+      return getDefaultMetadata()
+    }
+    const proposalData = proposal.decodedData?.args
+
+    const proposalDesc = proposalData?.description
     let proposalMetadata: ProposalMetadata | null = null
-    if (description) {
-      proposalMetadata = await getIpfsMetadata<ProposalMetadata>(toIPFSURL(description))
+    if (proposalDesc) {
+      proposalMetadata = await getIpfsMetadata<ProposalMetadata>(toIPFSURL(proposalDesc))
     }
 
-    const title = proposalMetadata?.title
+    const metadataTitle = proposalMetadata?.title
       ? `${proposalMetadata.title} | ${APPLICATION_NAME}`
-      : `Proposal ${id} | ${APPLICATION_NAME}`
+      : `Proposal | ${APPLICATION_NAME}`
 
-    const desc = proposalMetadata?.shortDescription
+    const metadataDesc = proposalMetadata?.shortDescription
       ? proposalMetadata.shortDescription
       : pagesMetadata.proposals.description
 
@@ -54,25 +61,25 @@ export async function generateMetadata({ params }: Props, _parent: ResolvingMeta
 
     const defaultProposalImage = {
       url: `${config.basePath}${pagesMetadata.proposals.image}`,
-      alt: `${title} | ${APPLICATION_NAME}`,
+      alt: `${metadataTitle} | ${APPLICATION_NAME}`,
       width: IMAGE_DIMENSION.width,
       height: IMAGE_DIMENSION.height,
     }
 
     return {
-      title,
-      description: desc,
+      title: metadataTitle,
+      description: metadataDesc,
       openGraph: {
-        title,
-        description: desc,
+        title: metadataTitle,
+        description: metadataDesc,
         type: "website",
         url: pageUrl,
         siteName: APPLICATION_NAME,
         images: [defaultProposalImage],
       },
       twitter: {
-        title,
-        description: desc,
+        title: metadataTitle,
+        description: metadataDesc,
         card: "summary_large_image",
         site: config.basePath,
         images: [defaultProposalImage],
