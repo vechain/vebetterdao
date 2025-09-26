7e3854c259777e8652c92fe92750b4fe55583d6f
dara-vechain <dara.heng@vechain.org>
2025-09-26 15:37:51 +0100

    fix: fix banner image not showing and enrich metadata  info

---
+++ apps/frontend/src/app/apps/[appId]/layout.tsx
@@ -0,0 +1,89 @@
+import { convertUriToUrl, getNodeJsThorClient } from "@/utils"
+import { getConfig } from "@repo/config"
+import { Metadata, ResolvingMetadata } from "next"
+
+//Need precise import to avoid having dapp-kit imported and indexed somewhere
+import { getXAppMetadata } from "@/api/contracts/xApps/getXAppMetadata"
+
+import { compareAddresses } from "@repo/utils/AddressUtils"
+import { X2EarnApps__factory } from "@vechain/vebetterdao-contracts"
+import { XApp } from "@/api"
+import { APPLICATION_NAME, IMAGE_DIMENSION, pagesMetadata } from "@/metadata/pages"
+import { getDefaultMetadata } from "@/utils/metadata"
+import { Props } from "./page"
+
+const abi = X2EarnApps__factory.abi
+const address = getConfig().x2EarnAppsContractAddress
+
+export async function generateMetadata({ params }: Props, _parent: ResolvingMetadata): Promise<Metadata> {
+  try {
+    const id = params.appId
+
+    if (!id) {
+      return getDefaultMetadata()
+    }
+
+    const thor = await getNodeJsThorClient()
+    const contract = thor.contracts.load(address, abi)
+    const clauses = [contract.clause.apps(), contract.clause.unendorsedApps()]
+
+    const res = await thor.transactions.executeMultipleClausesCall(clauses)
+    if (!res) return getDefaultMetadata()
+
+    const xApps = [...((res[0]?.result.plain as []) ?? []), ...((res[1]?.result.plain as []) ?? [])] as XApp[]
+    const app = xApps.find(app => compareAddresses(app.id, id))
+
+    if (!app) return getDefaultMetadata()
+
+    const baseUri = await contract.read.baseURI()
+    if (!baseUri) return getDefaultMetadata()
+
+    const metadata = await getXAppMetadata(`${baseUri}${app.metadataURI}`)
+    if (!metadata?.name || !metadata?.description) return getDefaultMetadata()
+
+    // Get the config and the page url
+    const config = getConfig()
+    const pageUrl = `${config.basePath}/apps/${id}`
+
+    // Safely handle banner image
+    const bannerImageUrl = metadata.banner
+      ? convertUriToUrl(metadata.banner)
+      : `${config.basePath}${pagesMetadata.apps.image}` // fallback to default apps metadata image
+
+    const title = `${metadata.name} | ${APPLICATION_NAME}`
+    const description = `${metadata.name} is part of ${APPLICATION_NAME}! ${metadata.description}`
+    const defaultAppImage = {
+      url: bannerImageUrl,
+      alt: `${metadata.name} | ${APPLICATION_NAME}`,
+      width: IMAGE_DIMENSION.width,
+      height: IMAGE_DIMENSION.height,
+    }
+
+    return {
+      title,
+      description,
+      openGraph: {
+        title,
+        description: metadata.description,
+        type: "website",
+        url: pageUrl,
+        siteName: APPLICATION_NAME,
+        ...(bannerImageUrl && {
+          images: [defaultAppImage],
+        }),
+      },
+      twitter: {
+        card: "summary_large_image",
+        title,
+        description,
+        site: config.basePath,
+        ...(bannerImageUrl && {
+          images: [defaultAppImage],
+        }),
+      },
+    }
+  } catch (error) {
+    console.error("Error generating metadata for app:", params.appId, error)
+    return getDefaultMetadata()
+  }
+}
--- apps/frontend/src/app/apps/[appId]/page.tsx
+++ apps/frontend/src/app/apps/[appId]/page.tsx
@@ -1,66 +1,6 @@
-import { getNodeJsThorClient } from "@/utils"
-import { getConfig } from "@repo/config"
-import { Metadata, ResolvingMetadata } from "next"
 import { AppDetailPage } from "./AppDetailPage"
 
-//Need precise import to avoid having dapp-kit imported and indexed somewhere
-import { getXAppMetadata } from "@/api/contracts/xApps/getXAppMetadata"
-
-import { getIpfsMetadata } from "@/api/ipfs"
-import { compareAddresses } from "@repo/utils/AddressUtils"
-import { X2EarnApps__factory } from "@vechain/vebetterdao-contracts"
-import { XApp } from "@/api"
-
-const abi = X2EarnApps__factory.abi
-const address = getConfig().x2EarnAppsContractAddress
-
-export async function generateMetadata({ params }: Props, _parent: ResolvingMetadata): Promise<Metadata> {
-  // read route params
-  const id = params.appId
-
-  // optionally access and extend (rather than replace) parent metadata
-  //   const previousImages = (await parent).openGraph?.images || []
-
-  const thor = await getNodeJsThorClient()
-  const contract = thor.contracts.load(address, abi)
-  const clauses = [contract.clause.apps(), contract.clause.unendorsedApps()]
-
-  const res = await thor.transactions.executeMultipleClausesCall(clauses)
-
-  if (!res) return {}
-
-  const xApps = [...((res[0]?.result.plain as []) ?? []), ...((res[1]?.result.plain as []) ?? [])] as XApp[]
-
-  const app = xApps.find(app => compareAddresses(app.id, id))
-
-  if (!app) return {}
-
-  const baseUri = await contract.read.baseURI()
-
-  const metadata = await getXAppMetadata(`${baseUri}${app.metadataURI}`)
-
-  if (!metadata) return {}
-
-  const bannerImage = await getIpfsMetadata<string>(metadata.banner, false)
-
-  return {
-    title: `${metadata.name} - VeBetter`,
-    description: `${metadata.name} is part of VeBetter! ${metadata.description}`,
-    openGraph: {
-      description: metadata.description,
-      images: [bannerImage],
-    },
-    twitter: {
-      title: `${metadata.name} - VeBetter`,
-      description: `${metadata.name} is part of VeBetter! ${metadata.description}`,
-      images: [bannerImage],
-      card: "summary_large_image",
-      site: getConfig().basePath,
-    },
-  }
-}
-
-type Props = {
+export type Props = {
   params: {
     appId: string
   }
