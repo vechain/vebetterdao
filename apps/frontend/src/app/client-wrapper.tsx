c8f920288eeb4b93bb9f6e79f09c833ccce5798e
Dara <dara.heng@vechain.org>
2025-09-26 19:06:48 +0900

    Feat:  Separate server/client in layout and improve metadata (#2626)
    * feat: seperate client and server root layout (prevent metadata override), align metadata setup with nextjs template and add metadata to main pages
    
    * chore: add keyword
    
    * feat: add opengraph and twitter image to each page
    
    * chore: add metadata for grants page
    
    * fix: remove the word 'DAO'
    
    * chore: add missing metadata
    
    * chore: update base path for root layout metadata
    
    * chore: move profile metadata to page
    
    * chore: bring back layout profile metadata

---
+++ apps/frontend/src/app/client-wrapper.tsx
@@ -0,0 +1,87 @@
+"use client"
+
+import { Container, Flex, VStack } from "@chakra-ui/react"
+import { Providers } from "./providers"
+import { Footer, TransactionModal } from "@/components"
+import dynamic from "next/dynamic"
+import { AnalyticsUtils } from "@/utils"
+import { getEnvDatadogApp, getEnvDatadogClient, getEnvDatadogEnv, getEnvMixPanel } from "@repo/config"
+import { useEffect } from "react"
+import { datadogRum } from "@datadog/browser-rum"
+import "@/i18n"
+
+const mixpanelToken = getEnvMixPanel()
+const isProduction = process.env.NODE_ENV === "production"
+const Navbar = dynamic(() => import("@/components/Navbar").then(mod => mod.Navbar), { ssr: false })
+const FreshDeskWidget = dynamic(() => import("@/components/FreshDeskWidget").then(mod => mod.FreshDeskWidget), {
+  ssr: false,
+})
+
+// Initialise Datadog RUM - get the app token and client token from environment variables
+const datadog_app_token = getEnvDatadogApp()
+const datadog_client_token = getEnvDatadogClient()
+const datadog_env = getEnvDatadogEnv()
+
+if (typeof window !== "undefined" && isProduction) {
+  datadogRum.init({
+    applicationId: datadog_app_token,
+    clientToken: datadog_client_token,
+    site: "datadoghq.eu",
+    service: "b3tr",
+    env: datadog_env,
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: "mask-user-input",
+  })
+}
+
+// workaround for "@iconscout/react-unicons and data-new-gr-c-s-check-loaded
+const error = console.error
+console.error = (...args: any) => {
+  if (/defaultProps/.test(args[0])) return
+  if (args?.[1]?.includes?.("data-new-gr-c-s-check-loaded,data-gr-ext-installed")) return
+  error(...args)
+}
+
+//TODO: Is there a better place to initialise mixpanel? next/script?
+if (typeof window !== "undefined" && mixpanelToken) {
+  AnalyticsUtils.initialise()
+}
+
+export function ClientWrapper({ children }: { children: React.ReactNode }) {
+  // set color mode of @uiw/react-md-editor
+  useEffect(() => {
+    document.documentElement.setAttribute("data-color-mode", "light")
+    return () => {
+      document.documentElement.removeAttribute("data-color-mode")
+    }
+  }, [])
+
+  return (
+    <Providers>
+      {isProduction && <FreshDeskWidget widgetId={103000007852} />}
+      <VStack minH="100vh" gap={0} align="stretch" bg="layout-bg">
+        <Navbar />
+        <Flex flex={1}>
+          <Container
+            mt={{ base: 2, md: 10 }}
+            mb={[20, 20, 20]}
+            px={[4, 4, 4]}
+            maxW="breakpoint-xl"
+            display={"flex"}
+            flex={1}
+            alignItems={"center"}
+            justifyContent={"flex-start"}
+            flexDirection={"column"}>
+            {children}
+          </Container>
+          <TransactionModal />
+        </Flex>
+        <Footer />
+      </VStack>
+    </Providers>
+  )
+}
--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,141 +1,65 @@
-"use client"
-// app/layout.tsx
-import { Container, Flex, VStack } from "@chakra-ui/react"
-import { Providers } from "./providers"
+import { getConfig } from "@repo/config"
+import { APPLICATION_NAME, IMAGE_DIMENSION, pagesMetadata } from "@/metadata/pages"
+import { Metadata, Viewport } from "next"
+import { ClientWrapper } from "./client-wrapper"
 
-// import { Inter, Instrument_Sans } from "next/font/google"
-// const inter = Inter({
-//   variable: "--font-inter",
-//   subsets: ["latin"],
-//   display: "swap",
-// })
-// const instrumentSans = Instrument_Sans({
-//   variable: "--font-instrument-sans",
-//   subsets: ["latin"],
-//   display: "swap",
-// })
+// Get metadata of the platform
+const basePath = getConfig()?.basePath
+const platformMetadata = pagesMetadata?.platform
+const title = platformMetadata?.title
+const metadataDesc = platformMetadata?.description
+const imageUrl = `${basePath}${platformMetadata?.image}`
+const imageExtension = platformMetadata?.imageExtension
 
-import { Footer, TransactionModal } from "@/components"
-import dynamic from "next/dynamic"
-import { AnalyticsUtils } from "@/utils"
-import { getConfig, getEnvDatadogApp, getEnvDatadogClient, getEnvDatadogEnv, getEnvMixPanel } from "@repo/config"
-import "@/i18n"
-import { useEffect } from "react"
-import { t } from "i18next"
-import { datadogRum } from "@datadog/browser-rum"
-
-const mixpanelToken = getEnvMixPanel()
-const isProduction = process.env.NODE_ENV === "production"
-const Navbar = dynamic(() => import("@/components/Navbar").then(mod => mod.Navbar), { ssr: false })
-const FreshDeskWidget = dynamic(() => import("@/components/FreshDeskWidget").then(mod => mod.FreshDeskWidget), {
-  ssr: false,
-})
-
-//TODO: Is there a better place to initialise mixpanel? next/script?
-typeof window != "undefined" && mixpanelToken && AnalyticsUtils.initialise()
-
-// Initialise Datadog RUM - get the app token and client token from environment variables
-const datadog_app_token = getEnvDatadogApp()
-const datadog_client_token = getEnvDatadogClient()
-const datadog_env = getEnvDatadogEnv()
-
-if (isProduction) {
-  datadogRum.init({
-    applicationId: datadog_app_token,
-    clientToken: datadog_client_token,
-    site: "datadoghq.eu",
-    service: "b3tr",
-    env: datadog_env,
-    sessionSampleRate: 100,
-    sessionReplaySampleRate: 20,
-    trackUserInteractions: true,
-    trackResources: true,
-    trackLongTasks: true,
-    defaultPrivacyLevel: "mask-user-input",
-  })
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1.0,
 }
-// workaround for "@iconscout/react-unicons and data-new-gr-c-s-check-loaded
-const error = console.error
-console.error = (...args: any) => {
-  if (/defaultProps/.test(args[0])) return
-  if (args?.[1]?.includes?.("data-new-gr-c-s-check-loaded,data-gr-ext-installed")) return
-  error(...args)
+
+// Export proper metadata for the platform with template support
+export const metadata: Metadata = {
+  title: title || "VeBetter Governance | Shape the Ecosystem and Earn B3TR Rewards",
+  description: metadataDesc,
+  applicationName: APPLICATION_NAME,
+  keywords: ["VeBetter", "B3TR", "governance", "sustainability", "VeChain", "Web3", "DAO"],
+  appleWebApp: {
+    title,
+  },
+  openGraph: {
+    title,
+    type: "website",
+    url: basePath,
+    description: metadataDesc,
+    siteName: APPLICATION_NAME,
+    images: [
+      {
+        url: imageUrl,
+        type: imageExtension,
+        width: IMAGE_DIMENSION.width,
+        height: IMAGE_DIMENSION.height,
+        alt: APPLICATION_NAME,
+      },
+    ],
+  },
+  twitter: {
+    title,
+    description: metadataDesc,
+    images: [imageUrl],
+    card: "summary_large_image",
+  },
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  // set color mode of @uiw/react-md-editor
-  useEffect(() => {
-    document.documentElement.setAttribute("data-color-mode", "light")
-    return () => {
-      document.documentElement.removeAttribute("data-color-mode")
-    }
-  }, [])
-
   return (
     <html
       suppressHydrationWarning
       lang="en"
       style={{
         scrollBehavior: "smooth",
-      }}
-      // className={`${inter.variable} ${instrumentSans.variable}`}
-    >
-      <head>
-        <title>{t("VeBetterDAO")}</title>
-        <meta name="description" content="Vote for your favourite sustainability Apps in VeBetterDAO's governance." />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {/* Favicons */}
-        <meta name="apple-mobile-web-app-title" content="VeBetterDAO" />
-
-        {/* Open Graph Metadata */}
-        <meta name="title" property="og:title" content="VeBetterDAO" />
-        <meta name="type" property="og:type" content="website" />
-        <meta name="url" property="og:url" content="%VITE_BASE_URL%" />
-        <meta
-          name="description"
-          property="og:description"
-          content="Vote for your favourite sustainability Apps in VeBetterDAO's governance."
-        />
-        <meta property="og:site_name" content="VeBetterDAO" />
-        <meta name="image" property="og:image" content={`${getConfig().basePath}/assets/images/social_image.webp`} />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" content="VeBetterDAO" />
-
-        {/* Twitter Metadata */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="VeBetterDAO" />
-        <meta
-          name="twitter:description"
-          content="Vote for your favourite sustainability Apps in VeBetterDAO's governance."
-        />
-        <meta name="twitter:image" content={`${getConfig().basePath}/assets/images/social_image.webp`} />
-        <meta name="twitter:image:alt" content="VeBetterDAO" />
-      </head>
+      }}>
+      <head />
       <body>
-        <Providers>
-          {isProduction && <FreshDeskWidget widgetId={103000007852} />}
-          <VStack minH="100vh" gap={0} align="stretch" bg="layout-bg">
-            <Navbar />
-            <Flex flex={1}>
-              <Container
-                mt={{ base: 2, md: 10 }}
-                mb={[20, 20, 20]}
-                px={[4, 4, 4]}
-                maxW="breakpoint-xl"
-                display={"flex"}
-                flex={1}
-                alignItems={"center"}
-                justifyContent={"flex-start"}
-                flexDirection={"column"}>
-                {children}
-              </Container>
-              <TransactionModal />
-            </Flex>
-            <Footer />
-          </VStack>
-        </Providers>
+        <ClientWrapper>{children}</ClientWrapper>
       </body>
     </html>
   )
