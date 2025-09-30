f0fdec92d7a90c514012a76a72fadf4f4c0cf640
akgunberk <akgunberk@yahoo.com>
2025-09-30 18:00:55 +0300

    feat: add grants pagination

--- apps/frontend/src/app/proposals/components/ProposalsPageContent.tsx
+++ apps/frontend/src/app/proposals/components/ProposalsPageContent.tsx
@@ -82,6 +82,7 @@ export const ProposalsPageContent = () => {
               proposal={proposal as ProposalEnriched & { isDepositReached: boolean }}
             />
           ))}
+
           {filteredProposals.length === 0 && !isLoading && (
             <NoProposalsCard
               onClick={onNewClick}
