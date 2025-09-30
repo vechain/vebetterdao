42cc96c9ea4fab786e3bceb50f941912c3e356b9
akgunberk <50302697+akgunberk@users.noreply.github.com>
2025-09-30 15:03:26 +0300

    fix: make benefits to dapps optional (#2663)
    * fix: make benefits to dapps optional
    
    * fix: make benefits to dapps optional
    
    * fix: make benefits to dapps optional for tooling type grants
    
    * fix: make benefits to dapps optional for tooling type grants
    
    * fix: make benefits to dapps optional for tooling type grants 2
    
    * fix: typecheck
    
    * feat: focus project name when saving draft tooltip shows

--- apps/frontend/src/app/grants/new/components/form/steps/AboutGrant.tsx
+++ apps/frontend/src/app/grants/new/components/form/steps/AboutGrant.tsx
@@ -50,6 +50,8 @@ export const AboutGrant = ({
   const { t } = useTranslation()
   const { data: session } = useSession()
 
+  const isToolingGrant = getValues("grantType") === "tooling"
+
   const mapUploadError = (error: string): string => {
     if (error === "FILE_INVALID_TYPE") {
       return t("File type not supported. Use PDF, JPG, JPEG, or PNG.")
@@ -551,9 +553,10 @@ export const AboutGrant = ({
                     label={t("Benefits to apps")}
                     placeholder={t("Benefits to apps")}
                     type="textarea"
+                    isOptional={isToolingGrant}
                     defaultValue={textAreasDefaultValues.benefitsToDApps}
                     register={register("benefitsToDApps", {
-                      required: t("Please describe benefits to dApps"),
+                      required: !isToolingGrant ? t("Please describe benefits to apps") : false,
                       maxLength: {
                         value: MAX_TEXT_AREA_LENGTH,
                         message: t("Text too long. Maximum allowed: {{amount}} characters.", {
