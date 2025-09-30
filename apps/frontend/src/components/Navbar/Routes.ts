98f3cca280d2067296324dfebe09cb4cb1cfb8e9
Victor Emanuel <versaujob@gmail.com>
2025-09-30 19:32:19 +0100

    feat: hiding grants from menu

--- apps/frontend/src/components/Navbar/Routes.ts
+++ apps/frontend/src/components/Navbar/Routes.ts
@@ -1,8 +1,8 @@
+import { FaUser } from "react-icons/fa"
 import { FaGear } from "react-icons/fa6"
+import { LiaBalanceScaleSolid, LiaChartPieSolid } from "react-icons/lia"
 import { LuHouse } from "react-icons/lu"
 import { PiSquaresFour } from "react-icons/pi"
-import { FaUser } from "react-icons/fa"
-import { LiaBalanceScaleSolid, LiaChartPieSolid } from "react-icons/lia"
 
 export interface Route {
   name: string
@@ -42,19 +42,20 @@ export const Routes: Route[] = [
     onClick: "/proposals",
     isVisible: true,
     icon: LiaBalanceScaleSolid,
-    description: "Create and vote on proposals with your VOT3 tokens to make changes on the ecosystem.",
-    subRoutes: [
-      {
-        name: "Proposals",
-        description: "Propose your ideas and let the community decide!",
-        onClick: "/proposals",
-      },
-      {
-        name: "Grants",
-        description: "Need funding? Apply for a grant and get support",
-        onClick: "/grants",
-      },
-    ],
+    description: "Propose your ideas and let the community decide!",
+    // description: "Create and vote on proposals with your VOT3 tokens to make changes on the ecosystem.",
+    // subRoutes: [
+    //   {
+    //     name: "Proposals",
+    //     description: "Propose your ideas and let the community decide!",
+    //     onClick: "/proposals",
+    //   },
+    //   {
+    //     name: "Grants",
+    //     description: "Need funding? Apply for a grant and get support",
+    //     onClick: "/grants",
+    //   },
+    // ],
   },
   { name: "Profile", onClick: "/profile", isVisible: true, icon: FaUser },
   { name: "Admin", onClick: "/admin", isVisible: true, icon: FaGear },
