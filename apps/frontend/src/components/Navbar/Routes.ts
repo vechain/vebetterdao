5c056626912285e146549ca4b6015f77309b812e
Victor Emanuel <versaujob@gmail.com>
2025-10-02 12:32:44 +0100

    feat: enabling grants

--- apps/frontend/src/components/Navbar/Routes.ts
+++ apps/frontend/src/components/Navbar/Routes.ts
@@ -42,20 +42,19 @@ export const Routes: Route[] = [
     onClick: "/proposals",
     isVisible: true,
     icon: LiaBalanceScaleSolid,
-    description: "Propose your ideas and let the community decide!",
-    // description: "Create and vote on proposals with your VOT3 tokens to make changes on the ecosystem.",
-    // subRoutes: [
-    //   {
-    //     name: "Proposals",
-    //     description: "Propose your ideas and let the community decide!",
-    //     onClick: "/proposals",
-    //   },
-    //   {
-    //     name: "Grants",
-    //     description: "Need funding? Apply for a grant and get support",
-    //     onClick: "/grants",
-    //   },
-    // ],
+    description: "Create and vote on proposals with your VOT3 tokens to make changes on the ecosystem.",
+    subRoutes: [
+      {
+        name: "Proposals",
+        description: "Propose your ideas and let the community decide!",
+        onClick: "/proposals",
+      },
+      {
+        name: "Grants",
+        description: "Need funding? Apply for a grant and get support",
+        onClick: "/grants",
+      },
+    ],
   },
   { name: "Profile", onClick: "/profile", isVisible: true, icon: FaUser },
   { name: "Admin", onClick: "/admin", isVisible: true, icon: FaGear },
