c8dbfd4cf93d74ce63edd4194a315d473c97feef
Dan Rusnac <rusnac14@gmail.com>
2025-10-15 09:16:41 +0200

    feat: deprecate interface

--- packages/contracts/contracts/deprecated/V6/XAllocationPoolV6.sol
+++ packages/contracts/contracts/deprecated/V6/XAllocationPoolV6.sol
@@ -23,7 +23,7 @@
 
 pragma solidity 0.8.20;
 
-import { IXAllocationPool } from "../../interfaces/IXAllocationPool.sol";
+import { IXAllocationPoolV6 as IXAllocationPool } from "./interfaces/IXAllocationPoolV6.sol";
 import { AccessControlUpgradeable } from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import { Checkpoints } from "@openzeppelin/contracts/utils/structs/Checkpoints.sol";
 import { Time } from "@openzeppelin/contracts/utils/types/Time.sol";
