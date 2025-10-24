22e969f4869e53c9dca5d889f50cedca214ae069
akgunberk <akgunberk@yahoo.com>
2025-10-24 15:52:40 +0300

    fix: revert logo back

--- apps/frontend/src/components/Navbar/NavbarLogo.tsx
+++ apps/frontend/src/components/Navbar/NavbarLogo.tsx
@@ -1,12 +1,12 @@
-import { Button, Icon } from "@chakra-ui/react"
+import { Icon, Link } from "@chakra-ui/react"
 import NextLink from "next/link"
 
 import VBDLogo from "@/components/Icons/svg/vebetter-dao-logo.svg"
 
 export const NavbarLogo = () => (
-  <Button asChild variant="link">
+  <Link asChild>
     <NextLink href={"/"}>
       <Icon w="32" as={VBDLogo} color="icon.default" />
     </NextLink>
-  </Button>
+  </Link>
 )
