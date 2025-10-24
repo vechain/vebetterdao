6eac9c718054f6e1e5000b595a8deb9a86774e18
Dan Rusnac <rusnac14@gmail.com>
2025-10-24 12:30:28 +0200

    chore: import

--- apps/frontend/src/api/contracts/xAllocationPool/hooks/useMultipleXAppsTotalEarnings.ts
+++ apps/frontend/src/api/contracts/xAllocationPool/hooks/useMultipleXAppsTotalEarnings.ts
@@ -1,6 +1,6 @@
 import { getConfig } from "@repo/config"
 import { useQuery, useQueryClient } from "@tanstack/react-query"
-import { XAllocationPool__factory } from "@vechain/vebetterdao-contracts"
+import { XAllocationPool__factory } from "@vechain/vebetterdao-contracts/factories/XAllocationPool__factory"
 import { DBAPool__factory } from "@vechain/vebetterdao-contracts/typechain-types"
 import { executeMultipleClausesCall, useThor, decodeEventLog } from "@vechain/vechain-kit"
 import { ethers } from "ethers"
