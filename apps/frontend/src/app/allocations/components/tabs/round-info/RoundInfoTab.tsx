d636283a1a58bfc3c3c06737d743ddbff1195941
akgunberk <akgunberk@yahoo.com>
2025-11-12 12:21:36 +0300

    feat: allocation desktop round info tab

--- apps/frontend/src/app/allocations/components/tabs/round-info/RoundInfoTab.tsx
+++ apps/frontend/src/app/allocations/components/tabs/round-info/RoundInfoTab.tsx
@@ -1,5 +1,5 @@
-import { Badge, Heading, HStack, IconButton, Text, VStack } from "@chakra-ui/react"
-import { NavArrowRight } from "iconoir-react"
+import { Badge, Button, Card, Flex, Grid, Heading, HStack, Icon, IconButton, Text, VStack } from "@chakra-ui/react"
+import { Activity, NavArrowLeft, NavArrowRight, SmartphoneDevice } from "iconoir-react"
 import NextLink from "next/link"
 
 import { RoundEarnings } from "@/app/allocations/history/page"
@@ -19,7 +19,7 @@ export function RoundInfoTab({
   const { cycleTotal, totalVoters, apps, xAllocationsAmount, treasuryAmount, vote2EarnAmount } = currentRoundDetails
   return (
     <VStack alignItems="stretch" gap="5" w="full" mt="2">
-      <VStack alignItems="stretch" gap="2">
+      <VStack hideFrom="md" alignItems="stretch" gap="2">
         <HStack gap="2">
           <Text textStyle="md" fontWeight="semibold">
             {"Round"} {currentRoundDetails.id}
@@ -30,6 +30,36 @@ export function RoundInfoTab({
           {"Aug 11 - Aug 18"}
         </Text>
       </VStack>
+      <Card.Root hideBelow="md" p="6" alignItems="center" justifyContent="space-between" flexDirection="row">
+        <Grid gridTemplateColumns="repeat(3,max-content)" divideX="1px" divideColor="border.secondary" columnGap="6">
+          <VStack gap="1" align="start">
+            <Text textStyle="md" color="text.subtle">
+              {"Round"}
+            </Text>
+            <Heading size="4xl">{currentRoundDetails.id}</Heading>
+          </VStack>
+          <VStack gap="1" pl="6" align="start">
+            <Text textStyle="md" color="text.subtle">
+              {"Round dates"}
+            </Text>
+            <Heading size="lg">{"Aug 3 - Aug 10"}</Heading>
+          </VStack>
+          <Flex h="full" pl="6" alignItems="flex-start">
+            <Badge variant="positive">{"Active"}</Badge>
+          </Flex>
+        </Grid>
+        <Flex columnGap="4">
+          <IconButton variant="outline" boxSize={"44px"}>
+            <NavArrowLeft />
+          </IconButton>
+          <IconButton variant="outline" boxSize={"44px"}>
+            <NavArrowRight />
+          </IconButton>
+          <Button variant="link" p="0" size="md">
+            {"View all rounds"}
+          </Button>
+        </Flex>
+      </Card.Root>
 
       <RoundDistributionCard
         roundDetails={{
@@ -54,11 +84,31 @@ export function RoundInfoTab({
             </NextLink>
           </IconButton>
         </HStack>
-
         {previous3RoundsEarnings.map(round => (
           <RoundHistoryCard key={round.roundId} round={round} />
         ))}
       </VStack>
+      <Grid gridTemplateColumns="repeat(2,1fr)" gap="6">
+        <Card.Root p="6">
+          <Card.Header as={HStack} gap="2">
+            <Icon as={Activity} boxSize="5" color="icon.default" />
+            <Heading size="lg" fontWeight="semibold">
+              {"Your voting activity"}
+            </Heading>
+          </Card.Header>
+        </Card.Root>
+        <Card.Root p="6">
+          <Card.Header as={HStack} justifyContent="space-between">
+            <Heading as={HStack} size="lg" fontWeight="semibold">
+              <Icon as={SmartphoneDevice} boxSize="5" color="icon.default" />
+              {"Active apps"}
+            </Heading>
+            <Badge variant="neutral" size="sm" rounded="sm">
+              {"12 apps"}
+            </Badge>
+          </Card.Header>
+        </Card.Root>
+      </Grid>
     </VStack>
   )
 }
