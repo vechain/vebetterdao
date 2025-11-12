c8f0e338da4d084b55aea5ed83a71eda50e21c1b
Victor Emanuel <versaujob@gmail.com>
2025-11-12 13:12:47 +0000

    refactor gmcard

--- apps/frontend/src/components/GmNFTAndNodeCard/GmCard.tsx
+++ apps/frontend/src/components/GmNFTAndNodeCard/GmCard.tsx
@@ -3,21 +3,23 @@ import NextLink from "next/link"
 import { useTranslation } from "react-i18next"
 import { FaChevronRight } from "react-icons/fa"
 
+import { convertUriToUrl } from "@/utils/uri"
+
 export const GmCard = ({
   title,
   subtitle,
   footer,
-  images,
+  imagesIpfsUri,
   href,
 }: {
   title?: string
   subtitle?: string
-  images?: string[]
+  imagesIpfsUri?: string[]
   footer?: string
   href?: string
 }) => {
   const { t } = useTranslation()
-  const plusCount = images?.length ? images?.length - 2 : 0
+  const plusCount = imagesIpfsUri?.length ? imagesIpfsUri?.length - 2 : 0
   return (
     <LinkBox flex={1}>
       <Card.Root bg="transparency.200" gap="2" p="4">
@@ -26,7 +28,7 @@ export const GmCard = ({
             <Text display="block" textStyle="sm" color="white" fontWeight="semibold">
               {subtitle}
             </Text>
-            {images && images?.length > 1 && (
+            {imagesIpfsUri && imagesIpfsUri?.length > 1 && (
               <HStack gap={1} textStyle="md" fontWeight="semibold">
                 <Text color="white" fontWeight="semibold">
                   {t("See all")}
@@ -41,9 +43,9 @@ export const GmCard = ({
             <NextLink href={href ?? ""}>
               <HStack alignItems="start">
                 <AvatarGroup rounded="lg" shape="square" size="xl" stacking="last-on-top" spaceX={"-1.5rem"}>
-                  {images?.slice(0, 2)?.map(image => (
+                  {imagesIpfsUri?.slice(0, 2)?.map(image => (
                     <Avatar.Root key={image} border="0" rounded="lg">
-                      <Avatar.Image rounded="lg" src={image} />
+                      <Avatar.Image rounded="lg" src={convertUriToUrl(image)} />
                     </Avatar.Root>
                   ))}
 
