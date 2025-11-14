03d45880c4e7c1adf32adbe92469ac397775437c
dara-vechain <dara.heng@vechain.org>
2025-11-14 11:10:10 +0000

    fix: handle 0 input

--- apps/frontend/src/app/allocations/components/AppRadioCard.tsx
+++ apps/frontend/src/app/allocations/components/AppRadioCard.tsx
@@ -21,35 +21,40 @@ export const AppRadioCard = ({
   appVoters,
   appVotesReceived,
   totalVotes,
-}: AppRadioCardProps) => (
-  <CheckboxCard.Root
-    rounded="lg"
-    p="3"
-    colorPalette="blue"
-    checked={checked}
-    onCheckedChange={onCheckedChange}
-    cursor="pointer">
-    <CheckboxCard.HiddenInput />
-    <CheckboxCard.Control alignItems="center" p="0" gap="3">
-      <CheckboxCard.Indicator rounded="sm" />
-      <AppImage appId={appId} gridRow="1 / 3" />
-      <CheckboxCard.Content gap="0.5">
-        <Heading fontSize="md">{appName}</Heading>
-        <Flex w="full" justifyContent="space-between">
-          <Text display="flex" gap="2" textStyle="xs">
-            <Icon as={Group} boxSize="4" />
-            {appVoters ?? 0}
-          </Text>
-          <Text textStyle="xs" fontWeight="bold">
-            {((appVotesReceived * 100n) / totalVotes).toString() + "%"}
-          </Text>
-        </Flex>
-        <Progress.Root w="full" size="xs" colorPalette="green" mt="1" value={50}>
-          <Progress.Track rounded="lg">
-            <Progress.Range />
-          </Progress.Track>
-        </Progress.Root>
-      </CheckboxCard.Content>
-    </CheckboxCard.Control>
-  </CheckboxCard.Root>
-)
+}: AppRadioCardProps) => {
+  const percentage = totalVotes > 0n ? Number((appVotesReceived * 100n) / totalVotes) : 0
+
+  return (
+    <CheckboxCard.Root
+      rounded="lg"
+      p="3"
+      colorPalette="blue"
+      checked={checked}
+      onCheckedChange={onCheckedChange}
+      cursor="pointer">
+      <CheckboxCard.HiddenInput />
+      <CheckboxCard.Control alignItems="center" p="0" gap="3">
+        <CheckboxCard.Indicator rounded="sm" />
+        <AppImage appId={appId} gridRow="1 / 3" />
+        <CheckboxCard.Content gap="0.5">
+          <Heading fontSize="md">{appName}</Heading>
+          <Flex w="full" justifyContent="space-between">
+            <Text display="flex" gap="2" textStyle="xs">
+              <Icon as={Group} boxSize="4" />
+              {appVoters ?? 0}
+            </Text>
+            <Text textStyle="xs" fontWeight="bold">
+              {percentage}
+              {"%"}
+            </Text>
+          </Flex>
+          <Progress.Root w="full" size="xs" colorPalette="green" mt="1" value={percentage}>
+            <Progress.Track rounded="lg">
+              <Progress.Range />
+            </Progress.Track>
+          </Progress.Root>
+        </CheckboxCard.Content>
+      </CheckboxCard.Control>
+    </CheckboxCard.Root>
+  )
+}
