47f5540209cf2e3a1202f911258a511a7ea8004e
Victor Emanuel <versaujob@gmail.com>
2025-11-17 15:17:07 +0000

    fix: update empty state of node profile tab

--- apps/frontend/src/app/profile/components/ProfileNodes/ProfileNodes.tsx
+++ apps/frontend/src/app/profile/components/ProfileNodes/ProfileNodes.tsx
@@ -1,8 +1,12 @@
-import { Card, VStack, Heading, Text, Skeleton } from "@chakra-ui/react"
+import { Card, VStack, Heading, Skeleton, Icon } from "@chakra-ui/react"
 import { compareAddresses } from "@repo/utils/AddressUtils"
 import { useWallet } from "@vechain/vechain-kit"
 import { useMemo } from "react"
 import { useTranslation } from "react-i18next"
+
+import { EmptyStateCard } from "@/components/EmptyStateCard"
+import NodeIcon from "@/components/Icons/svg/node.svg"
+import { STARGATE_URL } from "@/constants/links"
 
 import { useGetUserNodes, UserNode } from "../../../../api/contracts/xNodes/useGetUserNodes"
 
@@ -22,9 +26,22 @@ export const ProfileNodes = ({ address }: { address: string }) => {
           <Heading size="xl">{t("Nodes")}</Heading>
         </Card.Header>
         <Card.Body>
-          <Skeleton loading={isUserNodesLoading}>
+          <Skeleton loading={isUserNodesLoading} w="full" h="full" minH="400px" borderRadius="md">
             {userNodes?.allNodes?.length === 0 ? (
-              <Text>{t("No nodes found.")}</Text>
+              <EmptyStateCard
+                icon={
+                  <Icon boxSize={36}>
+                    <NodeIcon />
+                  </Icon>
+                }
+                title={t("No nodes found.")}
+                description={t("Visit Stargate to know more about nodes and how to get one.")}
+                rootProps={{ size: "sm", bg: "transparent" }}
+                action={{
+                  label: t("Visit Stargate"),
+                  onClick: () => window.open(STARGATE_URL, "_blank"),
+                }}
+              />
             ) : (
               <VStack gap="4" align="stretch">
                 {userNodes?.allNodes?.map((node: UserNode) => (
