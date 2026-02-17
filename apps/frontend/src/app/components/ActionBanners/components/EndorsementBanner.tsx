5fa65d5005ebddcdbfb3a7357c4055e1677f7545
Dan Rusnac <rusnac14@gmail.com>
2026-02-17 22:33:53 +0100

    fix: show endorsement banner only after login

--- apps/frontend/src/app/components/ActionBanners/components/EndorsementBanner.tsx
+++ apps/frontend/src/app/components/ActionBanners/components/EndorsementBanner.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@chakra-ui/react"
+import { useWallet } from "@vechain/vechain-kit"
 import { useRouter } from "next/navigation"
 import { useCallback } from "react"
 import { useTranslation } from "react-i18next"
@@ -8,9 +9,12 @@ import { BannerStorageKey, GenericBanner } from "../../Banners/GenericBanner"
 export const EndorsementBanner = () => {
   const { t } = useTranslation()
   const router = useRouter()
+  const { account } = useWallet()
   const handleClick = useCallback(() => {
     router.push("/nodes")
   }, [router])
+
+  if (!account?.address) return null
 
   return (
     <GenericBanner
